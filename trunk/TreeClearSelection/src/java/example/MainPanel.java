e3352553fbf7975b7275fd5401502665cabced9f
at.terai@gmail.com <at.terai@gmail.com@3ab87fda-ff15-71db-baf3-2d1a8ae44ab9>
2010-12-06 06:54:52 +0000

    add_TreeClearSelection

---
+++ trunk/TreeClearSelection/src/java/example/MainPanel.java
@@ -0,0 +1,54 @@
+package example;
+//-*- mode:java; encoding:utf8n; coding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+//@homepage@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.tree.*;
+
+public class MainPanel extends JPanel {
+    private final JTree tree = new JTree();
+    private final MouseListener ml = new MouseAdapter() {
+        public void mousePressed(MouseEvent e) {
+            JTree tree = (JTree)e.getSource();
+            if(tree.getRowForLocation(e.getX(), e.getY())<0) {
+                tree.clearSelection();
+            }
+        }
+    };
+    public MainPanel() {
+        super(new BorderLayout());
+        add(new JCheckBox(new AbstractAction("JTree#clearSelection: when user clicks empty surface") {
+            @Override public void actionPerformed(ActionEvent e) {
+                if(((JCheckBox)e.getSource()).isSelected()) {
+                    tree.addMouseListener(ml);
+                }else{
+                    tree.removeMouseListener(ml);
+                }
+            }
+        }), BorderLayout.NORTH);
+        add(new JScrollPane(tree));
+        setPreferredSize(new Dimension(320, 240));
+    }
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+    public static void createAndShowGUI() {
+        try{
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        JFrame frame = new JFrame("@title@");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new MainPanel());
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
