0151743e82ce5d26fbdffe26720bfe83530e5a96
at.terai@gmail.com <at.terai@gmail.com@3ab87fda-ff15-71db-baf3-2d1a8ae44ab9>
2011-01-15 08:15:07 +0000

    Use SwingUtilities.updateComponentTreeUI(fileChooser) instead of fileChooser.updateUI().

--- trunk/Locale/src/java/example/MainPanel.java
+++ trunk/Locale/src/java/example/MainPanel.java
@@ -8,75 +8,60 @@ import javax.swing.*;
 import javax.swing.border.*;
 
 public class MainPanel extends JPanel {
+    private final JComboBox combo = new JComboBox(makeLocaleModel());
+    private final JPanel panel = new JPanel(new BorderLayout(5, 5));
+    private final JFileChooser fileChooser = new JFileChooser();
     public MainPanel() {
         super(new BorderLayout());
         setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
         UIManager.put("FileChooser.readOnly", Boolean.TRUE);
         //Locale.setDefault(new Locale("en", "US"));
+        //Locale defaultLocale = JFileChooser.getDefaultLocale();
+        //JFileChooser.setDefaultLocale(defaultLocale);
+        //JFileChooser.setDefaultLocale(Locale.ENGLISH);
+        //JFileChooser.setDefaultLocale(new Locale("en", "US"));
+        //fileChooser.setLocale(new Locale("fr", "FR"));
 
-        DefaultComboBoxModel model = new DefaultComboBoxModel();
-        model.addElement(Locale.ENGLISH);
-        model.addElement(Locale.FRENCH);
-        model.addElement(Locale.GERMAN);
-        model.addElement(Locale.ITALIAN);
-        model.addElement(Locale.JAPANESE);
-        model.addElement(Locale.KOREAN);
-        model.addElement(Locale.CHINESE);
-        model.addElement(Locale.SIMPLIFIED_CHINESE);
-        model.addElement(Locale.TRADITIONAL_CHINESE);
-        model.addElement(Locale.FRANCE);
-        model.addElement(Locale.GERMANY);
-        model.addElement(Locale.ITALY);
-        model.addElement(Locale.JAPAN);
-        model.addElement(Locale.KOREA);
-        model.addElement(Locale.CHINA);
-        model.addElement(Locale.PRC);
-        model.addElement(Locale.TAIWAN);
-        model.addElement(Locale.UK);
-        model.addElement(Locale.US);
-        model.addElement(Locale.CANADA);
-        model.addElement(Locale.CANADA_FRENCH);
-        //model.addElement(Locale.ROOT);
-        final JComboBox combo = new JComboBox(model);
-
-        Box box = Box.createHorizontalBox();
-        box.setBorder(BorderFactory.createEmptyBorder(0,0,5,0));
-        box.add(new JLabel("Open JFileChooser"));
-//         final Locale defaultLocale = JFileChooser.getDefaultLocale();
-//         box.add(new JButton(new AbstractAction(defaultLocale.toString()) {
-//             public void actionPerformed(ActionEvent ae) {
-//                 JFileChooser.setDefaultLocale(defaultLocale);
-//                 JFileChooser fileChooser = new JFileChooser();
-//                 int retvalue = fileChooser.showOpenDialog(MainPanel.this);
-//             }
-//         }));
-//         box.add(new JButton(new AbstractAction("en_US") {
-//             public void actionPerformed(ActionEvent ae) {
-//                 //JFileChooser.setDefaultLocale(Locale.ENGLISH);
-//                 JFileChooser.setDefaultLocale(new Locale("en", "US"));
-//                 JFileChooser fileChooser = new JFileChooser();
-//                 int retvalue = fileChooser.showOpenDialog(MainPanel.this);
-//             }
-//         }));
-        box.add(Box.createHorizontalStrut(200));
-        //box.add(Box.createHorizontalGlue());
-
-        add(box, BorderLayout.NORTH);
-        add(combo);
-
-        final JFileChooser fileChooser = new JFileChooser();
-        add(new JButton(new AbstractAction("<-") {
-            @Override public void actionPerformed(ActionEvent ae) {
-                //JFileChooser fileChooser = new JFileChooser();
-                //fileChooser.setLocale(new Locale("fr", "FR"));
+        panel.setBorder(BorderFactory.createTitledBorder("Open JFileChooser"));
+        panel.add(combo);
+        panel.add(new JButton(new AbstractAction("<-") {
+            @Override public void actionPerformed(ActionEvent e) {
                 fileChooser.setLocale((Locale)combo.getSelectedItem());
-                fileChooser.updateUI();
-                int retvalue = fileChooser.showOpenDialog(MainPanel.this);
+                SwingUtilities.updateComponentTreeUI(fileChooser); //fileChooser.updateUI();
+                int retvalue = fileChooser.showOpenDialog(panel);
                 System.out.println(retvalue);
             }
         }), BorderLayout.EAST);
+        add(panel, BorderLayout.NORTH);
+        setPreferredSize(new Dimension(320, 200));
     }
-//     private void printLocale(JFileChooser fileChooser) {
+    private static ComboBoxModel makeLocaleModel() {
+        Locale[] array = {
+            Locale.ENGLISH,
+            Locale.FRENCH,
+            Locale.GERMAN,
+            Locale.ITALIAN,
+            Locale.JAPANESE,
+            Locale.KOREAN,
+            Locale.CHINESE,
+            Locale.SIMPLIFIED_CHINESE,
+            Locale.TRADITIONAL_CHINESE,
+            Locale.FRANCE,
+            Locale.GERMANY,
+            Locale.ITALY,
+            Locale.JAPAN,
+            Locale.KOREA,
+            Locale.CHINA,
+            Locale.PRC,
+            Locale.TAIWAN,
+            Locale.UK,
+            Locale.US,
+            Locale.CANADA,
+            Locale.CANADA_FRENCH,
+        };
+        return new DefaultComboBoxModel(array);
+    }
+//     private static void printLocale(JFileChooser fileChooser) {
 //         System.out.println("Locale: "+fileChooser.getLocale());
 //         System.out.println("DefaultLocale: "+fileChooser.getDefaultLocale());
 //     }
@@ -97,7 +82,6 @@ public class MainPanel extends JPanel {
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.getContentPane().add(new MainPanel());
         frame.pack();
-        frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
