8924549cf7f29456c615f7c44f27115725d8a95c
TERAI Atsuhiro <at.terai@gmail.com>
2011-03-07 11:35:27 +0000

    BufferedImage#createGraphics(), Graphics2D#dispose()

--- LargeCellEditor/src/java/example/MainPanel.java
+++ LargeCellEditor/src/java/example/MainPanel.java
@@ -172,6 +172,7 @@ class IconTable extends JTable {
                 g2.fillRoundRect(rect.x-i, rect.y+xoff,
                                  rect.width+i+i, rect.height-xoff+i, 5, 5);
             }
+            g2.dispose();
             g.drawImage(bufimg, 0, 0, null);
         }
     }
