5f8ec74ce48113b2dc7216eec2027740c6d8c182
at.terai@gmail.com <at.terai@gmail.com@3ab87fda-ff15-71db-baf3-2d1a8ae44ab9>
2011-03-14 07:02:02 +0000

    add_TreeLeafIcon

---
+++ trunk/TreeLeafIcon/src/java/example/MainPanel.java
@@ -0,0 +1,121 @@
+package example;
+//-*- mode:java; encoding:utf8n; coding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+//@homepage@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.tree.*;
+
+public class MainPanel extends JPanel {
+    private final JCheckBox folderCheck = new JCheckBox("OpenIcon, ClosedIcon");
+    private final JCheckBox leafCheck   = new JCheckBox("LeafIcon");
+    private final Icon emptyIcon = new EmptyIcon();
+    private final JTree tree = new JTree();
+    public MainPanel() {
+        super(new BorderLayout());
+
+        for(int i=0;i<tree.getRowCount();i++) {
+            tree.expandRow(i);
+        }
+
+        DefaultTreeCellRenderer r = (DefaultTreeCellRenderer)tree.getCellRenderer();
+        r.setOpenIcon(emptyIcon);
+        r.setClosedIcon(emptyIcon);
+        r.setLeafIcon(emptyIcon);
+        allNodesChanged(tree);
+
+        folderCheck.addActionListener(new ActionListener() {
+            @Override public void actionPerformed(ActionEvent e) {
+                DefaultTreeCellRenderer r = (DefaultTreeCellRenderer)tree.getCellRenderer();
+                if(((JCheckBox)e.getSource()).isSelected()) {
+                    r.setOpenIcon(r.getDefaultOpenIcon());
+                    r.setClosedIcon(r.getDefaultClosedIcon());
+                }else{
+                    r.setOpenIcon(emptyIcon);
+                    r.setClosedIcon(emptyIcon);
+                }
+                allNodesChanged(tree);
+            }
+        });
+        leafCheck.addActionListener(new ActionListener() {
+            @Override public void actionPerformed(ActionEvent e) {
+                DefaultTreeCellRenderer r = (DefaultTreeCellRenderer)tree.getCellRenderer();
+                if(((JCheckBox)e.getSource()).isSelected()) {
+                    r.setLeafIcon(r.getDefaultLeafIcon());
+                }else{
+                    r.setLeafIcon(emptyIcon);
+                }
+                allNodesChanged(tree);
+            }
+        });
+
+//         tree.setCellRenderer(new DefaultTreeCellRenderer() {
+//             @Override public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded, boolean leaf, int row, boolean hasFocus) {
+//                 super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus);
+//                 if(leaf) {
+//                     if(!leafCheck.isSelected()) {
+//                         setIcon(emptyIcon);
+//                     }else{
+//                         setIcon(getDefaultLeafIcon());
+//                     }
+//                 }else{
+//                     if(!folderCheck.isSelected()) {
+//                         setIcon(emptyIcon);
+//                     }else{
+//                         if(expanded) {
+//                             setIcon(getDefaultOpenIcon());
+//                         }else{
+//                             setIcon(getDefaultClosedIcon());
+//                         }
+//                     }
+//                 }
+//                 return this;
+//             }
+//         });
+
+        JPanel np = new JPanel();
+        np.add(folderCheck);
+        np.add(leafCheck);
+
+        add(np, BorderLayout.NORTH);
+        add(new JScrollPane(tree));
+        setPreferredSize(new Dimension(320, 240));
+    }
+
+    private static void allNodesChanged(JTree tree) {
+        DefaultTreeModel model = (DefaultTreeModel)tree.getModel();
+        DefaultMutableTreeNode root = (DefaultMutableTreeNode)model.getRoot();
+        java.util.Enumeration depth = root.depthFirstEnumeration();
+        while(depth.hasMoreElements()) {
+            model.nodeChanged((TreeNode)depth.nextElement());
+        }
+        //tree.revalidate();
+        //tree.repaint();
+    }
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+    public static void createAndShowGUI() {
+        try{
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        JFrame frame = new JFrame("@title@");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new MainPanel());
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
+class EmptyIcon implements Icon {
+    @Override public void paintIcon(Component c,Graphics g,int x,int y) {}
+    @Override public int getIconWidth()  { return 0; }
+    @Override public int getIconHeight() { return 0; }
+}
