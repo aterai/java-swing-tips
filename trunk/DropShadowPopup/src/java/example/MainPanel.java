a292f417783ddb7a1d059dd5ee9b91bf5cd3820e
at.terai@gmail.com <at.terai@gmail.com@3ab87fda-ff15-71db-baf3-2d1a8ae44ab9>
2011-04-19 06:39:43 +0000

    clean-up

--- trunk/DropShadowPopup/src/java/example/MainPanel.java
+++ trunk/DropShadowPopup/src/java/example/MainPanel.java
@@ -10,92 +10,41 @@ import javax.swing.border.*;
 
 class MainPanel extends JPanel {
     private final JCheckBox check = new JCheckBox("Paint Shadow", true);
+    private final JLabel label = new JLabel();
+    private JPopupMenu popup0;
+    private MyPopupMenu popup1;
     public MainPanel() {
         super(new BorderLayout());
-        ImageIcon icon = new ImageIcon(getClass().getResource("test.png"));
-        check.setFocusPainted(false);
-
-        final MyPopupMenu pop = new MyPopupMenu();
-        pop.add(new JMenuItem("Open"));
-        pop.add(new JMenuItem("Save"));
-        pop.add(new JMenuItem("Close"));
-        //pop.addSeparator();
-        JSeparator s = new JSeparator();
-        s.setOpaque(true);
-        pop.add(s);
-        pop.add(new JMenuItem("Exit"));
-
-        JLabel label = new JLabel(icon);
-        //Swing - Can popup menu events be consumed by other (e.g. background) components?
-        //http://forums.sun.com/thread.jspa?threadID=590867
-        label.addMouseListener(new MouseAdapter() {}); // JDK 1.5 JPanel, JLabel
-        label.setComponentPopupMenu(pop);
-//         addMouseListener(new MouseAdapter() {
-//             public void mouseReleased(MouseEvent e) {
-//                 if(e.isPopupTrigger()) {
-//                     Point pt = e.getPoint();
-//                     pop.show(e.getComponent(), pt.x, pt.y);
-//                 }
-//                 repaint();
-//             }
-//         });
+        label.setIcon(new ImageIcon(getClass().getResource("test.png")));
+        label.setComponentPopupMenu(popup1);
+        check.addActionListener(new ActionListener() {
+            @Override public void actionPerformed(ActionEvent e) {
+                JCheckBox c = (JCheckBox)e.getSource();
+                label.setComponentPopupMenu(c.isSelected()?popup1:popup0);
+            }
+        });
         add(check, BorderLayout.NORTH);
         add(label);
         setPreferredSize(new Dimension(320, 240));
     }
-    class MyPopupMenu extends JPopupMenu {
-        private static final int off = 6;
-        @Override public void paintComponent(Graphics g) {
-            int w = getWidth();
-            int h = getHeight();
-            BufferedImage shadow = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-            Graphics2D g2 = shadow.createGraphics();
-            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
-            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER,0.2f));
-            g2.setPaint(Color.BLACK);
-            for(int i=0;i<off;i++) {
-                g2.fillRoundRect(off, off, w-off-off+i, h-off-off+i, 10, 10);
-            }
-//             g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER,1.0f));
-//             g2.setPaint(getBackground());
-//             Insets i = getInsets();
-//             g2.fillRect(i.left, i.top, w-i.right, h-i.bottom);
-            g2.dispose();
-            ((Graphics2D)g).drawImage(shadow, 0, 0, this);
-        }
-        private Border inner = null;
-        @Override public void show(Component c, int x, int y) {
-            //updateUI();
-            if(inner==null) inner = getBorder();
-            setBorder(check.isSelected()?makeShadowBorder(c, new Point(x, y)):inner);
-            super.show(c, x, y);
-        }
-        private Border makeShadowBorder(Component c, Point p) {
-            Rectangle r = SwingUtilities.getWindowAncestor(c).getBounds();
-            Dimension d = this.getPreferredSize();
-            SwingUtilities.convertPointToScreen(p, c);
-            //System.out.println(r+" : "+p);
-
-            //pointed out by sawshun
-            Border outer;
-            if(r.contains(p.x, p.y, d.width+off, d.height+off)) {
-                outer = BorderFactory.createEmptyBorder(0,0,off,off);
-            }else{
-                outer = new ShadowBorder(off,off,this,p);
-            }
-            return BorderFactory.createCompoundBorder(outer, inner);
-        }
-//         private boolean isInRootPanel(Component root, Point p) {
-//             Rectangle r = root.getBounds();
-//             Dimension d = this.getPreferredSize();
-//             //pointed out by sawshun
-//             return r.contains(p.x, p.y, d.width+off, d.height+off);
-//             //    Point pt = new Point(p.x+d.width+off, p.y+d.height+off);
-//             //    SwingUtilities.convertPointToScreen(pt, root);
-//             //    return r.contains(pt);
-//         }
+    @Override public void updateUI() {
+        super.updateUI();
+        popup1 = new MyPopupMenu();
+        initPopupMenu(popup1);
     }
-
+    private static void initPopupMenu(JPopupMenu p) {
+        p.setOpaque(false);
+        p.add(new JMenuItem("Open"));
+        p.add(new JMenuItem("Save"));
+        p.add(new JMenuItem("Close"));
+        //Bug ID: 6595814 Nimbus LAF: Renderers, MenuSeparators, colors rollup bug
+        //http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6595814
+        //p.addSeparator();
+        JSeparator s = new JSeparator();
+        s.setOpaque(true);
+        p.add(s);
+        p.add(new JMenuItem("Exit"));
+    }
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             @Override public void run() {
@@ -118,23 +67,63 @@ class MainPanel extends JPanel {
     }
 }
 
+class MyPopupMenu extends JPopupMenu {
+    private static final int off = 4;
+    private BufferedImage shadow = null;
+    private Border inner = null;
+    @Override public void paintComponent(Graphics g) {
+        //super.paintComponent(g); //??? 1.7.0
+        ((Graphics2D)g).drawImage(shadow, 0, 0, this);
+    }
+    @Override public void show(Component c, int x, int y) {
+        if(inner==null) inner = getBorder();
+        setBorder(makeShadowBorder(c, new Point(x, y)));
+
+        Dimension d = getPreferredSize();
+        int w = d.width, h = d.height;
+        if(shadow==null || shadow.getWidth()!=w || shadow.getHeight()!=h) {
+            shadow = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2 = shadow.createGraphics();
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.2f));
+            g2.setPaint(Color.BLACK);
+            for(int i=0;i<off;i++) {
+                g2.fillRoundRect(off, off, w-off-off+i, h-off-off+i, 4, 4);
+            }
+            g2.dispose();
+        }
+        super.show(c, x, y);
+    }
+    private Border makeShadowBorder(Component c, Point p) {
+        Rectangle r = SwingUtilities.getWindowAncestor(c).getBounds();
+        Dimension d = this.getPreferredSize();
+        SwingUtilities.convertPointToScreen(p, c);
+        //System.out.println(r+" : "+p);
+        //pointed out by sawshun
+        Border outer;
+        if(r.contains(p.x, p.y, d.width+off, d.height+off)) {
+            outer = BorderFactory.createEmptyBorder(0,0,off,off);
+        }else{
+            outer = new ShadowBorder(off,off,this,p);
+        }
+        return BorderFactory.createCompoundBorder(outer, inner);
+    }
+}
+
 class ShadowBorder extends AbstractBorder {
     private final int xoff, yoff;
     private final Insets insets;
-    private BufferedImage screenShot = null;
+    private BufferedImage screen = null;
+    private BufferedImage shadow = null;
 
-    public ShadowBorder(int x, int y, JComponent c, Point p) { //JComponent c, int sx, int sy) {
+    public ShadowBorder(int x, int y, JComponent c, Point p) {
         this.xoff = x;
         this.yoff = y;
         this.insets = new Insets(0,0,xoff,yoff);
         try{
             Robot robot = new Robot();
-            //Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-            //Rectangle rect = new Rectangle(0, 0, screenSize.width, screenSize.height);
-            Dimension dim = c.getPreferredSize();
-            Rectangle rect = new Rectangle(p.x, p.y, dim.width+xoff, dim.height+yoff);
-            screenShot = robot.createScreenCapture(rect);
-            //System.out.println(rect.toString());
+            Dimension d = c.getPreferredSize();
+            screen = robot.createScreenCapture(new Rectangle(p.x, p.y, d.width+xoff, d.height+yoff));
         }catch (java.awt.AWTException ex) {
             ex.printStackTrace();
         }
@@ -142,23 +131,21 @@ class ShadowBorder extends AbstractBorder {
     @Override public Insets getBorderInsets(Component c) {
         return insets;
     }
-    @Override public void paintBorder(Component comp, Graphics g, int x, int y, int w, int h) {
-        if(screenShot==null) return;
-        BufferedImage shadow = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2 = shadow.createGraphics();
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER,0.2f));
-        g2.setPaint(Color.BLACK);
-        for(int i=0;i<xoff;i++) {
-            g2.fillRoundRect(xoff, xoff, w-xoff-xoff+i, h-xoff-xoff+i, 10,10);
+    @Override public void paintBorder(Component c, Graphics g, int x, int y, int w, int h) {
+        if(screen==null) return;
+        if(shadow==null || shadow.getWidth()!=w || shadow.getHeight()!=h) {
+            shadow = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2 = shadow.createGraphics();
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.2f));
+            g2.setPaint(Color.BLACK);
+            for(int i=0;i<xoff;i++) {
+                g2.fillRoundRect(xoff, xoff, w-xoff-xoff+i, h-xoff-xoff+i, 4, 4);
+            }
+            g2.dispose();
         }
-        g2.dispose();
-        Graphics2D gx = (Graphics2D) g;
-        //Point p = new Point(x, y);
-        //SwingUtilities.convertPointToScreen(p, comp);
-        //BufferedImage bi = screenShot; //.getSubimage(p.x, p.y, w, h);
-        //System.out.println(new Rectangle(p.x, p.y, w, h).toString());
-        gx.drawImage(screenShot, 0, 0, comp);
-        gx.drawImage(shadow, 0, 0, comp);
+        Graphics2D g2d = (Graphics2D)g;
+        g2d.drawImage(screen, 0, 0, c);
+        g2d.drawImage(shadow, 0, 0, c);
     }
 }
