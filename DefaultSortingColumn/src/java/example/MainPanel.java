3dd13c7bd7697859816f9a13a5623983882b1bab
TERAI Atsuhiro <at.terai@gmail.com>
2011-06-06 06:19:33 +0000

    add_DefaultSortingColumn

---
+++ DefaultSortingColumn/src/java/example/MainPanel.java
@@ -0,0 +1,55 @@
+package example;
+//-*- mode:java; encoding:utf8n; coding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+//@homepage@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Arrays;
+import javax.swing.*;
+import javax.swing.table.*;
+
+public class MainPanel extends JPanel {
+    private static final String[] columnNames = {"String", "Integer", "Boolean"};
+    private static final Object[][] data = {
+        {"aaa", 12, true}, {"bbb", 5, false},
+        {"CCC", 92, true}, {"DDD", 0, false}
+    };
+    private final DefaultTableModel model = new DefaultTableModel(data, columnNames) {
+        @Override public Class<?> getColumnClass(int column) {
+            return getValueAt(0, column).getClass();
+        }
+    };
+    public MainPanel() {
+        super(new BorderLayout());
+
+        JTable table = new JTable(model);
+        table.setAutoCreateRowSorter(true);
+        int index = 0;
+        //table.getRowSorter().toggleSortOrder(index); //SortOrder.ASCENDING
+        table.getRowSorter().setSortKeys(Arrays.asList(new RowSorter.SortKey(index, SortOrder.DESCENDING)));
+
+        add(new JScrollPane(table));
+        setPreferredSize(new Dimension(320, 200));
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+    public static void createAndShowGUI() {
+        try{
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        JFrame frame = new JFrame("@title@");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new MainPanel());
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
