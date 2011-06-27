80ed7f66d3a52007b2bba95e5ffffaf1ad40c0f3
at.terai@gmail.com <at.terai@gmail.com@3ab87fda-ff15-71db-baf3-2d1a8ae44ab9>
2011-06-27 05:44:06 +0000

    add_CurveLayout

---
+++ trunk/CurveLayout/src/java/example/MainPanel.java
@@ -0,0 +1,98 @@
+package example;
+//-*- mode:java; encoding:utf8n; coding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+//@homepage@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MainPanel extends JPanel {
+    private final static double a = 4.0;
+    public MainPanel() {
+        super(new BorderLayout());
+
+//         add(new JCheckBox(new AbstractAction("Tree.paintLines") {
+//             @Override public void actionPerformed(ActionEvent e) {
+//             }
+//         }), BorderLayout.NORTH);
+
+        JPanel panel1 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JPanel panel2 = new JPanel() {
+            @Override protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Insets i = getInsets();
+                g.translate(i.left, i.top);
+                g.setColor(Color.RED);
+                int w = getWidth() - i.left - i.right;
+                int px = 0;
+                int py = 0;
+                for(int x=0;x<w;x++) {
+                    int y = (int)Math.pow(x/a, 2.0);
+                    g.drawLine(px, py, x, y);
+                    px = x;
+                    py = y;
+                }
+                g.translate(-i.left, -i.top);
+            }
+        };
+        panel2.setLayout(new FlowLayout() {
+            public void layoutContainer(Container target) {
+                synchronized(target.getTreeLock()) {
+                    Insets insets = target.getInsets();
+                    int nmembers  = target.getComponentCount();
+                    if(nmembers<=0) return;
+                    int vgap = getVgap();
+                    int hgap = getHgap();
+                    int rowh = (target.getHeight() - (insets.top + insets.bottom + vgap*2))/nmembers;
+                    int x = insets.left + hgap;
+                    int y = insets.top  + vgap;
+                    for(int i=0;i<nmembers;i++) {
+                        Component m = target.getComponent(i);
+                        if(m.isVisible()) {
+                            Dimension d = m.getPreferredSize();
+                            m.setSize(d.width, d.height);
+                            m.setLocation(x, y);
+                            y += (vgap + Math.min(rowh, d.height));
+                            x = (int)(a * Math.sqrt(y));
+                        }
+                    }
+                }
+            }
+        });
+
+        JPanel p = new JPanel(new GridLayout(1, 2));
+        p.add(initPanel("FlowLayout(LEFT)", panel1));
+        p.add(initPanel("y=Math.pow(x/4.0,2.0)", panel2));
+        add(p);
+        setPreferredSize(new Dimension(320, 240));
+    }
+    private static JComponent initPanel(String title, JComponent p) {
+        p.setBorder(BorderFactory.createTitledBorder(title));
+        p.add(new JCheckBox("aaaaaaaaaaaaaaa"));
+        p.add(new JCheckBox("bbbbbbbb"));
+        p.add(new JCheckBox("ccccccccccc"));
+        p.add(new JCheckBox("ddddddd"));
+        p.add(new JCheckBox("eeeeeeeeeee"));
+        return p;
+    }
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+    public static void createAndShowGUI() {
+        try{
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        JFrame frame = new JFrame("@title@");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new MainPanel());
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
