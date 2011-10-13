8ec8dd21af102ca0c8c0c3ae8994051dce047019
TERAI Atsuhiro <at.terai@gmail.com>
2011-10-13 18:49:19 +0000

    comment

--- LineSpacing/src/java/example/MainPanel.java
+++ LineSpacing/src/java/example/MainPanel.java
@@ -9,10 +9,11 @@ import javax.swing.text.*;
 import javax.swing.text.html.*;
 public class MainPanel extends JPanel {
     private final JTextPane editor1 = new JTextPane();
-    private final JEditorPane editor2 = new JEditorPane();
+    private final JTextPane editor2 = new JTextPane();
     public MainPanel() {
         super(new BorderLayout());
         SimpleAttributeSet attr = new SimpleAttributeSet();
+        StyleConstants.setForeground(attr, Color.RED);
         StyleConstants.setFontSize(attr, 32);
 
         SimpleAttributeSet a = new SimpleAttributeSet();
@@ -21,9 +22,8 @@ public class MainPanel extends JPanel {
         //StyleConstants.setSpaceBelow(a, 5.0f);
         //StyleConstants.setLeftIndent(a, 5.0f);
         //StyleConstants.setRightIndent(a, 5.0f);
-        editor1.setParagraphAttributes(a, true);
-        editor1.setText("JTextPane, StyleConstants.setLineSpacing(...);\naaaaaaaa");
-        setDummyText(editor1, "1234567890", attr);
+        editor1.setParagraphAttributes(a, false);
+        setDummyText(editor1, attr);
 
 //         StyleSheet styleSheet = new StyleSheet();
 //         styleSheet.addRule("body {font-size: 24pt; line-height: 2.0}"); //XXX
@@ -33,29 +33,28 @@ public class MainPanel extends JPanel {
 //         editor1.setText("<html><body>12341234<br />asdf_fASdfasf_fasdf_affffFSDdfasdf<br />nasdfasFasdf<font size='32'>12341234<br />asdfasdf</font></body></html>");
 
         editor2.setEditorKit(new BottomInsetEditorKit());
-        editor2.setText("JEditorPane, BottomInsetEditorKit\nbbbbbbb");
-        setDummyText(editor2, "0987654321\nasdf", attr);
+        setDummyText(editor2, attr);
 
-        final JSplitPane sp = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+        JSplitPane sp = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
         sp.setTopComponent(new JScrollPane(editor1));
         sp.setBottomComponent(new JScrollPane(editor2));
         sp.setResizeWeight(0.5);
         add(sp);
         setPreferredSize(new Dimension(320, 240));
-        EventQueue.invokeLater(new Runnable() {
-            @Override public void run() {
-                sp.setDividerLocation(sp.getSize().height/2);
-            }
-        });
     }
-    private static void setDummyText(JEditorPane editor, String str, MutableAttributeSet attr) {
-        Document doc = editor.getDocument();
+    private static void setDummyText(JTextPane textPane, MutableAttributeSet attr) {
+        textPane.setText("12341234\nasdf fASdfasf fasdf affffFS Ddfasdf\nasdf asFasdf ");
         try{
-            doc.insertString(doc.getLength(), str, attr);
-            editor.setCaretPosition(doc.getLength());
+            StyledDocument doc = textPane.getStyledDocument();
+            doc.insertString(doc.getLength(), "134500698\n", attr);
         }catch(BadLocationException e) {
             e.printStackTrace();
         }
+//         StyledDocument doc = new DefaultStyledDocument();
+//         SimpleAttributeSet a = new SimpleAttributeSet();
+//         StyleConstants.setLineSpacing(a, .5f);
+//         doc.setParagraphAttributes(0, doc.getLength()-1, a, false);
+//         textPane.setStyledDocument(doc);
     }
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
