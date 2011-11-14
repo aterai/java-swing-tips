d1ef7199a14902b4a0051c9d00e770f62583a3d7
TERAI Atsuhiro <at.terai@gmail.com>
2011-11-14 03:10:24 +0000

    Test: JSplitPane#setResizeWeight

--- DividerLocation/src/java/example/MainPanel.java
+++ DividerLocation/src/java/example/MainPanel.java
@@ -11,22 +11,24 @@ import javax.swing.plaf.*;
 import javax.swing.table.*;
 
 public class MainPanel extends JPanel {
+    private final JRadioButton r0 = new JRadioButton("0.0");
+    private final JRadioButton r1 = new JRadioButton("0.5");
+    private final JRadioButton r2 = new JRadioButton("1.0");
+    private final String[] columnNames = {"String", "Integer", "Boolean"};
+    private final Object[][] data = {
+        {"aaa", 12, true}, {"bbb", 5, false},
+        {"CCC", 92, true}, {"DDD", 0, false}
+    };
+    private final DefaultTableModel model = new DefaultTableModel(data, columnNames) {
+        @Override public Class<?> getColumnClass(int column) {
+            return getValueAt(0, column).getClass();
+        }
+    };
+    private final JSplitPane sp = new JSplitPane(JSplitPane.VERTICAL_SPLIT,
+                                                 new JScrollPane(new JTable(model)),
+                                                 new JScrollPane(new JTree()));
     public JComponent makeUI() {
-        String[] columnNames = {"String", "Integer", "Boolean"};
-        Object[][] data = {
-            {"aaa", 12, true}, {"bbb", 5, false},
-            {"CCC", 92, true}, {"DDD", 0, false}
-        };
-        DefaultTableModel model = new DefaultTableModel(data, columnNames) {
-            @Override public Class<?> getColumnClass(int column) {
-                return getValueAt(0, column).getClass();
-            }
-        };
-        JScrollPane s1, s2;
-        final JSplitPane sp = new JSplitPane(JSplitPane.VERTICAL_SPLIT,
-                                             new JScrollPane(new JTable(model)),
-                                             new JScrollPane(new JTree()));
-
+//         JScrollPane s1, s2;
 //         System.out.println(s1.getPreferredSize());
 //         System.out.println(s2.getPreferredSize());
 //         s1.setPreferredSize(new Dimension(320, 100));
@@ -41,6 +43,27 @@ public class MainPanel extends JPanel {
     }
     public MainPanel() {
         super(new BorderLayout());
+        ActionListener al = new ActionListener() {
+            @Override public void actionPerformed(ActionEvent e) {
+                if(r2.isSelected()) {
+                    sp.setResizeWeight(1.0);
+                }else if(r1.isSelected()) {
+                    sp.setResizeWeight(0.5);
+                }else{
+                    sp.setResizeWeight(0.0);
+                }
+            }
+        };
+        ButtonGroup bg = new ButtonGroup();
+        JPanel p = new JPanel();
+        p.add(new JLabel("JSplitPane#setResizeWeight: "));
+        for(JRadioButton r: java.util.Arrays.asList(r0, r1, r2)) {
+            r.addActionListener(al);
+            bg.add(r);
+            p.add(r);
+        }
+        r0.setSelected(true);
+        add(p, BorderLayout.NORTH);
         add(makeUI());
         setPreferredSize(new Dimension(320, 240));
     }
