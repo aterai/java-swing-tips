d4874e9451bd7e2d07b3399b3da44204daeebe80
TERAI Atsuhiro <at.terai@gmail.com>
2011-12-19 14:30:45 +0000

    add_TreeDragScroll

---
+++ TreeDragScroll/src/java/example/MainPanel.java
@@ -0,0 +1,93 @@
+package example;
+//-*- mode:java; encoding:utf8n; coding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+//http://terai.xrea.jp/Swing/TreeDragScroll.html
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.tree.*;
+
+class MainPanel extends JPanel {
+    public MainPanel() {
+        super(new GridLayout(2, 1));
+
+        JTree tree1 = new JTree();
+        JTree tree2 = new JTree();
+        expandTree(tree1);
+        expandTree(tree2);
+
+        MouseAdapter ma = new DragScrollListener();
+        tree2.addMouseMotionListener(ma);
+        tree2.addMouseListener(ma);
+
+        add(makeTitledComponent(tree1, "Default"));
+        add(makeTitledComponent(tree2, "Drag scroll"));
+        setPreferredSize(new Dimension(320, 240));
+    }
+    private static void expandTree(JTree tree) {
+        DefaultMutableTreeNode root = (DefaultMutableTreeNode)tree.getModel().getRoot();
+        Enumeration e = root.breadthFirstEnumeration();
+        while (e.hasMoreElements()) {
+            DefaultMutableTreeNode node = (DefaultMutableTreeNode)e.nextElement();
+            if (node.isLeaf()) continue;
+            int row = tree.getRowForPath(new TreePath(node.getPath()));
+            tree.expandRow(row);
+        }
+    }
+    private static JComponent makeTitledComponent(JComponent c, String title) {
+        JScrollPane scroll = new JScrollPane(c);
+        scroll.setBorder(BorderFactory.createTitledBorder(title));
+        return scroll;
+    }
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+    public static void createAndShowGUI() {
+        try{
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        JFrame frame = new JFrame("TreeDragScroll");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new MainPanel());
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
+class DragScrollListener extends MouseAdapter {
+    private final Cursor defCursor = Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR);
+    private final Cursor hndCursor = Cursor.getPredefinedCursor(Cursor.HAND_CURSOR);
+    private final Point pp = new Point();
+    @Override public void mouseDragged(MouseEvent e) {
+        JComponent jc = (JComponent)e.getSource();
+        Container c = jc.getParent();
+        if(c instanceof JViewport) {
+            JViewport vport = (JViewport)c;
+            Point cp = SwingUtilities.convertPoint(jc,e.getPoint(),vport);
+            Point vp = vport.getViewPosition();
+            vp.translate(pp.x-cp.x, pp.y-cp.y);
+            jc.scrollRectToVisible(new Rectangle(vp, vport.getSize()));
+            pp.setLocation(cp);
+        }
+    }
+    @Override public void mousePressed(MouseEvent e) {
+        JComponent jc = (JComponent)e.getSource();
+        Container c = jc.getParent();
+        if(c instanceof JViewport) {
+            jc.setCursor(hndCursor);
+            JViewport vport = (JViewport)c;
+            Point cp = SwingUtilities.convertPoint(jc,e.getPoint(),vport);
+            pp.setLocation(cp);
+        }
+    }
+    @Override public void mouseReleased(MouseEvent e) {
+        ((JComponent)e.getSource()).setCursor(defCursor);
+    }
+}
