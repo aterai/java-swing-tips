51f894a73d51ea539db1206ed9dea167a63f0278
at.terai@gmail.com <at.terai@gmail.com@3ab87fda-ff15-71db-baf3-2d1a8ae44ab9>
2012-03-19 07:10:31 +0000

    add_MinimumThumbSize

---
+++ trunk/MinimumThumbSize/src/java/example/MainPanel.java
@@ -0,0 +1,49 @@
+package example;
+//-*- mode:java; encoding:utf8n; coding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+//@homepage@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MainPanel extends JPanel{
+    private static final String LF = "\n";
+    public MainPanel() {
+        super(new BorderLayout());
+        StringBuffer buf = new StringBuffer();
+        for(int i=0;i<1000;i++) buf.append(i+LF);
+
+        JSplitPane sp = new JSplitPane();
+        sp.setLeftComponent(new JScrollPane(new JTextArea(buf.toString())));
+
+        UIManager.put("ScrollBar.minimumThumbSize", new Dimension(32, 32));
+        sp.setRightComponent(new JScrollPane(new JTextArea(buf.toString())));
+
+        sp.setResizeWeight(.5);
+        add(sp);
+    }
+    private static final Dimension preferredSize = new Dimension(320, 240);
+    @Override public Dimension getPreferredSize() {
+        return preferredSize;
+    }
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+    public static void createAndShowGUI() {
+        try{
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        JFrame frame = new JFrame("@title@");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new MainPanel());
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
