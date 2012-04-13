09886e054a11d36574290a8a1da0353d1ba66331
TERAI Atsuhiro <at.terai@gmail.com>
2012-04-13 10:08:21 +0000

    Add CheckBoxStatusUpdateListener

--- CheckBoxNodeEditor/src/java/example/MainPanel.java
+++ CheckBoxNodeEditor/src/java/example/MainPanel.java
@@ -33,6 +33,7 @@ public class MainPanel extends JPanel {
                 node.setUserObject(new CheckBoxNode((String)o, Status.DESELECTED));
             }
         }
+        model.addTreeModelListener(new CheckBoxStatusUpdateListener());
         tree.setEditable(true);
         tree.setBorder(BorderFactory.createEmptyBorder(4,4,4,4));
 
@@ -42,79 +43,6 @@ public class MainPanel extends JPanel {
 
         tree.expandRow(0);
         //tree.setToggleClickCount(1);
-
-        model.addTreeModelListener(new TreeModelListener() {
-            private boolean adjusting = false;
-            @Override public void treeNodesChanged(TreeModelEvent e) {
-                if(adjusting) return;
-                adjusting = true;
-                TreePath parent = e.getTreePath();
-                Object[] children = e.getChildren();
-                DefaultTreeModel model = (DefaultTreeModel)e.getSource();
-
-                DefaultMutableTreeNode node;
-                CheckBoxNode c; // = (CheckBoxNode)node.getUserObject();
-                if(children!=null && children.length==1) {
-                    node = (DefaultMutableTreeNode)children[0];
-                    c = (CheckBoxNode)node.getUserObject();
-                    DefaultMutableTreeNode n = (DefaultMutableTreeNode)parent.getLastPathComponent();
-                    while(n!=null) {
-                        updateParentUserObject(n);
-                        DefaultMutableTreeNode tmp = (DefaultMutableTreeNode)n.getParent();
-                        if(tmp==null) {
-                            break;
-                        }else{
-                            n = tmp;
-                        }
-                    }
-                    model.nodeChanged(n);
-                }else{
-                    node = (DefaultMutableTreeNode)model.getRoot();
-                    c = (CheckBoxNode)node.getUserObject();
-                }
-                updateAllChildrenUserObject(node, c.status);
-                model.nodeChanged(node);
-                adjusting = false;
-            }
-            private void updateParentUserObject(DefaultMutableTreeNode parent) {
-                String label = ((CheckBoxNode)parent.getUserObject()).label;
-                int selectedCount = 0;
-                int indeterminateCount = 0;
-                java.util.Enumeration children = parent.children();
-                while(children.hasMoreElements()) {
-                    DefaultMutableTreeNode node = (DefaultMutableTreeNode)children.nextElement();
-                    CheckBoxNode check = (CheckBoxNode)node.getUserObject();
-                    if(check.status==Status.INDETERMINATE) {
-                        indeterminateCount++;
-                        break;
-                    }
-                    if(check.status==Status.SELECTED) selectedCount++;
-                }
-                if(indeterminateCount>0) {
-                    parent.setUserObject(new CheckBoxNode(label));
-                }else if(selectedCount==0) {
-                    parent.setUserObject(new CheckBoxNode(label, Status.DESELECTED));
-                }else if(selectedCount==parent.getChildCount()) {
-                    parent.setUserObject(new CheckBoxNode(label, Status.SELECTED));
-                }else{
-                    parent.setUserObject(new CheckBoxNode(label));
-                }
-            }
-            private void updateAllChildrenUserObject(DefaultMutableTreeNode root, Status status) {
-                java.util.Enumeration breadth = root.breadthFirstEnumeration();
-                while(breadth.hasMoreElements()) {
-                    DefaultMutableTreeNode node = (DefaultMutableTreeNode)breadth.nextElement();
-                    if(root==node) {
-                        continue;
-                    }
-                    CheckBoxNode check = (CheckBoxNode)node.getUserObject();
-                    node.setUserObject(new CheckBoxNode(check.label, status));
-                }
-            }
-            @Override public void treeNodesInserted(TreeModelEvent e) {}
-            @Override public void treeNodesRemoved(TreeModelEvent e) {}
-            @Override public void treeStructureChanged(TreeModelEvent e) {}
-        });
 
         setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
         add(new JScrollPane(tree));
@@ -191,6 +119,79 @@ class CheckBoxNode{
     @Override public String toString() {
         return label;
     }
+}
+
+class CheckBoxStatusUpdateListener implements TreeModelListener{
+    private boolean adjusting = false;
+    @Override public void treeNodesChanged(TreeModelEvent e) {
+        if(adjusting) return;
+        adjusting = true;
+        TreePath parent = e.getTreePath();
+        Object[] children = e.getChildren();
+        DefaultTreeModel model = (DefaultTreeModel)e.getSource();
+
+        DefaultMutableTreeNode node;
+        CheckBoxNode c; // = (CheckBoxNode)node.getUserObject();
+        if(children!=null && children.length==1) {
+            node = (DefaultMutableTreeNode)children[0];
+            c = (CheckBoxNode)node.getUserObject();
+            DefaultMutableTreeNode n = (DefaultMutableTreeNode)parent.getLastPathComponent();
+            while(n!=null) {
+                updateParentUserObject(n);
+                DefaultMutableTreeNode tmp = (DefaultMutableTreeNode)n.getParent();
+                if(tmp==null) {
+                    break;
+                }else{
+                    n = tmp;
+                }
+            }
+            model.nodeChanged(n);
+        }else{
+            node = (DefaultMutableTreeNode)model.getRoot();
+            c = (CheckBoxNode)node.getUserObject();
+        }
+        updateAllChildrenUserObject(node, c.status);
+        model.nodeChanged(node);
+        adjusting = false;
+    }
+    private void updateParentUserObject(DefaultMutableTreeNode parent) {
+        String label = ((CheckBoxNode)parent.getUserObject()).label;
+        int selectedCount = 0;
+        int indeterminateCount = 0;
+        java.util.Enumeration children = parent.children();
+        while(children.hasMoreElements()) {
+            DefaultMutableTreeNode node = (DefaultMutableTreeNode)children.nextElement();
+            CheckBoxNode check = (CheckBoxNode)node.getUserObject();
+            if(check.status==Status.INDETERMINATE) {
+                indeterminateCount++;
+                break;
+            }
+            if(check.status==Status.SELECTED) selectedCount++;
+        }
+        if(indeterminateCount>0) {
+            parent.setUserObject(new CheckBoxNode(label));
+        }else if(selectedCount==0) {
+            parent.setUserObject(new CheckBoxNode(label, Status.DESELECTED));
+        }else if(selectedCount==parent.getChildCount()) {
+            parent.setUserObject(new CheckBoxNode(label, Status.SELECTED));
+        }else{
+            parent.setUserObject(new CheckBoxNode(label));
+        }
+    }
+    private void updateAllChildrenUserObject(DefaultMutableTreeNode root, Status status) {
+        java.util.Enumeration breadth = root.breadthFirstEnumeration();
+        while(breadth.hasMoreElements()) {
+            DefaultMutableTreeNode node = (DefaultMutableTreeNode)breadth.nextElement();
+            if(root==node) {
+                continue;
+            }
+            CheckBoxNode check = (CheckBoxNode)node.getUserObject();
+            node.setUserObject(new CheckBoxNode(check.label, status));
+        }
+    }
+    @Override public void treeNodesInserted(TreeModelEvent e) {}
+    @Override public void treeNodesRemoved(TreeModelEvent e) {}
+    @Override public void treeStructureChanged(TreeModelEvent e) {}
 }
 
 //*
