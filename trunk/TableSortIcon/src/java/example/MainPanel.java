b1dee1c030090a5b4ee5ee3ccbb3ab7b2e984cd8
at.terai@gmail.com <at.terai@gmail.com@3ab87fda-ff15-71db-baf3-2d1a8ae44ab9>
2012-05-16 10:03:57 +0000

    Remove TestModel.java

--- trunk/TableSortIcon/src/java/example/MainPanel.java
+++ trunk/TableSortIcon/src/java/example/MainPanel.java
@@ -4,28 +4,24 @@ package example;
 //@homepage@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Arrays;
 import javax.swing.*;
-import javax.swing.event.*;
 import javax.swing.table.*;
 import javax.swing.plaf.*;
 
 class MainPanel extends JPanel {
-    private final TestModel model = new TestModel();
-    private final TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(model);
-    private final JTable table = new JTable(model) {
-        private final Color evenColor = new Color(245, 245, 245);
-        @Override public Component prepareRenderer(TableCellRenderer tcr, int row, int column) {
-            Component c = super.prepareRenderer(tcr, row, column);
-            if(isRowSelected(row)) {
-                c.setForeground(getSelectionForeground());
-                c.setBackground(getSelectionBackground());
-            }else{
-                c.setForeground(getForeground());
-                c.setBackground((row%2==0)?evenColor:getBackground());
-            }
-            return c;
+    private final String[] columnNames = {"String", "Integer", "Boolean"};
+    private final Object[][] data = {
+        {"aaa", 12, true}, {"bbb", 5, false},
+        {"CCC", 92, true}, {"DDD", 0, false}
+    };
+    private final DefaultTableModel model = new DefaultTableModel(data, columnNames) {
+        @Override public Class<?> getColumnClass(int column) {
+            return getValueAt(0, column).getClass();
         }
     };
+    private final JTable table = new JTable(model);
+    private final TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(model);
 
     private static final Icon emptyIcon = new Icon() {
         @Override public void paintIcon(Component c, Graphics g, int x, int y) {}
@@ -65,9 +61,12 @@ class MainPanel extends JPanel {
         box1.setBorder(BorderFactory.createEmptyBorder(0,5,0,5));
         ButtonGroup bg = new ButtonGroup();
         box1.add(new JLabel("Table Sort Icon: "));
-        box1.add(r0); bg.add(r0); r0.addActionListener(al);
-        box1.add(r1); bg.add(r1); r1.addActionListener(al);
-        box1.add(r2); bg.add(r2); r2.addActionListener(al);
+        for(JRadioButton rb: Arrays.asList(r0, r1, r2)) {
+            box1.add(rb);
+            box1.add(Box.createHorizontalStrut(5));
+            bg.add(rb);
+            rb.addActionListener(al);
+        }
         box1.add(Box.createHorizontalGlue());
         r0.setSelected(true);
         return box1;
@@ -76,33 +75,6 @@ class MainPanel extends JPanel {
     public MainPanel() {
         super(new BorderLayout());
         table.setRowSorter(sorter);
-        TableColumn col = table.getColumnModel().getColumn(0);
-        col.setMinWidth(60);
-        col.setMaxWidth(60);
-        col.setResizable(false);
-        model.addTest(new Test("Name 1", "comment..."));
-        model.addTest(new Test("Name 2", "Test"));
-        model.addTest(new Test("Name d", ""));
-        model.addTest(new Test("Name c", "Test cc"));
-        model.addTest(new Test("Name b", "Test bb"));
-        model.addTest(new Test("Name a", ""));
-        model.addTest(new Test("Name 0", "Test aa"));
-
-//         JCheckBox check = new JCheckBox(new AbstractAction("paintSortIcon") {
-//             @Override
-//             public void actionPerformed(ActionEvent e) {
-//                 JCheckBox cb = (JCheckBox)e.getSource();
-//                 if(cb.isSelected()) {
-//                     UIManager.put("Table.ascendingSortIcon",  defaultAscendingSortIcon);
-//                     UIManager.put("Table.descendingSortIcon", defaultDescendingSortIcon);
-//                 }else{
-//                     UIManager.put("Table.ascendingSortIcon",  new IconUIResource(emptyIcon));
-//                     UIManager.put("Table.descendingSortIcon", new IconUIResource(emptyIcon));
-//                 }
-//                 table.getTableHeader().repaint();
-//             }
-//         });
-//         check.setSelected(true);
         add(makeRadioPane(), BorderLayout.NORTH);
         add(new JButton(new AbstractAction("clear SortKeys") {
             @Override public void actionPerformed(ActionEvent e) {
