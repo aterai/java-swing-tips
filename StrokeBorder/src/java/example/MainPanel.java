665c5886059c3c9f4a316965c12bea8174fe47c7
TERAI Atsuhiro <at.terai@gmail.com>
2012-07-23 03:16:58 +0000

    add_StrokeBorder

---
+++ StrokeBorder/src/java/example/MainPanel.java
@@ -0,0 +1,108 @@
+package example;
+//-*- mode:java; encoding:utf8n; coding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+//@homepage@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class MainPanel extends JPanel{
+    private final JComboBox<JoinStyle> joinCombo = new JComboBox<>(JoinStyle.values());
+    private final JComboBox<EndCapStyle> endcapCombo = new JComboBox<>(EndCapStyle.values());
+    private final JTextField field = new JTextField("10, 20");
+    private final JLabel label = new JLabel();
+    public  final JButton button;
+    private float[] getDashArray() {
+        StringTokenizer st = new StringTokenizer(field.getText(), ",");
+        float[] list = new float[st.countTokens()];
+        int i=0;
+        try{
+            while(st.hasMoreTokens()) {
+                list[i] = (new Float(st.nextToken())).floatValue();
+                i++;
+            }
+        }catch(final NumberFormatException nfe) {
+            java.awt.Toolkit.getDefaultToolkit().beep();
+            JOptionPane.showMessageDialog(label, "Invalid input.\n"+nfe.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            float[] fd = {1.0f};
+            return fd;
+        }
+        return list;
+    }
+
+    private BasicStroke dashedStroke;
+    public MainPanel() {
+        super(new BorderLayout());
+        button = new JButton(new AbstractAction("Change") {
+            @Override public void actionPerformed(ActionEvent ae) {
+                dashedStroke = new BasicStroke(5.0f,
+                    ((EndCapStyle)endcapCombo.getSelectedItem()).style,
+                    ((JoinStyle)joinCombo.getSelectedItem()).style,
+                    5.0f, getDashArray(), 0.0f);
+                label.setBorder(BorderFactory.createStrokeBorder(dashedStroke, Color.RED));
+            }
+        });
+
+        JPanel p = new JPanel(new BorderLayout(2,2));
+        p.add(field); p.add(button, BorderLayout.EAST);
+        p.setBorder(BorderFactory.createTitledBorder("Comma Separated Values"));
+
+        JPanel p1 = new JPanel(new GridLayout(2,1));
+        p1.add(endcapCombo);
+        p1.add(joinCombo);
+
+        p.add(p1, BorderLayout.NORTH);
+
+        JPanel p2 = new JPanel(new BorderLayout());
+        p2.add(label);
+        p2.setBorder(BorderFactory.createEmptyBorder(8,8,8,8));
+        add(p, BorderLayout.NORTH);
+        add(p2);
+        setPreferredSize(new Dimension(320, 240));
+
+        button.doClick();
+    }
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+    public static void createAndShowGUI() {
+        try{
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        MainPanel p = new MainPanel();
+        JFrame frame = new JFrame("@title@");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().add(p);
+        frame.getRootPane().setDefaultButton(p.button);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
+
+enum JoinStyle {
+    JOIN_BEVEL(BasicStroke.JOIN_BEVEL),
+    JOIN_MITER(BasicStroke.JOIN_MITER),
+    JOIN_ROUND(BasicStroke.JOIN_ROUND);
+    public final int style;
+    private JoinStyle(int style) {
+        this.style = style;
+    }
+}
+
+enum EndCapStyle {
+    CAP_BUTT(BasicStroke.CAP_BUTT),
+    CAP_ROUND(BasicStroke.CAP_ROUND),
+    CAP_SQUARE(BasicStroke.CAP_SQUARE);
+    public final int style;
+    private EndCapStyle(int style) {
+        this.style = style;
+    }
+}
