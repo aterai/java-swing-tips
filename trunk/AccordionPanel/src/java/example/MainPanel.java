4c63187d3b1db00d6f9a7e20d0aaa63b3a017456
at.terai@gmail.com <at.terai@gmail.com@3ab87fda-ff15-71db-baf3-2d1a8ae44ab9>
2012-08-21 07:13:28 +0000

    Remove unnecessary code

--- trunk/AccordionPanel/src/java/example/MainPanel.java
+++ trunk/AccordionPanel/src/java/example/MainPanel.java
@@ -5,95 +5,88 @@ package example;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
+import java.util.List;
 import javax.swing.*;
 import javax.swing.border.*;
 
 public class MainPanel extends JPanel{
-    private final Box box = Box.createVerticalBox();
-    private final Component glue = Box.createVerticalGlue();
-    private final JSplitPane split = new JSplitPane();
-    private final JScrollPane scroll;
-
+    //private final Box box = Box.createVerticalBox();
+    //private final Component glue = Box.createVerticalGlue();
     public MainPanel() {
         super(new BorderLayout());
-        box.setOpaque(true);
-        box.setBackground(new Color(180,180,255));
-        box.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
-        scroll = new JScrollPane(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-                                 JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        scroll.getVerticalScrollBar().setUnitIncrement(25);
-        scroll.getViewport().add(box);
 
-        ExpansionListener rl = new ExpansionListener() {
-            @Override public void expansionStateChanged(ExpansionEvent e) {
-                box.revalidate();
-            }
-        };
-        for(ExpansionPanel asp: makeList()) {
-            addComp(asp);
-            asp.addExpansionListener(rl);
+        Box accordion = Box.createVerticalBox();
+        accordion.setOpaque(true);
+        accordion.setBackground(new Color(180, 180, 255));
+        accordion.setBorder(BorderFactory.createEmptyBorder(10, 5, 5, 5));
+        for(ExpansionPanel p: makeList()) {
+            accordion.add(p);
+            accordion.add(Box.createVerticalStrut(5));
         }
-        scroll.getViewport().addComponentListener(new ComponentAdapter() {
-            @Override public void componentResized(ComponentEvent e) {
-                box.revalidate();
-            }
-        });
-        scroll.setMinimumSize(new Dimension(100, 0));
+        accordion.add(Box.createVerticalGlue());
+
+        JScrollPane scroll = new JScrollPane(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                                             JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scroll.getVerticalScrollBar().setUnitIncrement(25);
+        scroll.getViewport().add(accordion);
+
+        JSplitPane split = new JSplitPane();
         split.setResizeWeight(0.5);
         split.setDividerSize(2);
         split.setLeftComponent(scroll);
-        split.setRightComponent(new JLabel(" "));
+        split.setRightComponent(new JLabel("Dummy"));
         add(split);
         setPreferredSize(new Dimension(320, 240));
     }
-    public void addComp(Component comp) {
-        box.remove(glue);
-        box.add(Box.createVerticalStrut(5));
-        box.add(comp);
-        box.add(glue);
-        box.revalidate();
-    }
 
-    private Vector<ExpansionPanel> makeList() {
-        Vector<ExpansionPanel> panelList = new Vector<ExpansionPanel>();
-        panelList.addElement(new ExpansionPanel("System Tasks") {
-            @Override public JPanel makePanel() {
-                JPanel pnl = new JPanel(new GridLayout(0,1));
-                JCheckBox c1 = new JCheckBox("aaaa");
-                JCheckBox c2 = new JCheckBox("aaaaaaa");
-                c1.setOpaque(false);
-                c2.setOpaque(false);
-                pnl.add(c1);
-                pnl.add(c2);
-                return pnl;
-            }
-        });
-        panelList.addElement(new ExpansionPanel("Other Places") {
-            @Override public JPanel makePanel() {
-                JPanel pnl = new JPanel(new GridLayout(0,1));
-                pnl.add(new JLabel("Desktop"));
-                pnl.add(new JLabel("My Network Places"));
-                pnl.add(new JLabel("My Documents"));
-                pnl.add(new JLabel("Shared Documents"));
-                return pnl;
-            }
-        });
-        panelList.addElement(new ExpansionPanel("Details") {
-            @Override public JPanel makePanel() {
-                JPanel pnl = new JPanel(new GridLayout(0,1));
-                ButtonGroup bg = new ButtonGroup();
-                JRadioButton b1 = new JRadioButton("aaa");
-                JRadioButton b2 = new JRadioButton("bbb");
-                JRadioButton b3 = new JRadioButton("ccc");
-                JRadioButton b4 = new JRadioButton("ddd");
-                for(JRadioButton b:Arrays.asList(b1,b2,b3,b4)) {
-                    b.setOpaque(false); pnl.add(b); bg.add(b);
+//     public void addComp(Component comp) {
+//         box.remove(glue);
+//         box.add(Box.createVerticalStrut(5));
+//         box.add(comp);
+//         box.add(glue);
+//         box.revalidate();
+//     }
+
+    private List<ExpansionPanel> makeList() {
+        return Arrays.asList(
+            new ExpansionPanel("System Tasks") {
+                @Override public JPanel makePanel() {
+                    JPanel pnl = new JPanel(new GridLayout(0,1));
+                    JCheckBox c1 = new JCheckBox("aaaa");
+                    JCheckBox c2 = new JCheckBox("aaaaaaa");
+                    c1.setOpaque(false);
+                    c2.setOpaque(false);
+                    pnl.add(c1);
+                    pnl.add(c2);
+                    return pnl;
                 }
-                b1.setSelected(true);
-                return pnl;
+            },
+            new ExpansionPanel("Other Places") {
+                @Override public JPanel makePanel() {
+                    JPanel pnl = new JPanel(new GridLayout(0,1));
+                    pnl.add(new JLabel("Desktop"));
+                    pnl.add(new JLabel("My Network Places"));
+                    pnl.add(new JLabel("My Documents"));
+                    pnl.add(new JLabel("Shared Documents"));
+                    return pnl;
+                }
+            },
+            new ExpansionPanel("Details") {
+                @Override public JPanel makePanel() {
+                    JPanel pnl = new JPanel(new GridLayout(0,1));
+                    ButtonGroup bg = new ButtonGroup();
+                    JRadioButton b1 = new JRadioButton("aaa");
+                    JRadioButton b2 = new JRadioButton("bbb");
+                    JRadioButton b3 = new JRadioButton("ccc");
+                    JRadioButton b4 = new JRadioButton("ddd");
+                    for(JRadioButton b:Arrays.asList(b1,b2,b3,b4)) {
+                        b.setOpaque(false); pnl.add(b); bg.add(b);
+                    }
+                    b1.setSelected(true);
+                    return pnl;
+                }
             }
-        });
-        return panelList;
+        );
     }
 
     public static void main(String[] args) {
@@ -131,7 +124,7 @@ abstract class ExpansionPanel extends JPanel{
             @Override protected void paintComponent(Graphics g) {
                 Graphics2D g2 = (Graphics2D)g;
                 //Insets ins = getInsets();
-                g2.setPaint(new GradientPaint(50, 0, Color.WHITE, getWidth(), getHeight(), new Color(200,200,255)));
+                g2.setPaint(new GradientPaint(50, 0, Color.WHITE, getWidth(), getHeight(), new Color(200, 200, 255)));
                 g2.fillRect(0, 0, getWidth(), getHeight());
                 super.paintComponent(g);
             }
@@ -171,7 +164,7 @@ abstract class ExpansionPanel extends JPanel{
         panel.setVisible(!panel.isVisible());
         label.setText(String.format("%s %s", panel.isVisible()?"\u25B3":"\u25BC", title));
         revalidate();
-        fireExpansionEvent();
+        //fireExpansionEvent();
         EventQueue.invokeLater(new Runnable() {
             @Override public void run() {
                 panel.scrollRectToVisible(panel.getBounds());
@@ -179,30 +172,30 @@ abstract class ExpansionPanel extends JPanel{
         });
     }
 
-    protected Vector<ExpansionListener> expansionListenerList = new Vector<ExpansionListener>();
-    public void addExpansionListener(ExpansionListener listener) {
-        if(!expansionListenerList.contains(listener)) expansionListenerList.add(listener);
-    }
-    public void removeExpansionListener(ExpansionListener listener) {
-        expansionListenerList.remove(listener);
-    }
-    public void fireExpansionEvent() {
-        Vector list = (Vector)expansionListenerList.clone();
-        Enumeration enm = list.elements();
-        ExpansionEvent e = new ExpansionEvent(this);
-        while(enm.hasMoreElements()) {
-            ExpansionListener listener = (ExpansionListener)enm.nextElement();
-            listener.expansionStateChanged(e);
-        }
-    }
+//     protected Vector<ExpansionListener> expansionListenerList = new Vector<ExpansionListener>();
+//     public void addExpansionListener(ExpansionListener listener) {
+//         if(!expansionListenerList.contains(listener)) expansionListenerList.add(listener);
+//     }
+//     public void removeExpansionListener(ExpansionListener listener) {
+//         expansionListenerList.remove(listener);
+//     }
+//     public void fireExpansionEvent() {
+//         Vector list = (Vector)expansionListenerList.clone();
+//         Enumeration enm = list.elements();
+//         ExpansionEvent e = new ExpansionEvent(this);
+//         while(enm.hasMoreElements()) {
+//             ExpansionListener listener = (ExpansionListener)enm.nextElement();
+//             listener.expansionStateChanged(e);
+//         }
+//     }
 }
 
-class ExpansionEvent extends EventObject{
-    public ExpansionEvent(Object source) {
-        super(source);
-    }
-}
-
-interface ExpansionListener{
-    public void expansionStateChanged(ExpansionEvent e);
-}
+// class ExpansionEvent extends EventObject{
+//     public ExpansionEvent(Object source) {
+//         super(source);
+//     }
+// }
+//
+// interface ExpansionListener{
+//     public void expansionStateChanged(ExpansionEvent e);
+// }
