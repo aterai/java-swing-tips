ac493e21108b6f327e5608c2dd8dc2c40b1f0e74
TERAI Atsuhiro <at.terai@gmail.com>
2013-01-13 10:34:21 +0000

    add_TranslucentTree

---
+++ TranslucentTree/src/java/example/MainPanel.java
@@ -0,0 +1,158 @@
+package example;
+//-*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+//@homepage@
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.image.*;
+import javax.swing.*;
+import javax.swing.tree.*;
+//import javax.swing.plaf.nimbus.*; //JDK 1.7.0
+
+public class MainPanel extends JPanel {
+    public MainPanel() {
+        super(new GridLayout(1,2,2,2));
+        JTree tree1 = new JTree() {
+            @Override public void updateUI() {
+                super.updateUI();
+                setCellRenderer(new TranslucentTreeCellRenderer());
+                setOpaque(false);
+                setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+            }
+        };
+        JTree tree2 = new JTree() {
+            //http://terai.xrea.jp/Swing/TreeRowSelection.html
+            private final Color SELC = new Color(100,100,255,100);
+            @Override public void paintComponent(Graphics g) {
+                if(getSelectionCount()>0) {
+                    for(int i: getSelectionRows()) {
+                        Rectangle r = getRowBounds(i);
+                        g.setColor(SELC);
+                        g.fillRect(0, r.y, getWidth(), r.height);
+                    }
+                }
+                super.paintComponent(g);
+                if(getLeadSelectionPath()!=null) {
+                    Rectangle r = getRowBounds(getRowForPath(getLeadSelectionPath()));
+                    g.setColor(SELC.darker());
+                    g.drawRect(0, r.y, getWidth()-1, r.height-1);
+                }
+            }
+            @Override public void updateUI() {
+                super.updateUI();
+                setCellRenderer(new TransparentTreeCellRenderer());
+                setOpaque(false);
+                setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+            }
+        };
+
+//         //NimbusLookAndFeel(SynthLookAndFeel) JDK 1.7.0
+//         UIDefaults d = new UIDefaults();
+//         d.put("Tree:TreeCell[Enabled+Selected].backgroundPainter", new TransparentTreeCellPainter());
+//         tree2.putClientProperty("Nimbus.Overrides", d);
+//         tree2.putClientProperty("Nimbus.Overrides.InheritDefaults", false);
+
+        add(makeTranslucentScrollPane(tree1));
+        add(makeTranslucentScrollPane(tree2));
+
+        setOpaque(false);
+        setPreferredSize(new Dimension(320, 240));
+    }
+    private static JScrollPane makeTranslucentScrollPane(JComponent c) {
+        JScrollPane scroll = new JScrollPane(c);
+        scroll.setOpaque(false);
+        scroll.getViewport().setOpaque(false);
+        return scroll;
+    }
+    private static TexturePaint makeCheckerTexture() {
+        int cs = 6;
+        int sz = cs*cs;
+        BufferedImage img = new BufferedImage(sz,sz,BufferedImage.TYPE_4BYTE_ABGR);
+        Graphics2D g2 = img.createGraphics();
+        g2.setPaint(new Color(220,220,220));
+        g2.fillRect(0,0,sz,sz);
+        g2.setPaint(new Color(200,200,200,200));
+        for(int i=0;i*cs<sz;i++) {
+            for(int j=0;j*cs<sz;j++) {
+                if((i+j)%2==0) g2.fillRect(i*cs, j*cs, cs, cs);
+            }
+        }
+        g2.dispose();
+        return new TexturePaint(img, new Rectangle(0,0,sz,sz));
+    }
+    public static void createAndShowGUI() {
+        try{
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        JFrame frame = new JFrame("@title@") {
+            @Override protected JRootPane createRootPane() {
+                //http://terai.xrea.jp/Swing/RootPaneBackground.html
+                JRootPane rp = new JRootPane() {
+                    private final TexturePaint texture = makeCheckerTexture();
+                    @Override protected void paintComponent(Graphics g) {
+                        super.paintComponent(g);
+                        Graphics2D g2 = (Graphics2D)g.create();
+                        g2.setPaint(texture);
+                        g2.fillRect(0, 0, getWidth(), getHeight());
+                        g2.dispose();
+                    }
+                    @Override public void updateUI() {
+                        super.updateUI();
+                        setOpaque(false);
+                    }
+                };
+                return rp;
+            }
+        };
+        ((JComponent)frame.getContentPane()).setOpaque(false);
+        frame.getContentPane().add(new MainPanel());
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
+
+// http://terai.xrea.jp/Swing/TreeBackgroundSelectionColor.html
+class TransparentTreeCellRenderer extends DefaultTreeCellRenderer {
+    @Override public Component getTreeCellRendererComponent(JTree tree, Object value, boolean isSelected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
+        JComponent c = (JComponent)super.getTreeCellRendererComponent(tree, value, isSelected, expanded, leaf, row, hasFocus);
+        c.setOpaque(false);
+        return c;
+    }
+    private final Color ALPHA_OF_ZERO = new Color(0, true);
+    @Override public Color getBackgroundNonSelectionColor() {
+        return ALPHA_OF_ZERO;
+    }
+    @Override public Color getBackgroundSelectionColor() {
+        return ALPHA_OF_ZERO;
+    }
+}
+
+class TranslucentTreeCellRenderer extends TransparentTreeCellRenderer {
+    private final Color backgroundSelectionColor = new Color(100, 100, 255, 100);
+    @Override public Color getBackgroundSelectionColor() {
+        return backgroundSelectionColor;
+    }
+}
+
+//http://terai.xrea.jp/Swing/NimbusColorPalette.html
+// // JDK 1.7.0
+// class TransparentTreeCellPainter extends AbstractRegionPainter {
+//     //private PaintContext ctx = null;
+//     @Override protected void doPaint(Graphics2D g, JComponent c, int width, int height, Object[] extendedCacheKeys) {
+//         //Do nothing
+//     }
+//     @Override protected final PaintContext getPaintContext() {
+//         return null;
+//     }
+// }
