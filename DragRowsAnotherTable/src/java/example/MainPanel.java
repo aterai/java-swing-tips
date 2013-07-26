f0b5dd57872259281d6a441d29bf33974c168d85
TERAI Atsuhiro <at.terai@gmail.com>
2013-07-26 10:58:49 +0000

    Bug ID: JDK-6967479 JTable sorter fires even if the model is empty

--- DragRowsAnotherTable/src/java/example/MainPanel.java
+++ DragRowsAnotherTable/src/java/example/MainPanel.java
@@ -28,7 +28,20 @@ public class MainPanel extends JPanel {
     private JTable makeDnDTable() {
         JTable t = new JTable(new DefaultTableModel(data, columnNames) {
             @Override public Class<?> getColumnClass(int column) {
-                return getValueAt(0, column).getClass();
+                //ArrayIndexOutOfBoundsException:  0 >= 0
+                //Bug ID: JDK-6967479 JTable sorter fires even if the model is empty
+                //http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6967479
+                //return getValueAt(0, column).getClass();
+                switch(column) {
+                  case 0:
+                    return String.class;
+                  case 1:
+                    return Number.class;
+                  case 2:
+                    return Boolean.class;
+                  default:
+                    return super.getColumnClass(column);
+                }
             }
         });
         t.getSelectionModel().setSelectionMode(
