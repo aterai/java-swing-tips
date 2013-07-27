710836dd1975d4066f8e0b17ac7feec60d830a16
TERAI Atsuhiro <at.terai@gmail.com>
2013-07-27 21:13:36 +0000

    add_TableHighlightRegexFilter

---
+++ TableHighlightRegexFilter/src/java/example/MainPanel.java
@@ -0,0 +1,151 @@
+package example;
+//-*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+//@homepage@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.regex.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.text.*;
+import javax.swing.table.*;
+
+public class MainPanel extends JPanel {
+    private static final Color warningColor = new Color(255, 200, 200);
+    private final JTextField field = new JTextField("ab+");
+    private final HighlightTableCellRenderer renderer = new HighlightTableCellRenderer();
+
+    private final String[] columnNames = {"A", "B"};
+    private final Object[][] data = {
+        {"aaa", "bbaacc"}, {"bbb", "defg"},
+        {"ccccbbbbaaabbbbaaeee", "xxx"}, {"dddaaabbbbb", "ccbbaa"},
+        {"cc cc bbbb aaa bbbb e", "xxx"}, {"ddd aaa b bbbb", "cc bbaa"}
+    };
+    private final DefaultTableModel model = new DefaultTableModel(data, columnNames) {
+        @Override public Class<?> getColumnClass(int column) {
+            return String.class;
+        }
+    };
+    private final TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(model);
+    private final JTable table = new JTable(model);
+
+    public MainPanel() {
+        super(new BorderLayout(5, 5));
+
+        table.setFillsViewportHeight(true);
+        table.setRowSorter(sorter);
+        table.setDefaultRenderer(String.class, renderer);
+
+        field.getDocument().addDocumentListener(new DocumentListener() {
+            @Override public void insertUpdate(DocumentEvent e) {
+                fireDocumentChangeEvent();
+            }
+            @Override public void removeUpdate(DocumentEvent e) {
+                fireDocumentChangeEvent();
+            }
+            @Override public void changedUpdate(DocumentEvent e) {}
+        });
+        fireDocumentChangeEvent();
+
+        JPanel sp = new JPanel(new BorderLayout(5, 5));
+        sp.add(new JLabel("regex pattern:"), BorderLayout.WEST);
+        sp.add(field);
+        sp.add(Box.createVerticalStrut(2), BorderLayout.SOUTH);
+        sp.setBorder(BorderFactory.createTitledBorder("Search"));
+
+        setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        add(sp, BorderLayout.NORTH);
+        add(new JScrollPane(table));
+        setPreferredSize(new Dimension(320, 240));
+    }
+
+    private void fireDocumentChangeEvent() {
+        field.setBackground(Color.WHITE);
+        String pattern = field.getText().trim();
+        if(pattern.isEmpty()) {
+            sorter.setRowFilter(null);
+            renderer.setPattern("");
+        }else if(renderer.setPattern(pattern)) {
+            try{
+                sorter.setRowFilter(RowFilter.regexFilter(pattern));
+            }catch(PatternSyntaxException ex) {
+                field.setBackground(warningColor);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+    public static void createAndShowGUI() {
+        try{
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        JFrame frame = new JFrame("@title@");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new MainPanel());
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
+
+class HighlightTableCellRenderer extends JTextField implements TableCellRenderer {
+    private static final Color backgroundSelectionColor = new Color(220, 240, 255);
+    private static final Highlighter.HighlightPainter highlightPainter
+        = new DefaultHighlighter.DefaultHighlightPainter(Color.YELLOW);
+    private String pattern = "";
+    private String prev = null;
+
+    public boolean setPattern(String str) {
+        if(str==null || str.equals(pattern)) {
+            return false;
+        }else{
+            prev = pattern;
+            pattern = str;
+            return true;
+        }
+    }
+    public HighlightTableCellRenderer() {
+        super();
+        setOpaque(true);
+        setBorder(BorderFactory.createEmptyBorder());
+        setForeground(Color.BLACK);
+        setBackground(Color.WHITE);
+        setEditable(false);
+    }
+    public void clearHighlights() {
+        Highlighter highlighter = getHighlighter();
+        for(Highlighter.Highlight h: highlighter.getHighlights()) {
+            highlighter.removeHighlight(h);
+        }
+    }
+    @Override public Component getTableCellRendererComponent(
+        JTable table, Object value, boolean isSelected,
+        boolean hasFocus, int row, int column) {
+        String txt = value!=null ? value.toString() : "";
+        clearHighlights();
+        setText(txt);
+        setBackground(isSelected ? backgroundSelectionColor : Color.WHITE);
+        if(pattern!=null && !pattern.isEmpty() && !pattern.equals(prev)) {
+            Matcher matcher = Pattern.compile(pattern).matcher(txt);
+            if(matcher.find()) {
+                int start = matcher.start();
+                int end   = matcher.end();
+                try{
+                    getHighlighter().addHighlight(start, end, highlightPainter);
+                }catch(BadLocationException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return this;
+    }
+}
