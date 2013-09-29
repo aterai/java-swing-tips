30862d7c6c73f6bf3894004fe0af3789c90e71a8
TERAI Atsuhiro <at.terai@gmail.com>
2013-09-29 15:46:51 +0000

    add_MenuItemAcceleratorAlignment

---
+++ MenuItemAcceleratorAlignment/src/java/sun/awt/resources/awt_ja.java
@@ -0,0 +1,11 @@
+package sun.awt.resources;
+import java.util.ListResourceBundle;
+//ant package
+//cd target
+//"%JAVA_HOME%\bin\java" -Xbootclasspath/p:example.jar -jar example.jar
+public class awt_ja extends ListResourceBundle {
+    @Override protected Object[][] getContents() {
+        System.out.println("---- awt_ja ----");
+        return new Object[][] { { "AWT.space", "XXXXX" } };
+    }
+}
