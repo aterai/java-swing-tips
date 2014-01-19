f8d3ace861f637512aa61f34648e0012cf9e0f27
at.terai@gmail.com <at.terai@gmail.com@3ab87fda-ff15-71db-baf3-2d1a8ae44ab9>
2014-01-19 14:48:35 +0000

    add_BigDecimalSpinnerModel

---
+++ trunk/BigDecimalSpinnerModel/src/java/example/MainPanel.java
@@ -0,0 +1,94 @@
+package example;
+//-*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+//@homepage@
+import java.awt.*;
+import java.math.*;
+import javax.swing.*;
+
+public class MainPanel extends JPanel {
+    public MainPanel() {
+        super(new BorderLayout());
+
+        Box box = Box.createVerticalBox();
+        box.add(new JLabel("SpinnerNumberModel(double, ...)"));
+        box.add(Box.createVerticalStrut(2));
+        JPanel p1 = new JPanel(new GridLayout(1, 2, 5, 5));
+        p1.add(new JSpinner(new SpinnerNumberModel(2.01, 2.00, 3.02, 0.01)));
+        p1.add(new JSpinner(new SpinnerNumberModel(29.7, 29.6, 30.2, 0.1)));
+        box.add(p1);
+        box.add(Box.createVerticalStrut(5));
+        box.add(new JLabel("BigDecimalSpinnerModel"));
+        box.add(Box.createVerticalStrut(2));
+        JPanel p2 = new JPanel(new GridLayout(1, 2, 5, 5));
+        p2.add(new JSpinner(new BigDecimalSpinnerModel(2.01, 2.00, 3.02, 0.01)));
+        p2.add(new JSpinner(new BigDecimalSpinnerModel(29.7, 29.6, 30.2, 0.1)));
+        box.add(p2);
+        box.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+
+        //TEST:
+        StringBuilder sb = new StringBuilder();
+        double d = 29.7 - 29.6 - 0.1;
+        sb.append(String.format("%f-%f-%f>=0:%b%n", 29.7, 29.6, 0.1, d>=0));
+        sb.append(String.format("abs(%f-%f-%f)<1.0e-14:%b%n", 29.7, 29.6, 0.1, Math.abs(d)<1.0e-14));
+        sb.append(String.format("abs(%f-%f-%f)<1.0e-15:%b%n", 29.7, 29.6, 0.1, Math.abs(d)<1.0e-15));
+
+        add(box, BorderLayout.NORTH);
+        add(new JScrollPane(new JTextArea(sb.toString())));
+        setPreferredSize(new Dimension(320, 240));
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+    public static void createAndShowGUI() {
+        try{
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        JFrame frame = new JFrame("@title@");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new MainPanel());
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
+
+class BigDecimalSpinnerModel extends SpinnerNumberModel {
+    public BigDecimalSpinnerModel(double value, double minimum, double maximum, double stepSize) {
+        super(value, minimum, maximum, stepSize);
+    }
+    @Override public Object getPreviousValue() {
+        return incrValue(-1);
+    }
+    @Override public Object getNextValue() {
+        return incrValue(+1);
+    }
+    private Number incrValue(int dir) {
+        Number v = getNumber();
+        BigDecimal value    = new BigDecimal(v.toString());
+        BigDecimal stepSize = new BigDecimal(getStepSize().toString());
+        BigDecimal maximum  = new BigDecimal(getMaximum().toString());
+        BigDecimal minimum  = new BigDecimal(getMinimum().toString());
+        BigDecimal newValue;
+
+        if(dir>0) {
+            newValue = value.add(stepSize);
+        }else{
+            newValue = value.subtract(stepSize);
+        }
+        if(maximum != null && maximum.compareTo(newValue) < 0) {
+            return null;
+        }
+        if(minimum != null && minimum.compareTo(newValue) > 0) {
+            return null;
+        }
+        return newValue;
+    }
+}
