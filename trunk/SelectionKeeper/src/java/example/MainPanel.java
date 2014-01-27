76b77378cafbcd0ca50f0aac090ee395a8ea288b
at.terai@gmail.com <at.terai@gmail.com@3ab87fda-ff15-71db-baf3-2d1a8ae44ab9>
2014-01-27 08:05:00 +0000

    refactor: PMD Method Returns Internal Array

--- trunk/SelectionKeeper/src/java/example/MainPanel.java
+++ trunk/SelectionKeeper/src/java/example/MainPanel.java
@@ -7,20 +7,20 @@ import javax.swing.*;
 import javax.swing.table.*;
 
 public final class MainPanel extends JPanel {
+    private final String[] columnNames = {"String", "Integer", "Boolean"};
+    private final Object[][] data = {
+        {"aaa", 12, true}, {"bbb", 5, false},
+        {"CCC", 92, true}, {"DDD", 0, false}
+    };
+    private final DefaultTableModel model = new DefaultTableModel(data, columnNames) {
+        @Override public Class<?> getColumnClass(int column) {
+            return getValueAt(0, column).getClass();
+        }
+    };
+    private final TableSorter sorter = new TableSorter(model);
+    private final JTable table = new JTable(sorter);
     private MainPanel() {
         super(new BorderLayout());
-        String[] columnNames = {"String", "Integer", "Boolean"};
-        Object[][] data = {
-            {"aaa", 12, true}, {"bbb", 5, false},
-            {"CCC", 92, true}, {"DDD", 0, false}
-        };
-        DefaultTableModel model = new DefaultTableModel(data, columnNames) {
-            @Override public Class<?> getColumnClass(int column) {
-                return getValueAt(0, column).getClass();
-            }
-        };
-        TableSorter sorter = new TableSorter(model);
-        JTable table = new JTable(sorter);
         sorter.setTableHeader(table.getTableHeader());
 
         add(new JScrollPane(table));
