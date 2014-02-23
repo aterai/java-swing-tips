7ca85aa9cba612ea830194d22796265bd66d8045
TERAI Atsuhiro <at.terai@gmail.com>
2014-02-23 15:02:36 +0000

    add_InvokesStopCellEditing

---
+++ InvokesStopCellEditing/src/java/example/MainPanel.java
@@ -0,0 +1,48 @@
+package example;
+//-*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+//@homepage@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public final class MainPanel extends JPanel {
+    private final JTree tree = new JTree();
+
+    public MainPanel() {
+        super(new BorderLayout(5, 5));
+        tree.setEditable(true);
+        add(new JCheckBox(new AbstractAction("terminateEditOnFocusLost") {
+            @Override public void actionPerformed(ActionEvent e) {
+                JCheckBox c = (JCheckBox) e.getSource();
+                tree.setInvokesStopCellEditing(c.isSelected());
+            }
+        }), BorderLayout.NORTH);
+        add(new JScrollPane(tree));
+        add(new JTextField(), BorderLayout.SOUTH);
+        setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        setPreferredSize(new Dimension(320, 240));
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+    public static void createAndShowGUI() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException |
+                 IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        }
+        JFrame frame = new JFrame("@title@");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new MainPanel());
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
