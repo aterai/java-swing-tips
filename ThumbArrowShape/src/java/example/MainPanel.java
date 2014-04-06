53bea884e695a3509345dd5077d9d80fd779bd2f
TERAI Atsuhiro <at.terai@gmail.com>
2014-04-06 15:11:37 +0000

    add: ThumbArrowShape

---
+++ ThumbArrowShape/src/java/example/MainPanel.java
@@ -0,0 +1,131 @@
+package example;
+//-*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+//@homepage@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.util.Arrays;
+import javax.swing.*;
+
+public final class MainPanel extends JPanel {
+    private final BoundedRangeModel model = new DefaultBoundedRangeModel(50, 0, 0, 100);
+    private final JSlider slider0 = new JSlider(JSlider.VERTICAL);
+    private final JSlider slider1 = new JSlider(JSlider.VERTICAL);
+    private final JSlider slider2 = new JSlider(JSlider.VERTICAL);
+    private final JSlider slider3 = new JSlider(JSlider.HORIZONTAL);
+    private final JSlider slider4 = new JSlider(JSlider.HORIZONTAL);
+    private final JSlider slider5 = new JSlider(JSlider.HORIZONTAL);
+
+    public MainPanel() {
+        super(new BorderLayout());
+
+        for (JSlider s: Arrays.asList(slider0, slider1, slider2, slider3, slider4, slider5)) {
+            s.setModel(model);
+        }
+        slider1.setMajorTickSpacing(20);
+        slider1.setPaintTicks(true);
+        slider2.putClientProperty("Slider.paintThumbArrowShape", Boolean.TRUE);
+
+        slider4.setMajorTickSpacing(20);
+        slider4.setPaintTicks(true);
+        slider5.putClientProperty("Slider.paintThumbArrowShape", Boolean.TRUE);
+
+        Box box1 = Box.createHorizontalBox();
+        box1.setBorder(BorderFactory.createEmptyBorder(20, 5, 20, 5));
+        box1.add(slider0);
+        box1.add(Box.createHorizontalStrut(20));
+        box1.add(slider1);
+        box1.add(Box.createHorizontalStrut(20));
+        box1.add(slider2);
+        box1.add(Box.createHorizontalGlue());
+
+        Box box2 = Box.createVerticalBox();
+        box2.setBorder(BorderFactory.createEmptyBorder(20, 0, 20, 5));
+        box2.add(makeTitledPanel("Default", slider3));
+        box2.add(Box.createVerticalStrut(20));
+        box2.add(makeTitledPanel("setPaintTicks", slider4));
+        box2.add(Box.createVerticalStrut(20));
+        box2.add(makeTitledPanel("Slider.paintThumbArrowShape", slider5));
+        box2.add(Box.createVerticalGlue());
+
+        add(box1, BorderLayout.WEST);
+        add(box2);
+        setPreferredSize(new Dimension(320, 240));
+    }
+    private static JComponent makeTitledPanel(String title, JComponent c) {
+        c.setBorder(BorderFactory.createTitledBorder(title));
+        return c;
+    }
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+    public static void createAndShowGUI() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException |
+                 IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        }
+        JMenuBar mb = new JMenuBar();
+        mb.add(LookAndFeelUtil.createLookAndFeelMenu());
+        JFrame frame = new JFrame("@title@");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new MainPanel());
+        frame.setJMenuBar(mb);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
+
+//http://java.net/projects/swingset3/sources/svn/content/trunk/SwingSet3/src/com/sun/swingset3/SwingSet3.java
+final class LookAndFeelUtil {
+    private static String lookAndFeel = UIManager.getLookAndFeel().getClass().getName();
+    private LookAndFeelUtil() { /* Singleton */ }
+    public static JMenu createLookAndFeelMenu() {
+        JMenu menu = new JMenu("LookAndFeel");
+        ButtonGroup lookAndFeelRadioGroup = new ButtonGroup();
+        for (UIManager.LookAndFeelInfo lafInfo: UIManager.getInstalledLookAndFeels()) {
+            menu.add(createLookAndFeelItem(lafInfo.getName(), lafInfo.getClassName(), lookAndFeelRadioGroup));
+        }
+        return menu;
+    }
+    private static JRadioButtonMenuItem createLookAndFeelItem(String lafName, String lafClassName, final ButtonGroup lookAndFeelRadioGroup) {
+        JRadioButtonMenuItem lafItem = new JRadioButtonMenuItem();
+        lafItem.setSelected(lafClassName.equals(lookAndFeel));
+        lafItem.setHideActionText(true);
+        lafItem.setAction(new AbstractAction() {
+            @Override public void actionPerformed(ActionEvent e) {
+                ButtonModel m = lookAndFeelRadioGroup.getSelection();
+                try {
+                    setLookAndFeel(m.getActionCommand());
+                } catch (ClassNotFoundException | InstantiationException |
+                         IllegalAccessException | UnsupportedLookAndFeelException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        lafItem.setText(lafName);
+        lafItem.setActionCommand(lafClassName);
+        lookAndFeelRadioGroup.add(lafItem);
+        return lafItem;
+    }
+    private static void setLookAndFeel(String lookAndFeel) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+        String oldLookAndFeel = LookAndFeelUtil.lookAndFeel;
+        if (!oldLookAndFeel.equals(lookAndFeel)) {
+            UIManager.setLookAndFeel(lookAndFeel);
+            LookAndFeelUtil.lookAndFeel = lookAndFeel;
+            updateLookAndFeel();
+            //firePropertyChange("lookAndFeel", oldLookAndFeel, lookAndFeel);
+        }
+    }
+    private static void updateLookAndFeel() {
+        for (Window window: Frame.getWindows()) {
+            SwingUtilities.updateComponentTreeUI(window);
+        }
+    }
+}
