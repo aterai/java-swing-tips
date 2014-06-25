22351d27fca27c555ffe4f2734bb4d2c0e41c441
at.terai@gmail.com <at.terai@gmail.com@3ab87fda-ff15-71db-baf3-2d1a8ae44ab9>
2014-06-25 07:53:27 +0000

    refactor: paint background circle

--- trunk/ProgressCircle/src/java/example/MainPanel.java
+++ trunk/ProgressCircle/src/java/example/MainPanel.java
@@ -101,7 +101,6 @@ class ProgressCircleUI extends BasicProgressBarUI {
         // draw the cells
         Graphics2D g2 = (Graphics2D) g.create();
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.setPaint(progressBar.getForeground());
         double degree = 360 * progressBar.getPercentComplete();
         double sz = Math.min(barRectWidth, barRectHeight);
         double cx = b.left + barRectWidth  * .5;
@@ -109,15 +108,26 @@ class ProgressCircleUI extends BasicProgressBarUI {
         double or = sz * .5;
         //double ir = or - 20;
         double ir = or * .5; //.8;
-        Shape inner = new Ellipse2D.Double(cx - ir, cy - ir, ir * 2, ir * 2);
-        Shape outer = new Arc2D.Double(cx - or, cy - or, sz, sz, 90 - degree, degree, Arc2D.PIE);
-        Area area = new Area(outer);
-        area.subtract(new Area(inner));
+        Shape inner  = new Ellipse2D.Double(cx - ir, cy - ir, ir * 2, ir * 2);
+        Shape outer  = new Ellipse2D.Double(cx - or, cy - or, sz, sz);
+        Shape sector = new Arc2D.Double(cx - or, cy - or, sz, sz, 90 - degree, degree, Arc2D.PIE);
+
+        Area foreground = new Area(sector);
+        Area background = new Area(outer);
+        Area hole = new Area(inner);
+
+        foreground.subtract(hole);
+        background.subtract(hole);
+
+        g2.setPaint(new Color(0xDDDDDD));
+        g2.fill(background);
+
         //AffineTransform at = AffineTransform.getScaleInstance(-1.0, 1.0);
         //at.translate(-(barRectWidth + b.left * 2), 0);
         //AffineTransform at = AffineTransform.getRotateInstance(Math.toRadians(degree), cx, cy);
         //g2.fill(at.createTransformedShape(area));
-        g2.fill(area);
+        g2.setPaint(progressBar.getForeground());
+        g2.fill(foreground);
         g2.dispose();
 
         // Deal with possible text painting
