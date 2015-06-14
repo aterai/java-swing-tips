0d358d7af3d76d58eff2672cbd224c8940f8b955
aterai <at.terai@gmail.com>
2015-06-15 00:01:45 +0900

    add: SurrendersFocusOnKeystroke

---
+++ SurrendersFocusOnKeystroke/src/java/example/MainPanel.java
@@ -0,0 +1,57 @@
+package example;
+//-*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+//@homepage@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.table.*;
+
+public final class MainPanel extends JPanel {
+    private final JCheckBox check = new JCheckBox("SurrendersFocusOnKeystroke");
+    private final String[] columnNames = {"String", "Integer", "Boolean"};
+    private final Object[][] data = {
+        {"aaa", 12, true}, {"bbb", 5, false},
+        {"CCC", 92, true}, {"DDD", 0, false}
+    };
+    private final DefaultTableModel model = new DefaultTableModel(data, columnNames) {
+        @Override public Class<?> getColumnClass(int column) {
+            return getValueAt(0, column).getClass();
+        }
+    };
+    private final JTable table = new JTable(model);
+
+    public MainPanel() {
+        super(new BorderLayout());
+
+        check.addActionListener(new ActionListener() {
+            @Override public void actionPerformed(ActionEvent e) {
+                table.setSurrendersFocusOnKeystroke(((JCheckBox) e.getSource()).isSelected());
+            }
+        });
+        add(check, BorderLayout.NORTH);
+        add(new JScrollPane(table));
+        setPreferredSize(new Dimension(320, 240));
+    }
+    public static void main(String... args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+    public static void createAndShowGUI() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException
+                 | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        }
+        JFrame frame = new JFrame("@title@");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new MainPanel());
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
