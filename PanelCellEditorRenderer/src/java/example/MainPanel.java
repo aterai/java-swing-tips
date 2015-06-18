4e057a020da1a67f7a86367f8294ca714c1984e5
aterai <at.terai@gmail.com>
2015-06-19 01:01:37 +0900

    PMD: GodClass

--- PanelCellEditorRenderer/src/java/example/MainPanel.java
+++ PanelCellEditorRenderer/src/java/example/MainPanel.java
@@ -22,21 +22,27 @@ public final class MainPanel extends JPanel {
             return getValueAt(0, column).getClass();
         }
     };
-    private final JTable table = new JTable(model);
+    private final JTable table = new JTable(model) {
+        @Override public void updateUI() {
+            super.updateUI();
+            EventQueue.invokeLater(new Runnable() {
+                @Override public void run() {
+                    TableColumn column = getColumnModel().getColumn(0);
+                    column.setCellRenderer(new SpinnerRenderer());
+                    column.setCellEditor(new SpinnerEditor());
+
+                    column = getColumnModel().getColumn(1);
+                    column.setCellRenderer(new ButtonsRenderer());
+                    column.setCellEditor(new ButtonsEditor());
+                    repaint();
+                }
+            });
+        }
+    };
     public MainPanel() {
         super(new BorderLayout());
-
         table.setRowHeight(36);
         table.setAutoCreateRowSorter(true);
-
-        TableColumn column = table.getColumnModel().getColumn(0);
-        column.setCellRenderer(new SpinnerRenderer());
-        column.setCellEditor(new SpinnerEditor());
-
-        column = table.getColumnModel().getColumn(1);
-        column.setCellRenderer(new ButtonsRenderer());
-        column.setCellEditor(new ButtonsEditor());
-
         add(new JScrollPane(table));
         setPreferredSize(new Dimension(320, 240));
     }
@@ -77,7 +83,44 @@ class SpinnerPanel extends JPanel {
         add(spinner, c);
     }
 }
+//*
+class SpinnerRenderer implements TableCellRenderer {
+    private final SpinnerPanel p = new SpinnerPanel();
+    @Override public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        p.setBackground(isSelected ? table.getSelectionBackground() : table.getBackground());
+        p.spinner.setValue((Integer) value);
+        return p;
+    }
+}
 
+class SpinnerEditor extends AbstractCellEditor implements TableCellEditor {
+    private final SpinnerPanel p = new SpinnerPanel();
+    @Override public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
+        p.setBackground(table.getSelectionBackground());
+        p.spinner.setValue((Integer) value);
+        return p;
+    }
+    @Override public Object getCellEditorValue() {
+        return p.spinner.getValue();
+    }
+    @Override public boolean isCellEditable(EventObject e) {
+        return true;
+    }
+    @Override public boolean shouldSelectCell(EventObject anEvent) {
+        return true;
+    }
+    @Override public boolean stopCellEditing() {
+        try {
+            p.spinner.commitEdit();
+        } catch (ParseException pe) {
+            Toolkit.getDefaultToolkit().beep();
+            return false;
+        }
+        fireEditingStopped();
+        return true;
+    }
+}
+/*/
 class SpinnerRenderer extends SpinnerPanel implements TableCellRenderer {
     public SpinnerRenderer() {
         super();
@@ -164,6 +207,7 @@ class SpinnerEditor extends SpinnerPanel implements TableCellEditor {
         }
     }
 }
+//*/
 
 class ButtonsPanel extends JPanel {
     public final List<JButton> buttons = Arrays.asList(new JButton("+"), new JButton("-"));
@@ -187,7 +231,68 @@ class ButtonsPanel extends JPanel {
         }
     }
 }
+//*
+class ButtonsRenderer implements TableCellRenderer {
+    private final ButtonsPanel p = new ButtonsPanel();
+    @Override public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        p.setBackground(isSelected ? table.getSelectionBackground() : table.getBackground());
+        p.label.setForeground(isSelected ? table.getSelectionForeground() : table.getForeground());
+        p.label.setText(Objects.toString(value, ""));
+        return p;
+    }
+}
 
+class ButtonsEditor extends AbstractCellEditor implements TableCellEditor {
+    private final ButtonsPanel p = new ButtonsPanel();
+    public ButtonsEditor() {
+        super();
+        p.buttons.get(0).addActionListener(new ActionListener() {
+            @Override public void actionPerformed(ActionEvent e) {
+                p.i++;
+                p.label.setText(Integer.toString(p.i));
+                fireEditingStopped();
+            }
+        });
+
+        p.buttons.get(1).addActionListener(new ActionListener() {
+            @Override public void actionPerformed(ActionEvent e) {
+                p.i--;
+                p.label.setText(Integer.toString(p.i));
+                fireEditingStopped();
+            }
+        });
+
+        p.addMouseListener(new MouseAdapter() {
+            @Override public void mousePressed(MouseEvent e) {
+                fireEditingStopped();
+            }
+        });
+    }
+    @Override public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
+        p.setBackground(table.getSelectionBackground());
+        p.label.setForeground(table.getSelectionForeground());
+        p.i = (Integer) value;
+        p.label.setText(Integer.toString(p.i));
+        return p;
+    }
+    @Override public Object getCellEditorValue() {
+        return p.i;
+    }
+    @Override public boolean isCellEditable(EventObject e) {
+        return true;
+    }
+    @Override public boolean shouldSelectCell(EventObject anEvent) {
+        return true;
+    }
+    @Override public boolean stopCellEditing() {
+        fireEditingStopped();
+        return true;
+    }
+    @Override public void cancelCellEditing() {
+        fireEditingCanceled();
+    }
+}
+/*/
 class ButtonsRenderer extends ButtonsPanel implements TableCellRenderer {
     public ButtonsRenderer() {
         super();
@@ -294,3 +399,4 @@ class ButtonsEditor extends ButtonsPanel implements TableCellEditor {
         }
     }
 }
+//*/
