a1819d6c3f74abc0832ecb6db0e43c5485f7ccff
aterai <at.terai@gmail.com>
2015-06-29 04:27:07 +0900

    add: TableHeaderTabArea

---
+++ TableHeaderTabArea/src/java/example/TabViewButtonUI.java
@@ -0,0 +1,6 @@
+package example;
+//-*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+//@homepage@
+import javax.swing.plaf.basic.BasicButtonUI;
+public class TabViewButtonUI extends BasicButtonUI { /* ButtonUI */ }
