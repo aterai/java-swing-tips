a94a4326d5aa6cf43808a2daeae18be762e8605d
aterai <at.terai@gmail.com>
2015-07-16 00:03:13 +0900

    PMD: GodClass

--- AnimeIcon/src/java/example/MainPanel.java
+++ AnimeIcon/src/java/example/MainPanel.java
@@ -187,9 +187,6 @@ class ProgressListener implements PropertyChangeListener {
 class AnimatedLabel extends JLabel implements ActionListener {
     private final Timer animator;
     private final transient AnimeIcon icon = new AnimeIcon();
-    //private final transient AnimeIcon2 icon = new AnimeIcon2();
-    //private final transient AnimeIcon3 icon = new AnimeIcon3();
-    //private final transient AnimeIcon4 icon = new AnimeIcon4();
     public AnimatedLabel() {
         super();
         animator = new Timer(100, this);
@@ -216,115 +213,118 @@ class AnimatedLabel extends JLabel implements ActionListener {
     }
 }
 
+// //TEST: 1
+// class AnimeIcon implements Icon {
+//     private static final Color ELLIPSE_COLOR = new Color(.5f, .5f, .5f);
+//     private static final double R  = 2d;
+//     private static final double SX = 1d;
+//     private static final double SY = 1d;
+//     private static final int WIDTH  = (int) (R * 8 + SX * 2);
+//     private static final int HEIGHT = (int) (R * 8 + SY * 2);
+//     private final List<Shape> list = new ArrayList<Shape>(Arrays.asList(
+//         new Ellipse2D.Double(SX + 3 * R, SY + 0 * R, 2 * R, 2 * R),
+//         new Ellipse2D.Double(SX + 5 * R, SY + 1 * R, 2 * R, 2 * R),
+//         new Ellipse2D.Double(SX + 6 * R, SY + 3 * R, 2 * R, 2 * R),
+//         new Ellipse2D.Double(SX + 5 * R, SY + 5 * R, 2 * R, 2 * R),
+//         new Ellipse2D.Double(SX + 3 * R, SY + 6 * R, 2 * R, 2 * R),
+//         new Ellipse2D.Double(SX + 1 * R, SY + 5 * R, 2 * R, 2 * R),
+//         new Ellipse2D.Double(SX + 0 * R, SY + 3 * R, 2 * R, 2 * R),
+//         new Ellipse2D.Double(SX + 1 * R, SY + 1 * R, 2 * R, 2 * R)));
+//
+//     private boolean isRunning;
+//     public void next() {
+//         if (isRunning) {
+//             list.add(list.remove(0));
+//         }
+//     }
+//     public void setRunning(boolean isRunning) {
+//         this.isRunning = isRunning;
+//     }
+//     @Override public void paintIcon(Component c, Graphics g, int x, int y) {
+//         Graphics2D g2 = (Graphics2D) g.create();
+//         g2.setPaint(Objects.nonNull(c) ? c.getBackground() : Color.WHITE);
+//         g2.fillRect(x, y, getIconWidth(), getIconHeight());
+//         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+//         g2.setColor(ELLIPSE_COLOR);
+//         g2.translate(x, y);
+//         int size = list.size();
+//         for (int i = 0; i < size; i++) {
+//             float alpha = isRunning ? (i + 1) / (float) size : .5f;
+//             g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
+//             g2.fill(list.get(i));
+//         }
+//         //g2.translate(-x, -y);
+//         g2.dispose();
+//     }
+//     @Override public int getIconWidth() {
+//         return WIDTH;
+//     }
+//     @Override public int getIconHeight() {
+//         return HEIGHT;
+//     }
+// }
+
+// //TEST: 2
+// class AnimeIcon implements Icon {
+//     private static final Color ELLIPSE_COLOR = new Color(.5f, .8f, .5f);
+//     private final List<Shape> list = new ArrayList<>();
+//     private final Dimension dim;
+//     private boolean isRunning;
+//     public AnimeIcon() {
+//         super();
+//         int r = 4;
+//         Shape s = new Ellipse2D.Float(0, 0, 2 * r, 2 * r);
+//         for (int i = 0; i < 8; i++) {
+//             AffineTransform at = AffineTransform.getRotateInstance(i * 2 * Math.PI / 8);
+//             at.concatenate(AffineTransform.getTranslateInstance(r, r));
+//             list.add(at.createTransformedShape(s));
+//         }
+//         //int d = (int) (r * 2 * (1 + 2 * Math.sqrt(2)));
+//         int d = (int) r * 2 * (1 + 3); // 2 * Math.sqrt(2) is nearly equal to 3.
+//         dim = new Dimension(d, d);
+//     }
+//     @Override public int getIconWidth() {
+//         return dim.width;
+//     }
+//     @Override public int getIconHeight() {
+//         return dim.height;
+//     }
+//     @Override public void paintIcon(Component c, Graphics g, int x, int y) {
+//         Graphics2D g2 = (Graphics2D) g.create();
+//         g2.setPaint(Objects.nonNull(c) ? c.getBackground() : Color.WHITE);
+//         g2.fillRect(x, y, getIconWidth(), getIconHeight());
+//         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+//         g2.setColor(ELLIPSE_COLOR);
+//         int xx = x + dim.width / 2;
+//         int yy = y + dim.height / 2;
+//         g2.translate(xx, yy);
+//         int size = list.size();
+//         for (int i = 0; i < size; i++) {
+//             float alpha = isRunning ? (i + 1) / (float) size : .5f;
+//             g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
+//             g2.fill(list.get(i));
+//         }
+//         //g2.translate(-xx, -yy);
+//         g2.dispose();
+//     }
+//     public void next() {
+//         if (isRunning) {
+//             list.add(list.remove(0));
+//         }
+//     }
+//     public void setRunning(boolean isRunning) {
+//         this.isRunning = isRunning;
+//     }
+// }
+
+//TEST: 3
 class AnimeIcon implements Icon {
-    private static final Color ELLIPSE_COLOR = new Color(.5f, .5f, .5f);
-    private static final double R  = 2d;
-    private static final double SX = 1d;
-    private static final double SY = 1d;
-    private static final int WIDTH  = (int) (R * 8 + SX * 2);
-    private static final int HEIGHT = (int) (R * 8 + SY * 2);
-    private final List<Shape> list = new ArrayList<Shape>(Arrays.asList(
-        new Ellipse2D.Double(SX + 3 * R, SY + 0 * R, 2 * R, 2 * R),
-        new Ellipse2D.Double(SX + 5 * R, SY + 1 * R, 2 * R, 2 * R),
-        new Ellipse2D.Double(SX + 6 * R, SY + 3 * R, 2 * R, 2 * R),
-        new Ellipse2D.Double(SX + 5 * R, SY + 5 * R, 2 * R, 2 * R),
-        new Ellipse2D.Double(SX + 3 * R, SY + 6 * R, 2 * R, 2 * R),
-        new Ellipse2D.Double(SX + 1 * R, SY + 5 * R, 2 * R, 2 * R),
-        new Ellipse2D.Double(SX + 0 * R, SY + 3 * R, 2 * R, 2 * R),
-        new Ellipse2D.Double(SX + 1 * R, SY + 1 * R, 2 * R, 2 * R)));
-
-    private boolean isRunning;
-    public void next() {
-        if (isRunning) {
-            list.add(list.remove(0));
-        }
-    }
-    public void setRunning(boolean isRunning) {
-        this.isRunning = isRunning;
-    }
-    @Override public void paintIcon(Component c, Graphics g, int x, int y) {
-        Graphics2D g2 = (Graphics2D) g.create();
-        g2.setPaint(Objects.nonNull(c) ? c.getBackground() : Color.WHITE);
-        g2.fillRect(x, y, getIconWidth(), getIconHeight());
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.setColor(ELLIPSE_COLOR);
-        g2.translate(x, y);
-        int size = list.size();
-        for (int i = 0; i < size; i++) {
-            float alpha = isRunning ? (i + 1) / (float) size : .5f;
-            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
-            g2.fill(list.get(i));
-        }
-        //g2.translate(-x, -y);
-        g2.dispose();
-    }
-    @Override public int getIconWidth() {
-        return WIDTH;
-    }
-    @Override public int getIconHeight() {
-        return HEIGHT;
-    }
-}
-
-class AnimeIcon2 implements Icon {
-    private static final Color ELLIPSE_COLOR = new Color(.5f, .8f, .5f);
-    private final List<Shape> list = new ArrayList<>();
-    private final Dimension dim;
-    private boolean isRunning;
-    public AnimeIcon2() {
-        super();
-        int r = 4;
-        Shape s = new Ellipse2D.Float(0, 0, 2 * r, 2 * r);
-        for (int i = 0; i < 8; i++) {
-            AffineTransform at = AffineTransform.getRotateInstance(i * 2 * Math.PI / 8);
-            at.concatenate(AffineTransform.getTranslateInstance(r, r));
-            list.add(at.createTransformedShape(s));
-        }
-        //int d = (int) (r * 2 * (1 + 2 * Math.sqrt(2)));
-        int d = (int) r * 2 * (1 + 3); // 2 * Math.sqrt(2) is nearly equal to 3.
-        dim = new Dimension(d, d);
-    }
-    @Override public int getIconWidth() {
-        return dim.width;
-    }
-    @Override public int getIconHeight() {
-        return dim.height;
-    }
-    @Override public void paintIcon(Component c, Graphics g, int x, int y) {
-        Graphics2D g2 = (Graphics2D) g.create();
-        g2.setPaint(Objects.nonNull(c) ? c.getBackground() : Color.WHITE);
-        g2.fillRect(x, y, getIconWidth(), getIconHeight());
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.setColor(ELLIPSE_COLOR);
-        int xx = x + dim.width / 2;
-        int yy = y + dim.height / 2;
-        g2.translate(xx, yy);
-        int size = list.size();
-        for (int i = 0; i < size; i++) {
-            float alpha = isRunning ? (i + 1) / (float) size : .5f;
-            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
-            g2.fill(list.get(i));
-        }
-        //g2.translate(-xx, -yy);
-        g2.dispose();
-    }
-    public void next() {
-        if (isRunning) {
-            list.add(list.remove(0));
-        }
-    }
-    public void setRunning(boolean isRunning) {
-        this.isRunning = isRunning;
-    }
-}
-
-class AnimeIcon3 implements Icon {
     private static final Color ELLIPSE_COLOR = new Color(.9f, .7f, .7f);
     private final List<Shape> list = new ArrayList<>();
     private final Dimension dim;
     private boolean isRunning;
     private int rotate = 45;
-    public AnimeIcon3() {
+    public AnimeIcon() {
         super();
         int r = 4;
         Shape s = new Ellipse2D.Float(0, 0, 2 * r, 2 * r);
@@ -362,7 +362,7 @@ class AnimeIcon3 implements Icon {
     }
     public void next() {
         if (isRunning) {
-            rotate = rotate < 360 ? rotate + 45 : 45;
+            rotate = (rotate + 45) % 360; //45 = 360 / 8
         }
     }
     public void setRunning(boolean isRunning) {
@@ -370,56 +370,57 @@ class AnimeIcon3 implements Icon {
     }
 }
 
-class AnimeIcon4 implements Icon {
-    private static final int R = 4;
-    private static final Color ELLIPSE_COLOR = new Color(.5f, .8f, .5f);
-    private final Dimension dim;
-    private boolean isRunning;
-    private final List<Shape> list = new ArrayList<>();
-    public AnimeIcon4() {
-        super();
-        int d = (int) R * 2 * (1 + 3);
-        dim = new Dimension(d, d);
-
-        Ellipse2D.Float cricle = new Ellipse2D.Float(R, R, d - 2 * R, d - 2 * R);
-        PathIterator i = new FlatteningPathIterator(cricle.getPathIterator(null), R);
-        float[] coords = new float[6];
-        int idx = 0;
-        while (!i.isDone()) {
-            i.currentSegment(coords);
-            if (idx < 8) { // XXX
-                list.add(new Ellipse2D.Float(coords[0] - R, coords[1] - R, 2 * R, 2 * R));
-                idx++;
-            }
-            i.next();
-        }
-    }
-    @Override public int getIconWidth() {
-        return dim.width;
-    }
-    @Override public int getIconHeight() {
-        return dim.height;
-    }
-    @Override public void paintIcon(Component c, Graphics g, int x, int y) {
-        Graphics2D g2 = (Graphics2D) g.create();
-        g2.setPaint(Objects.nonNull(c) ? c.getBackground() : Color.WHITE);
-        g2.fillRect(x, y, getIconWidth(), getIconHeight());
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.setColor(ELLIPSE_COLOR);
-        int size = list.size();
-        for (int i = 0; i < size; i++) {
-            float alpha = isRunning ? (i + 1) / (float) size : .5f;
-            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
-            g2.fill(list.get(i));
-        }
-        g2.dispose();
-    }
-    public void next() {
-        if (isRunning) {
-            list.add(list.remove(0));
-        }
-    }
-    public void setRunning(boolean isRunning) {
-        this.isRunning = isRunning;
-    }
-}
+// //TEST: 4
+// class AnimeIcon implements Icon {
+//     private static final int R = 4;
+//     private static final Color ELLIPSE_COLOR = new Color(.5f, .8f, .5f);
+//     private final Dimension dim;
+//     private boolean isRunning;
+//     private final List<Shape> list = new ArrayList<>();
+//     public AnimeIcon() {
+//         super();
+//         int d = (int) R * 2 * (1 + 3);
+//         dim = new Dimension(d, d);
+//
+//         Ellipse2D.Float cricle = new Ellipse2D.Float(R, R, d - 2 * R, d - 2 * R);
+//         PathIterator i = new FlatteningPathIterator(cricle.getPathIterator(null), R);
+//         float[] coords = new float[6];
+//         int idx = 0;
+//         while (!i.isDone()) {
+//             i.currentSegment(coords);
+//             if (idx < 8) { // XXX
+//                 list.add(new Ellipse2D.Float(coords[0] - R, coords[1] - R, 2 * R, 2 * R));
+//                 idx++;
+//             }
+//             i.next();
+//         }
+//     }
+//     @Override public int getIconWidth() {
+//         return dim.width;
+//     }
+//     @Override public int getIconHeight() {
+//         return dim.height;
+//     }
+//     @Override public void paintIcon(Component c, Graphics g, int x, int y) {
+//         Graphics2D g2 = (Graphics2D) g.create();
+//         g2.setPaint(Objects.nonNull(c) ? c.getBackground() : Color.WHITE);
+//         g2.fillRect(x, y, getIconWidth(), getIconHeight());
+//         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+//         g2.setColor(ELLIPSE_COLOR);
+//         int size = list.size();
+//         for (int i = 0; i < size; i++) {
+//             float alpha = isRunning ? (i + 1) / (float) size : .5f;
+//             g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
+//             g2.fill(list.get(i));
+//         }
+//         g2.dispose();
+//     }
+//     public void next() {
+//         if (isRunning) {
+//             list.add(list.remove(0));
+//         }
+//     }
+//     public void setRunning(boolean isRunning) {
+//         this.isRunning = isRunning;
+//     }
+// }
