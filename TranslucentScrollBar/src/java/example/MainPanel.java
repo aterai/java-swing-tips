91b40220e5da89a7fa948844969203efa1ff7ed4
aterai <aterai@outlook.com>
2015-12-08 00:33:11 +0900

    refactor: TranslucentScrollPaneLayout in, anonymous inner class out

--- TranslucentScrollBar/src/java/example/MainPanel.java
+++ TranslucentScrollBar/src/java/example/MainPanel.java
@@ -41,36 +41,7 @@ public final class MainPanel extends JPanel {
                 });
                 setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
                 setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-                setLayout(new ScrollPaneLayout() {
-                    @Override public void layoutContainer(Container parent) {
-                        if (parent instanceof JScrollPane) {
-                            JScrollPane scrollPane = (JScrollPane) parent;
-
-                            Rectangle availR = scrollPane.getBounds();
-                            availR.setLocation(0, 0); //availR.x = availR.y = 0;
-
-                            Insets insets = parent.getInsets();
-                            availR.x = insets.left;
-                            availR.y = insets.top;
-                            availR.width  -= insets.left + insets.right;
-                            availR.height -= insets.top  + insets.bottom;
-
-                            Rectangle vsbR = new Rectangle();
-                            vsbR.width  = 12;
-                            vsbR.height = availR.height;
-                            vsbR.x = availR.x + availR.width - vsbR.width;
-                            vsbR.y = availR.y;
-
-                            if (viewport != null) {
-                                viewport.setBounds(availR);
-                            }
-                            if (vsb != null) {
-                                vsb.setVisible(true);
-                                vsb.setBounds(vsbR);
-                            }
-                        }
-                    }
-                });
+                setLayout(new TranslucentScrollPaneLayout());
             }
         };
     }
@@ -94,6 +65,37 @@ public final class MainPanel extends JPanel {
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+    }
+}
+
+class TranslucentScrollPaneLayout extends ScrollPaneLayout {
+    @Override public void layoutContainer(Container parent) {
+        if (parent instanceof JScrollPane) {
+            JScrollPane scrollPane = (JScrollPane) parent;
+
+            Rectangle availR = scrollPane.getBounds();
+            availR.setLocation(0, 0); //availR.x = availR.y = 0;
+
+            Insets insets = parent.getInsets();
+            availR.x = insets.left;
+            availR.y = insets.top;
+            availR.width  -= insets.left + insets.right;
+            availR.height -= insets.top  + insets.bottom;
+
+            Rectangle vsbR = new Rectangle();
+            vsbR.width  = 12;
+            vsbR.height = availR.height;
+            vsbR.x = availR.x + availR.width - vsbR.width;
+            vsbR.y = availR.y;
+
+            if (viewport != null) {
+                viewport.setBounds(availR);
+            }
+            if (vsb != null) {
+                vsb.setVisible(true);
+                vsb.setBounds(vsbR);
+            }
+        }
     }
 }
 
