8abf695d85f3990cf19f08496629768d6cdc3901
aterai <aterai@outlook.com>
2016-02-21 00:00:18 +0900

    refactor: use the delegation pattern ListCellRenderer instead of inheritence

--- ClippedLRComboBox/src/java/example/MainPanel.java
+++ ClippedLRComboBox/src/java/example/MainPanel.java
@@ -7,12 +7,17 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public final class MainPanel extends JPanel {
-    private final JComboBox<LRItem> combo1 = makeComboBox(true);
-    private final JComboBox<LRItem> combo2 = makeComboBox(false);
-    public MainPanel() {
+    private MainPanel() {
         super(new BorderLayout());
-        add(makeTitledBox("MultiColumnComboBox", combo1), BorderLayout.NORTH);
-        add(makeTitledBox("DefaultComboBox", combo2), BorderLayout.SOUTH);
+        JComboBox<LRItem> combo = new JComboBox<LRItem>(makeModel()) {
+            @Override public void updateUI() {
+                //setRenderer(null);
+                super.updateUI();
+                setRenderer(new MultiColumnCellRenderer<LRItem>());
+            }
+        };
+        add(makeTitledBox("MultiColumnComboBox", combo), BorderLayout.NORTH);
+        add(makeTitledBox("DefaultComboBox", new JComboBox<LRItem>(makeModel())), BorderLayout.SOUTH);
         setPreferredSize(new Dimension(320, 240));
     }
     private static Box makeTitledBox(String title, JComboBox combo) {
@@ -24,8 +29,7 @@ public final class MainPanel extends JPanel {
         box.setBorder(BorderFactory.createTitledBorder(title));
         box.add(Box.createVerticalStrut(2));
         box.add(combo);
-        box.add(Box.createVerticalStrut(5));
-        box.add(new JSeparator());
+        box.add(Box.createVerticalStrut(2));
         box.add(leftTextField);
         box.add(Box.createVerticalStrut(2));
         box.add(rightTextField);
@@ -40,7 +44,7 @@ public final class MainPanel extends JPanel {
         });
         return box;
     }
-    private static JComboBox<LRItem> makeComboBox(boolean hasMultiColumn) {
+    private static ComboBoxModel<LRItem> makeModel() {
         DefaultComboBoxModel<LRItem> model = new DefaultComboBoxModel<>();
         model.addElement(new LRItem("asdfasdf", "846876"));
         model.addElement(new LRItem("bxcvzx", "asdfaasdfasdfasdfasdfsasd"));
@@ -52,11 +56,7 @@ public final class MainPanel extends JPanel {
         model.addElement(new LRItem("bbbbbbbb", "eeeee"));
         model.addElement(new LRItem("cccccccc", "fffffff"));
 
-        JComboBox<LRItem> combo = new JComboBox<>(model);
-        if (hasMultiColumn) {
-            combo.setRenderer(new MultiColumnCellRenderer<LRItem>(80));
-        }
-        return combo;
+        return model;
     }
     public static void main(String... args) {
         EventQueue.invokeLater(new Runnable() {
@@ -81,30 +81,41 @@ public final class MainPanel extends JPanel {
         frame.setVisible(true);
     }
 }
-class MultiColumnCellRenderer<E extends LRItem> extends JPanel implements ListCellRenderer<E> {
-    private final JLabel leftLabel = new JLabel();
-    private final JLabel rightLabel;
-    public MultiColumnCellRenderer(int rightWidth) {
-        super(new BorderLayout());
-        this.setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1));
 
-        leftLabel.setOpaque(false);
-        leftLabel.setBorder(BorderFactory.createEmptyBorder(0, 2, 0, 0));
-
-        final Dimension dim = new Dimension(rightWidth, 0);
-        rightLabel = new JLabel() {
-            @Override public Dimension getPreferredSize() {
-                return dim;
-            }
-        };
-        rightLabel.setOpaque(false);
-        rightLabel.setBorder(BorderFactory.createEmptyBorder(0, 2, 0, 2));
-        rightLabel.setForeground(Color.GRAY);
-        rightLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-
-        this.add(leftLabel);
-        this.add(rightLabel, BorderLayout.EAST);
-    }
+class MultiColumnCellRenderer<E extends LRItem> implements ListCellRenderer<E> {
+    private final JLabel leftLabel = new JLabel() {
+        @Override public void updateUI() {
+            super.updateUI();
+            setOpaque(false);
+            setBorder(BorderFactory.createEmptyBorder(0, 2, 0, 0));
+        }
+    };
+    private final JLabel rightLabel = new JLabel() {
+        @Override public void updateUI() {
+            super.updateUI();
+            setOpaque(false);
+            setBorder(BorderFactory.createEmptyBorder(0, 2, 0, 2));
+            setForeground(Color.GRAY);
+            setHorizontalAlignment(SwingConstants.RIGHT);
+        }
+        @Override public Dimension getPreferredSize() {
+            return new Dimension(80, 0);
+        }
+    };
+    private final JPanel renderer = new JPanel(new BorderLayout()) {
+        @Override public Dimension getPreferredSize() {
+            Dimension d = super.getPreferredSize();
+            return new Dimension(0, d.height);
+        }
+        @Override public void updateUI() {
+            super.updateUI();
+            setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1));
+            //TEST:
+            //setName("List.cellRenderer");
+            //setName("ComboBox.renderer");
+            //setName("ComboBox.listRenderer");
+        }
+    };
     @Override public Component getListCellRendererComponent(JList<? extends E> list, E value, int index, boolean isSelected, boolean cellHasFocus) {
         leftLabel.setText(value.getLeftText());
         rightLabel.setText(value.getRightText());
@@ -112,30 +123,25 @@ class MultiColumnCellRenderer<E extends LRItem> extends JPanel implements ListCe
         leftLabel.setFont(list.getFont());
         rightLabel.setFont(list.getFont());
 
+        renderer.add(leftLabel);
+        renderer.add(rightLabel, BorderLayout.EAST);
+
         if (index < 0) {
             leftLabel.setForeground(list.getForeground());
-            this.setOpaque(false);
+            renderer.setOpaque(false);
         } else {
             leftLabel.setForeground(isSelected ? list.getSelectionForeground() : list.getForeground());
-            this.setBackground(isSelected ? list.getSelectionBackground() : list.getBackground());
-            this.setOpaque(true);
+            renderer.setBackground(isSelected ? list.getSelectionBackground() : list.getBackground());
+            renderer.setOpaque(true);
         }
-        return this;
-    }
-    @Override public Dimension getPreferredSize() {
-        Dimension d = super.getPreferredSize();
-        return new Dimension(0, d.height);
-    }
-    @Override public void updateUI() {
-        super.updateUI();
-        this.setName("List.cellRenderer");
+        return renderer;
     }
 }
 
 class LRItem {
     private final String leftText;
     private final String rightText;
-    public LRItem(String strLeft, String strRight) {
+    protected LRItem(String strLeft, String strRight) {
         this.leftText = strLeft;
         this.rightText = strRight;
     }
