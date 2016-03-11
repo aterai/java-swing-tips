f32d6bcf78c3ce7674c2d79a9d38ae89b1f81a6f
aterai <aterai@outlook.com>
2016-03-12 00:21:31 +0900

    refactor: convert an anonymous inner class to a named outer class

--- Preferences/src/java/example/MainPanel.java
+++ Preferences/src/java/example/MainPanel.java
@@ -9,98 +9,48 @@ import java.util.prefs.*;
 import javax.swing.*;
 
 public final class MainPanel extends JPanel {
-    private static final String PREFIX = "xxx_";
-    private final Preferences prefs;
-    private final Dimension dim = new Dimension(320, 240);
-    private final Point pos = new Point();
-    private final JButton exitButton = new JButton(new AbstractAction("exit") {
-        @Override public void actionPerformed(ActionEvent e) {
-            saveLocation();
-            Optional.ofNullable(SwingUtilities.getWindowAncestor((Component) e.getSource())).ifPresent(w -> w.dispose());
-        }
-    });
-    private final JButton clearButton = new JButton(new AbstractAction("Preferences#clear() and JFrame#dispose()") {
-        @Override public void actionPerformed(ActionEvent e) {
-            try {
-                prefs.clear();
-                prefs.flush();
-            } catch (BackingStoreException ex) {
-                ex.printStackTrace();
-            }
-            Optional.ofNullable(SwingUtilities.getWindowAncestor((Component) e.getSource())).ifPresent(w -> w.dispose());
-        }
-    });
-
-    public MainPanel(JFrame frame) {
+    private final transient WindowPreferencesHandler handler = new WindowPreferencesHandler();
+    public MainPanel() {
         super(new BorderLayout());
-        this.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-        this.prefs = Preferences.userNodeForPackage(getClass());
-        frame.addWindowListener(new WindowAdapter() {
-            @Override public void windowClosing(WindowEvent e) {
-                saveLocation();
-                e.getWindow().dispose();
-            }
-        });
-        frame.addComponentListener(new ComponentAdapter() {
-            @Override public void componentMoved(ComponentEvent e) {
-                JFrame frame = (JFrame) e.getComponent();
-                if (frame.getExtendedState() == Frame.NORMAL) {
-                    Point pt = frame.getLocationOnScreen();
-                    if (pt.x < 0 || pt.y < 0) {
-                        return;
-                    }
-                    try {
-                        pos.setLocation(pt);
-                    } catch (IllegalComponentStateException icse) {
-                        icse.printStackTrace();
-                    }
-                }
-            }
-            @Override public void componentResized(ComponentEvent e) {
-                JFrame frame = (JFrame) e.getComponent();
-                if (frame.getExtendedState() == Frame.NORMAL) {
-                    dim.setSize(getSize());
-                }
-            }
-        });
 
         Box box = Box.createHorizontalBox();
         box.add(Box.createHorizontalGlue());
-        box.add(clearButton);
+        box.add(new JButton(new AbstractAction("Preferences#clear() and JFrame#dispose()") {
+            @Override public void actionPerformed(ActionEvent e) {
+                try {
+                    handler.prefs.clear();
+                    handler.prefs.flush();
+                } catch (BackingStoreException ex) {
+                    ex.printStackTrace();
+                }
+                Optional.ofNullable(SwingUtilities.getWindowAncestor((Component) e.getSource())).ifPresent(w -> w.dispose());
+            }
+        }));
         box.add(Box.createHorizontalStrut(2));
-        box.add(exitButton);
+        box.add(new JButton(new AbstractAction("exit") {
+            @Override public void actionPerformed(ActionEvent e) {
+                handler.saveLocation();
+                Optional.ofNullable(SwingUtilities.getWindowAncestor((Component) e.getSource())).ifPresent(w -> w.dispose());
+            }
+        }));
 
         add(new JLabel("TEST"));
+        setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         add(box, BorderLayout.SOUTH);
 
-        int wdim = prefs.getInt(PREFIX + "dimw", dim.width);
-        int hdim = prefs.getInt(PREFIX + "dimh", dim.height);
-        dim.setSize(wdim, hdim);
-        //setPreferredSize(dim);
-
-        Rectangle screen = frame.getGraphicsConfiguration().getBounds();
-        pos.setLocation(screen.x + screen.width / 2  - dim.width / 2,
-                        screen.y + screen.height / 2 - dim.height / 2);
-        int xpos = prefs.getInt(PREFIX + "locx", pos.x);
-        int ypos = prefs.getInt(PREFIX + "locy", pos.y);
-        pos.setLocation(xpos, ypos);
-        frame.setLocation(pos.x, pos.y);
+        EventQueue.invokeLater(() -> {
+            Component c = SwingUtilities.getRoot(getRootPane());
+            if (c instanceof JFrame) {
+                JFrame frame = (JFrame) c;
+                frame.addWindowListener(handler);
+                frame.addComponentListener(handler);
+                handler.initFrameSizeAndLocation(frame);
+            }
+        });
     }
     @Override public Dimension getPreferredSize() {
-        return dim;
+        return handler.dim;
     }
-    private void saveLocation() {
-        prefs.putInt(PREFIX + "locx", pos.x);
-        prefs.putInt(PREFIX + "locy", pos.y);
-        prefs.putInt(PREFIX + "dimw", dim.width);
-        prefs.putInt(PREFIX + "dimh", dim.height);
-        try {
-            prefs.flush();
-        } catch (BackingStoreException e) {
-            e.printStackTrace();
-        }
-    }
-
     public static void main(String... args) {
         EventQueue.invokeLater(new Runnable() {
             @Override public void run() {
@@ -112,14 +62,78 @@ public final class MainPanel extends JPanel {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (ClassNotFoundException | InstantiationException
-               | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+                 | IllegalAccessException | UnsupportedLookAndFeelException ex) {
             ex.printStackTrace();
         }
         JFrame frame = new JFrame("@title@");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.getContentPane().add(new MainPanel(frame));
+        frame.getContentPane().add(new MainPanel());
         frame.pack();
         //frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
+}
+
+class WindowPreferencesHandler extends WindowAdapter implements ComponentListener {
+    private static final String PREFIX = "xxx_";
+    public final Preferences prefs = Preferences.userNodeForPackage(getClass());
+    public final Dimension dim = new Dimension(320, 240);
+    public final Point pos = new Point();
+    public void initFrameSizeAndLocation(JFrame frame) {
+        int wdim = prefs.getInt(PREFIX + "dimw", dim.width);
+        int hdim = prefs.getInt(PREFIX + "dimh", dim.height);
+        dim.setSize(wdim, hdim);
+        //setPreferredSize(dim);
+        frame.pack();
+
+        Rectangle screen = frame.getGraphicsConfiguration().getBounds();
+        pos.setLocation(screen.x + screen.width / 2  - dim.width / 2, screen.y + screen.height / 2 - dim.height / 2);
+        int xpos = prefs.getInt(PREFIX + "locx", pos.x);
+        int ypos = prefs.getInt(PREFIX + "locy", pos.y);
+        pos.setLocation(xpos, ypos);
+        frame.setLocation(pos.x, pos.y);
+    }
+    public void saveLocation() {
+        prefs.putInt(PREFIX + "locx", pos.x);
+        prefs.putInt(PREFIX + "locy", pos.y);
+        prefs.putInt(PREFIX + "dimw", dim.width);
+        prefs.putInt(PREFIX + "dimh", dim.height);
+        try {
+            prefs.flush();
+        } catch (BackingStoreException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override public void componentHidden(ComponentEvent e) { /* not needed */ }
+    @Override public void componentMoved(ComponentEvent e) {
+        JFrame frame = (JFrame) e.getComponent();
+        if (frame.getExtendedState() == Frame.NORMAL) {
+            Point pt = frame.getLocationOnScreen();
+            if (pt.x < 0 || pt.y < 0) {
+                return;
+            }
+            try {
+                pos.setLocation(pt);
+            } catch (IllegalComponentStateException icse) {
+                icse.printStackTrace();
+            }
+        }
+    }
+    @Override public void componentResized(ComponentEvent e) {
+        JFrame frame = (JFrame) e.getComponent();
+        if (frame.getExtendedState() == Frame.NORMAL) {
+            dim.setSize(frame.getContentPane().getSize());
+        }
+    }
+    @Override public void componentShown(ComponentEvent e) { /* not needed */ }
+//     @Override public void windowActivated(WindowEvent e) { /* not needed */ }
+//     @Override public void windowClosed(WindowEvent e) { /* not needed */ }
+    @Override public void windowClosing(WindowEvent e) {
+        saveLocation();
+        e.getWindow().dispose();
+    }
+//     @Override public void windowDeactivated(WindowEvent e) { /* not needed */ }
+//     @Override public void windowDeiconified(WindowEvent e) { /* not needed */ }
+//     @Override public void windowIconified(WindowEvent e) { /* not needed */ }
+//     @Override public void windowOpened(WindowEvent e) { /* not needed */ }
 }
