cddeaf7400368280d43973801dfed9f34aa969fb
aterai <aterai@outlook.com>
2016-04-17 00:23:54 +0900

    refactor: use Table.focusCellHighlightBorder for ComboTableCellRenderer

--- NoArrowButtonComboCellEditor/src/java/example/MainPanel.java
+++ NoArrowButtonComboCellEditor/src/java/example/MainPanel.java
@@ -71,31 +71,39 @@ class LocalDateTimeTableCellRenderer extends DefaultTableCellRenderer {
     }
 }
 
-class LocalDateTimeTableCellEditor extends AbstractCellEditor implements TableCellEditor {
-    private final JComboBox<LocalDateTime> comboBox;
-    private LocalDateTime selectedDate;
-
-    public LocalDateTimeTableCellEditor() {
-        super();
-        UIManager.put("ComboBox.squareButton", Boolean.FALSE);
-        comboBox = new JComboBox<LocalDateTime>() {
+class ZeroSizeButtonUI extends BasicComboBoxUI {
+    @Override protected JButton createArrowButton() {
+        JButton button = new JButton() {
+            @Override public Dimension getPreferredSize() {
+                return new Dimension();
+            }
+            @Override public boolean isVisible() {
+                return false;
+            }
             @Override public void updateUI() {
                 super.updateUI();
                 setBorder(BorderFactory.createEmptyBorder());
-                setOpaque(true);
-                setRenderer(new LocalDateTimeCellRenderer<LocalDateTime>());
-                setUI(new BasicComboBoxUI() {
-                    @Override protected JButton createArrowButton() {
-                        JButton button = new JButton(); //.createArrowButton();
-                        button.setBorder(BorderFactory.createEmptyBorder());
-                        button.setVisible(false);
-                        return button;
-                    }
-                });
+                //setVisible(false);
             }
-        };
-        comboBox.putClientProperty("JComboBox.isTableCellEditor", Boolean.TRUE);
+        }; //.createArrowButton();
+        return button;
     }
+}
+
+class LocalDateTimeTableCellEditor extends AbstractCellEditor implements TableCellEditor {
+    private final JComboBox<LocalDateTime> comboBox = new JComboBox<LocalDateTime>() {
+        @Override public void updateUI() {
+            super.updateUI();
+            UIManager.put("ComboBox.squareButton", Boolean.FALSE);
+            putClientProperty("JComboBox.isTableCellEditor", Boolean.TRUE);
+            setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1));
+            setOpaque(false);
+            setRenderer(new LocalDateTimeCellRenderer<LocalDateTime>());
+            setUI(new ZeroSizeButtonUI());
+        }
+    };
+    private LocalDateTime selectedDate;
+
     @Override public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
         if (value instanceof LocalDateTime) {
             comboBox.setModel(new DefaultComboBoxModel<LocalDateTime>() {
@@ -104,7 +112,7 @@ class LocalDateTimeTableCellEditor extends AbstractCellEditor implements TableCe
                     return LocalDateTime.now().plusDays(index);
                 }
                 @Override public int getSize() {
-                    return 7;
+                    return 7; //in a week
                 }
                 @Override public Object getSelectedItem() {
                     return selectedDate;
@@ -115,6 +123,7 @@ class LocalDateTimeTableCellEditor extends AbstractCellEditor implements TableCe
             });
             selectedDate = (LocalDateTime) value;
         }
+        comboBox.setBorder(UIManager.getBorder("Table.focusCellHighlightBorder"));
         return comboBox;
     }
     @Override public Object getCellEditorValue() {
