66f156a92c73e33daf6f1540238e30096e5f0b33
aterai <aterai@outlook.com>
2016-04-21 00:21:28 +0900

    refactor: private static inner class out, package private class in

--- RadioButtonTextColor/src/java/example/MainPanel.java
+++ RadioButtonTextColor/src/java/example/MainPanel.java
@@ -9,6 +9,7 @@ public final class MainPanel extends JPanel {
     private MainPanel() {
         super(new GridLayout(1, 3));
         ButtonGroup bg = new ButtonGroup();
+
         Box box0 = Box.createVerticalBox();
         box0.setBorder(BorderFactory.createTitledBorder("Default"));
         int i = 0;
@@ -20,29 +21,35 @@ public final class MainPanel extends JPanel {
             box0.add(Box.createVerticalStrut(5));
             i++;
         }
+
         Box box1 = Box.createVerticalBox();
         box1.setBorder(BorderFactory.createTitledBorder("Text Color"));
         while (i < 8) {
             JRadioButton b = new ColorRadioButton("Text: " + i);
             bg.add(b);
             box1.add(b);
-            b.setAlignmentX(Component.LEFT_ALIGNMENT);
             box1.add(Box.createVerticalStrut(5));
-            b.setIcon(null);
-            b.setPressedIcon(null);
-            b.setSelectedIcon(null);
             i++;
         }
+
         Box box2 = Box.createVerticalBox();
         box2.setBorder(BorderFactory.createTitledBorder("Icon Color"));
         while (i < 12) {
-            JRadioButton b = new ColorRadioButton("Icon: " + i);
+            JRadioButton b = new ColorRadioButton("Icon: " + i) {
+                @Override public void updateUI() {
+                    super.updateUI();
+                    setIcon(new DefaultIcon());
+                    setPressedIcon(new PressedIcon());
+                    setSelectedIcon(new SelectedIcon());
+                    setRolloverIcon(new RolloverIcon());
+                }
+            };
             bg.add(b);
             box2.add(b);
-            b.setAlignmentX(Component.LEFT_ALIGNMENT);
             box2.add(Box.createVerticalStrut(5));
             i++;
         }
+
         add(box0);
         add(box1);
         add(box2);
@@ -73,103 +80,90 @@ public final class MainPanel extends JPanel {
 }
 
 class ColorRadioButton extends JRadioButton {
-    private static final Color DEFAULT_COLOR  = Color.BLACK;
-    private static final Color PRESSED_COLOR  = Color.GREEN;
-    private static final Color SELECTED_COLOR = Color.RED;
-    private static final Color ROLLOVER_COLOR = Color.BLUE;
-    private static final int ICON_SIZE = 16;
-
-    public ColorRadioButton(String text) {
+    protected ColorRadioButton(String text) {
         super(text);
-        setForeground(DEFAULT_COLOR);
-        setIcon(new DefaultIcon());
-        setPressedIcon(new PressedIcon());
-        setSelectedIcon(new SelectedIcon());
-        setRolloverIcon(new RolloverIcon());
-//         addChangeListener(new ChangeListener() {
-//             @Override public void stateChanged(ChangeEvent e) {
-//                 ButtonModel model = getModel();
-//                 if (!model.isEnabled()) {
-//                     setForeground(Color.GRAY);
-//                 } else if (model.isPressed() && model.isArmed()) {
-//                     setForeground(PRESSED_COLOR);
-//                 } else if (model.isSelected()) {
-//                     setForeground(SELECTED_COLOR);
-//                 } else if (isRolloverEnabled() && model.isRollover()) {
-//                     setForeground(ROLLOVER_COLOR);
-//                 } else {
-//                     setForeground(DEFAULT_COLOR);
-//                 }
-//             }
-//         });
+    }
+    @Override public void updateUI() {
+        super.updateUI();
+        setForeground(DefaultIcon.DEFAULT_COLOR);
+        setAlignmentX(Component.LEFT_ALIGNMENT);
     }
     @Override protected void fireStateChanged() {
         ButtonModel model = getModel();
         if (model.isEnabled()) {
             if (model.isPressed() && model.isArmed()) {
-                setForeground(PRESSED_COLOR);
+                setForeground(DefaultIcon.PRESSED_COLOR);
             } else if (model.isSelected()) {
-                setForeground(SELECTED_COLOR);
+                setForeground(DefaultIcon.SELECTED_COLOR);
             } else if (isRolloverEnabled() && model.isRollover()) {
-                setForeground(ROLLOVER_COLOR);
+                setForeground(DefaultIcon.ROLLOVER_COLOR);
             } else {
-                setForeground(DEFAULT_COLOR);
+                setForeground(DefaultIcon.DEFAULT_COLOR);
             }
         } else {
             setForeground(Color.GRAY);
         }
         super.fireStateChanged();
     }
-    private static class DefaultIcon implements Icon {
-        @Override public int getIconWidth() {
-            return ICON_SIZE * 2;
-        }
-        @Override public int getIconHeight() {
-            return ICON_SIZE;
-        }
-        @Override public void paintIcon(Component c, Graphics g, int x, int y) {
-            Graphics2D g2 = (Graphics2D) g.create();
-            g2.translate(x, y);
-            g2.setPaint(DEFAULT_COLOR);
-            g2.drawRect(0, 0, getIconWidth() - 1,     getIconHeight() - 1);
-            g2.drawRect(1, 1, getIconWidth() - 2 - 1, getIconHeight() - 2 - 1);
-            g2.dispose();
-        }
-    }
-    private static class PressedIcon extends DefaultIcon {
-        @Override public void paintIcon(Component c, Graphics g, int x, int y) {
-            Graphics2D g2 = (Graphics2D) g.create();
-            g2.translate(x, y);
-            g2.setPaint(PRESSED_COLOR);
-            g2.drawRect(0, 0, getIconWidth() - 1,     getIconHeight() - 1);
-            g2.drawRect(1, 1, getIconWidth() - 2 - 1, getIconHeight() - 2 - 1);
+}
 
-            g2.setPaint(SELECTED_COLOR);
-            g2.fillRect(4, 4, getIconWidth() - 8, getIconHeight() - 8);
-            g2.dispose();
-        }
+class DefaultIcon implements Icon {
+    protected static final Color DEFAULT_COLOR  = Color.BLACK;
+    protected static final Color PRESSED_COLOR  = Color.GREEN;
+    protected static final Color SELECTED_COLOR = Color.RED;
+    protected static final Color ROLLOVER_COLOR = Color.BLUE;
+    protected static final int ICON_SIZE = 16;
+    @Override public int getIconWidth() {
+        return ICON_SIZE * 2;
     }
-    private static class SelectedIcon extends DefaultIcon {
-        @Override public void paintIcon(Component c, Graphics g, int x, int y) {
-            Graphics2D g2 = (Graphics2D) g.create();
-            g2.translate(x, y);
-            g2.setPaint(SELECTED_COLOR);
-            g2.drawRect(0, 0, getIconWidth() - 1,     getIconHeight() - 1);
-            g2.drawRect(1, 1, getIconWidth() - 2 - 1, getIconHeight() - 2 - 1);
+    @Override public int getIconHeight() {
+        return ICON_SIZE;
+    }
+    @Override public void paintIcon(Component c, Graphics g, int x, int y) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.translate(x, y);
+        g2.setPaint(DEFAULT_COLOR);
+        g2.drawRect(0, 0, getIconWidth() - 1,     getIconHeight() - 1);
+        g2.drawRect(1, 1, getIconWidth() - 2 - 1, getIconHeight() - 2 - 1);
+        g2.dispose();
+    }
+}
 
-            g2.setPaint(PRESSED_COLOR);
-            g2.fillRect(6, 6, getIconWidth() - 12, getIconHeight() - 12);
-            g2.dispose();
-        }
+class PressedIcon extends DefaultIcon {
+    @Override public void paintIcon(Component c, Graphics g, int x, int y) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.translate(x, y);
+        g2.setPaint(PRESSED_COLOR);
+        g2.drawRect(0, 0, getIconWidth() - 1,     getIconHeight() - 1);
+        g2.drawRect(1, 1, getIconWidth() - 2 - 1, getIconHeight() - 2 - 1);
+
+        g2.setPaint(SELECTED_COLOR);
+        g2.fillRect(4, 4, getIconWidth() - 8, getIconHeight() - 8);
+        g2.dispose();
     }
-    private static class RolloverIcon extends DefaultIcon {
-        @Override public void paintIcon(Component c, Graphics g, int x, int y) {
-            Graphics2D g2 = (Graphics2D) g.create();
-            g2.translate(x, y);
-            g2.setPaint(ROLLOVER_COLOR);
-            g2.drawRect(0, 0, getIconWidth() - 1,     getIconHeight() - 1);
-            g2.drawRect(1, 1, getIconWidth() - 2 - 1, getIconHeight() - 2 - 1);
-            g2.dispose();
-        }
+}
+
+class SelectedIcon extends DefaultIcon {
+    @Override public void paintIcon(Component c, Graphics g, int x, int y) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.translate(x, y);
+        g2.setPaint(SELECTED_COLOR);
+        g2.drawRect(0, 0, getIconWidth() - 1,     getIconHeight() - 1);
+        g2.drawRect(1, 1, getIconWidth() - 2 - 1, getIconHeight() - 2 - 1);
+
+        g2.setPaint(PRESSED_COLOR);
+        g2.fillRect(6, 6, getIconWidth() - 12, getIconHeight() - 12);
+        g2.dispose();
+    }
+}
+
+class RolloverIcon extends DefaultIcon {
+    @Override public void paintIcon(Component c, Graphics g, int x, int y) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.translate(x, y);
+        g2.setPaint(ROLLOVER_COLOR);
+        g2.drawRect(0, 0, getIconWidth() - 1,     getIconHeight() - 1);
+        g2.drawRect(1, 1, getIconWidth() - 2 - 1, getIconHeight() - 2 - 1);
+        g2.dispose();
     }
 }
