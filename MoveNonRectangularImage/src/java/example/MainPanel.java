399cfba2e1d68c0d26391828d57419875590b223
aterai <aterai@outlook.com>
2016-05-20 00:11:15 +0900

    refactor: use ImageIO.read(...) to get BufferedImage

--- MoveNonRectangularImage/src/java/example/MainPanel.java
+++ MoveNonRectangularImage/src/java/example/MainPanel.java
@@ -5,44 +5,47 @@ package example;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.*;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public final class MainPanel extends JPanel {
     public MainPanel() {
-        super((LayoutManager) null);
-        add(makeLabelIcon());
+        super(new BorderLayout());
+
+        BufferedImage image;
+        try {
+            image = ImageIO.read(getClass().getResource("duke.gif"));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return;
+        }
+        JLabel icon = makeLabelIcon(image);
+        icon.setCursor(Cursor.getPredefinedCursor(Cursor.MOVE_CURSOR));
+        icon.setSize(icon.getPreferredSize());
+        icon.setLocation(20, 20);
+
+        JDesktopPane desktop = new JDesktopPane();
+        desktop.setOpaque(false);
+        desktop.add(icon);
+        add(desktop);
         setPreferredSize(new Dimension(320, 240));
     }
-    private JLabel makeLabelIcon() {
-        ImageIcon i = new ImageIcon(getClass().getResource("duke.gif"));
-        Dimension d = new Dimension(i.getIconWidth(), i.getIconHeight());
-        final BufferedImage image = new BufferedImage(d.width, d.height, BufferedImage.TYPE_INT_ARGB);
-        Graphics g = image.createGraphics();
-        i.paintIcon(null, g, 0, 0);
-        g.dispose();
-        final JLabel icon = new JLabel(i) {
+    private JLabel makeLabelIcon(BufferedImage image) {
+        MouseAdapter handler = new ComponentMoveHandler();
+        return new JLabel(new ImageIcon(image)) {
             @Override public boolean contains(int x, int y) {
                 return super.contains(x, y) && ((image.getRGB(x, y) >> 24) & 0xFF) != 0;
             }
-        };
-        icon.setCursor(Cursor.getPredefinedCursor(Cursor.MOVE_CURSOR));
-        MouseAdapter l = new MouseAdapter() {
-            private final transient Point start = new Point();
-            private Point loc;
-            @Override public void mousePressed(MouseEvent e) {
-                start.setLocation(e.getPoint());
-            }
-            @Override public void mouseDragged(MouseEvent e) {
-                loc = icon.getLocation(loc);
-                int x = loc.x - start.x + e.getX();
-                int y = loc.y - start.y + e.getY();
-                icon.setLocation(x, y);
+            @Override public void updateUI() {
+                removeMouseListener(handler);
+                removeMouseMotionListener(handler);
+                super.updateUI();
+                //handler = new ComponentMoveHandler();
+                addMouseListener(handler);
+                addMouseMotionListener(handler);
             }
         };
-        icon.addMouseListener(l);
-        icon.addMouseMotionListener(l);
-        icon.setBounds(new Rectangle(22, 22, d.width, d.height));
-        return icon;
     }
     public static void main(String... args) {
         EventQueue.invokeLater(new Runnable() {
@@ -64,5 +67,16 @@ public final class MainPanel extends JPanel {
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+    }
+}
+
+class ComponentMoveHandler extends MouseAdapter {
+    private final Point startPt = new Point();
+    @Override public void mousePressed(MouseEvent e) {
+        startPt.setLocation(e.getPoint());
+    }
+    @Override public void mouseDragged(MouseEvent e) {
+        Component c = e.getComponent();
+        c.setLocation(c.getX() - startPt.x + e.getX(), c.getY() - startPt.y + e.getY());
     }
 }
