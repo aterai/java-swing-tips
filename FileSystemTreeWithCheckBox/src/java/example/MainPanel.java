4d1a9d0d403212bee307949bc69b7ab4db0cb4e5
aterai <aterai@outlook.com>
2016-09-27 03:00:03 +0900

    fix: a bug that references the previous node icon

--- FileSystemTreeWithCheckBox/src/java/example/MainPanel.java
+++ FileSystemTreeWithCheckBox/src/java/example/MainPanel.java
@@ -241,10 +241,10 @@ class CheckBoxNodeEditor extends AbstractCellEditor implements TreeCellEditor {
                 } else {
                     checkBox.setIcon(null);
                 }
+                file = node.file;
                 l.setIcon(fileSystemView.getSystemIcon(file));
                 l.setText(fileSystemView.getSystemDisplayName(file));
                 checkBox.setSelected(node.status == Status.SELECTED);
-                file = node.file;
             }
             panel.add(l);
             return panel;
@@ -263,7 +263,7 @@ class CheckBoxNodeEditor extends AbstractCellEditor implements TreeCellEditor {
                 r.width = checkBox.getPreferredSize().width;
                 return r.contains(p);
             }).orElse(false);
-//             Point me = (MouseEvent) e;
+//             MouseEvent me = (MouseEvent) e;
 //             JTree tree = (JTree) e.getSource();
 //             TreePath path = tree.getPathForLocation(me.getX(), me.getY());
 //             Rectangle r = tree.getPathBounds(path);
