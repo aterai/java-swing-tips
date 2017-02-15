b72a126f005eb6b80d2a33c062df53113582ceb1
aterai <aterai@outlook.com>
2017-02-15 16:16:40 +0900

    chore: put it in one line

--- AllFonts/src/java/example/MainPanel.java
+++ AllFonts/src/java/example/MainPanel.java
@@ -20,12 +20,10 @@ public final class MainPanel extends JPanel {
     public MainPanel() {
         super(new BorderLayout());
         table.setAutoCreateRowSorter(true);
-        Font[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts();
-        for (Font f: fonts) {
-            model.addRow(new Object[] {f.getFamily(), f.getName(), f.getPSName()});
+        for (Font f: GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts()) {
+            model.addRow(new String[] {f.getFamily(), f.getName(), f.getPSName()});
         }
         add(new JScrollPane(table));
-        //add(new JScrollPane(new JList(fonts)));
         setPreferredSize(new Dimension(320, 240));
     }
     public static void main(String... args) {
