d29bc113b8ce65587a03e3b1137e1c145a39f0cc
aterai <aterai@outlook.com>
2017-02-27 16:01:18 +0900

    add: CalendarFieldStepSize

---
+++ CalendarFieldStepSize/src/java/example/MainPanel.java
@@ -0,0 +1,95 @@
+package example;
+//-*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+//@homepage@
+import java.awt.*;
+import java.text.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.text.*;
+
+public final class MainPanel extends JPanel {
+    private MainPanel() {
+        super(new GridLayout(2, 1));
+        SimpleDateFormat format = new SimpleDateFormat("mm:ss, SSS", Locale.getDefault());
+        DefaultFormatterFactory factory = new DefaultFormatterFactory(new DateFormatter(format));
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.clear(Calendar.MINUTE);
+        calendar.clear(Calendar.SECOND);
+        calendar.clear(Calendar.MILLISECOND);
+        Date d = calendar.getTime();
+
+        JSpinner spinner1 = new JSpinner(new SpinnerDateModel(d, null, null, Calendar.SECOND));
+        ((JSpinner.DefaultEditor) spinner1.getEditor()).getTextField().setFormatterFactory(factory);
+
+        HashMap<Integer, Integer> stepSizeMap = new HashMap<>();
+        stepSizeMap.put(Calendar.HOUR_OF_DAY, 1);
+        stepSizeMap.put(Calendar.MINUTE,      1);
+        stepSizeMap.put(Calendar.SECOND,      30);
+        stepSizeMap.put(Calendar.MILLISECOND, 500);
+
+        JSpinner spinner2 = new JSpinner(new SpinnerDateModel(d, null, null, Calendar.SECOND) {
+            @Override public Object getPreviousValue() {
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(getDate());
+                Integer calendarField = getCalendarField();
+                Integer stepSize = Optional.ofNullable(stepSizeMap.get(calendarField)).orElse(1);
+                cal.add(calendarField, -stepSize);
+                Date prev = cal.getTime();
+                //Comparable start = getStart();
+                //return ((start == null) || (start.compareTo(prev) <= 0)) ? prev : null;
+                return prev;
+            }
+            @Override public Object getNextValue() {
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(getDate());
+                Integer calendarField = getCalendarField();
+                Integer stepSize = Optional.ofNullable(stepSizeMap.get(calendarField)).orElse(1);
+                cal.add(calendarField, stepSize);
+                Date next = cal.getTime();
+                //Comparable end = getEnd();
+                //return ((end == null) || (end.compareTo(next) >= 0)) ? next : null;
+                return next;
+            }
+        });
+        ((JSpinner.DefaultEditor) spinner2.getEditor()).getTextField().setFormatterFactory(factory);
+
+        add(makeTitlePanel(spinner1, "Default SpinnerDateModel"));
+        add(makeTitlePanel(spinner2, "Override SpinnerDateModel#getNextValue(...)"));
+        setBorder(BorderFactory.createEmptyBorder(10, 5, 10, 5));
+        setPreferredSize(new Dimension(320, 240));
+    }
+    private static JComponent makeTitlePanel(JComponent cmp, String title) {
+        JPanel p = new JPanel(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.weightx = 1d;
+        c.fill    = GridBagConstraints.HORIZONTAL;
+        c.insets  = new Insets(5, 5, 5, 5);
+        p.add(cmp, c);
+        p.setBorder(BorderFactory.createTitledBorder(title));
+        return p;
+    }
+    public static void main(String... args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+    public static void createAndShowGUI() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException
+               | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        }
+        JFrame frame = new JFrame("@title@");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new MainPanel());
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
