18948366517b553628ae27518857eb01361c222f
aterai <aterai@outlook.com>
2017-04-05 18:27:04 +0900

    fix: JPopupMenu is enabled only on the selected node

--- TreeNodePopupMenu/src/java/example/MainPanel.java
+++ TreeNodePopupMenu/src/java/example/MainPanel.java
@@ -3,21 +3,21 @@ package example;
 // vim:set fileencoding=utf-8:
 //@homepage@
 import java.awt.*;
-import java.awt.event.*;
+import java.util.*;
 import javax.swing.*;
 import javax.swing.tree.*;
 
 public final class MainPanel extends JPanel {
-    private final JTree tree = new JTree();
-    public MainPanel() {
+    private MainPanel() {
         super(new BorderLayout());
+        JTree tree = new JTree();
         tree.setComponentPopupMenu(new TreePopupMenu());
+
+        JButton button = new JButton("Clear node selection");
+        button.addActionListener(e -> tree.clearSelection());
+
         add(new JScrollPane(tree));
-        add(new JButton(new AbstractAction("Clear node selection") {
-            @Override public void actionPerformed(ActionEvent e) {
-                tree.clearSelection();
-            }
-        }), BorderLayout.SOUTH);
+        add(button, BorderLayout.SOUTH);
         setPreferredSize(new Dimension(320, 240));
     }
 
@@ -47,18 +47,17 @@ public final class MainPanel extends JPanel {
 class TreePopupMenu extends JPopupMenu {
     protected TreePopupMenu() {
         super();
-        add(new AbstractAction("path") {
-            @Override public void actionPerformed(ActionEvent e) {
-                JTree tree = (JTree) getInvoker();
-                JOptionPane.showMessageDialog(tree, tree.getSelectionPaths(), "path", JOptionPane.INFORMATION_MESSAGE);
-            }
+        add("path").addActionListener(e -> {
+            JTree tree = (JTree) getInvoker();
+            JOptionPane.showMessageDialog(tree, tree.getSelectionPaths(), "path", JOptionPane.INFORMATION_MESSAGE);
         });
-        add(new JMenuItem("dummy"));
+        add("dummy");
     }
     @Override public void show(Component c, int x, int y) {
         if (c instanceof JTree) {
             JTree tree = (JTree) c;
-            if (tree.getSelectionCount() > 0) {
+            TreePath path = tree.getPathForLocation(x, y);
+            if (tree.getSelectionCount() > 0 && Arrays.asList(tree.getSelectionPaths()).contains(path)) {
                 super.show(c, x, y);
             }
         }
