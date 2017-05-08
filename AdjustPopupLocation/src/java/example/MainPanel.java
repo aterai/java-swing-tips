20997c68f0cb226c4c8e63b83ac2a86d44bace74
aterai <aterai@outlook.com>
2017-05-08 14:34:48 +0900

    add: AdjustPopupLocation

---
+++ AdjustPopupLocation/src/java/example/MainPanel.java
@@ -0,0 +1,75 @@
+package example;
+//-*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+//@homepage@
+import java.awt.*;
+// import java.awt.event.*;
+// import java.util.*;
+// import java.util.List;
+import javax.swing.*;
+
+public final class MainPanel extends JPanel {
+    private MainPanel() {
+        super(new BorderLayout());
+
+        JCheckBox check = new JCheckBox("Adjust JPopupMenu location", true);
+        check.setFocusPainted(false);
+
+        JPopupMenu popup = new JPopupMenu() {
+            @Override public void show(Component c, int x, int y) {
+                System.out.println(c.getClass().getName());
+                if (check.isSelected()) {
+                    Point p = new Point(x, y);
+                    Rectangle r = c.getBounds();
+                    Dimension d = getPreferredSize();
+                    if (p.x + d.width > r.width) {
+                        p.x -= d.width;
+                    }
+                    if (p.y + d.height > r.height) {
+                        p.y -= d.height;
+                    }
+                    super.show(c, Math.max(p.x, 0), Math.max(p.y, 0));
+                } else {
+                    super.show(c, x, y);
+                }
+            }
+        };
+        popup.add("aaa");
+        popup.add("bbbbbb");
+        popup.add("cc");
+        // setComponentPopupMenu(popup);
+
+        JLabel label = new JLabel("aaaaaaaaaaaa");
+        label.setOpaque(true);
+        // label.setInheritsPopupMenu(true);
+        // check.setInheritsPopupMenu(true);
+        label.setComponentPopupMenu(popup);
+
+
+        add(check, BorderLayout.NORTH);
+        add(label);
+        setBorder(BorderFactory.createLineBorder(Color.RED, 10));
+        setPreferredSize(new Dimension(320, 240));
+    }
+    public static void main(String... args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+    public static void createAndShowGUI() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException
+               | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        }
+        JFrame frame = new JFrame("@title@");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new MainPanel());
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
