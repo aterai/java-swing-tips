3886b2afc2c2b40b2c35396ff60cf3f5c2f677df
aterai <aterai@outlook.com>
2017-07-03 15:25:05 +0900

    add: TreeLineColor

--- TreeLineColor/src/java/example/MainPanel.java
+++ TreeLineColor/src/java/example/MainPanel.java
@@ -23,26 +23,31 @@ public final class MainPanel extends JPanel {
         tree1.putClientProperty("JTree.lineStyle", "Horizontal");
         // tree1.putClientProperty("JTree.lineStyle", "None");
 
-        JTree tree2 = new JTree();
-        tree2.setUI(new BasicTreeUI() {
-            private final Stroke horizontalLine = new BasicStroke(2f);
-            private final Stroke verticalLine = new BasicStroke(5f);
-            @Override public Color getHashColor() {
-                return Color.BLUE;
+        JTree tree2 = new JTree() {
+            @Override public void updateUI() {
+                super.updateUI();
+                UIManager.put("Tree.lineTypeDashed", Boolean.FALSE);
+                setUI(new MetalTreeUI() {
+                    private final Stroke horizontalLine = new BasicStroke(2f);
+                    private final Stroke verticalLine = new BasicStroke(5f);
+                    @Override public Color getHashColor() {
+                        return Color.BLUE;
+                    }
+                    @Override protected void paintHorizontalPartOfLeg(Graphics g, Rectangle clipBounds, Insets insets, Rectangle bounds, TreePath path, int row, boolean isExpanded, boolean hasBeenExpanded, boolean isLeaf) {
+                        Graphics2D g2 = (Graphics2D) g.create();
+                        g2.setStroke(horizontalLine);
+                        super.paintHorizontalPartOfLeg(g2, clipBounds, insets, bounds, path, row, isExpanded, hasBeenExpanded, isLeaf);
+                        g2.dispose();
+                    }
+                    @Override protected void paintVerticalPartOfLeg(Graphics g, Rectangle clipBounds, Insets insets, TreePath path) {
+                        Graphics2D g2 = (Graphics2D) g.create();
+                        g2.setStroke(verticalLine);
+                        super.paintVerticalPartOfLeg(g2, clipBounds, insets, path);
+                        g2.dispose();
+                    }
+                });
             }
-            @Override protected void paintHorizontalPartOfLeg(Graphics g, Rectangle clipBounds, Insets insets, Rectangle bounds, TreePath path, int row, boolean isExpanded, boolean hasBeenExpanded, boolean isLeaf) {
-                Graphics2D g2 = (Graphics2D) g.create();
-                g2.setStroke(horizontalLine);
-                super.paintHorizontalPartOfLeg(g2, clipBounds, insets, bounds, path, row, isExpanded, hasBeenExpanded, isLeaf);
-                g2.dispose();
-            }
-            @Override protected void paintVerticalPartOfLeg(Graphics g, Rectangle clipBounds, Insets insets, TreePath path) {
-                Graphics2D g2 = (Graphics2D) g.create();
-                g2.setStroke(verticalLine);
-                super.paintVerticalPartOfLeg(g2, clipBounds, insets, path);
-                g2.dispose();
-            }
-        });
+        };
 
         add(makeTitledPanel("lineTypeDashed", tree0));
         add(makeTitledPanel("lineStyle",      tree1));
