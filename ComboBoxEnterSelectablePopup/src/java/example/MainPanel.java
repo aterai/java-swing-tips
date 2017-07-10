70bd56d421bdd8456880f4f400e4d914cb4294d7
aterai <aterai@outlook.com>
2017-07-10 15:47:33 +0900

    add: ComboBoxEnterSelectablePopup

---
+++ ComboBoxEnterSelectablePopup/src/java/example/MainPanel.java
@@ -0,0 +1,70 @@
+package example;
+//-*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+//@homepage@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class MainPanel extends JPanel {
+    private final JTextArea log = new JTextArea();
+
+    public MainPanel() {
+        super(new BorderLayout(5, 5));
+
+        //TEST: System.out.println(UIManager.getBoolean("ComboBox.noActionOnKeyNavigation"));
+        //TEST: UIManager.put("ComboBox.noActionOnKeyNavigation", Boolean.TRUE);
+
+        JPanel p = new JPanel(new GridLayout(0, 1));
+        p.add(new JLabel("ComboBox.isEnterSelectablePopup: false(default)", SwingConstants.LEFT));
+        p.add(makeComboBox(false));
+        p.add(new JLabel("ComboBox.isEnterSelectablePopup: true", SwingConstants.LEFT));
+        p.add(makeComboBox(true));
+
+        add(p, BorderLayout.NORTH);
+        add(new JScrollPane(log));
+        setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        setPreferredSize(new Dimension(320, 240));
+    }
+    private JComboBox<String> makeComboBox(boolean isEnterSelectable) {
+        //UIManager.put("ComboBox.isEnterSelectablePopup", Boolean.TRUE);
+        JComboBox<String> combo = new JComboBox<>(new String[] {"aaa", "bbb", "CCC", "DDD"});
+        combo.setEditable(true);
+        combo.addPopupMenuListener(new PopupMenuListener() {
+            @Override public void popupMenuWillBecomeVisible(PopupMenuEvent e) {
+                UIManager.put("ComboBox.isEnterSelectablePopup", isEnterSelectable);
+                append("\nisEnterSelectablePopup: " + UIManager.getBoolean("ComboBox.isEnterSelectablePopup"));
+            }
+            @Override public void popupMenuWillBecomeInvisible(PopupMenuEvent e) { /* not needed */ }
+            @Override public void popupMenuCanceled(PopupMenuEvent e) { /* not needed */ }
+        });
+        combo.addActionListener(e -> append("ActionListener: " + combo.getSelectedItem()));
+        //TEST: combo.addItemListener(e -> append("ItemListener: " + combo.getSelectedItem()));
+        return combo;
+    }
+    protected void append(String text) {
+        log.append(text + "\n");
+        log.setCaretPosition(log.getDocument().getLength());
+    }
+    public static void main(String... args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+    public static void createAndShowGUI() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException
+               | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        }
+        JFrame frame = new JFrame("@title@");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new MainPanel());
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
