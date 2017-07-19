5164c35a50421e2d9dfef503a91ca9409043b79d
aterai <aterai@outlook.com>
2017-07-19 13:15:33 +0900

    refactor: use a lambda expression instead of an anonymous class(ActionListener)

--- OptionPaneDefaultFocus/src/java/example/MainPanel.java
+++ OptionPaneDefaultFocus/src/java/example/MainPanel.java
@@ -7,67 +7,66 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
 
-public class MainPanel extends JPanel {
-    private static final String DUMMY = "Hello";
-    protected final JTextArea textArea = new JTextArea();
-
-    public MainPanel() {
+public final class MainPanel extends JPanel {
+    private MainPanel() {
         super(new BorderLayout());
 
-        JTextField textField1 = new JTextField(DUMMY);
-        JTextField textField2 = new JTextField(DUMMY);
-        JTextField textField3 = new JTextField(DUMMY);
-        JTextField textField4 = new JTextField(DUMMY);
+        String dummy = "Hello";
+        JTextField textField1 = new JTextField(dummy);
+        JTextField textField2 = new JTextField(dummy);
+        JTextField textField3 = new JTextField(dummy);
+        JTextField textField4 = new JTextField(dummy);
 
         textField3.addHierarchyListener(new FocusHierarchyListener());
         textField4.addAncestorListener(new FocusAncestorListener());
 
+        JTextArea log = new JTextArea();
         JPanel p = new JPanel(new GridLayout(2, 2, 5, 5));
-        p.add(makePanel("Default",           textField1));
-        p.add(makePanel2("WindowListener",   textField2));
-        p.add(makePanel("HierarchyListener", textField3));
-        p.add(makePanel("AncestorListener",  textField4));
+        p.add(makePanel("Default", makeButton(textField1, log)));
+        p.add(makePanel("WindowListener", makeButton2(textField2, log)));
+        p.add(makePanel("HierarchyListener", makeButton(textField3, log)));
+        p.add(makePanel("AncestorListener", makeButton(textField4, log)));
         add(p, BorderLayout.NORTH);
-        add(new JScrollPane(textArea));
+        add(new JScrollPane(log));
         setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         setPreferredSize(new Dimension(320, 240));
     }
-    private JPanel makePanel(String title, JTextField textField) {
-        JPanel p = new JPanel(new BorderLayout());
-        p.setBorder(BorderFactory.createTitledBorder(title));
-        p.add(new JButton(new AbstractAction("show") {
-            @Override public void actionPerformed(ActionEvent e) {
-                int result = JOptionPane.showConfirmDialog(getRootPane(), textField, "Input Text", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
-                if (result == JOptionPane.OK_OPTION) {
-                    textArea.setText(textField.getText());
-                }
+    private static JButton makeButton(JTextField textField, JTextArea textArea) {
+        JButton button = new JButton("show");
+        button.addActionListener(e -> {
+            int result = JOptionPane.showConfirmDialog(textArea.getRootPane(), textField, "Input Text", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+            if (result == JOptionPane.OK_OPTION) {
+                textArea.setText(textField.getText());
             }
-        }));
-        return p;
+        });
+        return button;
     }
-    private JPanel makePanel2(String title, JTextField textField) {
+    private static JButton makeButton2(JTextField textField, JTextArea textArea) {
+        JButton button = new JButton("show");
+        button.addActionListener(e -> {
+            JOptionPane pane = new JOptionPane(textField, JOptionPane.PLAIN_MESSAGE, JOptionPane.OK_CANCEL_OPTION, null, null, null);
+            JDialog dialog = pane.createDialog(textArea.getRootPane(), "Input Text");
+            dialog.addWindowListener(new WindowAdapter() {
+                @Override public void windowOpened(WindowEvent e) {
+                    textField.requestFocusInWindow();
+                }
+            });
+            dialog.setVisible(true);
+            Object selectedValue = pane.getValue();
+            int result = JOptionPane.CLOSED_OPTION;
+            if (selectedValue instanceof Integer) {
+                result = ((Integer) selectedValue).intValue();
+            }
+            if (result == JOptionPane.OK_OPTION) {
+                textArea.setText(textField.getText());
+            }
+        });
+        return button;
+    }
+    private static JPanel makePanel(String title, JComponent c) {
         JPanel p = new JPanel(new BorderLayout());
         p.setBorder(BorderFactory.createTitledBorder(title));
-        p.add(new JButton(new AbstractAction("show") {
-            @Override public void actionPerformed(ActionEvent e) {
-                JOptionPane pane = new JOptionPane(textField, JOptionPane.PLAIN_MESSAGE, JOptionPane.OK_CANCEL_OPTION, null, null, null);
-                JDialog dialog = pane.createDialog(getRootPane(), "Input Text");
-                dialog.addWindowListener(new WindowAdapter() {
-                    @Override public void windowOpened(WindowEvent e) {
-                        textField.requestFocusInWindow();
-                    }
-                });
-                dialog.setVisible(true);
-                Object selectedValue = pane.getValue();
-                int result = JOptionPane.CLOSED_OPTION;
-                if (selectedValue instanceof Integer) {
-                    result = ((Integer) selectedValue).intValue();
-                }
-                if (result == JOptionPane.OK_OPTION) {
-                    textArea.setText(textField.getText());
-                }
-            }
-        }));
+        p.add(c);
         return p;
     }
     public static void main(String... args) {
