e8d1fb08472cfeadb08b3172738b1b832761a403
aterai <aterai@outlook.com>
2017-08-07 14:39:06 +0900

    add: RolloverModeToolBar

---
+++ RolloverModeToolBar/src/java/example/MainPanel.java
@@ -0,0 +1,78 @@
+package example;
+//-*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+//@homepage@
+import java.awt.*;
+// import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+// import javax.swing.plaf.*;
+
+public final class MainPanel extends JPanel {
+    private MainPanel() {
+        super(new BorderLayout());
+
+        JToolBar toolbar = new JToolBar();
+        System.out.println(toolbar.isRollover());
+        EventQueue.invokeLater(() -> System.out.println(toolbar.isRollover()));
+        // toolbar.setRollover(true);
+
+        JToggleButton tg1 = new JToggleButton("Tg1");
+        JToggleButton tg2 = new JToggleButton("Tg2");
+        JToggleButton tg3 = new JToggleButton("Tg3");
+        JButton button = new JButton("Button");
+        JRadioButton radio = new JRadioButton("RadioButton");
+
+        Dimension d = new Dimension(2, 2);
+        ButtonGroup bg = new ButtonGroup();
+        for (AbstractButton b: Arrays.asList(tg1, tg2, tg3, button, radio)) {
+            b.setFocusPainted(false);
+            //b.setRolloverEnabled(false);
+            //b.setContentAreaFilled(false);
+            toolbar.add(b);
+            toolbar.add(Box.createRigidArea(d));
+            bg.add(b);
+        }
+
+        JCheckBox check = new JCheckBox("setRollover");
+        check.addActionListener(e -> toolbar.setRollover(((AbstractButton) e.getSource()).isSelected()));
+        toolbar.add(Box.createGlue());
+        toolbar.add(check);
+
+        Box box = Box.createHorizontalBox();
+        box.add(new JLabel("setRolloverEnabled(false)"));
+        for (AbstractButton b: Arrays.asList(new JToggleButton("ToggleButton"), new JButton("Button"))) {
+            //b.setFocusPainted(false);
+            b.setRolloverEnabled(false);
+            //b.setContentAreaFilled(false);
+            box.add(b);
+            box.add(Box.createRigidArea(d));
+        }
+
+        add(toolbar, BorderLayout.NORTH);
+        add(new JScrollPane(new JTree()));
+        add(box, BorderLayout.SOUTH);
+        setPreferredSize(new Dimension(320, 240));
+    }
+    public static void main(String... args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+    public static void createAndShowGUI() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException
+               | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        }
+        JFrame frame = new JFrame("@title@");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new MainPanel());
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
