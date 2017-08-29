d113ed6dba0d04cf909fc8abf3da514b649de1de
aterai <aterai@outlook.com>
2017-08-29 14:39:15 +0900

    refactor: use a lambda expression instead of an anonymous class(ActionListener)

--- TabWithCheckBox/src/java/example/MainPanel.java
+++ TabWithCheckBox/src/java/example/MainPanel.java
@@ -11,58 +11,59 @@ import javax.swing.*;
 // import javax.swing.plaf.synth.*;
 
 public final class MainPanel extends JPanel {
+    private int count;
+
     private MainPanel() {
         super(new BorderLayout());
 
         JTabbedPane tabs = new JTabbedPane() {
             @Override public void addTab(String title, Component content) {
                 super.addTab(title, content);
-                /** //TEST:
-                JCheckBox check = new JCheckBox(title) {
-                    private final Rectangle viewRect = new Rectangle();
-                    private final Rectangle textRect = new Rectangle();
-                    private final Rectangle iconRect = new Rectangle();
-                    @Override public boolean contains(int x, int y) {
-                        Icon icon;
-                        ButtonUI ui = getUI();
-                        if (ui instanceof BasicRadioButtonUI) {
-                            icon = ((BasicRadioButtonUI) ui).getDefaultIcon();
-                        } else if (ui instanceof SynthButtonUI) {
-                            //icon = ((SynthButtonUI) ui).getDefaultIcon(this);
-                            SynthContext context = ((SynthButtonUI) ui).getContext(this);
-                            icon = context.getStyle().getIcon(context, "CheckBox.icon");
-                            //context.dispose();
-                        } else {
-                            icon = getIcon();
-                        }
-                        if (Objects.nonNull(icon)) {
-                            // layout the text and icon
-                            int width = getWidth();
-                            int height = getHeight();
-                            FontMetrics fm = getFontMetrics(getFont());
-                            Insets i = getInsets();
-                            viewRect.setBounds(i.left, i.top, width - i.right - i.left, height - i.bottom - i.top);
-                            textRect.setBounds(0, 0, 0, 0);
-                            iconRect.setBounds(0, 0, 0, 0);
-                            SwingUtilities.layoutCompoundLabel(
-                                this, fm, getText(), icon,
-                                getVerticalAlignment(), getHorizontalAlignment(),
-                                getVerticalTextPosition(), getHorizontalTextPosition(),
-                                viewRect, iconRect, textRect,
-                                getIconTextGap());
-                            return iconRect.contains(x, y);
-                        } else {
-                            return super.contains(x, y);
-                        }
-                    }
-                    @Override public void updateUI() {
-                        super.updateUI();
-                        setOpaque(false);
-                        setFocusable(false);
-                    }
-                };
-                setTabComponentAt(getTabCount() - 1, check);
-                /*/
+//                 //TEST:
+//                 JCheckBox check = new JCheckBox(title) {
+//                     private final Rectangle viewRect = new Rectangle();
+//                     private final Rectangle textRect = new Rectangle();
+//                     private final Rectangle iconRect = new Rectangle();
+//                     @Override public boolean contains(int x, int y) {
+//                         Icon icon;
+//                         ButtonUI ui = getUI();
+//                         if (ui instanceof BasicRadioButtonUI) {
+//                             icon = ((BasicRadioButtonUI) ui).getDefaultIcon();
+//                         } else if (ui instanceof SynthButtonUI) {
+//                             //icon = ((SynthButtonUI) ui).getDefaultIcon(this);
+//                             SynthContext context = ((SynthButtonUI) ui).getContext(this);
+//                             icon = context.getStyle().getIcon(context, "CheckBox.icon");
+//                             //context.dispose();
+//                         } else {
+//                             icon = getIcon();
+//                         }
+//                         if (Objects.nonNull(icon)) {
+//                             // layout the text and icon
+//                             int width = getWidth();
+//                             int height = getHeight();
+//                             FontMetrics fm = getFontMetrics(getFont());
+//                             Insets i = getInsets();
+//                             viewRect.setBounds(i.left, i.top, width - i.right - i.left, height - i.bottom - i.top);
+//                             textRect.setBounds(0, 0, 0, 0);
+//                             iconRect.setBounds(0, 0, 0, 0);
+//                             SwingUtilities.layoutCompoundLabel(
+//                                 this, fm, getText(), icon,
+//                                 getVerticalAlignment(), getHorizontalAlignment(),
+//                                 getVerticalTextPosition(), getHorizontalTextPosition(),
+//                                 viewRect, iconRect, textRect,
+//                                 getIconTextGap());
+//                             return iconRect.contains(x, y);
+//                         } else {
+//                             return super.contains(x, y);
+//                         }
+//                     }
+//                     @Override public void updateUI() {
+//                         super.updateUI();
+//                         setOpaque(false);
+//                         setFocusable(false);
+//                     }
+//                 };
+//                 setTabComponentAt(getTabCount() - 1, check);
                 JCheckBox check = new JCheckBox();
                 check.setOpaque(false);
                 check.setFocusable(false);
@@ -71,24 +72,26 @@ public final class MainPanel extends JPanel {
                 p.add(check, BorderLayout.WEST);
                 p.add(new JLabel(title));
                 setTabComponentAt(getTabCount() - 1, p);
-                //*/
             }
         };
         tabs.addTab("JTree", new JScrollPane(new JTree()));
         tabs.addTab("JLabel", new JLabel("aaaaaaaaaaaaa"));
 
+        JButton button = new JButton("Add");
+        button.addActionListener(e -> addTab(tabs));
+
         add(tabs);
-        add(new JButton(new AbstractAction("Add") {
-            private int count;
-            @Override public void actionPerformed(ActionEvent e) {
-                JComponent c = count % 2 == 0 ? new JTree() : new JLabel("Tab" + count);
-                tabs.addTab("Title" + count, c);
-                tabs.setSelectedIndex(tabs.getTabCount() - 1);
-                count++;
-            }
-        }), BorderLayout.SOUTH);
+        add(button, BorderLayout.SOUTH);
         setPreferredSize(new Dimension(320, 240));
     }
+
+    protected void addTab(JTabbedPane tabs) {
+        JComponent c = count % 2 == 0 ? new JTree() : new JLabel("Tab" + count);
+        tabs.addTab("Title" + count, c);
+        tabs.setSelectedIndex(tabs.getTabCount() - 1);
+        count++;
+    }
+
     public static void main(String... args) {
         EventQueue.invokeLater(new Runnable() {
             @Override public void run() {
