91f22c20c4f8ddc6eed3f53e46dd4d02cd124157
aterai <aterai@outlook.com>
2017-09-02 20:26:38 +0900

    refactor: use ItemListener instead of ActionListener for JRadioButton

--- DisableDefaultIcon/src/java/example/MainPanel.java
+++ DisableDefaultIcon/src/java/example/MainPanel.java
@@ -11,50 +11,55 @@ import javax.swing.*;
 
 public final class MainPanel extends JPanel {
     private final URL url = getClass().getResource("16x16transparent.png");
+
     public MainPanel() {
         super(new BorderLayout());
-        final JRadioButton r1 = new JRadioButton("img=null");
-        final JRadioButton r2 = new JRadioButton("img=new ImageIcon(\"\").getImage()");
-        final JRadioButton r3 = new JRadioButton("img=new BufferedImage(1, 1, TYPE_INT_ARGB)");
-        final JRadioButton r4 = new JRadioButton("img=toolkit.createImage(url_16x16transparent)", true);
 
-        ActionListener al = e -> {
-            AbstractButton b = (AbstractButton) e.getSource();
-            Image image = null;
-            if (b.equals(r2)) {
-                //JDK 1.5
-                image = new ImageIcon("").getImage();
-            } else if (b.equals(r3)) {
-                //size=(1x1)
-                image = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
-            } else if (b.equals(r4)) {
-                //16x16transparent.png
-                image = Toolkit.getDefaultToolkit().createImage(url);
-            }
-            Container c = b.getTopLevelAncestor();
-            if (c instanceof JFrame) {
-                ((JFrame) c).setIconImage(image);
-            }
-        };
-        EventQueue.invokeLater(() -> {
-            Image image = Toolkit.getDefaultToolkit().createImage(url);
-            Container c = getTopLevelAncestor();
-            if (c instanceof JFrame) {
-                ((JFrame) c).setIconImage(image);
+        JRadioButton r1 = new JRadioButton("img=null");
+        r1.addItemListener(e -> {
+            if (e.getStateChange() == ItemEvent.SELECTED) {
+                setIconImage(null);
             }
         });
+        JRadioButton r2 = new JRadioButton("img=new ImageIcon(\"\").getImage()");
+        r2.addItemListener(e -> {
+            if (e.getStateChange() == ItemEvent.SELECTED) {
+                setIconImage(new ImageIcon("").getImage()); // JDK 1.5
+            }
+        });
+
+        JRadioButton r3 = new JRadioButton("img=new BufferedImage(1, 1, TYPE_INT_ARGB)");
+        r3.addItemListener(e -> {
+            if (e.getStateChange() == ItemEvent.SELECTED) {
+                setIconImage(new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB)); // size=(1x1)
+            }
+        });
+
+        JRadioButton r4 = new JRadioButton("img=toolkit.createImage(url_16x16transparent)", true);
+        r4.addItemListener(e -> {
+            if (e.getStateChange() == ItemEvent.SELECTED) {
+                setIconImage(Toolkit.getDefaultToolkit().createImage(url)); // 16x16transparent.png
+            }
+        });
+
+        EventQueue.invokeLater(() -> setIconImage(Toolkit.getDefaultToolkit().createImage(url)));
 
         Box box = Box.createVerticalBox();
         box.setBorder(BorderFactory.createTitledBorder("frame.setIconImage(img)"));
         ButtonGroup bg = new ButtonGroup();
         for (AbstractButton b: Arrays.asList(r1, r2, r3, r4)) {
-            b.addActionListener(al);
             bg.add(b);
             box.add(b);
             box.add(Box.createVerticalStrut(5));
         }
         add(box);
         setPreferredSize(new Dimension(320, 240));
+    }
+    protected void setIconImage(Image image) {
+        Container c = getTopLevelAncestor();
+        if (c instanceof JFrame) {
+            ((JFrame) c).setIconImage(image);
+        }
     }
     public static void main(String... args) {
         EventQueue.invokeLater(new Runnable() {
