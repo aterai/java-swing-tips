614e0812f20dd8baedf7564422c8ead29d48eea2
aterai <aterai@outlook.com>
2017-09-03 14:58:30 +0900

    refactor: use ItemListener instead of ActionListener for JRadioButton

--- DividerLocation/src/java/example/MainPanel.java
+++ DividerLocation/src/java/example/MainPanel.java
@@ -9,42 +9,53 @@ import javax.swing.*;
 import javax.swing.table.*;
 
 public final class MainPanel extends JPanel {
-    private final JRadioButton r0 = new JRadioButton("0.0", true);
-    private final JRadioButton r1 = new JRadioButton("0.5");
-    private final JRadioButton r2 = new JRadioButton("1.0");
-    private final String[] columnNames = {"String", "Integer", "Boolean"};
-    private final Object[][] data = {
-        {"aaa", 12, true}, {"bbb", 5, false},
-        {"CCC", 92, true}, {"DDD", 0, false}
-    };
-    private final TableModel model = new DefaultTableModel(data, columnNames) {
-        @Override public Class<?> getColumnClass(int column) {
-            return getValueAt(0, column).getClass();
-        }
-    };
-    private final JSplitPane sp = new JSplitPane(JSplitPane.VERTICAL_SPLIT, new JScrollPane(new JTable(model)), new JScrollPane(new JTree()));
-    public MainPanel() {
+    private MainPanel() {
         super(new BorderLayout());
-        ActionListener al = e -> {
-            if (r2.isSelected()) {
-                sp.setResizeWeight(1d);
-            } else if (r1.isSelected()) {
-                sp.setResizeWeight(.5);
-            } else {
-                sp.setResizeWeight(0d);
+
+        String[] columnNames = {"String", "Integer", "Boolean"};
+        Object[][] data = {
+            {"aaa", 12, true}, {"bbb", 5, false},
+            {"CCC", 92, true}, {"DDD", 0, false}
+        };
+        TableModel model = new DefaultTableModel(data, columnNames) {
+            @Override public Class<?> getColumnClass(int column) {
+                return getValueAt(0, column).getClass();
             }
         };
+        JSplitPane sp = new JSplitPane(JSplitPane.VERTICAL_SPLIT, new JScrollPane(new JTable(model)), new JScrollPane(new JTree()));
+
+        JRadioButton r0 = new JRadioButton("0.0", true);
+        r0.addItemListener(e -> {
+            if (e.getStateChange() == ItemEvent.SELECTED) {
+                sp.setResizeWeight(0d);
+            }
+        });
+
+        JRadioButton r1 = new JRadioButton("0.5");
+        r1.addItemListener(e -> {
+            if (e.getStateChange() == ItemEvent.SELECTED) {
+                sp.setResizeWeight(.5);
+            }
+        });
+
+        JRadioButton r2 = new JRadioButton("1.0");
+        r2.addItemListener(e -> {
+            if (e.getStateChange() == ItemEvent.SELECTED) {
+                sp.setResizeWeight(1d);
+            }
+        });
+
         ButtonGroup bg = new ButtonGroup();
         JPanel p = new JPanel();
         p.add(new JLabel("JSplitPane#setResizeWeight: "));
         for (JRadioButton r: Arrays.asList(r0, r1, r2)) {
-            r.addActionListener(al);
             bg.add(r);
             p.add(r);
         }
         add(p, BorderLayout.NORTH);
         add(sp);
         setPreferredSize(new Dimension(320, 240));
+
         EventQueue.invokeLater(() -> {
             sp.setDividerLocation(.5);
             //sp.setResizeWeight(.5);
