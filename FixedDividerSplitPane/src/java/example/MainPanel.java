921d2e50cedac9e42baa66c30b54e0b101028d9b
aterai <aterai@outlook.com>
2017-09-04 15:33:30 +0900

    add: FixedDividerSplitPane

---
+++ FixedDividerSplitPane/src/java/example/MainPanel.java
@@ -0,0 +1,61 @@
+package example;
+//-*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+//@homepage@
+import java.awt.*;
+import javax.swing.*;
+
+public final class MainPanel extends JPanel {
+    private MainPanel() {
+        super(new BorderLayout());
+
+        JSplitPane sp = new JSplitPane(JSplitPane.VERTICAL_SPLIT, new JScrollPane(new JTree()), new JScrollPane(new JTextArea()));
+        sp.setOneTouchExpandable(true);
+
+        JCheckBox check1 = new JCheckBox("setEnabled(...)", true);
+        check1.addActionListener(e -> sp.setEnabled(((JCheckBox) e.getSource()).isSelected()));
+
+        int dividerSize = UIManager.getInt("SplitPane.dividerSize");
+        JCheckBox check2 = new JCheckBox("setDividerSize(0)");
+        check2.addActionListener(e -> sp.setDividerSize(((JCheckBox) e.getSource()).isSelected() ? 0 : dividerSize));
+
+//         //TEST:
+//         Component divider = ((BasicSplitPaneUI) sp.getUI()).getDivider();
+//         JCheckBox check3 = new JCheckBox("Divider#setEnabled(...)", true);
+//         check3.addActionListener(e -> divider.setEnabled(((JCheckBox) e.getSource()).isSelected()));
+
+        JPanel p = new JPanel(new GridLayout(1, 0));
+        p.setBorder(BorderFactory.createTitledBorder("JSplitPane"));
+        p.add(check1);
+        p.add(check2);
+
+        add(p, BorderLayout.NORTH);
+        add(sp);
+        setPreferredSize(new Dimension(320, 240));
+
+        EventQueue.invokeLater(() -> {
+            sp.setDividerLocation(.5);
+        });
+    }
+    public static void main(String... args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+    public static void createAndShowGUI() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException
+               | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        }
+        JFrame frame = new JFrame("@title@");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new MainPanel());
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
