3ef0523cfbca2e13e7c8d8c3e57c425bb2f6b340
aterai <aterai@outlook.com>
2017-09-19 20:11:16 +0900

    CheckStyle: use SuppressWarningsFilter:@SuppressWarnings(checkstyle:typename) instead of SuppressionCommentFilter://CHECKSTYLE:OFF

--- MenuItemAcceleratorAlignment/src/java/sun/awt/resources/awt_ja.java
+++ MenuItemAcceleratorAlignment/src/java/sun/awt/resources/awt_ja.java
@@ -3,11 +3,9 @@ import java.util.ListResourceBundle;
 //ant package
 //cd target
 //"%JAVA_HOME%\bin\java" -Xbootclasspath/p:example.jar -jar example.jar
-//CHECKSTYLE:OFF
 
-@SuppressWarnings("PMD.ClassNamingConventions") // Class names should begin with an uppercase character
+@SuppressWarnings({"PMD.ClassNamingConventions", "checkstyle:typename"}) // Class names should begin with an uppercase character
 public final class awt_ja extends ListResourceBundle {
-//CHECKSTYLE:ON
     @Override protected Object[][] getContents() {
         System.out.println("---- awt_ja ----");
         return new Object[][] {{"AWT.space", "XXXXX"}};
