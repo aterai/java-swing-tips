41f2dfaa8462de70ed5e1d1ad133925b05aca570
aterai <aterai@outlook.com>
2017-11-14 15:43:54 +0900

    refactor: use InputEvent.MOUSE_EVENT_MASK instead of BUTTON1_MASK

--- UpdateComboBoxItem/src/java/example/MainPanel.java
+++ UpdateComboBoxItem/src/java/example/MainPanel.java
@@ -154,7 +154,7 @@ class CheckedComboBox<E extends CheckableItem> extends JComboBox<E> {
         removeActionListener(listener);
         super.updateUI();
         listener = e -> {
-            if (e.getModifiers() == InputEvent.BUTTON1_MASK) {
+            if ((e.getModifiers() & InputEvent.MOUSE_EVENT_MASK) != 0) {
                 updateItem(getSelectedIndex());
                 keepOpen = true;
             }
