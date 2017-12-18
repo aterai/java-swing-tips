85c4f9cb37bb3dcecead5d5c7b38dbc289769a6e
aterai <aterai@outlook.com>
2017-12-18 14:44:37 +0900

    add: MultiClickThreshhold

---
+++ MultiClickThreshhold/src/java/example/MainPanel.java
@@ -0,0 +1,72 @@
+package example;
+//-*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+//@homepage@
+import java.awt.*;
+import javax.swing.*;
+
+public final class MainPanel extends JPanel {
+    private int num;
+
+    private MainPanel() {
+        super(new BorderLayout());
+
+        JDesktopPane desktop = new JDesktopPane();
+
+        JButton button = new JButton("add");
+        button.addActionListener(e -> addInternalFrame(desktop));
+
+        long lv = button.getMultiClickThreshhold();
+        JSpinner spinner = new JSpinner(new SpinnerNumberModel(Long.valueOf(lv), Long.valueOf(0), Long.valueOf(10000), Long.valueOf(100)));
+        spinner.addChangeListener(e -> {
+            JSpinner s = (JSpinner) e.getSource();
+            button.setMultiClickThreshhold((long) s.getValue());
+        });
+
+        JMenuBar mb = new JMenuBar();
+        mb.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
+        mb.add(new JLabel("MultiClickThreshhold: "));
+        mb.add(spinner);
+        mb.add(Box.createHorizontalGlue());
+        mb.add(button);
+
+        add(desktop);
+        add(mb, BorderLayout.NORTH);
+        setPreferredSize(new Dimension(320, 240));
+    }
+    private void addInternalFrame(JDesktopPane desktop) {
+        JInternalFrame f = createFrame("#" + num, num * 10, num * 10);
+        desktop.add(f);
+        desktop.getDesktopManager().activateFrame(f);
+        num++;
+    }
+    private static JInternalFrame createFrame(String t, int x, int y) {
+        JInternalFrame f = new JInternalFrame(t, true, true, true, true);
+        f.setSize(200, 100);
+        f.setLocation(x, y);
+        f.setVisible(true);
+        return f;
+    }
+    public static void main(String... args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+    public static void createAndShowGUI() {
+        try {
+            System.out.println(UIManager.get("OptionPane.buttonClickThreshhold"));
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (ClassNotFoundException | InstantiationException
+               | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        }
+        JFrame frame = new JFrame("@title@");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new MainPanel());
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
