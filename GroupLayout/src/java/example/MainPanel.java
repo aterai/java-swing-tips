7f176d7c56e9c63cc9895dd3cf1d17c88460d848
aterai <aterai@outlook.com>
2017-12-24 05:05:27 +0900

    CheckStyle: LocalVariableName

--- GroupLayout/src/java/example/MainPanel.java
+++ GroupLayout/src/java/example/MainPanel.java
@@ -12,15 +12,15 @@ public final class MainPanel extends JPanel {
     private final JTextField tf2 = new JTextField();
     private final JTextField tf3 = new JTextField();
     private final JTextField tf4 = new JTextField();
-    private final JLabel label1  = new JLabel("0123456789_0123456789abc:");
-    private final JLabel label2  = new JLabel("GroupLayout:");
-    private final JLabel label3  = new JLabel("0123456789_0123456789abc:");
-    private final JLabel label4  = new JLabel("GridBagLayout:");
+    private final JLabel label1 = new JLabel("0123456789_0123456789abc:");
+    private final JLabel label2 = new JLabel("GroupLayout:");
+    private final JLabel label3 = new JLabel("0123456789_0123456789abc:");
+    private final JLabel label4 = new JLabel("GridBagLayout:");
 
-    public MainPanel() {
+    private MainPanel() {
         super(new GridLayout(2, 1));
 
-        //GroupLayout
+        // GroupLayout
         JPanel p1 = new JPanel();
         p1.setBorder(BorderFactory.createTitledBorder("GroupLayout"));
         GroupLayout layout = new GroupLayout(p1);
@@ -28,33 +28,33 @@ public final class MainPanel extends JPanel {
         layout.setAutoCreateGaps(true);
         layout.setAutoCreateContainerGaps(true);
 
-        GroupLayout.SequentialGroup hGroup = layout.createSequentialGroup();
+        GroupLayout.SequentialGroup hgroup = layout.createSequentialGroup();
 
-        hGroup.addGroup(layout.createParallelGroup().addComponent(label1).addComponent(label2));
-        hGroup.addGroup(layout.createParallelGroup().addComponent(tf1).addComponent(tf2));
-        layout.setHorizontalGroup(hGroup);
+        hgroup.addGroup(layout.createParallelGroup().addComponent(label1).addComponent(label2));
+        hgroup.addGroup(layout.createParallelGroup().addComponent(tf1).addComponent(tf2));
+        layout.setHorizontalGroup(hgroup);
 
-        GroupLayout.SequentialGroup vGroup = layout.createSequentialGroup();
-        vGroup.addGroup(layout.createParallelGroup(Alignment.BASELINE).addComponent(label1).addComponent(tf1));
-        vGroup.addGroup(layout.createParallelGroup(Alignment.BASELINE).addComponent(label2).addComponent(tf2));
-        layout.setVerticalGroup(vGroup);
+        GroupLayout.SequentialGroup vgroup = layout.createSequentialGroup();
+        vgroup.addGroup(layout.createParallelGroup(Alignment.BASELINE).addComponent(label1).addComponent(tf1));
+        vgroup.addGroup(layout.createParallelGroup(Alignment.BASELINE).addComponent(label2).addComponent(tf2));
+        layout.setVerticalGroup(vgroup);
 
-        //GridBagLayout
+        // GridBagLayout
         JPanel p2 = new JPanel(new GridBagLayout());
-        Border inside  = BorderFactory.createEmptyBorder(10, 5 + 2, 10, 10 + 2);
+        Border inside = BorderFactory.createEmptyBorder(10, 5 + 2, 10, 10 + 2);
         Border outside = BorderFactory.createTitledBorder("GridBagLayout");
         p2.setBorder(BorderFactory.createCompoundBorder(outside, inside));
         GridBagConstraints c = new GridBagConstraints();
 
-        c.insets  = new Insets(5, 5, 5, 0);
-        c.anchor  = GridBagConstraints.LINE_START;
-        c.gridx   = 0;
+        c.insets = new Insets(5, 5, 5, 0);
+        c.anchor = GridBagConstraints.LINE_START;
+        c.gridx = 0;
         p2.add(label3, c);
         p2.add(label4, c);
 
-        c.fill    = GridBagConstraints.HORIZONTAL;
+        c.fill = GridBagConstraints.HORIZONTAL;
         c.weightx = 1d;
-        c.gridx   = 1;
+        c.gridx = 1;
         p2.add(tf3, c);
         p2.add(tf4, c);
 
