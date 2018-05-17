05986b613de93413f2df6b3924ed58258a53f261
aterai <aterai@outlook.com>
2018-05-17 16:17:11 +0900

    refactor: use StreamSupport.stream(...) instead of while(...)

--- ExtractFramesFromAnimatedGif/src/java/example/MainPanel.java
+++ ExtractFramesFromAnimatedGif/src/java/example/MainPanel.java
@@ -9,6 +9,7 @@ import java.net.URISyntaxException;
 import java.nio.file.*;
 import java.util.*;
 import java.util.List;
+import java.util.stream.StreamSupport;
 import javax.imageio.*;
 import javax.imageio.stream.*;
 import javax.swing.*;
@@ -36,29 +37,54 @@ public final class MainPanel extends JPanel {
         add(new JScrollPane(p));
         setPreferredSize(new Dimension(320, 240));
     }
-    // https://community.oracle.com/thread/1271862 Reading gif animation frame rates and such?
-    private List<BufferedImage> loadFromStream(ImageInputStream imageStream) throws IOException {
-        String format = "gif";
-        String meta = "javax_imageio_gif_image_1.0";
-        ImageReader reader = null;
-        Iterator<ImageReader> readers = ImageIO.getImageReaders(imageStream);
-        while (readers.hasNext()) {
-            reader = readers.next();
-            String metaFormat = reader.getOriginatingProvider().getNativeImageMetadataFormatName();
-            if (format.equalsIgnoreCase(reader.getFormatName()) && meta.equals(metaFormat)) {
-                break;
-            }
-        }
-        ImageReader imageReader = Objects.requireNonNull(reader, "Can not read image format!");
-        boolean isGif = format.equalsIgnoreCase(imageReader.getFormatName());
-        imageReader.setInput(imageStream, false, !isGif);
+    // // https://community.oracle.com/thread/1271862 Reading gif animation frame rates and such?
+    // private List<BufferedImage> loadFromStream(ImageInputStream imageStream) throws IOException {
+    //     String format = "gif";
+    //     String meta = "javax_imageio_gif_image_1.0";
+    //     ImageReader reader = null;
+    //     Iterator<ImageReader> readers = ImageIO.getImageReaders(imageStream);
+    //     while (readers.hasNext()) {
+    //         reader = readers.next();
+    //         String metaFormat = reader.getOriginatingProvider().getNativeImageMetadataFormatName();
+    //         if (format.equalsIgnoreCase(reader.getFormatName()) && meta.equals(metaFormat)) {
+    //             break;
+    //         }
+    //     }
+    //     ImageReader imageReader = Objects.requireNonNull(reader, "Can not read image format!");
+    //     boolean isGif = format.equalsIgnoreCase(imageReader.getFormatName());
+    //     imageReader.setInput(imageStream, false, !isGif);
+    //     List<BufferedImage> list = new ArrayList<>();
+    //     for (int i = 0; i < imageReader.getNumImages(true); i++) {
+    //         IIOImage frame = imageReader.readAll(i, null);
+    //         list.add((BufferedImage) frame.getRenderedImage());
+    //     }
+    //     imageReader.dispose();
+    //     return list;
+    // }
+    private static List<BufferedImage> loadFromStream(ImageInputStream imageStream) throws IOException {
+        Iterable<ImageReader> it = () -> ImageIO.getImageReaders(imageStream);
+        ImageReader reader = StreamSupport.stream(it.spliterator(), false)
+            .filter(MainPanel::checkGifFormat)
+            .findFirst()
+            .orElseThrow(() -> new IOException("Can not read image format!"));
+        reader.setInput(imageStream, false, false);
         List<BufferedImage> list = new ArrayList<>();
-        for (int i = 0; i < imageReader.getNumImages(true); i++) {
-            IIOImage frame = imageReader.readAll(i, null);
+        for (int i = 0; i < reader.getNumImages(true); i++) {
+            IIOImage frame = reader.readAll(i, null);
             list.add((BufferedImage) frame.getRenderedImage());
         }
-        imageReader.dispose();
+        reader.dispose();
         return list;
+    }
+    private static boolean checkGifFormat(ImageReader reader) {
+        String metaFormat = reader.getOriginatingProvider().getNativeImageMetadataFormatName();
+        String name;
+        try {
+            name = reader.getFormatName();
+        } catch (IOException ex) {
+            name = "";
+        }
+        return "gif".equalsIgnoreCase(name) && "javax_imageio_gif_image_1.0".equals(metaFormat);
     }
     public static void main(String... args) {
         EventQueue.invokeLater(new Runnable() {
