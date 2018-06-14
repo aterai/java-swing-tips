6e08f6b0619420189296627cafea201ab95dcc8a
aterai <aterai@outlook.com>
2018-06-14 11:50:00 +0900

    refactor: use the delegation pattern TableCellEditor instead of inheritence

--- MultipleButtonsInTableCell/src/java/example/MainPanel.java
+++ MultipleButtonsInTableCell/src/java/example/MainPanel.java
@@ -7,17 +7,12 @@ import java.awt.event.*;
 import java.util.*;
 import java.util.List;
 import javax.swing.*;
-import javax.swing.event.*;
 import javax.swing.table.*;
 
 public final class MainPanel extends JPanel {
-    public MainPanel() {
+    private MainPanel() {
         super(new BorderLayout());
-        add(new JScrollPane(makeTable()));
-        setBorder(BorderFactory.createTitledBorder("Multiple Buttons in a Table Cell"));
-        setPreferredSize(new Dimension(320, 240));
-    }
-    private JTable makeTable() {
+
         String empty = "";
         String[] columnNames = {"String", "Button"};
         Object[][] data = {
@@ -28,15 +23,21 @@ public final class MainPanel extends JPanel {
                 return getValueAt(0, column).getClass();
             }
         };
-        JTable table = new JTable(model);
-        table.setRowHeight(36);
-        table.setAutoCreateRowSorter(true);
-        // table.addMouseListener(new CellButtonsMouseListener());
-        // ButtonsEditorRenderer er = new ButtonsEditorRenderer(table);
-        TableColumn column = table.getColumnModel().getColumn(1);
-        column.setCellRenderer(new ButtonsRenderer());
-        column.setCellEditor(new ButtonsEditor(table));
-        return table;
+        JTable table = new JTable(model) {
+            @Override public void updateUI() {
+                super.updateUI();
+                setRowHeight(36);
+                setAutoCreateRowSorter(true);
+
+                TableColumn column = getColumnModel().getColumn(1);
+                column.setCellRenderer(new ButtonsRenderer());
+                column.setCellEditor(new ButtonsEditor(this));
+            }
+        };
+
+        add(new JScrollPane(table));
+        setBorder(BorderFactory.createTitledBorder("Multiple Buttons in a Table Cell"));
+        setPreferredSize(new Dimension(320, 240));
     }
 
     public static void main(String... args) {
@@ -91,19 +92,18 @@ class ButtonsPanel extends JPanel {
             add(b);
         }
     }
-//     @Override public void updateUI() {
-//         super.updateUI();
-//     }
 }
 
-class ButtonsRenderer extends ButtonsPanel implements TableCellRenderer {
-    @Override public void updateUI() {
-        super.updateUI();
-        setName("Table.cellRenderer");
-    }
+class ButtonsRenderer implements TableCellRenderer {
+    private final ButtonsPanel panel = new ButtonsPanel() {
+        @Override public void updateUI() {
+            super.updateUI();
+            setName("Table.cellRenderer");
+        }
+    };
     @Override public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-        this.setBackground(isSelected ? table.getSelectionBackground() : table.getBackground());
-        return this;
+        panel.setBackground(isSelected ? table.getSelectionBackground() : table.getBackground());
+        return panel;
     }
 }
 
@@ -132,8 +132,9 @@ class EditAction extends AbstractAction {
     }
 }
 
-class ButtonsEditor extends ButtonsPanel implements TableCellEditor {
-    protected transient ChangeEvent changeEvent;
+// delegation pattern
+class ButtonsEditor extends AbstractCellEditor implements TableCellEditor {
+    protected final ButtonsPanel panel = new ButtonsPanel();
     protected final JTable table;
     private class EditingStopHandler extends MouseAdapter implements ActionListener {
         @Override public void mousePressed(MouseEvent e) {
@@ -144,7 +145,7 @@ class ButtonsEditor extends ButtonsPanel implements TableCellEditor {
                 // DEBUG: view button click -> control key down + edit button(same cell) press -> remain selection color
                 ButtonModel m = ((JButton) e.getComponent()).getModel();
                 if (m.isPressed() && table.isRowSelected(table.getEditingRow()) && e.isControlDown()) {
-                    setBackground(table.getBackground());
+                    panel.setBackground(table.getBackground());
                 }
             }
         }
@@ -155,77 +156,120 @@ class ButtonsEditor extends ButtonsPanel implements TableCellEditor {
     protected ButtonsEditor(JTable table) {
         super();
         this.table = table;
-        buttons.get(0).setAction(new ViewAction(table));
-        buttons.get(1).setAction(new EditAction(table));
+        panel.buttons.get(0).setAction(new ViewAction(table));
+        panel.buttons.get(1).setAction(new EditAction(table));
 
         EditingStopHandler handler = new EditingStopHandler();
-        for (JButton b: buttons) {
+        for (JButton b: panel.buttons) {
             b.addMouseListener(handler);
             b.addActionListener(handler);
         }
-        addMouseListener(handler);
+        panel.addMouseListener(handler);
     }
     @Override public Component getTableCellEditorComponent(JTable tbl, Object value, boolean isSelected, int row, int column) {
-        this.setBackground(tbl.getSelectionBackground());
-        return this;
+        panel.setBackground(tbl.getSelectionBackground());
+        return panel;
     }
     @Override public Object getCellEditorValue() {
         return "";
     }
-
-    // Copied from AbstractCellEditor
-    // protected EventListenerList listenerList = new EventListenerList();
-    // protected transient ChangeEvent changeEvent;
-    @Override public boolean isCellEditable(EventObject e) {
-        return true;
-    }
-    @Override public boolean shouldSelectCell(EventObject anEvent) {
-        return true;
-    }
-    @Override public boolean stopCellEditing() {
-        fireEditingStopped();
-        return true;
-    }
-    @Override public void cancelCellEditing() {
-        fireEditingCanceled();
-    }
-    @Override public void addCellEditorListener(CellEditorListener l) {
-        listenerList.add(CellEditorListener.class, l);
-    }
-    @Override public void removeCellEditorListener(CellEditorListener l) {
-        listenerList.remove(CellEditorListener.class, l);
-    }
-    public CellEditorListener[] getCellEditorListeners() {
-        return listenerList.getListeners(CellEditorListener.class);
-    }
-    protected void fireEditingStopped() {
-        // Guaranteed to return a non-null array
-        Object[] listeners = listenerList.getListenerList();
-        // Process the listeners last to first, notifying
-        // those that are interested in this event
-        for (int i = listeners.length - 2; i >= 0; i -= 2) {
-            if (listeners[i] == CellEditorListener.class) {
-                // Lazily create the event:
-                if (Objects.isNull(changeEvent)) {
-                    changeEvent = new ChangeEvent(this);
-                }
-                ((CellEditorListener) listeners[i + 1]).editingStopped(changeEvent);
-            }
-        }
-    }
-    protected void fireEditingCanceled() {
-        // Guaranteed to return a non-null array
-        Object[] listeners = listenerList.getListenerList();
-        // Process the listeners last to first, notifying
-        // those that are interested in this event
-        for (int i = listeners.length - 2; i >= 0; i -= 2) {
-            if (listeners[i] == CellEditorListener.class) {
-                // Lazily create the event:
-                if (Objects.isNull(changeEvent)) {
-                    changeEvent = new ChangeEvent(this);
-                }
-                ((CellEditorListener) listeners[i + 1]).editingCanceled(changeEvent);
-            }
-        }
-    }
 }
+
+// // inheritence to extend a class
+// class ButtonsEditor extends ButtonsPanel implements TableCellEditor {
+//     protected transient ChangeEvent changeEvent;
+//     protected final JTable table;
+//     private class EditingStopHandler extends MouseAdapter implements ActionListener {
+//         @Override public void mousePressed(MouseEvent e) {
+//             Object o = e.getSource();
+//             if (o instanceof TableCellEditor) {
+//                 actionPerformed(null);
+//             } else if (o instanceof JButton) {
+//                 // DEBUG: view button click -> control key down + edit button(same cell) press -> remain selection color
+//                 ButtonModel m = ((JButton) e.getComponent()).getModel();
+//                 if (m.isPressed() && table.isRowSelected(table.getEditingRow()) && e.isControlDown()) {
+//                     setBackground(table.getBackground());
+//                 }
+//             }
+//         }
+//         @Override public void actionPerformed(ActionEvent e) {
+//             EventQueue.invokeLater(() -> fireEditingStopped());
+//         }
+//     }
+//     protected ButtonsEditor(JTable table) {
+//         super();
+//         this.table = table;
+//         buttons.get(0).setAction(new ViewAction(table));
+//         buttons.get(1).setAction(new EditAction(table));
+//
+//         EditingStopHandler handler = new EditingStopHandler();
+//         for (JButton b: buttons) {
+//             b.addMouseListener(handler);
+//             b.addActionListener(handler);
+//         }
+//         addMouseListener(handler);
+//     }
+//     @Override public Component getTableCellEditorComponent(JTable tbl, Object value, boolean isSelected, int row, int column) {
+//         this.setBackground(tbl.getSelectionBackground());
+//         return this;
+//     }
+//     @Override public Object getCellEditorValue() {
+//         return "";
+//     }
+//
+//     // Copied from AbstractCellEditor
+//     // protected EventListenerList listenerList = new EventListenerList();
+//     // protected transient ChangeEvent changeEvent;
+//     @Override public boolean isCellEditable(EventObject e) {
+//         return true;
+//     }
+//     @Override public boolean shouldSelectCell(EventObject anEvent) {
+//         return true;
+//     }
+//     @Override public boolean stopCellEditing() {
+//         fireEditingStopped();
+//         return true;
+//     }
+//     @Override public void cancelCellEditing() {
+//         fireEditingCanceled();
+//     }
+//     @Override public void addCellEditorListener(CellEditorListener l) {
+//         listenerList.add(CellEditorListener.class, l);
+//     }
+//     @Override public void removeCellEditorListener(CellEditorListener l) {
+//         listenerList.remove(CellEditorListener.class, l);
+//     }
+//     public CellEditorListener[] getCellEditorListeners() {
+//         return listenerList.getListeners(CellEditorListener.class);
+//     }
+//     protected void fireEditingStopped() {
+//         // Guaranteed to return a non-null array
+//         Object[] listeners = listenerList.getListenerList();
+//         // Process the listeners last to first, notifying
+//         // those that are interested in this event
+//         for (int i = listeners.length - 2; i >= 0; i -= 2) {
+//             if (listeners[i] == CellEditorListener.class) {
+//                 // Lazily create the event:
+//                 if (Objects.isNull(changeEvent)) {
+//                     changeEvent = new ChangeEvent(this);
+//                 }
+//                 ((CellEditorListener) listeners[i + 1]).editingStopped(changeEvent);
+//             }
+//         }
+//     }
+//     protected void fireEditingCanceled() {
+//         // Guaranteed to return a non-null array
+//         Object[] listeners = listenerList.getListenerList();
+//         // Process the listeners last to first, notifying
+//         // those that are interested in this event
+//         for (int i = listeners.length - 2; i >= 0; i -= 2) {
+//             if (listeners[i] == CellEditorListener.class) {
+//                 // Lazily create the event:
+//                 if (Objects.isNull(changeEvent)) {
+//                     changeEvent = new ChangeEvent(this);
+//                 }
+//                 ((CellEditorListener) listeners[i + 1]).editingCanceled(changeEvent);
+//             }
+//         }
+//     }
+// }
