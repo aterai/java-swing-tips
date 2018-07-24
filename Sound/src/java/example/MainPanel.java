ca2b5cd560f27baa2e8d9890319ab700a80695b9
aterai <aterai@outlook.com>
2018-07-24 14:41:04 +0900

    Checkstyle: AvoidStarImport(javax.sound.sampled)

--- Sound/src/java/example/MainPanel.java
+++ Sound/src/java/example/MainPanel.java
@@ -3,8 +3,14 @@ package example;
 // vim:set fileencoding=utf-8:
 // @homepage@
 import java.awt.*;
-import java.io.*;
-import javax.sound.sampled.*;
+import java.io.IOException;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.*;
 
 public final class MainPanel extends JPanel {
