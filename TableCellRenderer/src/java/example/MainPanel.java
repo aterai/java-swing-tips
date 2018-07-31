6880be657e1c3f608e938e57edf23b133f7dd2a8
aterai <aterai@outlook.com>
2018-07-31 15:37:57 +0900

    Checkstyle: AvoidStarImport(java.awt.font)

--- TableCellRenderer/src/java/example/MainPanel.java
+++ TableCellRenderer/src/java/example/MainPanel.java
@@ -3,8 +3,8 @@ package example;
 // vim:set fileencoding=utf-8:
 // @homepage@
 import java.awt.*;
-import java.awt.font.FontMetrics;
 import java.awt.font.FontRenderContext;
+import java.awt.font.GlyphMetrics;
 import java.awt.font.GlyphVector;
 import java.awt.geom.*;
 import java.util.Objects;
