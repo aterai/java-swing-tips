b52697f120478ab1a787df99a962c7b99fa52d2a
aterai <aterai@outlook.com>
2018-08-19 16:57:18 +0900

    Checkstyle: AvoidStarImport(java.util)

--- TriangleTickSlider/src/java/example/MainPanel.java
+++ TriangleTickSlider/src/java/example/MainPanel.java
@@ -3,7 +3,9 @@ package example;
 // vim:set fileencoding=utf-8:
 // @homepage@
 import java.awt.*;
-import java.util.*;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.Objects;
 import javax.swing.*;
 
 public final class MainPanel extends JPanel {
