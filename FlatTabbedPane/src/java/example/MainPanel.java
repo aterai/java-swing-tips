0604ffec28847b17f26f254e6e2b1ae927a78772
aterai <aterai@outlook.com>
2018-08-20 16:27:42 +0900

    add: FlatTabbedPane

---
+++ FlatTabbedPane/src/java/example/MainPanel.java
@@ -0,0 +1,75 @@
+package example;
+// -*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+// @homepage@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicTabbedPaneUI;
+
+public final class MainPanel extends JPanel {
+    public static final Color SELECTED_BG = new Color(255, 150, 0);
+    public static final Color UNSELECTED_BG = new Color(255, 50, 0);
+    private MainPanel() {
+        super(new BorderLayout());
+        UIManager.put("TabbedPane.tabInsets", new Insets(5, 10, 5, 10));
+        UIManager.put("TabbedPane.selectedLabelShift", 0);
+        UIManager.put("TabbedPane.labelShift", 0);
+
+        // UIManager.put("TabbedPane.foreground", Color.WHITE);
+        // UIManager.put("TabbedPane.selectedForeground", Color.WHITE);
+        // UIManager.put("TabbedPane.unselectedBackground", UNSELECTED_BG);
+        UIManager.put("TabbedPane.tabAreaBackground", UNSELECTED_BG);
+
+        JTabbedPane tabs = new JTabbedPane() {
+            @Override public void updateUI() {
+                super.updateUI();
+                setUI(new BasicTabbedPaneUI() {
+                    @Override protected void paintFocusIndicator(Graphics g, int tabPlacement, Rectangle[] rects, int tabIndex, Rectangle iconRect, Rectangle textRect, boolean isSelected) {
+                        // Do not paint anything
+                    }
+                    @Override protected void paintTabBorder(Graphics g, int tabPlacement, int tabIndex, int x, int y, int w, int h, boolean isSelected) {
+                        // Do not paint anything
+                    }
+                    @Override  protected void paintTabBackground(Graphics g, int tabPlacement, int tabIndex, int x, int y, int w, int h, boolean isSelected) {
+                        g.setColor(isSelected ? SELECTED_BG : UNSELECTED_BG);
+                        g.fillRect(x - 1, y, w + 1, h); // -1: default labelShift? magic number
+                    }
+                });
+                setOpaque(true);
+                setForeground(Color.WHITE);
+                setTabPlacement(SwingConstants.LEFT);
+                setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+            }
+        };
+
+        // http://www.icongalore.com/ XP Style Icons - Windows Application Icon, Software XP Icons
+        tabs.addTab("A", new ImageIcon(getClass().getResource("wi0009-32.png")), new JScrollPane(new JTree()));
+        tabs.addTab("B", new ImageIcon(getClass().getResource("wi0054-32.png")), new JScrollPane(new JTextArea()));
+        tabs.addTab("C", new ImageIcon(getClass().getResource("wi0062-32.png")), new JScrollPane(new JTree()));
+        tabs.addTab("D", new ImageIcon(getClass().getResource("wi0063-32.png")), new JScrollPane(new JTextArea()));
+
+        add(tabs);
+        setPreferredSize(new Dimension(320, 240));
+    }
+    public static void main(String... args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override public void run() {
+                createAndShowGui();
+            }
+        });
+    }
+    public static void createAndShowGui() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException
+               | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        }
+        JFrame frame = new JFrame("@title@");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new MainPanel());
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
