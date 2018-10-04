d214ecea8788e67c781852b90e65a9f87febd637
aterai <aterai@outlook.com>
2018-10-04 23:06:20 +0900

    Checkstyle: AvoidStarImport(javax.swing.event)

--- EnumSet/src/java/example/MainPanel.java
+++ EnumSet/src/java/example/MainPanel.java
@@ -12,8 +12,13 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import javax.swing.*;
-import javax.swing.event.*;
-import javax.swing.table.*;
+import javax.swing.event.CellEditorListener;
+import javax.swing.event.ChangeEvent;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellEditor;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableModel;
 
 public final class MainPanel extends JPanel {
     private MainPanel() {
--- VerticalNavigationMenu/src/java/example/MainPanel.java
+++ VerticalNavigationMenu/src/java/example/MainPanel.java
@@ -9,9 +9,16 @@ import java.util.Enumeration;
 import java.util.Objects;
 import java.util.Optional;
 import javax.swing.*;
-import javax.swing.event.*;
+import javax.swing.event.TreeExpansionEvent;
+import javax.swing.event.TreeWillExpandListener;
 import javax.swing.plaf.basic.BasicTreeUI;
-import javax.swing.tree.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.ExpandVetoException;
+import javax.swing.tree.TreeModel;
+import javax.swing.tree.TreePath;
+import javax.swing.tree.TreeSelectionModel;
 
 public final class MainPanel extends JPanel {
     private MainPanel() {
