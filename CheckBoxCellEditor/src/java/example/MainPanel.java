2031e2ead9821e42ac101fb78a777276ab77dc1a
aterai <aterai@outlook.com>
2018-10-22 20:51:30 +0900

    refactor: use JTable#removeEditor() instead of JTable#getCellEditor().stopCellEditing()

--- CheckBoxCellEditor/src/java/example/MainPanel.java
+++ CheckBoxCellEditor/src/java/example/MainPanel.java
@@ -125,15 +125,12 @@ class CheckBoxPanelEditor extends AbstractCellEditor implements TableCellEditor 
             Class<JTable> clz = JTable.class;
             Optional.ofNullable(SwingUtilities.getAncestorOfClass(clz, e.getComponent()))
                 .filter(clz::isInstance).map(clz::cast)
-                .filter(table -> table.isEditing() && !table.getCellEditor().stopCellEditing())
-                .ifPresent(table -> table.getCellEditor().cancelCellEditing());
-            // Container c = SwingUtilities.getAncestorOfClass(JTable.class, e.getComponent());
-            // if (c instanceof JTable) {
-            //     JTable table = (JTable) c;
-            //     if (table.isEditing() && !table.getCellEditor().stopCellEditing()) {
-            //         table.getCellEditor().cancelCellEditing();
-            //     }
-            // }
+                .filter(JTable::isEditing)
+                .ifPresent(JTable::removeEditor);
+            // Optional.ofNullable(SwingUtilities.getAncestorOfClass(clz, e.getComponent()))
+            //     .filter(clz::isInstance).map(clz::cast)
+            //     .filter(table -> table.isEditing() && !table.getCellEditor().stopCellEditing())
+            //     .ifPresent(table -> table.getCellEditor().cancelCellEditing());
         }
     }
 }
