4b054e8f7eff0587838924f7262f1d8de0f2c84c
aterai <aterai@outlook.com>
2018-11-11 16:03:03 +0900

    Checkstyle: AvoidStarImport(java.awt.event)

--- GetAllPopupMenus/src/java/example/MainPanel.java
+++ GetAllPopupMenus/src/java/example/MainPanel.java
@@ -3,7 +3,9 @@ package example;
 // vim:set fileencoding=utf-8:
 // @homepage@
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
--- OverlayBorderLayout/src/java/example/MainPanel.java
+++ OverlayBorderLayout/src/java/example/MainPanel.java
@@ -3,7 +3,8 @@ package example;
 // vim:set fileencoding=utf-8:
 // @homepage@
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
