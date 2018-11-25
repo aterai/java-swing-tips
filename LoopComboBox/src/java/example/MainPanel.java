e3ce56d1057a9726ef2b6a1c989b65d0d7dbe4c1
aterai <aterai@outlook.com>
2018-11-25 17:05:14 +0900

    Checkstyle: AvoidStarImport(java.awt.event)

--- LoopComboBox/src/java/example/MainPanel.java
+++ LoopComboBox/src/java/example/MainPanel.java
@@ -3,7 +3,8 @@ package example;
 // vim:set fileencoding=utf-8:
 // @homepage@
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 import java.util.stream.IntStream;
 import javax.swing.*;
 
