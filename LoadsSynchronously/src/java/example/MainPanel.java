1cc7d41d40521f4a202434d722aa8d1c72a5e72d
aterai <aterai@outlook.com>
2019-01-28 18:14:43 +0900

    PMD: UnusedImports

--- LoadsSynchronously/src/java/example/MainPanel.java
+++ LoadsSynchronously/src/java/example/MainPanel.java
@@ -8,7 +8,6 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.io.UncheckedIOException;
 import java.util.Collections;
 import java.util.stream.IntStream;
 import javax.imageio.ImageIO;
