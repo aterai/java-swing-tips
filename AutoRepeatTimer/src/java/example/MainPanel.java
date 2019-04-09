31dc16ee903254e9d291e26e0c1022641745f464
aterai <aterai@outlook.com>
2019-04-09 16:17:13 +0900

    refactor: use JTextField instead of JLabel

--- AutoRepeatTimer/src/java/example/MainPanel.java
+++ AutoRepeatTimer/src/java/example/MainPanel.java
@@ -12,43 +12,43 @@ import java.awt.event.MouseEvent;
 import java.math.BigInteger;
 import java.util.Objects;
 import javax.swing.*;
+import javax.swing.text.JTextComponent;
 
 public final class MainPanel extends JPanel {
   private MainPanel() {
     super(new GridBagLayout());
 
-    JLabel label = new JLabel("0") {
+    JTextField field = new JTextField("0") {
       @Override public Dimension getMaximumSize() {
         return getPreferredSize();
       }
 
       @Override public Dimension getPreferredSize() {
-        return new Dimension(100, 100);
+        return new Dimension(100, 50);
       }
     };
-    label.setOpaque(true);
-    label.setBackground(Color.WHITE);
-    label.setHorizontalAlignment(SwingConstants.CENTER);
-    label.setFont(label.getFont().deriveFont(30f));
+    field.setHorizontalAlignment(SwingConstants.CENTER);
+    field.setFont(field.getFont().deriveFont(30f));
+    field.setEditable(false);
 
     Box box = Box.createHorizontalBox();
     box.add(Box.createHorizontalGlue());
-    box.add(makeButton(-5, label));
-    box.add(makeButton(-1, label));
-    box.add(label);
-    box.add(makeButton(+1, label));
-    box.add(makeButton(+5, label));
+    box.add(makeButton(-5, field));
+    box.add(makeButton(-1, field));
+    box.add(field);
+    box.add(makeButton(+1, field));
+    box.add(makeButton(+5, field));
     box.add(Box.createHorizontalGlue());
 
     add(box);
     setPreferredSize(new Dimension(320, 240));
   }
 
-  private static JButton makeButton(int extent, JLabel view) {
+  private static JButton makeButton(int extent, JTextField view) {
     String title = String.format("%+d", extent);
     JButton button = new JButton(title) {
       @Override public Dimension getMaximumSize() {
-        return new Dimension(50, 100);
+        return new Dimension(50, 50);
       }
     };
     AutoRepeatHandler handler = new AutoRepeatHandler(extent, view);
@@ -84,10 +84,10 @@ public final class MainPanel extends JPanel {
 class AutoRepeatHandler extends MouseAdapter implements ActionListener {
   private final Timer autoRepeatTimer;
   private final BigInteger extent;
-  private final JLabel view;
+  private final JTextComponent view;
   private JButton arrowButton;
 
-  protected AutoRepeatHandler(int extent, JLabel view) {
+  protected AutoRepeatHandler(int extent, JTextComponent view) {
     super();
     this.extent = BigInteger.valueOf(extent);
     this.view = view;
