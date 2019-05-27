ac89818e903b23169598a17849807c2ef52c8864
aterai <aterai@outlook.com>
2019-05-27 15:22:04 +0900

    add: BorderPaintedFlat

---
+++ BorderPaintedFlat/src/java/example/MainPanel.java
@@ -0,0 +1,129 @@
+// -*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+// @homepage@
+
+package example;
+
+import java.awt.*;
+import javax.swing.*;
+
+public final class MainPanel extends JPanel {
+  private MainPanel() {
+    super(new BorderLayout(5, 5));
+
+    Box box1 = Box.createVerticalBox();
+    box1.setBorder(BorderFactory.createTitledBorder("setBorderPainted: false"));
+
+    JCheckBox c0 = new JCheckBox("setBorderPaintedFlat: false");
+    c0.setBorderPainted(false);
+    c0.setBorderPaintedFlat(false);
+    box1.add(c0);
+    box1.add(Box.createVerticalStrut(5));
+
+    JCheckBox c1 = new JCheckBox("setBorderPaintedFlat: true");
+    c1.setBorderPainted(false);
+    c1.setBorderPaintedFlat(true);
+    box1.add(c1);
+
+    Box box2 = Box.createVerticalBox();
+    box2.setBorder(BorderFactory.createTitledBorder("setBorderPainted: true"));
+
+    JCheckBox c2 = new JCheckBox("setBorderPaintedFlat: true");
+    c2.setBorderPainted(true);
+    c2.setBorderPaintedFlat(true);
+    box2.add(c2);
+    box2.add(Box.createVerticalStrut(5));
+
+    JCheckBox c3 = new JCheckBox("setBorderPaintedFlat: false");
+    c3.setBorderPainted(true);
+    c3.setBorderPaintedFlat(false);
+    box2.add(c3);
+    box2.add(Box.createVerticalStrut(5));
+
+    Box box = Box.createVerticalBox();
+    box.add(box1);
+    box.add(Box.createVerticalStrut(10));
+    box.add(box2);
+    add(box, BorderLayout.NORTH);
+    setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+    setPreferredSize(new Dimension(320, 240));
+  }
+
+  public static void main(String... args) {
+    EventQueue.invokeLater(new Runnable() {
+      @Override public void run() {
+        createAndShowGui();
+      }
+    });
+  }
+
+  public static void createAndShowGui() {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+      ex.printStackTrace();
+      Toolkit.getDefaultToolkit().beep();
+    }
+    JMenuBar mb = new JMenuBar();
+    mb.add(LookAndFeelUtil.createLookAndFeelMenu());
+
+    JFrame frame = new JFrame("@title@");
+    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    frame.getContentPane().add(new MainPanel());
+    frame.setJMenuBar(mb);
+    frame.pack();
+    frame.setLocationRelativeTo(null);
+    frame.setVisible(true);
+  }
+}
+
+// @see https://java.net/projects/swingset3/sources/svn/content/trunk/SwingSet3/src/com/sun/swingset3/SwingSet3.java
+final class LookAndFeelUtil {
+  private static String lookAndFeel = UIManager.getLookAndFeel().getClass().getName();
+
+  private LookAndFeelUtil() {
+    /* Singleton */
+  }
+
+  public static JMenu createLookAndFeelMenu() {
+    JMenu menu = new JMenu("LookAndFeel");
+    ButtonGroup lafGroup = new ButtonGroup();
+    for (UIManager.LookAndFeelInfo lafInfo: UIManager.getInstalledLookAndFeels()) {
+      menu.add(createLookAndFeelItem(lafInfo.getName(), lafInfo.getClassName(), lafGroup));
+    }
+    return menu;
+  }
+
+  private static JMenuItem createLookAndFeelItem(String lafName, String lafClassName, ButtonGroup lafGroup) {
+    JRadioButtonMenuItem lafItem = new JRadioButtonMenuItem(lafName, lafClassName.equals(lookAndFeel));
+    lafItem.setActionCommand(lafClassName);
+    lafItem.setHideActionText(true);
+    lafItem.addActionListener(e -> {
+      ButtonModel m = lafGroup.getSelection();
+      try {
+        setLookAndFeel(m.getActionCommand());
+      } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+        ex.printStackTrace();
+        Toolkit.getDefaultToolkit().beep();
+      }
+    });
+    lafGroup.add(lafItem);
+    return lafItem;
+  }
+
+  private static void setLookAndFeel(String lookAndFeel) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+    String oldLookAndFeel = LookAndFeelUtil.lookAndFeel;
+    if (!oldLookAndFeel.equals(lookAndFeel)) {
+      UIManager.setLookAndFeel(lookAndFeel);
+      LookAndFeelUtil.lookAndFeel = lookAndFeel;
+      updateLookAndFeel();
+      // firePropertyChange("lookAndFeel", oldLookAndFeel, lookAndFeel);
+    }
+  }
+
+  private static void updateLookAndFeel() {
+    for (Window window: Frame.getWindows()) {
+      SwingUtilities.updateComponentTreeUI(window);
+    }
+  }
+}
