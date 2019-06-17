33ef2d3efedb61a0ef9a5018a0cd15df6532bceb
aterai <aterai@outlook.com>
2019-06-17 16:19:37 +0900

    add: PositionInGridLayout

---
+++ PositionInGridLayout/src/java/example/MainPanel.java
@@ -0,0 +1,60 @@
+// -*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+// @homepage@
+
+package example;
+
+import java.awt.*;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public final class MainPanel extends JPanel {
+  private MainPanel() {
+    super(new BorderLayout());
+
+    GridLayout gl = new GridLayout(5, 7, 5, 5);
+    JPanel p = new JPanel(gl);
+    p.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+
+    JTextArea log = new JTextArea(3, 0);
+    ActionListener al = e -> {
+      Component c = (Component) e.getSource();
+      int idx = p.getComponentZOrder(c);
+      int row = idx / gl.getColumns();
+      int col = idx % gl.getColumns();
+      log.append(String.format("Row: %d, Column: %d%n", row + 1, col + 1));
+    };
+    for (int i = 0; i < gl.getRows() * gl.getColumns(); i++) {
+      JButton b = new JButton();
+      b.addActionListener(al);
+      p.add(b);
+    }
+
+    add(p);
+    add(new JScrollPane(log), BorderLayout.SOUTH);
+    setPreferredSize(new Dimension(320, 240));
+  }
+
+  public static void main(String... args) {
+    EventQueue.invokeLater(new Runnable() {
+      @Override public void run() {
+        createAndShowGui();
+      }
+    });
+  }
+
+  public static void createAndShowGui() {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+      ex.printStackTrace();
+      Toolkit.getDefaultToolkit().beep();
+    }
+    JFrame frame = new JFrame("@title@");
+    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    frame.getContentPane().add(new MainPanel());
+    frame.pack();
+    frame.setLocationRelativeTo(null);
+    frame.setVisible(true);
+  }
+}
