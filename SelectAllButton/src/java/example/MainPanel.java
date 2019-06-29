96087a64245b5fef9b2a2dfe81781211475a482e
aterai <aterai@outlook.com>
2019-06-30 01:30:50 +0900

    Checkstyle: Indentation

--- SelectAllButton/src/java/example/MainPanel.java
+++ SelectAllButton/src/java/example/MainPanel.java
@@ -12,11 +12,11 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 
 public final class MainPanel extends JPanel {
-   private final String[] columnNames = {"String", "Integer", "Boolean"};
-   private final Object[][] data = {
-      {"aaa", 12, true}, {"bbb", 5, false},
-      {"CCC", 92, true}, {"DDD", 0, false}
-    };
+  private final String[] columnNames = {"String", "Integer", "Boolean"};
+  private final Object[][] data = {
+    {"aaa", 12, true}, {"bbb", 5, false},
+    {"CCC", 92, true}, {"DDD", 0, false}
+  };
   private final TableModel model = new DefaultTableModel(data, columnNames) {
     @Override public Class<?> getColumnClass(int column) {
       return getValueAt(0, column).getClass();
