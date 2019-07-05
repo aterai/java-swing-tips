323cfd5c2c94791c68551c9d4f485dc9ac95abfb
aterai <aterai@outlook.com>
2019-07-05 15:23:10 +0900

    PMD: CloseResource

--- MidiSystem/src/java/example/MainPanel.java
+++ MidiSystem/src/java/example/MainPanel.java
@@ -5,65 +5,99 @@
 package example;
 
 import java.awt.*;
+import java.awt.event.HierarchyEvent;
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
-import javax.sound.midi.Sequence;
 import javax.sound.midi.Sequencer;
 import javax.swing.*;
 
-public final class MainPanel extends JPanel {
+public class MainPanel extends JPanel {
   private static final byte END_OF_TRACK = 0x2F;
-  private long tickpos;
-  private final JButton start = new JButton("start");
-  private final JButton stop = new JButton("stop");
-  private final JButton init = new JButton("init");
+  private final JButton start = makeButton("start");
+  private final JButton pause = makeButton("pause");
+  private final JButton reset = makeButton("reset");
+  protected long tickpos;
+  protected Sequencer sequencer;
 
   public MainPanel() {
     super(new BorderLayout(5, 5));
-    Sequencer sequencer;
+
     URL url = getClass().getResource("Mozart_toruko_k.mid");
-    try {
-      Sequence s = MidiSystem.getSequence(url);
-      sequencer = MidiSystem.getSequencer();
-      sequencer.open();
-      sequencer.setSequence(s);
-    } catch (InvalidMidiDataException | MidiUnavailableException | IOException ex) {
-      ex.printStackTrace();
-      add(new JLabel(ex.toString()));
-      start.setEnabled(false);
-      stop.setEnabled(false);
-      init.setEnabled(false);
-      return;
-    }
-    sequencer.addMetaEventListener(e -> {
-      if (e.getType() == END_OF_TRACK) {
+    SwingWorker<Void, Long> worker = new SwingWorker<Void, Long>() {
+      @Override public Void doInBackground() {
+        try (Sequencer s = MidiSystem.getSequencer()) {
+          sequencer = s;
+          sequencer.open();
+          sequencer.setSequence(MidiSystem.getSequence(url));
+          sequencer.addMetaEventListener(e -> {
+            if (e.getType() == END_OF_TRACK) {
+              publish(0L);
+            }
+          });
+
+          try {
+            while (sequencer.isOpen()) {
+              if (sequencer.isRunning()) {
+                publish(sequencer.getTickPosition());
+              }
+              Thread.sleep(1000);
+            }
+          } catch (InterruptedException ex) {
+            System.out.println(ex.getMessage());
+          }
+        } catch (InvalidMidiDataException | MidiUnavailableException | IOException ex) {
+          ex.printStackTrace();
+          publish(0L);
+        } finally {
+          System.out.println("try-with-resources: AutoCloseable");
+        }
+        return null;
+      }
+
+      @Override protected void process(List<Long> chunks) {
+        for (Long tp: chunks) {
+          tickpos = tp;
+          if (tickpos == 0) {
+            initButtons(true);
+          }
+        }
+      }
+
+      @Override public void done() {
         tickpos = 0;
-        start.setEnabled(true);
-        stop.setEnabled(false);
+        initButtons(true);
+      }
+    };
+    worker.execute();
+
+    addHierarchyListener(e -> {
+      if ((e.getChangeFlags() & HierarchyEvent.DISPLAYABILITY_CHANGED) != 0 && !e.getComponent().isDisplayable()) {
+        System.out.println("DISPLAYABILITY_CHANGED");
+        worker.cancel(true);
       }
     });
+
     start.addActionListener(e -> {
       sequencer.setTickPosition(tickpos);
       sequencer.start();
-      stop.setEnabled(true);
-      start.setEnabled(false);
+      initButtons(false);
     });
-    stop.addActionListener(e -> {
-      tickpos = sequencer.getTickPosition();
+
+    pause.setEnabled(false);
+    pause.addActionListener(e -> {
       sequencer.stop();
-      start.setEnabled(true);
-      stop.setEnabled(false);
+      initButtons(true);
     });
-    init.addActionListener(e -> {
+
+    reset.addActionListener(e -> {
       sequencer.stop();
       tickpos = 0;
-      start.setEnabled(true);
-      stop.setEnabled(false);
+      initButtons(true);
     });
-    stop.setEnabled(false);
 
     JTextArea label = new JTextArea("Wolfgang Amadeus Mozart\nPiano Sonata No. 11 in A major, K 331\n(Turkish Rondo)");
     label.setBorder(BorderFactory.createTitledBorder("MIDI"));
@@ -74,13 +108,24 @@ public final class MainPanel extends JPanel {
     box.setBorder(BorderFactory.createEmptyBorder(5, 5, 0, 0));
     box.add(Box.createHorizontalGlue());
     box.add(start);
-    box.add(stop);
-    box.add(init);
+    box.add(pause);
+    box.add(reset);
 
     add(label);
     add(box, BorderLayout.SOUTH);
     setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
     setPreferredSize(new Dimension(320, 240));
+  }
+
+  protected void initButtons(boolean flg) {
+    start.setEnabled(flg);
+    pause.setEnabled(!flg);
+  }
+
+  private static JButton makeButton(String title) {
+    JButton b = new JButton(title);
+    b.setFocusable(false);
+    return b;
   }
 
   public static void main(String... args) {
@@ -99,7 +144,8 @@ public final class MainPanel extends JPanel {
       Toolkit.getDefaultToolkit().beep();
     }
     JFrame frame = new JFrame("@title@");
-    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    // frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
     frame.getContentPane().add(new MainPanel());
     frame.pack();
     frame.setLocationRelativeTo(null);
