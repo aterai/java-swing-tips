c809fdc535c8dc855915daaec2c8087b8a97fd7b
aterai <aterai@outlook.com>
2019-07-23 17:19:12 +0900

    Checkstyle: VisibilityModifier

--- TabSet/src/java/example/MainPanel.java
+++ TabSet/src/java/example/MainPanel.java
@@ -6,7 +6,6 @@ package example;
 
 import java.awt.*;
 import java.util.Arrays;
-import java.util.List;
 import javax.swing.*;
 import javax.swing.text.Style;
 import javax.swing.text.StyleConstants;
@@ -14,36 +13,35 @@ import javax.swing.text.StyleContext;
 import javax.swing.text.TabSet;
 import javax.swing.text.TabStop;
 
-public class MainPanel extends JPanel {
-  protected final JCheckBox check = new JCheckBox("vertical grid lines", true);
-  protected final JTextPane textPane = new JTextPane() {
-    @Override protected void paintComponent(Graphics g) {
-      super.paintComponent(g);
-      if (check.isSelected()) {
-        int ox = getInsets().left;
-        int h = getHeight();
-        g.setColor(Color.RED);
-        g.drawLine(ox, 0, ox, h);
-        g.drawLine(ox + 100, 0, ox + 100, h);
-        g.drawLine(ox + 200, 0, ox + 200, h);
-        g.drawLine(ox + 300, 0, ox + 300, h);
-        g.setColor(Color.ORANGE);
-        g.drawLine(ox + 50, 0, ox + 50, h);
-        g.drawLine(ox + 150, 0, ox + 150, h);
-        g.drawLine(ox + 250, 0, ox + 250, h);
-      }
-    }
-  };
-
-  public MainPanel() {
+public final class MainPanel extends JPanel {
+  private MainPanel() {
     super(new BorderLayout());
+    JCheckBox check = new JCheckBox("vertical grid lines", true);
+    check.addActionListener(e -> repaint());
 
-    List<String> li = Arrays.asList(
+    JTextPane textPane = new JTextPane() {
+      @Override protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (check.isSelected()) {
+          int ox = getInsets().left;
+          int h = getHeight();
+          g.setColor(Color.RED);
+          g.drawLine(ox, 0, ox, h);
+          g.drawLine(ox + 100, 0, ox + 100, h);
+          g.drawLine(ox + 200, 0, ox + 200, h);
+          g.drawLine(ox + 300, 0, ox + 300, h);
+          g.setColor(Color.ORANGE);
+          g.drawLine(ox + 50, 0, ox + 50, h);
+          g.drawLine(ox + 150, 0, ox + 150, h);
+          g.drawLine(ox + 250, 0, ox + 250, h);
+        }
+      }
+    };
+    textPane.setText(String.join("\n", Arrays.asList(
         String.join("\t", Arrays.asList("LEFT1", "CENTER1", "RIGHT1", "3.14")),
         String.join("\t", Arrays.asList("LEFT22", "CENTER22", "RIGHT22", "12.3")),
         String.join("\t", Arrays.asList("LEFT333", "CENTER333", "RIGHT333", "123.45")),
-        String.join("\t", Arrays.asList("LEFT4444", "CENTER4444", "RIGHT4444", "0.9876")));
-    textPane.setText(String.join("\n", li));
+        String.join("\t", Arrays.asList("LEFT4444", "CENTER4444", "RIGHT4444", "0.9876")))));
 
     // MutableAttributeSet attr = new SimpleAttributeSet();
     Style attr = textPane.getStyle(StyleContext.DEFAULT_STYLE);
@@ -57,7 +55,6 @@ public class MainPanel extends JPanel {
     textPane.setParagraphAttributes(attr, false);
     // textPane.getStyledDocument().setParagraphAttributes(0, textPane.getDocument().getLength(), attr, false);
 
-    check.addActionListener(e -> repaint());
     add(new JScrollPane(textPane));
     add(check, BorderLayout.SOUTH);
     setPreferredSize(new Dimension(320, 240));
