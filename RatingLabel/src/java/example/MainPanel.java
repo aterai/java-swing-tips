bb441a92d69a6537f44248ac482029a029e70018
aterai <aterai@outlook.com>
2019-09-14 16:44:39 +0900

    refactor: use MouseAdapter instead of implements MouseListener, MouseMotionListener

--- RatingLabel/src/java/example/MainPanel.java
+++ RatingLabel/src/java/example/MainPanel.java
@@ -5,9 +5,8 @@
 package example;
 
 import java.awt.*;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
 import java.awt.image.FilteredImageSource;
 import java.awt.image.ImageFilter;
 import java.awt.image.ImageProducer;
@@ -17,7 +16,7 @@ import java.util.List;
 import javax.swing.*;
 
 public final class MainPanel extends JPanel {
-  public MainPanel() {
+  private MainPanel() {
     super(new GridLayout(2, 2, 4, 4));
     // PI Diagona Icons Pack 1.0 - Download Royalty Free Icons and Stock Images For Web & Graphics Design
     // http://www.freeiconsdownload.com/Free_Downloads.asp?id=60
@@ -25,22 +24,22 @@ public final class MainPanel extends JPanel {
     ImageProducer ip = defaultIcon.getImage().getSource();
 
     // 1
-    List<ImageIcon> list = Arrays.asList(
+    List<ImageIcon> list1 = Arrays.asList(
         makeStarImageIcon(ip, new SelectedImageFilter(1f, .5f, .5f)),
         makeStarImageIcon(ip, new SelectedImageFilter(.5f, 1f, .5f)),
         makeStarImageIcon(ip, new SelectedImageFilter(1f, .5f, 1f)),
         makeStarImageIcon(ip, new SelectedImageFilter(.5f, .5f, 1f)),
         makeStarImageIcon(ip, new SelectedImageFilter(1f, 1f, .5f)));
-    add(makeStarRatingPanel("gap=0", new LevelBar(defaultIcon, list, 0)));
+    add(makeStarRatingPanel("gap=0", new LevelBar(defaultIcon, list1, 0)));
 
     // 2
-    list = Arrays.asList(
-      makeStarImageIcon(ip, new SelectedImageFilter(.2f, .5f, .5f)),
-      makeStarImageIcon(ip, new SelectedImageFilter(0f, 1f, .2f)),
-      makeStarImageIcon(ip, new SelectedImageFilter(1f, 1f, .2f)),
-      makeStarImageIcon(ip, new SelectedImageFilter(.8f, .4f, .2f)),
-      makeStarImageIcon(ip, new SelectedImageFilter(1f, .1f, .1f)));
-    add(makeStarRatingPanel("gap=1+1", new LevelBar(defaultIcon, list, 1) {
+    List<ImageIcon> list2 = Arrays.asList(
+        makeStarImageIcon(ip, new SelectedImageFilter(.2f, .5f, .5f)),
+        makeStarImageIcon(ip, new SelectedImageFilter(0f, 1f, .2f)),
+        makeStarImageIcon(ip, new SelectedImageFilter(1f, 1f, .2f)),
+        makeStarImageIcon(ip, new SelectedImageFilter(.8f, .4f, .2f)),
+        makeStarImageIcon(ip, new SelectedImageFilter(1f, .1f, .1f)));
+    add(makeStarRatingPanel("gap=1+1", new LevelBar(defaultIcon, list2, 1) {
       @Override protected void repaintIcon(int index) {
         for (int i = 0; i < labelList.size(); i++) {
           labelList.get(i).setIcon(i <= index ? iconList.get(index) : defaultIcon);
@@ -50,18 +49,18 @@ public final class MainPanel extends JPanel {
     }));
 
     // 3
-    list = Arrays.asList(
-      makeStarImageIcon(ip, new SelectedImageFilter(.6f, .6f, 0f)),
-      makeStarImageIcon(ip, new SelectedImageFilter(.7f, .7f, 0f)),
-      makeStarImageIcon(ip, new SelectedImageFilter(.8f, .8f, 0f)),
-      makeStarImageIcon(ip, new SelectedImageFilter(.9f, .9f, 0f)),
-      makeStarImageIcon(ip, new SelectedImageFilter(1f, 1f, 0f)));
-    add(makeStarRatingPanel("gap=2+2", new LevelBar(defaultIcon, list, 2)));
+    List<ImageIcon> list3 = Arrays.asList(
+        makeStarImageIcon(ip, new SelectedImageFilter(.6f, .6f, 0f)),
+        makeStarImageIcon(ip, new SelectedImageFilter(.7f, .7f, 0f)),
+        makeStarImageIcon(ip, new SelectedImageFilter(.8f, .8f, 0f)),
+        makeStarImageIcon(ip, new SelectedImageFilter(.9f, .9f, 0f)),
+        makeStarImageIcon(ip, new SelectedImageFilter(1f, 1f, 0f)));
+    add(makeStarRatingPanel("gap=2+2", new LevelBar(defaultIcon, list3, 2)));
 
     // 4
     ImageIcon star = makeStarImageIcon(ip, new SelectedImageFilter(1f, 1f, 0f));
-    list = Arrays.asList(star, star, star, star, star);
-    add(makeStarRatingPanel("gap=1+1", new LevelBar(defaultIcon, list, 1)));
+    List<ImageIcon> list4 = Arrays.asList(star, star, star, star, star);
+    add(makeStarRatingPanel("gap=1+1", new LevelBar(defaultIcon, list4, 1)));
     setPreferredSize(new Dimension(320, 240));
   }
 
@@ -101,26 +100,52 @@ public final class MainPanel extends JPanel {
   }
 }
 
-class LevelBar extends JPanel implements MouseListener, MouseMotionListener {
+class LevelBar extends JPanel {
   private final int gap;
   protected final List<ImageIcon> iconList;
   protected final List<JLabel> labelList = Arrays.asList(
       new JLabel(), new JLabel(), new JLabel(), new JLabel(), new JLabel()
   );
   protected final ImageIcon defaultIcon;
-  private int clicked = -1;
+  protected int clicked = -1;
+  private transient MouseAdapter handler;
 
   protected LevelBar(ImageIcon defaultIcon, List<ImageIcon> list, int gap) {
     super(new GridLayout(1, 5, gap * 2, gap * 2));
     this.defaultIcon = defaultIcon;
     this.iconList = list;
     this.gap = gap;
-    for (JLabel l: labelList) {
-      l.setIcon(defaultIcon);
-      add(l);
-    }
-    addMouseListener(this);
-    addMouseMotionListener(this);
+    EventQueue.invokeLater(() -> {
+      for (JLabel l: labelList) {
+        l.setIcon(defaultIcon);
+        add(l);
+      }
+    });
+  }
+
+  @Override public void updateUI() {
+    removeMouseListener(handler);
+    removeMouseMotionListener(handler);
+    super.updateUI();
+    handler = new MouseAdapter() {
+      @Override public void mouseMoved(MouseEvent e) {
+        repaintIcon(getSelectedIconIndex(e.getPoint()));
+      }
+
+      @Override public void mouseEntered(MouseEvent e) {
+        repaintIcon(getSelectedIconIndex(e.getPoint()));
+      }
+
+      @Override public void mouseClicked(MouseEvent e) {
+        clicked = getSelectedIconIndex(e.getPoint());
+      }
+
+      @Override public void mouseExited(MouseEvent e) {
+        repaintIcon(clicked);
+      }
+    };
+    addMouseListener(handler);
+    addMouseMotionListener(handler);
   }
 
   public void clear() {
@@ -137,7 +162,7 @@ class LevelBar extends JPanel implements MouseListener, MouseMotionListener {
     repaintIcon(clicked);
   }
 
-  private int getSelectedIconIndex(Point p) {
+  protected int getSelectedIconIndex(Point p) {
     for (int i = 0; i < labelList.size(); i++) {
       Rectangle r = labelList.get(i).getBounds();
       r.grow(gap, gap);
@@ -153,34 +178,6 @@ class LevelBar extends JPanel implements MouseListener, MouseMotionListener {
       labelList.get(i).setIcon(i <= index ? iconList.get(i) : defaultIcon);
     }
     repaint();
-  }
-
-  @Override public void mouseMoved(MouseEvent e) {
-    repaintIcon(getSelectedIconIndex(e.getPoint()));
-  }
-
-  @Override public void mouseEntered(MouseEvent e) {
-    repaintIcon(getSelectedIconIndex(e.getPoint()));
-  }
-
-  @Override public void mouseClicked(MouseEvent e) {
-    clicked = getSelectedIconIndex(e.getPoint());
-  }
-
-  @Override public void mouseExited(MouseEvent e) {
-    repaintIcon(clicked);
-  }
-
-  @Override public void mouseDragged(MouseEvent e) {
-    /* not needed */
-  }
-
-  @Override public void mousePressed(MouseEvent e) {
-    /* not needed */
-  }
-
-  @Override public void mouseReleased(MouseEvent e) {
-    /* not needed */
   }
 }
 
