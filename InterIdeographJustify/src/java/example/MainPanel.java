ea4299be642521fc18e3fd323a7c0dcce74caf6c
aterai <aterai@outlook.com>
2019-10-12 01:32:18 +0900

    refactor: use Font#getSize2D() instead of Font#getSize()

--- InterIdeographJustify/src/java/example/MainPanel.java
+++ InterIdeographJustify/src/java/example/MainPanel.java
@@ -14,34 +14,32 @@ import javax.swing.table.TableColumn;
 import javax.swing.table.TableModel;
 
 public final class MainPanel extends JPanel {
-  private final String[] columnNames = {"Justified", "Default"};
-  private final Object[][] data = {
-    {"会社名", ""},
-    {"所在地", ""},
-    {"電話番号", ""},
-    {"設立", ""},
-    {"代表取締役", ""},
-    {"事業内容", ""}
-  };
-  private final TableModel model = new DefaultTableModel(data, columnNames);
-  private final JTable table = new JTable(model) {
-    private final Color evenColor = new Color(0xF5_F5_FF);
-    @Override public Component prepareRenderer(TableCellRenderer tcr, int row, int column) {
-      Component c = super.prepareRenderer(tcr, row, column);
-      if (isRowSelected(row)) {
-        c.setForeground(getSelectionForeground());
-        c.setBackground(getSelectionBackground());
-      } else {
-        c.setForeground(getForeground());
-        c.setBackground(row % 2 == 0 ? evenColor : getBackground());
-      }
-      return c;
-    }
-  };
-
-  public MainPanel() {
+  private MainPanel() {
     super(new BorderLayout());
-
+    String[] columnNames = {"Justified", "Default"};
+    Object[][] data = {
+      {"会社名", ""},
+      {"所在地", ""},
+      {"電話番号", ""},
+      {"設立", ""},
+      {"代表取締役", ""},
+      {"事業内容", ""}
+    };
+    TableModel model = new DefaultTableModel(data, columnNames);
+    JTable table = new JTable(model) {
+      private final Color evenColor = new Color(0xF5_F5_FF);
+      @Override public Component prepareRenderer(TableCellRenderer tcr, int row, int column) {
+        Component c = super.prepareRenderer(tcr, row, column);
+        if (isRowSelected(row)) {
+          c.setForeground(getSelectionForeground());
+          c.setBackground(getSelectionBackground());
+        } else {
+          c.setForeground(getForeground());
+          c.setBackground(row % 2 == 0 ? evenColor : getBackground());
+        }
+        return c;
+      }
+    };
     table.setFocusable(false);
     table.setRowSelectionAllowed(true);
     table.setShowVerticalLines(false);
@@ -121,53 +119,57 @@ class JustifiedLabel extends JLabel {
     g2.fillRect(0, 0, d.width, d.height);
     g2.setPaint(getForeground());
     // int baseline = getBaseline(d.width, d.height);
-    int baseline = ins.top + font.getSize();
-    layout.draw(g2, (float) ins.left, (float) baseline);
+    float baseline = ins.top + font.getSize2D();
+    layout.draw(g2, (float) ins.left, baseline);
     g2.dispose();
   }
 }
 
 // // https://ateraimemo.com/Swing/JustifiedLabel.html
 // class JustifiedLabel extends JLabel {
-//   private GlyphVector gvtext;
+//   private GlyphVector gvText;
 //   private int prevWidth = -1;
 //   protected JustifiedLabel() {
 //     this(null);
 //   }
+//
 //   protected JustifiedLabel(String str) {
 //     super(str);
 //   }
+//
 //   @Override public void setText(String text) {
 //     super.setText(text);
 //     prevWidth = -1;
 //   }
+//
 //   @Override protected void paintComponent(Graphics g) {
 //     Graphics2D g2 = (Graphics2D) g.create();
 //     Insets ins = getInsets();
 //     int w = getSize().width - ins.left - ins.right;
 //     if (w != prevWidth) {
-//       gvtext = getJustifiedGlyphVector(w, getText(), getFont(), g2.getFontRenderContext());
+//       gvText = getJustifiedGlyphVector(w, getText(), getFont(), g2.getFontRenderContext());
 //       prevWidth = w;
 //     }
-//     if (Objects.nonNull(gvtext)) {
+//     if (Objects.nonNull(gvText)) {
 //       g2.setPaint(getBackground());
 //       g2.fillRect(0, 0, getWidth(), getHeight());
 //       g2.setPaint(getForeground());
-//       g2.drawGlyphVector(gvtext, ins.left, ins.top + getFont().getSize());
+//       g2.drawGlyphVector(gvText, ins.left, ins.top + getFont().getSize());
 //     } else {
 //       super.paintComponent(g);
 //     }
 //     g2.dispose();
 //   }
+//
 //   private GlyphVector getJustifiedGlyphVector(int width, String str, Font font, FontRenderContext frc) {
 //     GlyphVector gv = font.createGlyphVector(frc, str);
 //     Rectangle2D r = gv.getVisualBounds();
-//     float jwidth = (float) width;
-//     float vwidth = (float) r.getWidth();
-//     if (jwidth > vwidth) {
+//     float jw = (float) width;
+//     float vw = (float) r.getWidth();
+//     if (jw > vw) {
 //       int num = gv.getNumGlyphs();
-//       float xx = (jwidth - vwidth) / (float) (num - 1);
-//       float xpos = num == 1 ? (jwidth - vwidth) * .5f : 0f;
+//       float xx = (jw - vw) / (float) (num - 1);
+//       float xpos = num == 1 ? (jw - vw) * .5f : 0f;
 //       Point2D gmPos = new Point2D.Float();
 //       for (int i = 0; i < num; i++) {
 //         GlyphMetrics gm = gv.getGlyphMetrics(i);
