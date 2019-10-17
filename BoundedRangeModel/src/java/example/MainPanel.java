6e8abd66b09baba81d69096f36b97133920d1c5f
aterai <aterai@outlook.com>
2019-10-17 15:00:43 +0900

    refactor: anonymous new TableCellRenderer() can be replaced with lambda

--- BoundedRangeModel/src/java/example/MainPanel.java
+++ BoundedRangeModel/src/java/example/MainPanel.java
@@ -17,35 +17,41 @@ import javax.swing.event.MouseInputAdapter;
 import javax.swing.event.MouseInputListener;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
 
 public class MainPanel extends JPanel {
   protected static final Color THUMB_COLOR = new Color(0, 0, 255, 50);
   protected static final String PATTERN = "Swing";
   protected final List<Integer> emphasisIndices = new ArrayList<>();
   protected final DefaultTableModel model = new DefaultTableModel(0, 2);
-  protected final JTable table = new JTable(model);
+  protected final JTable table = new JTable(model) {
+    @Override public void updateUI() {
+      setDefaultRenderer(Object.class, null);
+      super.updateUI();
+      TableCellRenderer renderer = new DefaultTableCellRenderer();
+      setDefaultRenderer(Object.class, (tbl, value, isSelected, hasFocus, row, column) -> {
+        Component c = renderer.getTableCellRendererComponent(tbl, value, isSelected, hasFocus, row, column);
+        if (emphasisIndices.contains(row)) {
+          c.setBackground(Color.YELLOW);
+        } else {
+          c.setBackground(isSelected ? tbl.getSelectionBackground() : tbl.getBackground());
+        }
+        return c;
+      });
+      setFillsViewportHeight(true);
+    }
+  };
   protected final JScrollPane scroll = new JScrollPane(table);
   protected final JLabel label = new JLabel();
   protected final JScrollBar scrollbar = new JScrollBar(Adjustable.VERTICAL);
 
-  public MainPanel() {
+  private MainPanel() {
     super(new BorderLayout());
     IntStream.range(0, 100)
-      .mapToObj(i -> i % 19 == 0 || i % 17 == 0 ? PATTERN : "aaaaa")
-      .map(s -> new Object[] {s, ""})
-      .forEach(model::addRow);
-    table.setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {
-      @Override public Component getTableCellRendererComponent(JTable t, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-        super.getTableCellRendererComponent(t, value, isSelected, hasFocus, row, column);
-        if (emphasisIndices.contains(row)) {
-          setBackground(Color.YELLOW);
-        } else {
-          setBackground(isSelected ? t.getSelectionBackground() : t.getBackground());
-        }
-        return this;
-      }
-    });
-    table.setFillsViewportHeight(true);
+        .mapToObj(i -> i % 19 == 0 || i % 17 == 0 ? PATTERN : "Java")
+        .map(s -> new Object[] {s, ""})
+        .forEach(model::addRow);
+
     scroll.setVerticalScrollBar(scrollbar);
     scrollbar.getModel().addChangeListener(e -> label.repaint());
 
@@ -104,35 +110,35 @@ public class MainPanel extends JPanel {
   private class HighlightIcon implements Icon {
     @Override public void paintIcon(Component c, Graphics g, int x, int y) {
       JViewport vport = Objects.requireNonNull((JViewport) SwingUtilities.getAncestorOfClass(JViewport.class, table));
-      Rectangle vrect = vport.getBounds();
-      Rectangle trect = table.getBounds();
-      Rectangle crect = SwingUtilities.calculateInnerArea(label, label.getBounds());
+      Rectangle viewRect = vport.getBounds();
+      Rectangle tableRect = table.getBounds();
+      Rectangle cellRect = SwingUtilities.calculateInnerArea(label, label.getBounds());
       // Insets insets = ((JComponent) c).getInsets();
       // Insets insets = label.getInsets();
 
       // paint Background
       g.setColor(Color.WHITE);
-      g.fillRect(crect.x, crect.y, crect.width, crect.height);
+      g.fillRect(cellRect.x, cellRect.y, cellRect.width, cellRect.height);
 
-      // double sy = (crect.height - insets.top - insets.bottom) / trect.getHeight();
-      double sy = crect.getHeight() / trect.getHeight();
+      // double sy = (cellRect.height - insets.top - insets.bottom) / tableRect.getHeight();
+      double sy = cellRect.getHeight() / tableRect.getHeight();
       AffineTransform at = AffineTransform.getScaleInstance(1d, sy);
       // paint Highlight
       g.setColor(Color.YELLOW);
       emphasisIndices.forEach(viewIndex -> {
         Rectangle r = table.getCellRect(viewIndex, 0, true);
         Rectangle s = at.createTransformedShape(r).getBounds();
-        g.fillRect(x, crect.y + s.y, getIconWidth(), Math.max(2, s.height - 2));
+        g.fillRect(x, cellRect.y + s.y, getIconWidth(), Math.max(2, s.height - 2));
       });
       // paint Thumb
       if (scrollbar.isVisible()) {
-        Rectangle thumbRect = new Rectangle(vrect);
+        Rectangle thumbRect = new Rectangle(viewRect);
         thumbRect.y = vport.getViewPosition().y;
         g.setColor(THUMB_COLOR);
         Rectangle r = at.createTransformedShape(thumbRect).getBounds();
-        g.fillRect(x, crect.y + r.y, getIconWidth(), r.height);
+        g.fillRect(x, cellRect.y + r.y, getIconWidth(), r.height);
         g.setColor(THUMB_COLOR.darker());
-        g.drawRect(x, crect.y + r.y, getIconWidth() - 1, r.height - 1);
+        g.drawRect(x, cellRect.y + r.y, getIconWidth() - 1, r.height - 1);
       }
     }
 
