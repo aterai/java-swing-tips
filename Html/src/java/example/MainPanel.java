93f11e7b5fd6c127386be112b7213ac13bff920c
aterai <aterai@outlook.com>
2019-10-26 01:05:58 +0900

    refactor: field can be converted to a local variable

--- Html/src/java/example/MainPanel.java
+++ Html/src/java/example/MainPanel.java
@@ -10,24 +10,24 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 
 public final class MainPanel extends JPanel {
-  private final String[] columnNames = {"String", "Integer", "Boolean"};
-  private final Object[][] data = {
-    {"<html>Comment<p>etc.", 12, true}, {"bbb", 5, false},
-    {"CCC", 92, true}, {"DDD", 0, false}
-  };
-  private final TableModel model = new DefaultTableModel(data, columnNames) {
-    @Override public Class<?> getColumnClass(int column) {
-      return getValueAt(0, column).getClass();
-    }
-  };
-  private final JTable table = new JTable(model);
-  private final JTabbedPane tab = new JTabbedPane();
-
-  public MainPanel() {
+  private MainPanel() {
     super(new BorderLayout());
+    JTabbedPane tab = new JTabbedPane();
     tab.addTab("<html>Test<p>Test</p></html>", new JLabel("Test1"));
     tab.addTab("<html>Test<p>test", new JLabel("Test2"));
 
+    String[] columnNames = {"String", "Integer", "Boolean"};
+    Object[][] data = {
+      {"<html>Comment<p>etc.", 12, true}, {"bbb", 5, false},
+      {"CCC", 92, true}, {"DDD", 0, false}
+    };
+    TableModel model = new DefaultTableModel(data, columnNames) {
+      @Override
+      public Class<?> getColumnClass(int column) {
+        return getValueAt(0, column).getClass();
+      }
+    };
+    JTable table = new JTable(model);
     table.setAutoCreateRowSorter(true);
     table.setRowSelectionAllowed(true);
     table.setRowHeight(32);
