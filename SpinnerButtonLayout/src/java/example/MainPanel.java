38262d46c6749a953a3fb8c4a6b66acec4bb25e7
aterai <aterai@outlook.com>
2019-10-30 17:17:04 +0900

    refactor: field can be converted to a local variable

--- SpinnerButtonLayout/src/java/example/MainPanel.java
+++ SpinnerButtonLayout/src/java/example/MainPanel.java
@@ -12,32 +12,34 @@ import javax.swing.*;
 import javax.swing.plaf.basic.BasicSpinnerUI;
 
 public final class MainPanel extends JPanel {
-  private final JSpinner spinner0 = new JSpinner(new SpinnerNumberModel(10, 0, 1000, 1));
-  private final JSpinner spinner1 = new JSpinner(new SpinnerNumberModel(10, 0, 1000, 1));
-  private final JSpinner spinner2 = new JSpinner(new SpinnerNumberModel(10, 0, 1000, 1)) {
-    @Override public void updateUI() {
-      super.updateUI();
-      setUI(new BasicSpinnerUI() {
-        @Override protected LayoutManager createLayout() {
-          return new SpinnerLayout();
-        }
-      });
-    }
-  };
-  private final JSpinner spinner3 = new JSpinner(new SpinnerNumberModel(10, 0, 1000, 1)) {
-    @Override public void setLayout(LayoutManager mgr) {
-      super.setLayout(new SpinnerLayout());
-    }
-  };
-
-  public MainPanel() {
+  private MainPanel() {
     super(new BorderLayout());
-    spinner1.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+    SpinnerModel model = new SpinnerNumberModel(10, 0, 1000, 1);
 
     Box box = Box.createVerticalBox();
-    box.add(makeTitledPanel("Default", spinner0));
+    box.add(makeTitledPanel("Default", new JSpinner(model)));
+
+    JSpinner spinner1 = new JSpinner(model);
+    spinner1.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
     box.add(makeTitledPanel("RIGHT_TO_LEFT", spinner1));
+
+    JSpinner spinner2 = new JSpinner(model) {
+      @Override public void updateUI() {
+        super.updateUI();
+        setUI(new BasicSpinnerUI() {
+          @Override protected LayoutManager createLayout() {
+            return new SpinnerLayout();
+          }
+        });
+      }
+    };
     box.add(makeTitledPanel("L(Prev), R(Next)", spinner2));
+
+    JSpinner spinner3 = new JSpinner(model) {
+      @Override public void setLayout(LayoutManager mgr) {
+        super.setLayout(new SpinnerLayout());
+      }
+    };
     box.add(makeTitledPanel("L(Prev), R(Next)", spinner3));
 
     add(box, BorderLayout.NORTH);
