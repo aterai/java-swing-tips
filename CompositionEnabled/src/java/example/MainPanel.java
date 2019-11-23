566f6c565070e7b4fa24bb5e6d1b4e33c0145d8f
aterai <aterai@outlook.com>
2019-11-23 15:46:05 +0900

    refactor: field can be converted to a local variable

--- CompositionEnabled/src/java/example/MainPanel.java
+++ CompositionEnabled/src/java/example/MainPanel.java
@@ -7,55 +7,40 @@ package example;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableModel;
 
-public class MainPanel extends JPanel {
+public final class MainPanel extends JPanel {
   private final JCheckBox check = new JCheckBox("一時ウィンドウ(入力モード)->enterでセル編集開始");
-  private final String[] columnNames = {"A", "B", "C"};
-  private final Object[][] data = {
-    {"aaa", "eeee", "l"}, {"bbb", "ff", "ggg"},
-    {"CCC", "kkk", "jj"}, {"DDD", "ii", "hhh"}
-  };
-  private final TableModel model = new DefaultTableModel(data, columnNames) {
-    @Override public Class<?> getColumnClass(int column) {
-      return getValueAt(0, column).getClass();
-    }
-  };
-  private final JTable table = new JTable(model) {
-    @Override protected boolean processKeyBinding(KeyStroke ks, KeyEvent e, int condition, boolean pressed) {
-      // System.out.println("key: " + ks.toString());
-      boolean retValue = super.processKeyBinding(ks, e, condition, pressed);
-      if (!check.isSelected()) {
-        return retValue;
-      }
-      if (isTabOrEnterKey(ks)) {
-        System.out.println("tab or enter typed");
-        return retValue;
-      }
-      if (getInputContext().isCompositionEnabled() && !isEditing() && !pressed && !ks.isOnKeyRelease()) {
-        int selectedRow = getSelectedRow();
-        int selectedColumn = getSelectedColumn();
-        if (selectedRow != -1 && selectedColumn != -1) {
-          boolean dummy = editCellAt(selectedRow, selectedColumn);
-          System.out.println("editCellAt: " + dummy);
-        }
-      }
-      return retValue;
-    }
 
-    protected boolean isTabOrEnterKey(KeyStroke ks) {
-      return KeyStroke.getKeyStroke('\t').equals(ks) || KeyStroke.getKeyStroke('\n').equals(ks);
-    }
-  };
-
-  public MainPanel() {
+  private  MainPanel() {
     super(new BorderLayout());
+    JTable table = new JTable(4, 3) {
+      @Override protected boolean processKeyBinding(KeyStroke ks, KeyEvent e, int condition, boolean pressed) {
+        // System.out.println("key: " + ks.toString());
+        if (!check.isSelected() || isTabOrEnterKey(ks)) {
+          System.out.println("tab or enter typed");
+          return super.processKeyBinding(ks, e, condition, pressed);
+        }
+        if (getInputContext().isCompositionEnabled() && !isEditing() && !pressed && !ks.isOnKeyRelease()) {
+          int selectedRow = getSelectedRow();
+          int selectedColumn = getSelectedColumn();
+          if (selectedRow != -1 && selectedColumn != -1) {
+            boolean dummy = editCellAt(selectedRow, selectedColumn);
+            System.out.println("editCellAt: " + dummy);
+          }
+        }
+        return super.processKeyBinding(ks, e, condition, pressed);
+      }
+
+      protected boolean isTabOrEnterKey(KeyStroke ks) {
+        return KeyStroke.getKeyStroke('\t').equals(ks) || KeyStroke.getKeyStroke('\n').equals(ks);
+      }
+    };
+    table.putClientProperty("terminateEditOnFocusLost", Boolean.TRUE);
     // table.setSurrendersFocusOnKeystroke(true);
     // table.setShowGrid(false);
     // table.setShowHorizontalLines(false);
     // table.setShowVerticalLines(false);
-    table.putClientProperty("terminateEditOnFocusLost", Boolean.TRUE);
+
     add(check, BorderLayout.NORTH);
     add(new JScrollPane(table));
     setPreferredSize(new Dimension(320, 240));
