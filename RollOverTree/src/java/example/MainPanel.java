2e7f81ac9c94a0888f45f5b7e222379119779596
aterai <aterai@outlook.com>
2019-12-26 14:49:31 +0900

    refactor: new TreeCellRenderer() can be replaced with lambda

--- RollOverTree/src/java/example/MainPanel.java
+++ RollOverTree/src/java/example/MainPanel.java
@@ -17,35 +17,29 @@ import javax.swing.tree.TreeModel;
 public final class MainPanel extends JPanel {
   private MainPanel() {
     super(new BorderLayout());
-    add(new JScrollPane(makeTree(makeModel())));
-    setPreferredSize(new Dimension(320, 240));
-  }
 
-  private static JTree makeTree(TreeModel model) {
-    return new JTree(model) {
+    JTree tree = new JTree(makeModel()) {
       private final Color rolloverRowColor = new Color(0xDC_F0_FF);
       private int rollOverRowIndex = -1;
       private transient MouseMotionListener listener;
       @Override public void updateUI() {
         removeMouseMotionListener(listener);
+        setCellRenderer(null);
         super.updateUI();
-        setCellRenderer(new DefaultTreeCellRenderer() {
-          @Override public Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
-            Component c = super.getTreeCellRendererComponent(tree, value, selected, expanded, leaf, row, hasFocus);
-            if (c instanceof JComponent) {
-              JComponent jc = (JComponent) c;
-              if (row == rollOverRowIndex) {
-                jc.setOpaque(true);
-                jc.setBackground(rolloverRowColor);
-                if (selected) {
-                  jc.setForeground(getTextNonSelectionColor());
-                }
-              } else {
-                jc.setOpaque(false);
-              }
+        DefaultTreeCellRenderer r = new DefaultTreeCellRenderer();
+        setCellRenderer((tree, value, selected, expanded, leaf, row, hasFocus) -> {
+          Component c = r.getTreeCellRendererComponent(tree, value, selected, expanded, leaf, row, hasFocus);
+          boolean isRollOver = row == rollOverRowIndex;
+          if (isRollOver) {
+            c.setBackground(rolloverRowColor);
+            if (selected) {
+              c.setForeground(r.getTextNonSelectionColor());
             }
-            return c;
           }
+          if (c instanceof JComponent) {
+            ((JComponent) c).setOpaque(isRollOver);
+          }
+          return c;
         });
         listener = new MouseAdapter() {
           @Override public void mouseMoved(MouseEvent e) {
@@ -59,22 +53,25 @@ public final class MainPanel extends JPanel {
         addMouseMotionListener(listener);
       }
     };
+
+    add(new JScrollPane(tree));
+    setPreferredSize(new Dimension(320, 240));
   }
 
-  private static DefaultTreeModel makeModel() {
+  private static TreeModel makeModel() {
     DefaultMutableTreeNode set1 = new DefaultMutableTreeNode("Set 001");
     set1.add(new DefaultMutableTreeNode("111111111"));
     set1.add(new DefaultMutableTreeNode("22222222222"));
     set1.add(new DefaultMutableTreeNode("33333"));
 
     DefaultMutableTreeNode set2 = new DefaultMutableTreeNode("Set 002");
-    set2.add(new DefaultMutableTreeNode("asdfasdfas"));
-    set2.add(new DefaultMutableTreeNode("asdf"));
+    set2.add(new DefaultMutableTreeNode("444444444"));
+    set2.add(new DefaultMutableTreeNode("5555"));
 
     DefaultMutableTreeNode set3 = new DefaultMutableTreeNode("Set 003");
-    set3.add(new DefaultMutableTreeNode("asdfasdfasdf"));
-    set3.add(new DefaultMutableTreeNode("qwerqwer"));
-    set3.add(new DefaultMutableTreeNode("zvxcvzxcvzxzxcvzxcv"));
+    set3.add(new DefaultMutableTreeNode("666666666666"));
+    set3.add(new DefaultMutableTreeNode("777777777"));
+    set3.add(new DefaultMutableTreeNode("88888888888888"));
 
     DefaultMutableTreeNode root = new DefaultMutableTreeNode("Root");
     root.add(set1);
