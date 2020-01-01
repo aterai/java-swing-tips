31951e93777c38ec1366715f666e3adb68ba6081
aterai <aterai@outlook.com>
2020-01-02 05:05:26 +0900

    refactor: replace 'StringBuilder' with 'String'

--- BigDecimalSpinnerModel/src/java/example/MainPanel.java
+++ BigDecimalSpinnerModel/src/java/example/MainPanel.java
@@ -15,6 +15,7 @@ public final class MainPanel extends JPanel {
     Box box = Box.createVerticalBox();
     box.add(new JLabel("SpinnerNumberModel(double, ...)"));
     box.add(Box.createVerticalStrut(2));
+
     JPanel p1 = new JPanel(new GridLayout(1, 2, 5, 5));
     p1.add(new JSpinner(new SpinnerNumberModel(2.01, 2.00, 3.02, .01)));
     p1.add(new JSpinner(new SpinnerNumberModel(29.7, 29.6, 30.2, .1)));
@@ -22,6 +23,7 @@ public final class MainPanel extends JPanel {
     box.add(Box.createVerticalStrut(5));
     box.add(new JLabel("BigDecimalSpinnerModel"));
     box.add(Box.createVerticalStrut(2));
+
     JPanel p2 = new JPanel(new GridLayout(1, 2, 5, 5));
     p2.add(new JSpinner(new BigDecimalSpinnerModel(2.01, 2.00, 3.02, .01)));
     p2.add(new JSpinner(new BigDecimalSpinnerModel(29.7, 29.6, 30.2, .1)));
@@ -29,14 +31,13 @@ public final class MainPanel extends JPanel {
     box.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 
     // TEST:
-    StringBuilder sb = new StringBuilder();
     double d = 29.7 - 29.6 - .1;
-    sb.append(String.format("%f-%f-%f>=0:%b%n", 29.7, 29.6, .1, d >= 0))
-      .append(String.format("abs(%f-%f-%f)<1.0e-14:%b%n", 29.7, 29.6, .1, Math.abs(d) < 1.0e-14))
-      .append(String.format("abs(%f-%f-%f)<1.0e-15:%b%n", 29.7, 29.6, .1, Math.abs(d) < 1.0e-15));
+    String str1 = String.format("%f-%f-%f>=0:%b%n", 29.7, 29.6, .1, d >= .0);
+    String str2 = String.format("abs(%f-%f-%f)<1.0e-14:%b%n", 29.7, 29.6, .1, Math.abs(d) < 1.0e-14);
+    String str3 = String.format("abs(%f-%f-%f)<1.0e-15:%b%n", 29.7, 29.6, .1, Math.abs(d) < 1.0e-15);
 
     add(box, BorderLayout.NORTH);
-    add(new JScrollPane(new JTextArea(sb.toString())));
+    add(new JScrollPane(new JTextArea(str1 + str2 + str3)));
     setPreferredSize(new Dimension(320, 240));
   }
 
@@ -66,14 +67,15 @@ class BigDecimalSpinnerModel extends SpinnerNumberModel {
   }
 
   @Override public Object getPreviousValue() {
-    return incrValue(-1);
+    return incrValue2(-1);
   }
 
   @Override public Object getNextValue() {
-    return incrValue(+1);
+    return incrValue2(+1);
   }
 
-  private Number incrValue(int dir) {
+  // @see SpinnerNumberModel#incrValue(int dir)
+  private Number incrValue2(int dir) {
     BigDecimal value = BigDecimal.valueOf((Double) getNumber());
     BigDecimal stepSize = BigDecimal.valueOf((Double) getStepSize());
     BigDecimal newValue = dir > 0 ? value.add(stepSize) : value.subtract(stepSize);
