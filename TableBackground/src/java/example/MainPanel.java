9fc53951c595dbe57724915fc18697feeb6cf7b8
aterai <aterai@outlook.com>
2020-02-17 20:37:06 +0900

    refactor: field can be converted to a local variable

--- TableBackground/src/java/example/MainPanel.java
+++ TableBackground/src/java/example/MainPanel.java
@@ -8,33 +8,31 @@ import java.awt.*;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
+import javax.swing.table.TableModel;
 
 public final class MainPanel extends JPanel {
-  private final String[] columnNames = {"String", "Integer", "Boolean"};
-  private final Object[][] data = {
-    {"aaa", 12, true}, {"bbb", 5, false},
-    {"CCC", 92, true}, {"DDD", 0, false}
-  };
-  private final DefaultTableModel model = new DefaultTableModel(data, columnNames) {
-    @Override public Class<?> getColumnClass(int column) {
-      // ArrayIndexOutOfBoundsException: 0 >= 0
-      // [JDK-6967479] JTable sorter fires even if the model is empty - Java Bug System
-      // https://bugs.openjdk.java.net/browse/JDK-6967479
-      // return getValueAt(0, column).getClass();
-      switch (column) {
-        case 0: return String.class;
-        case 1: return Number.class;
-        case 2: return Boolean.class;
-        default: return super.getColumnClass(column);
-      }
-    }
-  };
-  private final JTable table = new JTable(model);
-  private final JCheckBox check = new JCheckBox("viewport setOpaque", true);
-  private final JButton button = new JButton("Choose background color");
-
-  public MainPanel() {
+  private MainPanel() {
     super(new BorderLayout());
+    String[] columnNames = {"String", "Integer", "Boolean"};
+    Object[][] data = {
+        {"aaa", 12, true}, {"bbb", 5, false},
+        {"CCC", 92, true}, {"DDD", 0, false}
+    };
+    TableModel model = new DefaultTableModel(data, columnNames) {
+      @Override public Class<?> getColumnClass(int column) {
+        // ArrayIndexOutOfBoundsException: 0 >= 0
+        // [JDK-6967479] JTable sorter fires even if the model is empty - Java Bug System
+        // https://bugs.openjdk.java.net/browse/JDK-6967479
+        // return getValueAt(0, column).getClass();
+        switch (column) {
+          case 0: return String.class;
+          case 1: return Number.class;
+          case 2: return Boolean.class;
+          default: return super.getColumnClass(column);
+        }
+      }
+    };
+    JTable table = new JTable(model);
     table.setAutoCreateRowSorter(true);
     table.setRowSelectionAllowed(true);
     table.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
@@ -53,11 +51,13 @@ public final class MainPanel extends JPanel {
     scroll.getViewport().setOpaque(true);
     // scroll.getViewport().setBackground(Color.WHITE);
 
+    JCheckBox check = new JCheckBox("viewport setOpaque", true);
     check.addActionListener(e -> {
       scroll.getViewport().setOpaque(((JCheckBox) e.getSource()).isSelected());
       scroll.repaint();
     });
 
+    JButton button = new JButton("Choose background color");
     button.addActionListener(e -> {
       Color color = JColorChooser.showDialog(getRootPane(), "background color", scroll.getViewport().getBackground());
       scroll.getViewport().setBackground(color);
