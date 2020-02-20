37b48e03bef15fdb137c2e07d4ea053bf28e54d6
aterai <aterai@outlook.com>
2020-02-21 01:56:13 +0900

    refactor: field can be converted to a local variable

--- SelectAllButton/src/java/example/MainPanel.java
+++ SelectAllButton/src/java/example/MainPanel.java
@@ -12,56 +12,53 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 
 public final class MainPanel extends JPanel {
-  private final String[] columnNames = {"String", "Integer", "Boolean"};
-  private final Object[][] data = {
-    {"aaa", 12, true}, {"bbb", 5, false},
-    {"CCC", 92, true}, {"DDD", 0, false}
-  };
-  private final TableModel model = new DefaultTableModel(data, columnNames) {
-    @Override public Class<?> getColumnClass(int column) {
-      return getValueAt(0, column).getClass();
-    }
-  };
-  private final JTable table = new JTable(model);
-  private final Action selectAllAction = new AbstractAction("selectAll") {
-    @Override public void actionPerformed(ActionEvent e) {
-      e.setSource(table);
-      table.getActionMap().get("selectAll").actionPerformed(e);
-    }
-  };
-  private final Action copyAction = new AbstractAction("copy") {
-    @Override public void actionPerformed(ActionEvent e) {
-      e.setSource(table);
-      table.getActionMap().get("copy").actionPerformed(e);
-    }
-  };
-
   private MainPanel() {
     super(new BorderLayout());
+    String[] columnNames = {"String", "Integer", "Boolean"};
+    Object[][] data = {
+      {"aaa", 12, true}, {"bbb", 5, false},
+      {"CCC", 92, true}, {"DDD", 0, false}
+    };
+    TableModel model = new DefaultTableModel(data, columnNames) {
+      @Override public Class<?> getColumnClass(int column) {
+        return getValueAt(0, column).getClass();
+      }
+    };
+    JTable table = new JTable(model);
+
+    Action selectAllAction = new AbstractAction("selectAll") {
+      @Override public void actionPerformed(ActionEvent e) {
+        e.setSource(table);
+        table.getActionMap().get("selectAll").actionPerformed(e);
+      }
+    };
+    Action copyAction = new AbstractAction("copy") {
+      @Override public void actionPerformed(ActionEvent e) {
+        e.setSource(table);
+        table.getActionMap().get("copy").actionPerformed(e);
+      }
+    };
 
     JPanel p = new JPanel();
     p.add(new JButton(selectAllAction));
     p.add(new JButton(copyAction));
 
-    JSplitPane sp = new JSplitPane(JSplitPane.VERTICAL_SPLIT); // Panel(new GridLayout(2, 1));
+    JSplitPane sp = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
     sp.setTopComponent(new JScrollPane(table));
     sp.setBottomComponent(new JScrollPane(new JTextArea()));
     sp.setResizeWeight(.5);
 
-    add(p, BorderLayout.NORTH);
-    add(sp);
-    setPreferredSize(new Dimension(320, 240));
-    EventQueue.invokeLater(() -> getRootPane().setJMenuBar(createMenuBar()));
-  }
-
-  private JMenuBar createMenuBar() {
     JMenuBar menuBar = new JMenuBar();
     JMenu menu = new JMenu("Edit");
     menu.setMnemonic(KeyEvent.VK_E);
     menuBar.add(menu);
     menu.add(selectAllAction);
     menu.add(copyAction);
-    return menuBar;
+
+    add(p, BorderLayout.NORTH);
+    add(sp);
+    setPreferredSize(new Dimension(320, 240));
+    EventQueue.invokeLater(() -> getRootPane().setJMenuBar(menuBar));
   }
 
   public static void main(String[] args) {
