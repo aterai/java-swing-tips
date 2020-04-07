6f961cef91f2dd87a809169b68570966ae4fc569
aterai <aterai@outlook.com>
2020-04-07 17:27:44 +0900

    refactor: statement lambda can be replaced with expression lambda

--- HonorDisplayProperties/src/java/example/MainPanel.java
+++ HonorDisplayProperties/src/java/example/MainPanel.java
@@ -45,12 +45,12 @@ public final class MainPanel extends JPanel {
     editor.setText(buf.toString());
 
     JCheckBox check = new JCheckBox("JEditorPane.HONOR_DISPLAY_PROPERTIES");
-    check.addActionListener(e -> {
-      editor.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, ((JCheckBox) e.getSource()).isSelected());
-      // HTMLEditorKit htmlEditorKit = (HTMLEditorKit) editor.getEditorKit();
-      // StyleSheet styles = htmlEditorKit.getStyleSheet();
-      // styles.addRule("body {font-size: 64pt;}");
-    });
+    check.addActionListener(e ->
+        editor.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, ((JCheckBox) e.getSource()).isSelected()));
+    // check.addActionListener(e -> {
+    //   HTMLEditorKit kit = (HTMLEditorKit) editor.getEditorKit();
+    //   kit.getStyleSheet().addRule("body {font-size: 64pt;}");
+    // });
 
     add(check, BorderLayout.NORTH);
     add(new JScrollPane(editor));
