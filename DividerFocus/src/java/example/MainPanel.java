93b91d80fa95817edb5443f6739f04370f465b70
aterai <aterai@outlook.com>
2020-05-11 18:33:33 +0900

    add: DividerFocus

---
+++ DividerFocus/src/java/example/MainPanel.java
@@ -0,0 +1,60 @@
+// -*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+// @homepage@
+
+package example;
+
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicSplitPaneUI;
+
+public final class MainPanel extends JPanel {
+  private MainPanel() {
+    super(new GridLayout(2, 1));
+
+    JSplitPane splitPane = new JSplitPane();
+    Container divider = ((BasicSplitPaneUI) splitPane.getUI()).getDivider();
+    divider.addMouseListener(new MouseAdapter() {
+      @Override public void mousePressed(MouseEvent e) {
+        super.mousePressed(e);
+        splitPane.requestFocusInWindow();
+        // or
+        // Action startResize = splitPane.getActionMap().get("startResize");
+        // startResize.actionPerformed(new ActionEvent(splitPane, ActionEvent.ACTION_PERFORMED, "startResize"));
+      }
+    });
+
+    add(makeTitledPanel("Default", new JSplitPane()));
+    add(makeTitledPanel("Divider.addMouseListener", splitPane));
+    setPreferredSize(new Dimension(320, 240));
+  }
+
+  private static Component makeTitledPanel(String title, Component c) {
+    JPanel p = new JPanel(new BorderLayout());
+    p.setBorder(BorderFactory.createTitledBorder(title));
+    p.add(c);
+    return p;
+  }
+
+
+  public static void main(String[] args) {
+    EventQueue.invokeLater(MainPanel::createAndShowGui);
+  }
+
+  private static void createAndShowGui() {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+      ex.printStackTrace();
+      Toolkit.getDefaultToolkit().beep();
+    }
+    JFrame frame = new JFrame("@title@");
+    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    frame.getContentPane().add(new MainPanel());
+    frame.pack();
+    frame.setLocationRelativeTo(null);
+    frame.setVisible(true);
+  }
+}
