7910c2a10ef89b8db8f110171e46b729b88fd6b2
aterai <aterai@outlook.com>
2020-06-25 18:13:49 +0900

    refactor: call to 'Thread.sleep()' in a loop, probably busy-waiting

--- NimbusColorPalette/src/java/example/MainPanel.java
+++ NimbusColorPalette/src/java/example/MainPanel.java
@@ -96,15 +96,19 @@ public final class MainPanel extends JPanel implements HierarchyListener {
 }
 
 class BackgroundTask extends SwingWorker<String, Void> {
-  @Override public String doInBackground() throws InterruptedException {
+  @Override protected String doInBackground() throws InterruptedException {
     int current = 0;
     int lengthOfTask = 100;
     while (current <= lengthOfTask && !isCancelled()) {
-      Thread.sleep(50); // dummy task
+      doSomething();
       setProgress(100 * current / lengthOfTask);
       current++;
     }
     return "Done";
+  }
+
+  protected void doSomething() throws InterruptedException {
+    Thread.sleep(50); // dummy task
   }
 }
 
