8304923b2f6dce70c9cac77cd87c8ef32c0a7838
aterai <aterai@outlook.com>
2020-07-17 15:25:49 +0900

    refactor: field can be converted to a local variable

--- ColumnSelectability/src/java/example/MainPanel.java
+++ ColumnSelectability/src/java/example/MainPanel.java
@@ -11,43 +11,42 @@ import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableModel;
 
 public final class MainPanel extends JPanel {
-  private final String[] columnNames = {"String", "Integer", "Boolean"};
-  private final Object[][] data = {
-    {"aaa", 12, true}, {"bbb", 5, false},
-    {"CCC", 92, true}, {"DDD", 0, false}
-  };
-  private final TableModel model = new DefaultTableModel(data, columnNames) {
-    @Override public Class<?> getColumnClass(int column) {
-      return getValueAt(0, column).getClass();
-    }
-  };
-  private final JTable table = new JTable(model) {
-    protected boolean isColumnSelectable(int column) {
-      return convertColumnIndexToModel(column) == 0;
-    }
-
-    @Override public void changeSelection(int rowIndex, int columnIndex, boolean toggle, boolean extend) {
-      if (!isColumnSelectable(columnIndex)) {
-        return;
-      }
-      super.changeSelection(rowIndex, columnIndex, toggle, extend);
-    }
-
-    @Override public boolean isCellEditable(int row, int column) {
-      return isColumnSelectable(column);
-    }
-
-    @Override public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
-      if (isColumnSelectable(column)) {
-        return super.prepareRenderer(renderer, row, column);
-      } else {
-        return renderer.getTableCellRendererComponent(this, getValueAt(row, column), false, false, row, column);
-      }
-    }
-  };
-
   private MainPanel() {
     super(new GridLayout(2, 1));
+    String[] columnNames = {"String", "Integer", "Boolean"};
+    Object[][] data = {
+        {"aaa", 12, true}, {"bbb", 5, false},
+        {"CCC", 92, true}, {"DDD", 0, false}
+    };
+    TableModel model = new DefaultTableModel(data, columnNames) {
+      @Override public Class<?> getColumnClass(int column) {
+        return getValueAt(0, column).getClass();
+      }
+    };
+    JTable table = new JTable(model) {
+      protected boolean isColumnSelectable(int column) {
+        return convertColumnIndexToModel(column) == 0;
+      }
+
+      @Override public void changeSelection(int rowIndex, int columnIndex, boolean toggle, boolean extend) {
+        if (!isColumnSelectable(columnIndex)) {
+          return;
+        }
+        super.changeSelection(rowIndex, columnIndex, toggle, extend);
+      }
+
+      @Override public boolean isCellEditable(int row, int column) {
+        return isColumnSelectable(column);
+      }
+
+      @Override public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
+        if (isColumnSelectable(column)) {
+          return super.prepareRenderer(renderer, row, column);
+        } else {
+          return renderer.getTableCellRendererComponent(this, getValueAt(row, column), false, false, row, column);
+        }
+      }
+    };
     table.setCellSelectionEnabled(true);
     // table.putClientProperty("Table.isFileList", Boolean.TRUE);
     // table.getTableHeader().setReorderingAllowed(false);
