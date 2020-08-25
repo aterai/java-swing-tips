6a6236f7efb032a423fd0fc3cb47f0d92e1c7e44
aterai <aterai@outlook.com>
2020-08-26 02:43:05 +0900

    refactor: field can be converted to a local variable

--- CheckBoxCellEditor/src/java/example/MainPanel.java
+++ CheckBoxCellEditor/src/java/example/MainPanel.java
@@ -18,39 +18,37 @@ import javax.swing.table.TableCellEditor;
 import javax.swing.table.TableModel;
 
 public final class MainPanel extends JPanel {
-  private final String[] columnNames = {"String", "Boolean"};
-  private final Object[][] data = {
-    {"AAA", true}, {"bbb", false},
-    {"CCC", true}, {"ddd", false},
-    {"EEE", true}, {"fff", false},
-  };
-  private final TableModel model = new DefaultTableModel(data, columnNames) {
-    @Override public Class<?> getColumnClass(int column) {
-      return column == 1 ? Boolean.class : super.getColumnClass(column);
-    }
-
-    @Override public boolean isCellEditable(int row, int column) {
-      return column == 1;
-    }
-  };
-  private final JTable table = new JTable(model) {
-    @Override public void updateUI() {
-      // setDefaultRenderer(Boolean.class, null);
-      setDefaultEditor(Boolean.class, null);
-      super.updateUI();
-      // setDefaultRenderer(Boolean.class, new CheckBoxPanelRenderer());
-      setDefaultEditor(Boolean.class, new CheckBoxPanelEditor());
-    }
-  };
-
   private MainPanel() {
     super(new BorderLayout());
-    table.putClientProperty("terminateEditOnFocusLost", Boolean.TRUE);
-    table.setRowHeight(24);
-    table.setRowSelectionAllowed(true);
-    table.setShowVerticalLines(false);
-    table.setIntercellSpacing(new Dimension(0, 1));
-    table.setFocusable(false);
+    String[] columnNames = {"String", "Boolean"};
+    Object[][] data = {
+      {"AAA", true}, {"bbb", false}, {"CCC", true},
+      {"ddd", false}, {"EEE", true}, {"fff", false},
+    };
+    TableModel model = new DefaultTableModel(data, columnNames) {
+      @Override public Class<?> getColumnClass(int column) {
+        return column == 1 ? Boolean.class : super.getColumnClass(column);
+      }
+
+      @Override public boolean isCellEditable(int row, int column) {
+        return column == 1;
+      }
+    };
+    JTable table = new JTable(model) {
+      @Override public void updateUI() {
+        // setDefaultRenderer(Boolean.class, null);
+        setDefaultEditor(Boolean.class, null);
+        super.updateUI();
+        // setDefaultRenderer(Boolean.class, new CheckBoxPanelRenderer());
+        setDefaultEditor(Boolean.class, new CheckBoxPanelEditor());
+        putClientProperty("terminateEditOnFocusLost", Boolean.TRUE);
+        setRowHeight(24);
+        setRowSelectionAllowed(true);
+        setShowVerticalLines(false);
+        setIntercellSpacing(new Dimension(0, 1));
+        setFocusable(false);
+      }
+    };
     add(new JScrollPane(table));
     setPreferredSize(new Dimension(320, 240));
   }
@@ -147,6 +145,7 @@ class CheckBoxPanelEditor extends AbstractCellEditor implements TableCellEditor 
     }
   }
 }
+
 // class CheckBoxPanelEditor extends AbstractCellEditor implements TableCellEditor {
 //   private final JPanel p = new JPanel(new GridBagLayout());
 //   private final JCheckBox checkBox = new JCheckBox();
@@ -159,15 +158,18 @@ class CheckBoxPanelEditor extends AbstractCellEditor implements TableCellEditor 
 //     p.add(checkBox);
 //     p.setBorder(UIManager.getBorder("Table.noFocusBorder"));
 //   }
+//
 //   @Override public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
 //     checkBox.setSelected(Objects.equals(value, Boolean.TRUE));
 //     p.setBackground(table.getSelectionBackground());
 //     return p;
 //   }
+//
 //   @Override public Object getCellEditorValue() {
 //     return checkBox.isSelected();
 //   }
 // }
+//
 // class CheckBoxPanelRenderer implements TableCellRenderer {
 //   private final JPanel p = new JPanel(new GridBagLayout());
 //   private final JCheckBox checkBox = new JCheckBox();
@@ -178,6 +180,7 @@ class CheckBoxPanelEditor extends AbstractCellEditor implements TableCellEditor 
 //     p.add(checkBox);
 //     p.setBorder(UIManager.getBorder("Table.noFocusBorder"));
 //   }
+//
 //   @Override public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
 //     checkBox.setSelected(Objects.equals(value, Boolean.TRUE));
 //     p.setBackground(isSelected ? table.getSelectionBackground() : table.getBackground());
