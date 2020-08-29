39a4fff0be1084e1613ff5913b0b221a66ddb508
aterai <aterai@outlook.com>
2020-08-30 02:24:24 +0900

    refactor: field can be converted to a local variable

--- DividerDraggingColor/src/java/example/MainPanel.java
+++ DividerDraggingColor/src/java/example/MainPanel.java
@@ -6,27 +6,14 @@ package example;
 
 import java.awt.*;
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableModel;
 
 public final class MainPanel extends JPanel {
-  private final String[] columnNames = {"String", "Integer", "Boolean"};
-  private final Object[][] data = {
-    {"aaa", 12, true}, {"bbb", 5, false},
-    {"CCC", 92, true}, {"DDD", 0, false}
-  };
-  private final TableModel model = new DefaultTableModel(data, columnNames) {
-    @Override public Class<?> getColumnClass(int column) {
-      return getValueAt(0, column).getClass();
-    }
-  };
-
   private MainPanel() {
     super(new BorderLayout());
     UIManager.put("SplitPaneDivider.draggingColor", new Color(0x64_FF_64_64, true));
 
     JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
-    splitPane.setTopComponent(new JScrollPane(new JTable(model)));
+    splitPane.setTopComponent(new JScrollPane(new JTable(5, 3)));
     splitPane.setBottomComponent(new JScrollPane(new JTree()));
     splitPane.setDividerSize(24);
     // splitPane.setContinuousLayout(false);
