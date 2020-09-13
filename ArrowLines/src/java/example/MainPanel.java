1f9512f402ac24f015f5b46094a9ee51c3cf155a
aterai <aterai@outlook.com>
2020-09-14 00:57:43 +0900

    add: ArrowLines

---
+++ ArrowLines/src/java/example/MainPanel.java
@@ -0,0 +1,87 @@
+// -*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+// @homepage@
+
+package example;
+
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Path2D;
+import java.util.Arrays;
+import java.util.List;
+import javax.swing.*;
+
+public final class MainPanel extends JPanel {
+  private MainPanel() {
+    super(new BorderLayout());
+    List<Arrow> arrows = Arrays.asList(
+        new Arrow(new Point(50, 50), new Point(100, 150)),
+        new Arrow(new Point(250, 50), new Point(150, 50)));
+
+    JPanel p = new JPanel() {
+      @Override protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setStroke(new BasicStroke(4));
+        g2.setColor(Color.BLACK);
+        for (Arrow a: arrows) {
+          a.draw(g2);
+        }
+        g2.dispose();
+      }
+    };
+    add(p);
+    setPreferredSize(new Dimension(320, 240));
+  }
+
+  public static void main(String[] args) {
+    EventQueue.invokeLater(MainPanel::createAndShowGui);
+  }
+
+  private static void createAndShowGui() {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+      ex.printStackTrace();
+      Toolkit.getDefaultToolkit().beep();
+    }
+    JFrame frame = new JFrame("@title@");
+    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    frame.getContentPane().add(new MainPanel());
+    frame.pack();
+    frame.setLocationRelativeTo(null);
+    frame.setVisible(true);
+  }
+}
+
+class Arrow {
+  private final Point start = new Point();
+  private final Point end = new Point();
+  private final Path2D arrowHead;
+
+  protected Arrow(Point start, Point end) {
+    this.start.setLocation(start);
+    this.end.setLocation(end);
+    arrowHead = makeArrowHead(new Dimension(8, 8));
+  }
+
+  protected Path2D makeArrowHead(Dimension size) {
+    Path2D path = new Path2D.Double();
+    double t = size.height;
+    double w = size.width * .5;
+    path.moveTo(0d, -w);
+    path.lineTo(t, 0d);
+    path.lineTo(0d, w);
+    path.closePath();
+    return path;
+  }
+
+  public void draw(Graphics2D g2) {
+    g2.drawLine(start.x, start.y, end.x, end.y);
+    arrowHead.transform(AffineTransform.getRotateInstance(end.getX() - start.getX(), end.getY() - start.getY()));
+    arrowHead.transform(AffineTransform.getTranslateInstance(end.getX(), end.getY()));
+    g2.fill(arrowHead);
+    g2.draw(arrowHead);
+  }
+}
