650ab338cb37f568c19dac6d8ac50c156aa8ef92
aterai <aterai@outlook.com>
2020-09-28 00:42:18 +0900

    add: PasswordFieldCutCopyAllowed

---
+++ PasswordFieldCutCopyAllowed/src/java/example/MainPanel.java
@@ -0,0 +1,55 @@
+// -*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+// @homepage@
+
+package example;
+
+import java.awt.*;
+import javax.swing.*;
+
+public final class MainPanel extends JPanel {
+  private MainPanel() {
+    super(new BorderLayout(10, 10));
+    JPasswordField password = new JPasswordField();
+    password.putClientProperty("JPasswordField.cutCopyAllowed", Boolean.TRUE);
+
+    JPanel p = new JPanel(new GridLayout(2, 1));
+    p.add(makeTitledPanel("JPasswordField.cutCopyAllowed: false(default)", new JPasswordField()));
+    p.add(makeTitledPanel("JPasswordField.cutCopyAllowed: true", password));
+
+    add(p, BorderLayout.NORTH);
+    add(new JScrollPane(new JTextArea()));
+    setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+    setPreferredSize(new Dimension(320, 240));
+  }
+
+  private static Component makeTitledPanel(String title, Component cmp) {
+    JPanel p = new JPanel(new GridBagLayout());
+    p.setBorder(BorderFactory.createTitledBorder(title));
+    GridBagConstraints c = new GridBagConstraints();
+    c.weightx = 1d;
+    c.fill = GridBagConstraints.HORIZONTAL;
+    c.insets = new Insets(5, 5, 5, 5);
+    p.add(cmp, c);
+    return p;
+  }
+
+  public static void main(String[] args) {
+    EventQueue.invokeLater(MainPanel::createAndShowGui);
+  }
+
+  private static void createAndShowGui() {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+      ex.printStackTrace();
+      Toolkit.getDefaultToolkit().beep();
+    }
+    JFrame frame = new JFrame("@title@");
+    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    frame.getContentPane().add(new MainPanel());
+    frame.pack();
+    frame.setLocationRelativeTo(null);
+    frame.setVisible(true);
+  }
+}
