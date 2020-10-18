e69407e331618fa2abc4e7bbc42532f14c021a83
aterai <aterai@outlook.com>
2020-10-19 02:58:55 +0900

    refactor: class 'QuadrantRotateIcon' is never used

--- RotatedIcon/src/java/example/MainPanel.java
+++ RotatedIcon/src/java/example/MainPanel.java
@@ -13,18 +13,10 @@ public final class MainPanel extends JPanel {
   private MainPanel() {
     super();
     Icon i = new ImageIcon(getClass().getResource("duke.gif"));
-    // Icon i = UIManager.getIcon("OptionPane.warningIcon");
     add(makeLabel("Default", i));
     add(makeLabel("Rotate: 180", new RotateIcon(i, 180)));
     add(makeLabel("Rotate:  90", new RotateIcon(i, 90)));
     add(makeLabel("Rotate: -90", new RotateIcon(i, -90)));
-
-    // setOpaque(true);
-    // setBackground(Color.RED);
-    // add(makeLabel("Rotate: 180", new QuadrantRotateIcon(i, QuadrantRotate.VERTICAL_FLIP)));
-    // add(makeLabel("Rotate: 90", new QuadrantRotateIcon(i, QuadrantRotate.CLOCKWISE)));
-    // add(makeLabel("Rotate: -90", new QuadrantRotateIcon(i, QuadrantRotate.COUNTER_CLOCKWISE)));
-
     setBorder(BorderFactory.createEmptyBorder(0, 32, 0, 32));
     setPreferredSize(new Dimension(320, 240));
   }
@@ -91,22 +83,7 @@ class RotateIcon implements Icon {
         trans = AffineTransform.getTranslateInstance(0, 0);
         break;
     }
-    // if (numquadrants == 1 || numquadrants == -3) {
-    //   trans = AffineTransform.getTranslateInstance(dim.height, 0);
-    //   int v = dim.width;
-    //   dim.width = dim.height;
-    //   dim.height = v;
-    // } else if (numquadrants == -1 || numquadrants == 3) {
-    //   trans = AffineTransform.getTranslateInstance(0, dim.width);
-    //   int v = dim.width;
-    //   dim.width = dim.height;
-    //   dim.height = v;
-    // } else if (Math.abs(numquadrants) == 2) {
-    //   trans = AffineTransform.getTranslateInstance(dim.width, dim.height);
-    // } else {
-    //   trans = AffineTransform.getTranslateInstance(0, 0);
-    // }
-    trans.quadrantRotate(numquadrants);
+    trans.rotate(Math.toRadians(90d * numquadrants));
   }
 
   @Override public void paintIcon(Component c, Graphics g, int x, int y) {
@@ -122,61 +99,5 @@ class RotateIcon implements Icon {
 
   @Override public int getIconHeight() {
     return dim.height;
-  }
-}
-
-enum QuadrantRotate {
-  CLOCKWISE(1),
-  VERTICAL_FLIP(2),
-  COUNTER_CLOCKWISE(-1);
-  private final int numquadrants;
-  QuadrantRotate(int numquadrants) {
-    this.numquadrants = numquadrants;
-  }
-
-  public int getNumQuadrants() {
-    return numquadrants;
-  }
-
-}
-
-class QuadrantRotateIcon implements Icon {
-  private final QuadrantRotate rotate;
-  private final Icon icon;
-
-  protected QuadrantRotateIcon(Icon icon, QuadrantRotate rotate) {
-    this.icon = icon;
-    this.rotate = rotate;
-  }
-
-  @Override public void paintIcon(Component c, Graphics g, int x, int y) {
-    int w = icon.getIconWidth();
-    int h = icon.getIconHeight();
-    Graphics2D g2 = (Graphics2D) g.create();
-    g2.translate(x, y);
-    switch (rotate) {
-      case CLOCKWISE:
-        g2.translate(h, 0);
-        break;
-      case VERTICAL_FLIP:
-        g2.translate(w, h);
-        break;
-      case COUNTER_CLOCKWISE:
-        g2.translate(0, w);
-        break;
-      default:
-        throw new AssertionError("Unknown QuadrantRotateIcon");
-    }
-    g2.rotate(Math.toRadians(90d * rotate.getNumQuadrants()));
-    icon.paintIcon(c, g2, 0, 0);
-    g2.dispose();
-  }
-
-  @Override public int getIconWidth() {
-    return rotate == QuadrantRotate.VERTICAL_FLIP ? icon.getIconWidth() : icon.getIconHeight();
-  }
-
-  @Override public int getIconHeight() {
-    return rotate == QuadrantRotate.VERTICAL_FLIP ? icon.getIconHeight() : icon.getIconWidth();
   }
 }
