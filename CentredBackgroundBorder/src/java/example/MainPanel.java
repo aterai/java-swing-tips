a046215d39b495cae74a42b713c32921e9f6b2a6
aterai <aterai@outlook.com>
2020-11-19 17:41:20 +0900

    refactor: use try-with-resources

--- CentredBackgroundBorder/src/java/example/MainPanel.java
+++ CentredBackgroundBorder/src/java/example/MainPanel.java
@@ -11,6 +11,7 @@ import java.awt.image.BufferedImageOp;
 import java.awt.image.ByteLookupTable;
 import java.awt.image.LookupOp;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.Optional;
 import javax.imageio.ImageIO;
@@ -48,8 +49,8 @@ public final class MainPanel extends JPanel {
 
   private static BufferedImage getFilteredImage(URL url) {
     BufferedImage image = Optional.ofNullable(url).map(u -> {
-      try {
-        return ImageIO.read(u);
+      try (InputStream s = u.openStream()) {
+        return ImageIO.read(s);
       } catch (IOException ex) {
         return makeMissingImage();
       }
