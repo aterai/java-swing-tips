3eef19d76f390ecf1205cab8b50cab58137e5dd0
aterai <aterai@outlook.com>
2021-01-18 01:11:56 +0900

    add: ListEditor

---
+++ ListEditor/src/java/example/MainPanel.java
@@ -0,0 +1,314 @@
+// -*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+// @homepage@
+
+package example;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Objects;
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.event.MouseInputAdapter;
+import javax.swing.text.JTextComponent;
+
+public final class MainPanel extends JPanel {
+  private MainPanel() {
+    super(new BorderLayout());
+    DefaultListModel<ListItem> model = new DefaultListModel<>();
+    model.addElement(new ListItem("red", new ColorIcon(Color.RED)));
+    model.addElement(new ListItem("green", new ColorIcon(Color.GREEN)));
+    model.addElement(new ListItem("blue", new ColorIcon(Color.BLUE)));
+    model.addElement(new ListItem("cyan", new ColorIcon(Color.CYAN)));
+    model.addElement(new ListItem("darkGray", new ColorIcon(Color.DARK_GRAY)));
+    model.addElement(new ListItem("gray", new ColorIcon(Color.GRAY)));
+    model.addElement(new ListItem("lightGray", new ColorIcon(Color.LIGHT_GRAY)));
+    model.addElement(new ListItem("magenta", new ColorIcon(Color.MAGENTA)));
+    model.addElement(new ListItem("orange", new ColorIcon(Color.ORANGE)));
+    model.addElement(new ListItem("pink", new ColorIcon(Color.PINK)));
+    model.addElement(new ListItem("yellow", new ColorIcon(Color.YELLOW)));
+    model.addElement(new ListItem("black", new ColorIcon(Color.BLACK)));
+    model.addElement(new ListItem("white", new ColorIcon(Color.WHITE)));
+
+    JList<ListItem> list = new EditableList<>(model);
+    add(new JScrollPane(list));
+    setPreferredSize(new Dimension(320, 240));
+  }
+
+  public static void main(String[] args) {
+    EventQueue.invokeLater(MainPanel::createAndShowGui);
+  }
+
+  private static void createAndShowGui() {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+      ex.printStackTrace();
+      Toolkit.getDefaultToolkit().beep();
+    }
+    JFrame frame = new JFrame("@title@");
+    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    frame.getContentPane().add(new MainPanel());
+    frame.pack();
+    frame.setLocationRelativeTo(null);
+    frame.setVisible(true);
+  }
+}
+
+class ListItemListCellRenderer<E extends ListItem> implements ListCellRenderer<E> {
+  protected static final Color SELECTED_COLOR = new Color(0xAE_16_64_FF, true);
+  private final JLabel label = new JLabel("", null, SwingConstants.CENTER) {
+    @Override protected void paintComponent(Graphics g) {
+      super.paintComponent(g);
+      if (SELECTED_COLOR.equals(getBackground())) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setPaint(SELECTED_COLOR);
+        g2.fillRect(0, 0, getWidth(), getHeight());
+        g2.dispose();
+      }
+    }
+  };
+  private final JPanel renderer = new JPanel(new BorderLayout());
+  private final Border focusBorder = UIManager.getBorder("List.focusCellHighlightBorder");
+  private final Border noFocusBorder; // = UIManager.getBorder("List.noFocusBorder");
+
+  protected ListItemListCellRenderer() {
+    Border b = UIManager.getBorder("List.noFocusBorder");
+    if (Objects.isNull(b)) { // Nimbus???
+      Insets i = focusBorder.getBorderInsets(renderer);
+      b = BorderFactory.createEmptyBorder(i.top, i.left, i.bottom, i.right);
+    }
+    noFocusBorder = b;
+    label.setVerticalTextPosition(SwingConstants.BOTTOM);
+    label.setHorizontalTextPosition(SwingConstants.CENTER);
+    label.setForeground(renderer.getForeground());
+    label.setBackground(renderer.getBackground());
+    label.setBorder(noFocusBorder);
+    label.setOpaque(false);
+    renderer.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
+    renderer.add(label);
+    renderer.setOpaque(true);
+  }
+
+  @Override public Component getListCellRendererComponent(JList<? extends E> list, E value, int index, boolean isSelected, boolean cellHasFocus) {
+    label.setText(value.title);
+    label.setBorder(cellHasFocus ? focusBorder : noFocusBorder);
+    label.setIcon(value.icon);
+    if (isSelected) {
+      label.setForeground(list.getSelectionForeground());
+      renderer.setBackground(SELECTED_COLOR);
+    } else {
+      label.setForeground(list.getForeground());
+      renderer.setBackground(list.getBackground());
+    }
+    return renderer;
+  }
+}
+
+class ListItem {
+  public final Icon icon;
+  public final String title;
+
+  protected ListItem(String title, Icon icon) {
+    this.title = title;
+    this.icon = icon;
+  }
+}
+
+class ColorIcon implements Icon {
+  private final Color color;
+
+  protected ColorIcon(Color color) {
+    this.color = color;
+  }
+
+  @Override public void paintIcon(Component c, Graphics g, int x, int y) {
+    Graphics2D g2 = (Graphics2D) g.create();
+    g2.translate(x, y);
+    g2.setPaint(color);
+    g2.fillRect(0, 0, getIconWidth(), getIconHeight());
+    g2.setPaint(Color.BLACK);
+    g2.drawRect(0, 0, getIconWidth(), getIconHeight());
+    g2.dispose();
+  }
+
+  @Override public int getIconWidth() {
+    return 32;
+  }
+
+  @Override public int getIconHeight() {
+    return 32;
+  }
+}
+
+// https://github.com/aterai/java-swing-tips/blob/master/ClearSelection/src/java/example/MainPanel.java
+class ClearSelectionListener extends MouseInputAdapter {
+  private boolean startOutside;
+
+  private static <E> void clearSelectionAndFocus(JList<E> list) {
+    list.clearSelection();
+    list.getSelectionModel().setAnchorSelectionIndex(-1);
+    list.getSelectionModel().setLeadSelectionIndex(-1);
+  }
+
+  private static <E> boolean contains(JList<E> list, Point pt) {
+    for (int i = 0; i < list.getModel().getSize(); i++) {
+      if (list.getCellBounds(i, i).contains(pt)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  @Override public void mousePressed(MouseEvent e) {
+    JList<?> list = (JList<?>) e.getComponent();
+    startOutside = !contains(list, e.getPoint());
+    if (startOutside) {
+      clearSelectionAndFocus(list);
+    }
+  }
+
+  @Override public void mouseReleased(MouseEvent e) {
+    startOutside = false;
+  }
+
+  @Override public void mouseDragged(MouseEvent e) {
+    JList<?> list = (JList<?>) e.getComponent();
+    if (contains(list, e.getPoint())) {
+      startOutside = false;
+    } else if (startOutside) {
+      clearSelectionAndFocus(list);
+    }
+  }
+}
+
+class EditableList<E extends ListItem> extends JList<E> {
+  private transient MouseInputAdapter handler;
+  protected final Container glassPane = new EditorGlassPane();
+  protected final JTextField editor = new JTextField();
+  protected final Action startEditing = new AbstractAction() {
+    @Override public void actionPerformed(ActionEvent e) {
+      getRootPane().setGlassPane(glassPane);
+      int idx = getSelectedIndex();
+      ListItem item = getSelectedValue();
+      Rectangle rect = getCellBounds(idx, idx);
+      Point p = SwingUtilities.convertPoint(EditableList.this, rect.getLocation(), glassPane);
+      rect.setLocation(p);
+      int h = editor.getPreferredSize().height;
+      rect.y = rect.y + rect.height - h - 1;
+      rect.height = h;
+      rect.grow(-2, 0);
+      editor.setBounds(rect);
+      editor.setText(item.title);
+      editor.selectAll();
+      glassPane.add(editor);
+      glassPane.setVisible(true);
+      editor.requestFocusInWindow();
+    }
+  };
+  protected final Action cancelEditing = new AbstractAction() {
+    @Override public void actionPerformed(ActionEvent e) {
+      glassPane.setVisible(false);
+    }
+  };
+  protected final Action renameTitle = new AbstractAction() {
+    @Override public void actionPerformed(ActionEvent e) {
+      ListModel<E> m = getModel();
+      String title = editor.getText().trim();
+      if (!title.isEmpty() && m instanceof DefaultListModel<?>) {
+        @SuppressWarnings("unchecked")
+        DefaultListModel<ListItem> model = (DefaultListModel<ListItem>) getModel();
+        int index = getSelectedIndex();
+        ListItem item = m.getElementAt(index);
+        model.remove(index);
+        model.add(index, new ListItem(editor.getText(), item.icon));
+        setSelectedIndex(index);
+      }
+      glassPane.setVisible(false);
+    }
+  };
+
+  protected EditableList(DefaultListModel<E> model) {
+    super(model);
+    editor.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+    editor.setHorizontalAlignment(SwingConstants.CENTER);
+    // editor.setOpaque(false);
+    // editor.setLineWrap(true);
+
+    InputMap im = editor.getInputMap(JComponent.WHEN_FOCUSED);
+    im.put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "rename-title");
+    im.put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "cancel-editing");
+
+    ActionMap am = editor.getActionMap();
+    am.put("rename-title", renameTitle);
+    am.put("cancel-editing", cancelEditing);
+
+    addMouseListener(new MouseAdapter() {
+      @Override public void mouseClicked(MouseEvent e) {
+        int idx = getSelectedIndex();
+        Rectangle rect = getCellBounds(idx, idx);
+        if (rect == null) {
+          return;
+        }
+        int h = editor.getPreferredSize().height;
+        rect.y = rect.y + rect.height - h;
+        rect.height = h;
+        boolean isDoubleClick = e.getClickCount() >= 2;
+        if (isDoubleClick && rect.contains(e.getPoint())) {
+          startEditing.actionPerformed(new ActionEvent(e.getComponent(), ActionEvent.ACTION_PERFORMED, ""));
+        }
+      }
+    });
+    getInputMap(JComponent.WHEN_FOCUSED).put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "start-editing");
+    getActionMap().put("start-editing", startEditing);
+  }
+
+  @Override public void updateUI() {
+    removeMouseListener(handler);
+    setSelectionForeground(null);
+    setSelectionBackground(null);
+    setCellRenderer(null);
+    super.updateUI();
+    setLayoutOrientation(JList.HORIZONTAL_WRAP);
+    getSelectionModel().setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+    setVisibleRowCount(0);
+    setFixedCellWidth(56);
+    setFixedCellHeight(56);
+    setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+    setCellRenderer(new ListItemListCellRenderer<>());
+    handler = new ClearSelectionListener();
+    addMouseListener(handler);
+  }
+
+  protected JTextComponent getEditorTextField() {
+    return editor;
+  }
+
+  private class EditorGlassPane extends JComponent {
+    protected EditorGlassPane() {
+      super();
+      setOpaque(false);
+      setFocusTraversalPolicy(new DefaultFocusTraversalPolicy() {
+        @Override public boolean accept(Component c) {
+          return Objects.equals(c, getEditorTextField());
+        }
+      });
+      addMouseListener(new MouseAdapter() {
+        @Override public void mouseClicked(MouseEvent e) {
+          if (!getEditorTextField().getBounds().contains(e.getPoint())) {
+            renameTitle.actionPerformed(new ActionEvent(e.getComponent(), ActionEvent.ACTION_PERFORMED, ""));
+          }
+        }
+      });
+    }
+
+    @Override public void setVisible(boolean flag) {
+      super.setVisible(flag);
+      setFocusTraversalPolicyProvider(flag);
+      setFocusCycleRoot(flag);
+    }
+  }
+}
