73e28f7be691c4f1eb0b156762e64523f3d322a2
aterai <aterai@outlook.com>
2021-02-07 02:18:33 +0900

    refactor: use new JPanel(new BorderLayout()) instead of JPanel#setBorder(new BorderLayout())

--- JLayeredPane1/src/java/example/MainPanel.java
+++ JLayeredPane1/src/java/example/MainPanel.java
@@ -9,7 +9,6 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.Objects;
 import javax.swing.*;
-import javax.swing.border.Border;
 
 /**
  * JLayeredPane1.
@@ -18,24 +17,53 @@ import javax.swing.border.Border;
  */
 public final class MainPanel extends JPanel {
   private static final int BACK_LAYER = 1;
-  // private static final int FORE_LAYER = 2;
   private static final Font FONT = new Font(Font.MONOSPACED, Font.PLAIN, 12);
   private static final int[] COLORS = {
-      0xDD_DD_DD, 0xAA_AA_FF, 0xFF_AA_AA, 0xAA_FF_AA, 0xFF_FF_AA, 0xFF_AA_FF, 0xAA_FF_FF
+    0xDD_DD_DD, 0xAA_AA_FF, 0xFF_AA_AA, 0xAA_FF_AA, 0xFF_FF_AA, 0xFF_AA_FF, 0xAA_FF_FF
   };
-  private final JLayeredPane layerPane;
 
   private MainPanel() {
     super(new BorderLayout());
-
-    layerPane = new BackImageLayeredPane(new ImageIcon(getClass().getResource("tokeidai.jpg")).getImage());
+    Image image = new ImageIcon(getClass().getResource("tokeidai.jpg")).getImage();
+    JLayeredPane layer = new BackImageLayeredPane(image);
     for (int i = 0; i < 7; i++) {
-      JPanel p = createPanel(i);
-      p.setLocation(i * 70 + 20, i * 50 + 15);
-      layerPane.add(p, BACK_LAYER);
+      JPanel p = createPanel(layer, i);
+      p.setLocation(i * 20 + 10, i * 20 + 5);
+      layer.add(p, BACK_LAYER);
     }
-    add(layerPane);
+    add(layer);
     setPreferredSize(new Dimension(320, 240));
+  }
+
+  private static JPanel createPanel(JLayeredPane layerPane, int i) {
+    String s = "<html><font color=#333333>Header:" + i + "</font></html>";
+
+    JLabel label = new JLabel(s);
+    label.setFont(FONT);
+    label.setOpaque(true);
+    label.setHorizontalAlignment(SwingConstants.CENTER);
+    label.setBackground(getColor(COLORS[i], .85f));
+    label.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
+
+    JTextArea text = new JTextArea();
+    text.setMargin(new Insets(4, 4, 4, 4));
+    text.setLineWrap(true);
+    text.setOpaque(false);
+
+    JPanel p = new JPanel(new BorderLayout());
+    p.setOpaque(true);
+    p.setBackground(new Color(COLORS[i]));
+    p.setBorder(BorderFactory.createLineBorder(getColor(COLORS[i], .5f), 1));
+
+    // for moving the Window
+    DragMouseListener li = new DragMouseListener(layerPane);
+    p.addMouseListener(li);
+    p.addMouseMotionListener(li);
+
+    p.add(label, BorderLayout.NORTH);
+    p.add(text);
+    p.setSize(new Dimension(120, 100));
+    return p;
   }
 
   private static Color getColor(int i, float f) {
@@ -43,41 +71,6 @@ public final class MainPanel extends JPanel {
     int g = (int) ((i >> 8 & 0xFF) * f);
     int b = (int) ((i & 0xFF) * f);
     return new Color(r, g, b);
-  }
-
-  private JPanel createPanel(int i) {
-    String s = "<html><font color=#333333>ヘッダーだよん:" + i + "</font></html>";
-    JLabel label = new JLabel(s);
-    label.setFont(FONT);
-    label.setOpaque(true);
-    label.setHorizontalAlignment(SwingConstants.CENTER);
-    label.setBackground(getColor(COLORS[i], .85f));
-    Border border1 = BorderFactory.createEmptyBorder(4, 4, 4, 4);
-    label.setBorder(border1);
-
-    JTextArea text = new JTextArea();
-    text.setMargin(new Insets(4, 4, 4, 4));
-    text.setLineWrap(true);
-    text.setOpaque(false);
-
-    JPanel p = new JPanel();
-    p.setOpaque(true);
-    p.setBackground(new Color(COLORS[i]));
-
-    Color col = getColor(COLORS[i], .5f);
-    Border border = BorderFactory.createLineBorder(col, 1);
-    p.setBorder(border);
-
-    // ウインド移動用の処理
-    DragMouseListener li = new DragMouseListener(layerPane);
-    p.addMouseListener(li);
-    p.addMouseMotionListener(li);
-
-    p.setLayout(new BorderLayout());
-    p.add(label, BorderLayout.NORTH);
-    p.add(text);
-    p.setSize(new Dimension(120, 100));
-    return p;
   }
 
   public static void main(String[] args) {
@@ -100,7 +93,7 @@ public final class MainPanel extends JPanel {
   }
 }
 
-// タイトル部分のマウスクリックでパネルを最上位にもってくる。ドラッグで移動。
+// A mouse click on the title brings the panel to the top. It also moves by dragging.
 class DragMouseListener extends MouseAdapter {
   private final JLayeredPane parent;
   private final Point origin = new Point();
@@ -113,13 +106,11 @@ class DragMouseListener extends MouseAdapter {
   @Override public void mousePressed(MouseEvent e) {
     JComponent panel = (JComponent) e.getComponent();
     origin.setLocation(e.getPoint());
-    // 選択された部品を上へ
     parent.moveToFront(panel);
   }
 
   @Override public void mouseDragged(MouseEvent e) {
     JComponent panel = (JComponent) e.getComponent();
-    // ずれた分だけ JPanel を移動させる
     int dx = e.getX() - origin.x;
     int dy = e.getY() - origin.y;
     Point pt = panel.getLocation();
@@ -127,7 +118,7 @@ class DragMouseListener extends MouseAdapter {
   }
 }
 
-// 背景画像を描画する JLayeredPane
+// Draw a background image JLayeredPane
 class BackImageLayeredPane extends JLayeredPane {
   private final transient Image bgImage;
 
