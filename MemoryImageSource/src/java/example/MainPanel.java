577ccfbe36654b0dbad1f3c1978c57a81ccc484a
aterai <aterai@outlook.com>
2021-02-11 06:32:58 +0900

    refactor: method 'createCheckerTexture(int)' is never used

--- MemoryImageSource/src/java/example/MainPanel.java
+++ MemoryImageSource/src/java/example/MainPanel.java
@@ -8,6 +8,7 @@ import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 import java.awt.image.ImageProducer;
 import java.awt.image.MemoryImageSource;
@@ -42,9 +43,9 @@ public final class MainPanel extends JPanel {
 }
 
 class PaintPanel extends JPanel implements MouseMotionListener, MouseListener {
-  private Point startPoint = new Point();
+  private final Point startPoint = new Point();
   private final transient BufferedImage backImage;
-  private static final Paint TEXTURE = TextureUtils.createCheckerTexture(6, new Color(0x32_C8_96_64, true));
+  private static final Paint TEXTURE = createCheckerTexture(6, new Color(0x32_C8_96_64, true));
   private final Rectangle rect = new Rectangle(320, 240);
   private final int[] pixels = new int[rect.width * rect.height];
   private final transient ImageProducer src = new MemoryImageSource(rect.width, rect.height, pixels, 0, rect.width);
@@ -70,46 +71,65 @@ class PaintPanel extends JPanel implements MouseMotionListener, MouseListener {
   }
 
   @Override public void mouseDragged(MouseEvent e) {
-    double xdelta = e.getX() - startPoint.getX();
-    double ydelta = e.getY() - startPoint.getY();
-    double delta = Math.max(Math.abs(xdelta), Math.abs(ydelta));
+    double dx = e.getX() - startPoint.getX();
+    double dy = e.getY() - startPoint.getY();
+    double delta = Math.max(Math.abs(dx), Math.abs(dy));
 
-    double xincrement = xdelta / delta;
-    double yincrement = ydelta / delta;
-    double xstart = startPoint.x;
-    double ystart = startPoint.y;
-    Point pt = new Point();
+    double ix = dx / delta;
+    double iy = dy / delta;
+    double sx = startPoint.x;
+    double sy = startPoint.y;
+    Point2D pt = new Point2D.Double();
     for (int i = 0; i < delta; i++) {
-      pt.setLocation((int) xstart, (int) ystart);
+      pt.setLocation(sx, sy);
       // if (pt.x < 0 || pt.y < 0 || pt.x >= rect.width || pt.y >= rect.height) {
       if (!rect.contains(pt)) {
         break;
       }
       paintStamp(pt, penColor);
       // src.newPixels(pt.x - 2, pt.y - 2, 4, 4);
-      xstart += xincrement;
-      ystart += yincrement;
+      sx += ix;
+      sy += iy;
     }
-    startPoint = e.getPoint();
+    startPoint.setLocation(e.getPoint());
   }
 
-  private void paintStamp(Point p, int penc) {
+  private void paintStamp(Point2D pt, int penColor) {
+    int px = (int) pt.getX();
+    int py = (int) pt.getY();
     // 1 x 1:
-    // pixels[p.x + p.y * 320] = penc;
+    // pixels[px + py * 320] = penColor;
     // 3 x 3 square:
     for (int n = -1; n <= 1; n++) {
       for (int m = -1; m <= 1; m++) {
-        int t = p.x + n + (p.y + m) * rect.width;
+        int t = px + n + (py + m) * rect.width;
         if (t >= 0 && t < rect.width * rect.height) {
-          pixels[t] = penc;
+          pixels[t] = penColor;
         }
       }
     }
-    repaint(p.x - 2, p.y - 2, 4, 4);
+    repaint(px - 2, py - 2, 4, 4);
+  }
+
+  public static TexturePaint createCheckerTexture(int cs, Color color) {
+    int size = cs * cs;
+    BufferedImage img = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB);
+    Graphics2D g2 = img.createGraphics();
+    g2.setPaint(color);
+    g2.fillRect(0, 0, size, size);
+    for (int i = 0; i * cs < size; i++) {
+      for (int j = 0; j * cs < size; j++) {
+        if ((i + j) % 2 == 0) {
+          g2.fillRect(i * cs, j * cs, cs, cs);
+        }
+      }
+    }
+    g2.dispose();
+    return new TexturePaint(img, new Rectangle(size, size));
   }
 
   @Override public void mousePressed(MouseEvent e) {
-    startPoint = e.getPoint();
+    startPoint.setLocation(e.getPoint());
     penColor = e.getButton() == MouseEvent.BUTTON1 ? 0xFF_00_00_00 : 0x0;
   }
 
@@ -134,53 +154,25 @@ class PaintPanel extends JPanel implements MouseMotionListener, MouseListener {
   }
 }
 
-final class TextureUtils {
-  private static final Color DEFAULT_COLOR = new Color(0x64_64_64_64, true);
-
-  private TextureUtils() {
-    /* HideUtilityClassConstructor */
-  }
-
-  public static TexturePaint createCheckerTexture(int cs, Color color) {
-    int size = cs * cs;
-    BufferedImage img = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB);
-    Graphics2D g2 = img.createGraphics();
-    g2.setPaint(color);
-    g2.fillRect(0, 0, size, size);
-    for (int i = 0; i * cs < size; i++) {
-      for (int j = 0; j * cs < size; j++) {
-        if ((i + j) % 2 == 0) {
-          g2.fillRect(i * cs, j * cs, cs, cs);
-        }
-      }
-    }
-    g2.dispose();
-    return new TexturePaint(img, new Rectangle(size, size));
-  }
-
-  public static TexturePaint createCheckerTexture(int cs) {
-    return createCheckerTexture(cs, DEFAULT_COLOR);
-  }
-}
-
-// class PaintPanel extends JPanel implements MouseMotionListener, MouseListener {
+// class PaintPanel2 extends JPanel implements MouseMotionListener, MouseListener {
 //   private static final Color ERASER = new Color(0x0, true);
 //   private boolean isPen = true;
-//   private Point startPoint = new Point();
-//   private BufferedImage currentImage = null;
-//   private BufferedImage backImage = null;
-//   private TexturePaint texture = makeTexturePaint();
-//   protected PaintPanel() {
+//   private final Point startPoint = new Point();
+//   private final transient BufferedImage currentImage;
+//   private final transient BufferedImage backImage;
+//
+//   protected PaintPanel2() {
 //     super();
 //     addMouseMotionListener(this);
 //     addMouseListener(this);
 //     currentImage = new BufferedImage(320, 240, BufferedImage.TYPE_INT_ARGB);
 //     backImage = new BufferedImage(320, 240, BufferedImage.TYPE_INT_ARGB);
 //     Graphics2D g2 = backImage.createGraphics();
-//     g2.setPaint(texture);
+//     g2.setPaint(makeTexturePaint());
 //     g2.fillRect(0, 0, 320, 240);
 //     g2.dispose();
 //   }
+//
 //   private static BufferedImage makeBGImage() {
 //     Color color = new Color(0x32_C8_96_64, true);
 //     int cs = 6;
@@ -199,10 +191,12 @@ final class TextureUtils {
 //     g2.dispose();
 //     return img;
 //   }
+//
 //   private static TexturePaint makeTexturePaint() {
 //     BufferedImage img = makeBGImage();
 //     return new TexturePaint(img, new Rectangle(img.getWidth(), img.getHeight()));
 //   }
+//
 //   @Override protected void paintComponent(Graphics g) {
 //     super.paintComponent(g);
 //     // if (Objects.nonNull(backImage)) {
@@ -210,6 +204,7 @@ final class TextureUtils {
 //     // if (Objects.nonNull(currentImage)) {
 //     g.drawImage(currentImage, 0, 0, this);
 //   }
+//
 //   @Override public void mouseDragged(MouseEvent e) {
 //     Point pt = e.getPoint();
 //     Graphics2D g2 = currentImage.createGraphics();
@@ -222,16 +217,32 @@ final class TextureUtils {
 //     }
 //     g2.drawLine(startPoint.x, startPoint.y, pt.x, pt.y);
 //     g2.dispose();
-//     startPoint = pt;
+//     startPoint.setLocation(pt);
 //     repaint();
 //   }
+//
 //   @Override public void mousePressed(MouseEvent e) {
-//     startPoint = e.getPoint();
+//     startPoint.setLocation(e.getPoint());
 //     isPen = e.getButton() == MouseEvent.BUTTON1;
 //   }
-//   @Override public void mouseMoved(MouseEvent e) {}
-//   @Override public void mouseExited(MouseEvent e) {}
-//   @Override public void mouseEntered(MouseEvent e) {}
-//   @Override public void mouseReleased(MouseEvent e) {}
-//   @Override public void mouseClicked(MouseEvent e) {}
+//
+//   @Override public void mouseMoved(MouseEvent e) {
+//     /* not needed */
+//   }
+//
+//   @Override public void mouseExited(MouseEvent e) {
+//     /* not needed */
+//   }
+//
+//   @Override public void mouseEntered(MouseEvent e) {
+//     /* not needed */
+//   }
+//
+//   @Override public void mouseReleased(MouseEvent e) {
+//     /* not needed */
+//   }
+//
+//   @Override public void mouseClicked(MouseEvent e) {
+//     /* not needed */
+//   }
 // }
