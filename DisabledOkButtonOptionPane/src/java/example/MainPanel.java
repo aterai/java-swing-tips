b9fba3659705e4179f64f34a5647e7892be1015a
aterai <aterai@outlook.com>
2021-05-30 06:29:36 +0900

    PMD: CognitiveComplexity

--- DisabledOkButtonOptionPane/src/java/example/MainPanel.java
+++ DisabledOkButtonOptionPane/src/java/example/MainPanel.java
@@ -5,6 +5,7 @@
 package example;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.HierarchyEvent;
 import javax.swing.*;
 import javax.swing.border.Border;
@@ -12,57 +13,49 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
 public final class MainPanel extends JPanel {
+  private final JTextArea log = new JTextArea();
+
   private MainPanel() {
     super(new BorderLayout());
-    JTextArea log = new JTextArea();
     JPanel p = new JPanel(new GridLayout(1, 2, 5, 5));
 
-    JTextField field1 = new JTextField();
-    JButton button1 = new JButton("show");
-    button1.addActionListener(e -> {
-      Component p1 = log.getRootPane();
-      int ret = JOptionPane.showConfirmDialog(
-          p1, field1, "Input text", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
-      if (ret == JOptionPane.OK_OPTION) {
-        log.setText(field1.getText());
-      }
-    });
-    p.add(makeTitledPanel("Default", button1));
+    JTextField tf = new JTextField();
+    p.add(makeTitledPanel("Default", new JButton(makeAction(tf, tf))));
 
-    JPanel panel2 = new JPanel(new GridLayout(2, 1));
-    JTextField field2 = new JTextField();
-    Border enabledBorder = field2.getBorder();
-    Insets i = enabledBorder.getBorderInsets(field2);
+    JPanel panel = new JPanel(new GridLayout(2, 1));
+    JTextField field = new JTextField();
+    Border enabledBorder = field.getBorder();
+    Insets i = enabledBorder.getBorderInsets(field);
     Border disabledBorder = BorderFactory.createCompoundBorder(
         BorderFactory.createLineBorder(Color.RED),
         BorderFactory.createEmptyBorder(i.top - 1, i.left - 1, i.bottom - 1, i.right - 1));
     String disabledMessage = "Text is required to create ...";
-    JLabel label2 = new JLabel(" ");
-    label2.setForeground(Color.RED);
-    panel2.add(field2);
-    panel2.add(label2);
-    if (field2.getText().isEmpty()) {
-      field2.setBorder(disabledBorder);
-      label2.setText(disabledMessage);
+    JLabel label = new JLabel(" ");
+    label.setForeground(Color.RED);
+    panel.add(field);
+    panel.add(label);
+    if (field.getText().isEmpty()) {
+      field.setBorder(disabledBorder);
+      label.setText(disabledMessage);
     }
-    field2.addHierarchyListener(e -> {
+    field.addHierarchyListener(e -> {
       Component c = e.getComponent();
       if ((e.getChangeFlags() & HierarchyEvent.SHOWING_CHANGED) != 0 && c.isShowing()) {
         EventQueue.invokeLater(c::requestFocusInWindow);
       }
     });
-    field2.getDocument().addDocumentListener(new DocumentListener() {
+    field.getDocument().addDocumentListener(new DocumentListener() {
       private void update() {
-        boolean verified = !field2.getText().isEmpty();
-        JButton b = field2.getRootPane().getDefaultButton();
+        boolean verified = !field.getText().isEmpty();
+        JButton b = field.getRootPane().getDefaultButton();
         if (verified) {
           b.setEnabled(true);
-          field2.setBorder(enabledBorder);
-          label2.setText(" ");
+          field.setBorder(enabledBorder);
+          label.setText(" ");
         } else {
           b.setEnabled(false);
-          field2.setBorder(disabledBorder);
-          label2.setText(disabledMessage);
+          field.setBorder(disabledBorder);
+          label.setText(disabledMessage);
         }
       }
 
@@ -78,27 +71,32 @@ public final class MainPanel extends JPanel {
         update();
       }
     });
-    JButton button2 = new JButton("show");
-    button2.addActionListener(e -> {
-      Component p2 = log.getRootPane();
-      EventQueue.invokeLater(() -> {
-        JButton b = field2.getRootPane().getDefaultButton();
-        if (b != null && field2.getText().isEmpty()) {
-          b.setEnabled(false);
-        }
-      });
-      int ret = JOptionPane.showConfirmDialog(
-          p2, panel2, "Input text", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
-      if (ret == JOptionPane.OK_OPTION) {
-        log.setText(field2.getText());
+    JButton button = new JButton(makeAction(field, panel));
+    button.addActionListener(e -> EventQueue.invokeLater(() -> {
+      JButton b = field.getRootPane().getDefaultButton();
+      if (b != null && field.getText().isEmpty()) {
+        b.setEnabled(false);
       }
-    });
-    p.add(makeTitledPanel("Disabled OK button", button2));
+    }));
+    p.add(makeTitledPanel("Disabled OK button", button));
 
     add(p, BorderLayout.NORTH);
     add(new JScrollPane(log));
     setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
     setPreferredSize(new Dimension(320, 240));
+  }
+
+  private AbstractAction makeAction(JTextField field, Component c) {
+    return new AbstractAction("show") {
+      @Override public void actionPerformed(ActionEvent e) {
+        Component p = log.getRootPane();
+        int ret = JOptionPane.showConfirmDialog(
+            p, c, "Input text", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+        if (ret == JOptionPane.OK_OPTION) {
+          log.setText(field.getText());
+        }
+      }
+    };
   }
 
   private static Component makeTitledPanel(String title, Component c) {
