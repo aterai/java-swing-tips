843e9119d69015280a56ff89cff28cce308e9069
aterai <aterai@outlook.com>
2021-08-18 12:22:04 +0900

    refactor: argument 'getClass().getResource(...)' might be null

--- SplashScreen/src/java/example/MainPanel.java
+++ SplashScreen/src/java/example/MainPanel.java
@@ -5,6 +5,11 @@
 package example;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Optional;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public final class MainPanel extends JPanel {
@@ -19,6 +24,17 @@ public final class MainPanel extends JPanel {
     }
     add(new JScrollPane(new JTree()));
     setPreferredSize(new Dimension(320, 240));
+  }
+
+  private static Image makeMissingImage() {
+    Icon missingIcon = new MissingIcon();
+    int w = missingIcon.getIconWidth();
+    int h = missingIcon.getIconHeight();
+    BufferedImage bi = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+    Graphics2D g2 = bi.createGraphics();
+    missingIcon.paintIcon(null, g2, 0, 0);
+    g2.dispose();
+    return bi;
   }
 
   public static void main(String[] args) {
@@ -37,8 +53,16 @@ public final class MainPanel extends JPanel {
     JWindow splashScreen = new JWindow();
     EventQueue.invokeLater(() -> {
       System.out.println("splashScreen show start / EDT: " + EventQueue.isDispatchThread());
-      ImageIcon img = new ImageIcon(MainPanel.class.getResource("splash.png"));
-      splashScreen.getContentPane().add(new JLabel(img));
+      String path = "example/splash.png";
+      ClassLoader cl = Thread.currentThread().getContextClassLoader();
+      Image img = Optional.ofNullable(cl.getResource(path)).map(url -> {
+        try (InputStream s = url.openStream()) {
+          return ImageIO.read(s);
+        } catch (IOException ex) {
+          return makeMissingImage();
+        }
+      }).orElseGet(MainPanel::makeMissingImage);
+      splashScreen.getContentPane().add(new JLabel(new ImageIcon(img)));
       splashScreen.pack();
       splashScreen.setLocationRelativeTo(null);
       splashScreen.setVisible(true);
@@ -66,6 +90,34 @@ public final class MainPanel extends JPanel {
   }
 }
 
+class MissingIcon implements Icon {
+  @Override public void paintIcon(Component c, Graphics g, int x, int y) {
+    Graphics2D g2 = (Graphics2D) g.create();
+
+    int w = getIconWidth();
+    int h = getIconHeight();
+    int gap = w / 5;
+
+    g2.setColor(Color.WHITE);
+    g2.fillRect(x, y, w, h);
+
+    g2.setColor(Color.RED);
+    g2.setStroke(new BasicStroke(w / 8f));
+    g2.drawLine(x + gap, y + gap, x + w - gap, y + h - gap);
+    g2.drawLine(x + gap, y + h - gap, x + w - gap, y + gap);
+
+    g2.dispose();
+  }
+
+  @Override public int getIconWidth() {
+    return 320;
+  }
+
+  @Override public int getIconHeight() {
+    return 240;
+  }
+}
+
 //   public static void main(String[] args) {
 //     System.out.println("main start / EDT: " + EventQueue.isDispatchThread());
 //     EventQueue.invokeLater(MainPanel::createAndShowGui);
@@ -80,8 +132,8 @@ public final class MainPanel extends JPanel {
 //     }
 //     System.out.println("splashScreen show start / EDT: " + EventQueue.isDispatchThread());
 //     JWindow splashScreen = new JWindow();
-//     ImageIcon img = new ImageIcon(MainPanel.class.getResource("splash.png"));
-//     splashScreen.getContentPane().add(new JLabel(img));
+//     Image img = ...
+//     splashScreen.getContentPane().add(new JLabel(new ImageIcon(img)));
 //     splashScreen.pack();
 //     splashScreen.setLocationRelativeTo(null);
 //     splashScreen.setVisible(true);
