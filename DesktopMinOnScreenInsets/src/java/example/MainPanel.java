e2144ddf7c4fc58b58874c0b77a5f26098dc057a
aterai <aterai@outlook.com>
2021-11-15 01:41:02 +0900

    add: DesktopMinOnScreenInsets

---
+++ DesktopMinOnScreenInsets/src/java/example/MainPanel.java
@@ -0,0 +1,74 @@
+// -*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+// @homepage@
+
+package example;
+
+import java.awt.*;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.util.concurrent.atomic.AtomicInteger;
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicInternalFrameUI;
+
+public final class MainPanel extends JPanel {
+  private static final int OFFSET = 30;
+  private static final AtomicInteger OPEN_COUNTER = new AtomicInteger();
+
+  private MainPanel() {
+    super(new BorderLayout());
+    System.out.println(UIManager.get("Desktop.minOnScreenInsets"));
+
+    JInternalFrame f = createInternalFrame();
+    Dimension d = ((BasicInternalFrameUI) f.getUI()).getNorthPane().getPreferredSize();
+    UIManager.put("Desktop.minOnScreenInsets", new Insets(d.height, 16, 3, 16));
+    UIManager.put("Desktop.background", Color.LIGHT_GRAY);
+
+    JDesktopPane desktop = new JDesktopPane();
+    desktop.add(f);
+
+    JMenu menu = new JMenu("Window");
+    menu.setMnemonic(KeyEvent.VK_W);
+    JMenuItem menuItem = menu.add("New");
+    menuItem.setMnemonic(KeyEvent.VK_N);
+    menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.ALT_DOWN_MASK));
+    menuItem.setActionCommand("new");
+    menuItem.addActionListener(e -> desktop.add(createInternalFrame()));
+
+    JMenuBar menuBar = new JMenuBar();
+    menuBar.add(menu);
+    EventQueue.invokeLater(() -> getRootPane().setJMenuBar(menuBar));
+
+    add(desktop);
+    setPreferredSize(new Dimension(320, 240));
+  }
+
+  private static JInternalFrame createInternalFrame() {
+    String title = String.format("Document #%s", OPEN_COUNTER.getAndIncrement());
+    JInternalFrame f = new JInternalFrame(title, true, true, true, true);
+    f.getContentPane().add(new JScrollPane(new JTree()));
+    f.setSize(160, 100);
+    f.setLocation(OFFSET * OPEN_COUNTER.intValue(), OFFSET * OPEN_COUNTER.intValue());
+    EventQueue.invokeLater(() -> f.setVisible(true));
+    return f;
+  }
+
+  public static void main(String[] args) {
+    EventQueue.invokeLater(MainPanel::createAndShowGui);
+  }
+
+  private static void createAndShowGui() {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+      ex.printStackTrace();
+      Toolkit.getDefaultToolkit().beep();
+    }
+    JFrame frame = new JFrame("@title@");
+    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    frame.getContentPane().add(new MainPanel());
+    frame.pack();
+    frame.setLocationRelativeTo(null);
+    frame.setVisible(true);
+  }
+}
