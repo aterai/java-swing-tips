349f04177ecd474bfcd2f4669dede0229b421a9f
aterai <aterai@outlook.com>
2021-11-29 06:34:41 +0900

    add: ExtendTabsToBase

---
+++ ExtendTabsToBase/src/java/example/MainPanel.java
@@ -0,0 +1,62 @@
+// -*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+// @homepage@
+
+package example;
+
+import java.awt.*;
+import javax.swing.*;
+
+public final class MainPanel extends JPanel {
+  private MainPanel() {
+    super(new GridLayout(2, 1));
+    // TEST: UIManager.put("TabbedPane.tabRunOverlay", -4);
+    // UIManager.put("TabbedPane.extendTabsToBase", Boolean.TRUE); // Default
+    // UIManager.put("TabbedPane.extendTabsToBase", Boolean.FALSE);
+    JTabbedPane tabbedPane = makeTabbedPane();
+    UIDefaults d = UIManager.getLookAndFeelDefaults();
+    d.put("TabbedPane.extendTabsToBase", Boolean.FALSE);
+    tabbedPane.putClientProperty("Nimbus.Overrides", d);
+    tabbedPane.putClientProperty("Nimbus.Overrides.InheritDefaults", false);
+
+    add(makeTitledPanel("TabbedPane.extendTabsToBase: true", makeTabbedPane()));
+    add(makeTitledPanel("TabbedPane.extendTabsToBase: false", tabbedPane));
+    setPreferredSize(new Dimension(320, 240));
+  }
+
+  private static JTabbedPane makeTabbedPane() {
+    JTabbedPane tabbedPane = new JTabbedPane();
+    tabbedPane.addTab("JTable", new JScrollPane(new JTable(8, 3)));
+    tabbedPane.addTab("JTree", new JScrollPane(new JTree()));
+    tabbedPane.addTab("JLabel", new JLabel("label"));
+    tabbedPane.addTab("JButton", new JButton("button"));
+    tabbedPane.addTab("JSplitPane", new JSplitPane());
+    return tabbedPane;
+  }
+
+  private static Component makeTitledPanel(String title, Component c) {
+    JPanel p = new JPanel(new BorderLayout());
+    p.setBorder(BorderFactory.createTitledBorder(title));
+    p.add(c);
+    return p;
+  }
+
+  public static void main(String[] args) {
+    EventQueue.invokeLater(MainPanel::createAndShowGui);
+  }
+
+  private static void createAndShowGui() {
+    try {
+      UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+      ex.printStackTrace();
+      Toolkit.getDefaultToolkit().beep();
+    }
+    JFrame frame = new JFrame("@title@");
+    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    frame.getContentPane().add(new MainPanel());
+    frame.pack();
+    frame.setLocationRelativeTo(null);
+    frame.setVisible(true);
+  }
+}
