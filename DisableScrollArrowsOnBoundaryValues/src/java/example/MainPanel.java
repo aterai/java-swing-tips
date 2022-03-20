dd5f24f7cd14f5922cb5683fa6d4d7da20a24283
aterai <aterai@outlook.com>
2022-03-21 03:45:16 +0900

    add: DisableScrollArrowsOnBoundaryValues

---
+++ DisableScrollArrowsOnBoundaryValues/src/java/example/MainPanel.java
@@ -0,0 +1,52 @@
+// -*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+// @homepage@
+
+package example;
+
+import java.awt.*;
+import java.util.Optional;
+import javax.swing.*;
+
+public final class MainPanel extends JPanel {
+  private MainPanel() {
+    super(new GridLayout(1, 2));
+    JScrollPane scroll = new JScrollPane(new JTable(100, 3));
+    scroll.getVerticalScrollBar().addAdjustmentListener(e -> {
+      JScrollBar scrollBar = (JScrollBar) e.getAdjustable();
+      BoundedRangeModel m = scrollBar.getModel();
+      int value = m.getValue();
+      if (value == m.getMaximum() - m.getExtent()) {
+        Optional.ofNullable(scrollBar.getComponent(0)).ifPresent(b -> b.setEnabled(false));
+      } else if (value == m.getMinimum()) {
+        Optional.ofNullable(scrollBar.getComponent(1)).ifPresent(b -> b.setEnabled(false));
+      } else {
+        for (Component button : scrollBar.getComponents()) {
+          button.setEnabled(true);
+        }
+      }
+    });
+    add(new JScrollPane(new JTable(100, 3)));
+    add(scroll);
+    setPreferredSize(new Dimension(320, 240));
+  }
+
+  public static void main(String[] args) {
+    EventQueue.invokeLater(MainPanel::createAndShowGui);
+  }
+
+  private static void createAndShowGui() {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+      ex.printStackTrace();
+      Toolkit.getDefaultToolkit().beep();
+    }
+    JFrame frame = new JFrame("@title@");
+    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    frame.getContentPane().add(new MainPanel());
+    frame.pack();
+    frame.setLocationRelativeTo(null);
+    frame.setVisible(true);
+  }
+}
