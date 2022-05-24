009549ab0bd6677f14060174f572a9df31d2207c
aterai <aterai@outlook.com>
2022-05-24 18:29:44 +0900

    refactor: argument 'getClass().getResource(...)' might be null

--- FrameIcon/src/java/example/MainPanel.java
+++ FrameIcon/src/java/example/MainPanel.java
@@ -5,19 +5,32 @@
 package example;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Optional;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public final class MainPanel extends JPanel {
   private MainPanel() {
     super(new BorderLayout());
+    String path = "example/16x16.png";
+    ClassLoader cl = Thread.currentThread().getContextClassLoader();
+    Image image = Optional.ofNullable(cl.getResource(path)).map(url -> {
+      try (InputStream s = url.openStream()) {
+        return ImageIO.read(s);
+      } catch (IOException ex) {
+        return makeMissingImage();
+      }
+    }).orElseGet(MainPanel::makeMissingImage);
 
-    Image icon = Toolkit.getDefaultToolkit().createImage(getClass().getResource("16x16.png"));
     JCheckBox check = new JCheckBox("setIconImage");
     check.addActionListener(e -> {
       JCheckBox c = (JCheckBox) e.getSource();
       Container w = c.getTopLevelAncestor();
       if (w instanceof Window) {
-        ((Window) w).setIconImage(c.isSelected() ? icon : null);
+        ((Window) w).setIconImage(c.isSelected() ? image : null);
       }
     });
 
@@ -27,6 +40,17 @@ public final class MainPanel extends JPanel {
     add(p, BorderLayout.NORTH);
     setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
     setPreferredSize(new Dimension(320, 240));
+  }
+
+  private static Image makeMissingImage() {
+    Icon missingIcon = UIManager.getIcon("html.missingImage");
+    int iw = missingIcon.getIconWidth();
+    int ih = missingIcon.getIconHeight();
+    BufferedImage bi = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+    Graphics2D g2 = bi.createGraphics();
+    missingIcon.paintIcon(null, g2, (16 - iw) / 2, (16 - ih) / 2);
+    g2.dispose();
+    return bi;
   }
 
   public static void main(String[] args) {
