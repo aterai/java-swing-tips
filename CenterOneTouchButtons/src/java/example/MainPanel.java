46e759cb99205800a657b5cfbbcbf75cbeb4ab30
aterai <aterai@outlook.com>
2022-08-08 02:25:44 +0900

    add: CenterOneTouchButtons

---
+++ CenterOneTouchButtons/src/java/example/MainPanel.java
@@ -0,0 +1,120 @@
+// -*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+// @homepage@
+
+package example;
+
+import java.awt.*;
+import javax.swing.*;
+
+public final class MainPanel extends JPanel {
+  private MainPanel() {
+    super(new BorderLayout());
+    JSplitPane splitPane = new JSplitPane();
+    splitPane.setTopComponent(new JScrollPane(new JTable(8, 3)));
+    splitPane.setBottomComponent(new JScrollPane(new JTree()));
+    splitPane.setOneTouchExpandable(true);
+    splitPane.setDividerSize(32);
+
+    String key = "SplitPane.centerOneTouchButtons";
+    JCheckBox check = new JCheckBox(key, UIManager.getBoolean(key)) {
+      @Override public void updateUI() {
+        super.updateUI();
+        EventQueue.invokeLater(() -> {
+          boolean b = UIManager.getLookAndFeelDefaults().getBoolean(key);
+          setSelected(b);
+          updateCenterOneTouchButtons(splitPane, b);
+        });
+      }
+    };
+    check.setOpaque(false);
+    check.addActionListener(e -> {
+      JCheckBox cb = (JCheckBox) e.getSource();
+      updateCenterOneTouchButtons(splitPane, cb.isSelected());
+    });
+
+    JMenuBar mb = new JMenuBar();
+    mb.add(LookAndFeelUtil.createLookAndFeelMenu());
+    mb.add(Box.createHorizontalStrut(2));
+    mb.add(check);
+    EventQueue.invokeLater(() -> getRootPane().setJMenuBar(mb));
+
+    add(splitPane);
+    setBorder(BorderFactory.createEmptyBorder(25, 5, 25, 5));
+    setPreferredSize(new Dimension(320, 240));
+  }
+
+  public static void updateCenterOneTouchButtons(JSplitPane splitPane, boolean b) {
+    UIManager.put("SplitPane.centerOneTouchButtons", b);
+    SwingUtilities.updateComponentTreeUI(splitPane);
+  }
+
+  public static void main(String[] args) {
+    EventQueue.invokeLater(MainPanel::createAndShowGui);
+  }
+
+  private static void createAndShowGui() {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+      ex.printStackTrace();
+      Toolkit.getDefaultToolkit().beep();
+    }
+    JFrame frame = new JFrame("@title@");
+    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    frame.getContentPane().add(new MainPanel());
+    frame.pack();
+    frame.setLocationRelativeTo(null);
+    frame.setVisible(true);
+  }
+}
+
+// @see https://java.net/projects/swingset3/sources/svn/content/trunk/SwingSet3/src/com/sun/swingset3/SwingSet3.java
+final class LookAndFeelUtil {
+  private static String lookAndFeel = UIManager.getLookAndFeel().getClass().getName();
+
+  private LookAndFeelUtil() {
+    /* Singleton */
+  }
+
+  public static JMenu createLookAndFeelMenu() {
+    JMenu menu = new JMenu("LookAndFeel");
+    ButtonGroup lafGroup = new ButtonGroup();
+    for (UIManager.LookAndFeelInfo lafInfo : UIManager.getInstalledLookAndFeels()) {
+      menu.add(createLookAndFeelItem(lafInfo.getName(), lafInfo.getClassName(), lafGroup));
+    }
+    return menu;
+  }
+
+  private static JMenuItem createLookAndFeelItem(String laf, String lafClass, ButtonGroup bg) {
+    JMenuItem lafItem = new JRadioButtonMenuItem(laf, lafClass.equals(lookAndFeel));
+    lafItem.setActionCommand(lafClass);
+    lafItem.setHideActionText(true);
+    lafItem.addActionListener(e -> {
+      ButtonModel m = bg.getSelection();
+      try {
+        setLookAndFeel(m.getActionCommand());
+      } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+        UIManager.getLookAndFeel().provideErrorFeedback((Component) e.getSource());
+      }
+    });
+    bg.add(lafItem);
+    return lafItem;
+  }
+
+  private static void setLookAndFeel(String lookAndFeel) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+    String oldLookAndFeel = LookAndFeelUtil.lookAndFeel;
+    if (!oldLookAndFeel.equals(lookAndFeel)) {
+      UIManager.setLookAndFeel(lookAndFeel);
+      LookAndFeelUtil.lookAndFeel = lookAndFeel;
+      updateLookAndFeel();
+      // firePropertyChange("lookAndFeel", oldLookAndFeel, lookAndFeel);
+    }
+  }
+
+  private static void updateLookAndFeel() {
+    for (Window window : Window.getWindows()) {
+      SwingUtilities.updateComponentTreeUI(window);
+    }
+  }
+}
