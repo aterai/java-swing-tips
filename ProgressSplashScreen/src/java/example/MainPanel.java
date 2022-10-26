814d3210dff1c6933bffc1f423752938fcbcbb9e
aterai <aterai@outlook.com>
2022-10-27 03:50:34 +0900

    PMD: SystemPrintln

--- ProgressSplashScreen/src/java/example/MainPanel.java
+++ ProgressSplashScreen/src/java/example/MainPanel.java
@@ -38,9 +38,9 @@ public final class MainPanel extends JPanel {
   }
 
   public static void main(String[] args) {
-    System.out.println("main start / EDT: " + EventQueue.isDispatchThread());
+    // System.out.println("main start / EDT: " + EventQueue.isDispatchThread());
     createAndShowGui();
-    System.out.println("main end");
+    // System.out.println("main end");
   }
 
   private static void createAndShowGui() {
