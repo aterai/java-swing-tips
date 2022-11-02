eb99e251346c9bf3d7c4567112236081f96f31b9
aterai <aterai@outlook.com>
2022-11-03 07:57:30 +0900

    refactor: update header JLabel border and layout

--- HeaderFooterComboPopup/src/java/example/MainPanel.java
+++ HeaderFooterComboPopup/src/java/example/MainPanel.java
@@ -93,10 +93,10 @@ class HeaderFooterComboPopup extends BasicComboPopup {
   }
 
   protected void configureHeader() {
-    header = new JLabel("History");
-    header.setBorder(BorderFactory.createEmptyBorder(4, 5, 4, 0));
-    header.setMaximumSize(new Dimension(Short.MAX_VALUE, 20));
-    header.setAlignmentX(1f);
+    header = new JLabel("History", SwingConstants.CENTER);
+    header.setBorder(BorderFactory.createEmptyBorder(5, 0, 5, 0));
+    header.setMaximumSize(new Dimension(Short.MAX_VALUE, 24));
+    header.setAlignmentX(Component.CENTER_ALIGNMENT);
   }
 
   protected void configureFooter() {
