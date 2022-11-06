3a6c701bbb1b2b9ab6fcbfd67b0d8a7607f4c465
aterai <aterai@outlook.com>
2022-11-07 00:05:53 +0900

    add: NavigateSelectedTab

---
+++ NavigateSelectedTab/src/java/example/MainPanel.java
@@ -0,0 +1,111 @@
+// -*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+// @homepage@
+
+package example;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.util.Objects;
+import java.util.stream.IntStream;
+import javax.swing.*;
+
+public final class MainPanel extends JPanel {
+  private MainPanel() {
+    super(new BorderLayout());
+    JTabbedPane tabs = new JTabbedPane();
+    tabs.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+    IntStream.range(0, 20).forEach(i -> {
+      String title = "title" + i;
+      tabs.addTab(title, new JScrollPane(new JTextArea(title)));
+    });
+
+    JCheckBox layout = new JCheckBox("SCROLL_TAB_LAYOUT", true);
+    layout.setFocusable(false);
+    layout.addActionListener(e -> {
+      boolean b = ((JCheckBox) e.getSource()).isSelected();
+      tabs.setTabLayoutPolicy(b ? JTabbedPane.SCROLL_TAB_LAYOUT : JTabbedPane.WRAP_TAB_LAYOUT);
+    });
+
+    JCheckBox placement = new JCheckBox("TOP", true);
+    placement.setFocusable(false);
+    placement.addActionListener(e -> {
+      boolean b = ((JCheckBox) e.getSource()).isSelected();
+      tabs.setTabPlacement(b ? SwingConstants.TOP : SwingConstants.LEFT);
+    });
+
+    InputMap im0 = tabs.getInputMap(JComponent.WHEN_FOCUSED);
+    InputMap im1 = tabs.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+    ActionMap am = tabs.getActionMap();
+
+    String prev = "navigatePrevious";
+    am.put(prev, new TabNavigateAction(tabs, am.get(prev)));
+    im0.put(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0), prev);
+    im1.put(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, InputEvent.ALT_DOWN_MASK), prev);
+    im0.put(KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0), prev);
+    im1.put(KeyStroke.getKeyStroke(KeyEvent.VK_UP, InputEvent.ALT_DOWN_MASK), prev);
+
+    String next = "navigateNext";
+    am.put(next, new TabNavigateAction(tabs, am.get(next)));
+    im0.put(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0), next);
+    im1.put(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, InputEvent.ALT_DOWN_MASK), next);
+    im0.put(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0), next);
+    im1.put(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, InputEvent.ALT_DOWN_MASK), next);
+
+    Box box = Box.createHorizontalBox();
+    box.add(layout);
+    box.add(placement);
+
+    add(tabs);
+    add(box, BorderLayout.SOUTH);
+    setPreferredSize(new Dimension(320, 240));
+  }
+
+  public static void main(String[] args) {
+    EventQueue.invokeLater(MainPanel::createAndShowGui);
+  }
+
+  private static void createAndShowGui() {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+      ex.printStackTrace();
+      Toolkit.getDefaultToolkit().beep();
+    }
+    JFrame frame = new JFrame("@title@");
+    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    frame.getContentPane().add(new MainPanel());
+    frame.pack();
+    frame.setLocationRelativeTo(null);
+    frame.setVisible(true);
+  }
+}
+
+class TabNavigateAction extends AbstractAction {
+  private final JTabbedPane tabs;
+  private final transient Action action;
+
+  protected TabNavigateAction(JTabbedPane tabbedPane, Action action) {
+    super();
+    this.tabs = tabbedPane;
+    this.action = action;
+  }
+
+  @Override public void actionPerformed(ActionEvent e) {
+    if (action != null && action.isEnabled()) {
+      boolean isWrap = tabs.getTabLayoutPolicy() == JTabbedPane.WRAP_TAB_LAYOUT;
+      boolean isAltDown = (e.getModifiers() & ActionEvent.ALT_MASK) != 0;
+      Object name = action.getValue(Action.NAME);
+      int base = tabs.getSelectedIndex();
+      boolean prev = Objects.equals(name, "navigatePrevious") && base != 0;
+      boolean next = Objects.equals(name, "navigateNext") && base != tabs.getTabCount() - 1;
+      if (isWrap || isAltDown || prev || next) {
+        action.actionPerformed(new ActionEvent(tabs,
+            ActionEvent.ACTION_PERFORMED, null,
+            e.getWhen(), e.getModifiers()));
+      }
+    }
+  }
+}
