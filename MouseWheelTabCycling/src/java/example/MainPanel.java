8cfb6fb76634b9f0ad721dfe5da1a267219d5c0b
aterai <aterai@outlook.com>
2022-11-07 21:24:46 +0900

    add: add scrollTabsForwardAction, scrollTabsBackwardAction example

--- MouseWheelTabCycling/src/java/example/MainPanel.java
+++ MouseWheelTabCycling/src/java/example/MainPanel.java
@@ -7,14 +7,27 @@ package example;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ItemEvent;
+import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 import java.util.Optional;
+import java.util.stream.IntStream;
 import javax.swing.*;
 
 public final class MainPanel extends JPanel {
+  private final JCheckBox check = new JCheckBox("scroll tabs");
+
   private MainPanel() {
     super(new BorderLayout());
-    JTabbedPane tabbedPane = makeWheelTabbedPane();
+    JTabbedPane tabbedPane = new JTabbedPane(SwingConstants.TOP, JTabbedPane.SCROLL_TAB_LAYOUT) {
+      private transient MouseWheelListener handler;
+
+      @Override public void updateUI() {
+        removeMouseWheelListener(handler);
+        super.updateUI();
+        handler = new TabWheelHandler();
+        addMouseWheelListener(handler);
+      }
+    };
     tabbedPane.addTab("JLabel1", new JLabel("JLabel1"));
     tabbedPane.addTab("JLabel2", new JLabel("JLabel2"));
     tabbedPane.addTab("JLabel(disabled)", new JLabel("JLabel"));
@@ -23,6 +36,10 @@ public final class MainPanel extends JPanel {
     tabbedPane.addTab("JPanel", new JLabel("JPanel"));
     tabbedPane.addTab("JTree", new JScrollPane(new JTree()));
     tabbedPane.addTab("JTextArea", new JScrollPane(new JTextArea("JTextArea")));
+    IntStream.range(0, 20).forEach(i -> {
+      String title = "title" + i;
+      tabbedPane.addTab(title, new JScrollPane(new JLabel(title)));
+    });
 
     JComboBox<? extends Enum<?>> comboBox = new JComboBox<>(TabPlacements.values());
     comboBox.addItemListener(e -> {
@@ -32,6 +49,7 @@ public final class MainPanel extends JPanel {
     });
     Box box = Box.createHorizontalBox();
     box.add(Box.createHorizontalGlue());
+    box.add(check);
     box.add(new JLabel("TabPlacement: "));
     box.add(Box.createHorizontalStrut(2));
     box.add(comboBox);
@@ -42,34 +60,30 @@ public final class MainPanel extends JPanel {
     setPreferredSize(new Dimension(320, 240));
   }
 
-  private static JTabbedPane makeWheelTabbedPane() {
-    return new JTabbedPane(SwingConstants.TOP, JTabbedPane.SCROLL_TAB_LAYOUT) {
-      private transient MouseWheelListener handler;
-
-      @Override public void updateUI() {
-        removeMouseWheelListener(handler);
-        super.updateUI();
-        handler = e -> {
-          JTabbedPane src = (JTabbedPane) e.getComponent();
-          if (!getTabAreaBounds(src).contains(e.getPoint())) {
-            return;
-          }
-          double dir = (e.isControlDown() ? -1 : 1) * e.getPreciseWheelRotation();
-          int id = ActionEvent.ACTION_PERFORMED;
-          String cmd = dir > 0 ? "navigateNext" : "navigatePrevious";
-          ActionEvent event = new ActionEvent(src, id, cmd, e.getWhen(), e.getModifiersEx());
-          src.getActionMap().get(cmd).actionPerformed(event);
-          // int idx = src.getSelectedIndex() + (int) dir;
-          // if (idx < 0) {
-          //   idx = src.getTabCount() - 1;
-          // } else if (idx >= src.getTabCount()) {
-          //   idx = 0;
-          // }
-          // src.setSelectedIndex(idx);
-        };
-        addMouseWheelListener(handler);
+  private class TabWheelHandler implements MouseWheelListener {
+    @Override public void mouseWheelMoved(MouseWheelEvent e) {
+      JTabbedPane src = (JTabbedPane) e.getComponent();
+      if (!getTabAreaBounds(src).contains(e.getPoint())) {
+        return;
       }
-    };
+      boolean dir = (e.isControlDown() ? -1 : 1) * e.getPreciseWheelRotation() > 0;
+      int id = ActionEvent.ACTION_PERFORMED;
+      String cmd;
+      if (check.isSelected()) {
+        cmd = dir ? "scrollTabsForwardAction" : "scrollTabsBackwardAction";
+      } else {
+        cmd = dir ? "navigateNext" : "navigatePrevious";
+      }
+      ActionEvent event = new ActionEvent(src, id, cmd, e.getWhen(), e.getModifiersEx());
+      src.getActionMap().get(cmd).actionPerformed(event);
+      // int idx = src.getSelectedIndex() + (int) dir;
+      // if (idx < 0) {
+      //   idx = src.getTabCount() - 1;
+      // } else if (idx >= src.getTabCount()) {
+      //   idx = 0;
+      // }
+      // src.setSelectedIndex(idx);
+    }
   }
 
   public static Rectangle getTabAreaBounds(JTabbedPane tabbedPane) {
