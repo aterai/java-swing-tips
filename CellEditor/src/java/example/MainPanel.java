8a35f3eb843d50a563d2a89896603f8efca47d71
aterai <aterai@outlook.com>
2022-12-31 01:32:38 +0900

    Checkstyle: VisibilityModifier

--- CellEditor/src/java/example/MainPanel.java
+++ CellEditor/src/java/example/MainPanel.java
@@ -15,20 +15,22 @@ public final class MainPanel extends JPanel {
   private MainPanel() {
     super(new BorderLayout());
     JCheckBox modelCheck = new JCheckBox("isCellEditable return false");
-    RowDataModel model = new RowDataModel() {
+
+    String[] columnNames = {"No.", "Name", "Comment"};
+    Object[][] data = {
+        {0, "Name 0", "comment..."}, {1, "Name 1", "Test"},
+        {2, "Name d", "ee"}, {3, "Name c", "Test cc"},
+        {4, "Name b", "Test bb"}, {5, "Name a", "ff"}
+    };
+    DefaultTableModel model = new DefaultTableModel(data, columnNames) {
+      @Override public Class<?> getColumnClass(int column) {
+        return getValueAt(0, column).getClass();
+      }
+
       @Override public boolean isCellEditable(int row, int col) {
-        return !modelCheck.isSelected();
+        return col != 0 && !modelCheck.isSelected();
       }
     };
-    model.addRowData(new RowData("Name 1", "Comment"));
-    model.addRowData(new RowData("Name 2", "Test"));
-    model.addRowData(new RowData("Name d", "ee"));
-    model.addRowData(new RowData("Name c", "Test cc"));
-    model.addRowData(new RowData("Name b", "Test bb"));
-    model.addRowData(new RowData("Name a", "ff"));
-    model.addRowData(new RowData("Name 0", "Test aa"));
-    model.addRowData(new RowData("Name 0", "gg"));
-
     JTable table = new JTable(model);
     TableColumn col = table.getColumnModel().getColumn(0);
     col.setMinWidth(50);
@@ -75,66 +77,5 @@ public final class MainPanel extends JPanel {
     frame.pack();
     frame.setLocationRelativeTo(null);
     frame.setVisible(true);
-  }
-}
-
-class RowDataModel extends DefaultTableModel {
-  private static final ColumnContext[] COLUMN_ARRAY = {
-      new ColumnContext("No.", Integer.class, false),
-      new ColumnContext("Name", String.class, true),
-      new ColumnContext("Comment", String.class, true)
-  };
-  private int number;
-
-  public void addRowData(RowData t) {
-    Object[] obj = {number, t.getName(), t.getComment()};
-    super.addRow(obj);
-    number++;
-  }
-
-  @Override public boolean isCellEditable(int row, int col) {
-    return COLUMN_ARRAY[col].isEditable;
-  }
-
-  @Override public Class<?> getColumnClass(int column) {
-    return COLUMN_ARRAY[column].columnClass;
-  }
-
-  @Override public int getColumnCount() {
-    return COLUMN_ARRAY.length;
-  }
-
-  @Override public String getColumnName(int column) {
-    return COLUMN_ARRAY[column].columnName;
-  }
-
-  private static class ColumnContext {
-    public final String columnName;
-    public final Class<?> columnClass;
-    public final boolean isEditable;
-
-    protected ColumnContext(String columnName, Class<?> columnClass, boolean isEditable) {
-      this.columnName = columnName;
-      this.columnClass = columnClass;
-      this.isEditable = isEditable;
-    }
-  }
-}
-
-class RowData {
-  private final String name;
-  private final String comment;
-
-  protected RowData(String name, String comment) {
-    this.name = name;
-    this.comment = comment;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public String getComment() {
-    return comment;
   }
 }
