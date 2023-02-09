5d78d926e6a2fa7738dbb02d5bbd443a58e3b6e3
aterai <aterai@outlook.com>
2023-02-10 00:30:12 +0900

    refactor: add example to blur JButton with JLayer

--- BlurButton/src/java/example/MainPanel.java
+++ BlurButton/src/java/example/MainPanel.java
@@ -11,39 +11,42 @@ import java.awt.image.Kernel;
 import java.util.Optional;
 import java.util.stream.Stream;
 import javax.swing.*;
+import javax.swing.plaf.LayerUI;
 
 public final class MainPanel extends JPanel {
   private MainPanel() {
-    super(new BorderLayout());
-    JPanel p0 = new JPanel();
-    p0.setBorder(BorderFactory.createTitledBorder("Default JButton"));
-    JButton b0 = new JButton("Default JButton");
-    p0.add(b0);
+    super(new BorderLayout(10, 10));
+    JPanel p = new JPanel();
+    p.setBorder(BorderFactory.createTitledBorder("Apply a blur effect if disabled"));
 
-    JPanel p1 = new JPanel();
-    p1.setBorder(BorderFactory.createTitledBorder("Blurred JButton1"));
-    JButton b1 = new BlurredButton("Blurred JButton1");
-    p1.add(b1);
+    JButton b0 = new JButton("<html>Default <font color='red'>JButton");
+    p.add(b0);
 
-    JPanel p2 = new JPanel();
-    p2.setBorder(BorderFactory.createTitledBorder("Blurred JButton(ConvolveOp.EDGE_NO_OP)"));
-    JButton b2 = new BlurButton("Blurred JButton2");
-    p2.add(b2);
+    JButton b1 = new BlurredButton("Blurred JButton");
+    p.add(b1);
 
-    Box box = Box.createVerticalBox();
-    Stream.of(p0, p1, p2).forEach(p -> {
-      box.add(p);
-      box.add(Box.createVerticalStrut(10));
-    });
+    JButton b2 = new BlurButton("Blurred JButton(ConvolveOp.EDGE_NO_OP)");
+    p.add(b2);
 
-    JToggleButton button = new JToggleButton("setEnabled(false)");
+    JButton b3 = new JButton("<html>Blurred <font color='blue'>JLayer");
+    p.add(new JLayer<>(b3, new BlurLayerUI<>()));
+
+    JCheckBox button = new JCheckBox("setEnabled", true);
     button.addActionListener(e -> {
-      boolean f = !((AbstractButton) e.getSource()).isSelected();
-      Stream.of(b0, b1, b2).forEach(b -> b.setEnabled(f));
+      boolean f = ((AbstractButton) e.getSource()).isSelected();
+      Stream.of(b0, b1, b2, b3).forEach(b -> b.setEnabled(f));
     });
 
-    add(box, BorderLayout.NORTH);
-    add(button, BorderLayout.SOUTH);
+    Box box = Box.createHorizontalBox();
+    box.add(Box.createHorizontalGlue());
+    box.add(button);
+
+    JMenuBar mb = new JMenuBar();
+    mb.add(LookAndFeelUtils.createLookAndFeelMenu());
+    EventQueue.invokeLater(() -> getRootPane().setJMenuBar(mb));
+
+    add(p);
+    add(box, BorderLayout.SOUTH);
     setPreferredSize(new Dimension(320, 240));
   }
 
@@ -97,8 +100,8 @@ class BlurredButton extends JButton {
       //   buf = new BufferedImage(iw, ih, BufferedImage.TYPE_INT_ARGB);
       // }
       buf = Optional.ofNullable(buf)
-        .filter(bi -> bi.getWidth() == getWidth() && bi.getHeight() == getHeight())
-        .orElseGet(() -> new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB));
+          .filter(bi -> bi.getWidth() == getWidth() && bi.getHeight() == getHeight())
+          .orElseGet(() -> new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB));
       Graphics2D g2 = buf.createGraphics();
       g2.setFont(g.getFont()); // pointed out by 八ツ玉舘
       super.paintComponent(g2);
@@ -125,9 +128,14 @@ class BlurButton extends JButton {
     if (isEnabled()) {
       super.paintComponent(g);
     } else {
+      // if (Objects.isNull(buf) || iw != getWidth() || ih != getHeight()) {
+      //   iw = getWidth();
+      //   ih = getHeight();
+      //   buf = new BufferedImage(iw, ih, BufferedImage.TYPE_INT_ARGB);
+      // }
       buf = Optional.ofNullable(buf)
-        .filter(bi -> bi.getWidth() == getWidth() && bi.getHeight() == getHeight())
-        .orElseGet(() -> new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB));
+          .filter(bi -> bi.getWidth() == getWidth() && bi.getHeight() == getHeight())
+          .orElseGet(() -> new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB));
       Graphics2D g2 = buf.createGraphics();
       g2.setFont(g.getFont()); // pointed out by 八ツ玉舘
       super.paintComponent(g2);
@@ -141,4 +149,91 @@ class BlurButton extends JButton {
   //   d.width += 3 * 3;
   //   return d;
   // }
+}
+
+// https://ateraimemo.com/Swing/ButtonDisabledHtmlText.html
+// https://github.com/aterai/java-swing-tips/tree/master/ButtonDisabledHtmlText
+class BlurLayerUI<V extends AbstractButton> extends LayerUI<V> {
+  private static final ConvolveOp CONVOLVE_OP = new ConvolveOp(new Kernel(3, 3, new float[] {
+      .05f, .05f, .05f,
+      .05f, .60f, .05f,
+      .05f, .05f, .05f
+  }), ConvolveOp.EDGE_NO_OP, null);
+  private transient BufferedImage buf;
+
+  @Override public void paint(Graphics g, JComponent c) {
+    if (c instanceof JLayer) {
+      Component view = ((JLayer<?>) c).getView();
+      if (view.isEnabled()) {
+        // super.paint(g, (JComponent) view);
+        view.paint(g);
+      } else {
+        Dimension d = view.getSize();
+        buf = Optional.ofNullable(buf)
+            .filter(bi -> bi.getWidth() == d.width && bi.getHeight() == d.height)
+            .orElseGet(() -> new BufferedImage(d.width, d.height, BufferedImage.TYPE_INT_ARGB));
+        Graphics2D g2 = buf.createGraphics();
+        view.paint(g2);
+        g2.dispose();
+        g.drawImage(CONVOLVE_OP.filter(buf, null), 0, 0, c);
+      }
+    }
+  }
+}
+
+// @see SwingSet3/src/com/sun/swingset3/SwingSet3.java
+final class LookAndFeelUtils {
+  private static String lookAndFeel = UIManager.getLookAndFeel().getClass().getName();
+
+  private LookAndFeelUtils() {
+    /* Singleton */
+  }
+
+  public static JMenu createLookAndFeelMenu() {
+    JMenu menu = new JMenu("LookAndFeel");
+    ButtonGroup buttonGroup = new ButtonGroup();
+    for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+      AbstractButton b = makeButton(info);
+      initLookAndFeelAction(info, b);
+      menu.add(b);
+      buttonGroup.add(b);
+    }
+    return menu;
+  }
+
+  private static AbstractButton makeButton(UIManager.LookAndFeelInfo info) {
+    boolean selected = info.getClassName().equals(lookAndFeel);
+    return new JRadioButtonMenuItem(info.getName(), selected);
+  }
+
+  public static void initLookAndFeelAction(UIManager.LookAndFeelInfo info, AbstractButton b) {
+    String cmd = info.getClassName();
+    b.setText(info.getName());
+    b.setActionCommand(cmd);
+    b.setHideActionText(true);
+    b.addActionListener(e -> setLookAndFeel(cmd));
+  }
+
+  private static void setLookAndFeel(String newLookAndFeel) {
+    String oldLookAndFeel = lookAndFeel;
+    if (!oldLookAndFeel.equals(newLookAndFeel)) {
+      try {
+        UIManager.setLookAndFeel(newLookAndFeel);
+        lookAndFeel = newLookAndFeel;
+      } catch (UnsupportedLookAndFeelException ignored) {
+        Toolkit.getDefaultToolkit().beep();
+      } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
+        ex.printStackTrace();
+        return;
+      }
+      updateLookAndFeel();
+      // firePropertyChange("lookAndFeel", oldLookAndFeel, newLookAndFeel);
+    }
+  }
+
+  private static void updateLookAndFeel() {
+    for (Window window : Window.getWindows()) {
+      SwingUtilities.updateComponentTreeUI(window);
+    }
+  }
 }
