4fd6c643f5d83afe614af8d97fef2a57ea7871e2
aterai <aterai@outlook.com>
2023-02-14 01:30:39 +0900

    refactor: changed to center the hint string on the height of the JComboBox editor

--- ComboEditorPlaceholder/src/java/example/MainPanel.java
+++ ComboEditorPlaceholder/src/java/example/MainPanel.java
@@ -9,6 +9,7 @@ import java.awt.event.FocusEvent;
 import java.awt.event.ItemEvent;
 import java.util.Optional;
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.plaf.LayerUI;
 import javax.swing.plaf.basic.BasicComboBoxEditor;
 import javax.swing.text.JTextComponent;
@@ -43,12 +44,27 @@ public final class MainPanel extends JPanel {
     //     }
     //   }
     //
-    //   private boolean isWindowsLnF() {
-    //     return getUI().getClass().getName().contains("WindowsComboBoxUI");
-    //   }
     // };
-    JComboBox<String> combo2 = new JComboBox<>();
-    combo2.setEditable(true);
+    JComboBox<String> combo2 = new JComboBox<String>() {
+      @Override public void updateUI() {
+        super.updateUI();
+        setEditable(true);
+        setEditor(new BasicComboBoxEditor() {
+          private Component editorComponent;
+
+          @Override public Component getEditorComponent() {
+            editorComponent = Optional.ofNullable(editorComponent).orElseGet(() -> {
+              JTextComponent tc = (JTextComponent) super.getEditorComponent();
+              return new JLayer<>(tc, new PlaceholderLayerUI<>("- Select type -"));
+            });
+            return editorComponent;
+          }
+        });
+          Border b1 = UIManager.getLookAndFeelDefaults().getBorder("ComboBox.border");
+          Border b2 = BorderFactory.createEmptyBorder(0, 2, 0, 0);
+          setBorder(BorderFactory.createCompoundBorder(b1, b2));
+      }
+    };
 
     combo1.addItemListener(e -> {
       if (e.getStateChange() == ItemEvent.SELECTED) {
@@ -57,21 +73,6 @@ public final class MainPanel extends JPanel {
         combo2.setSelectedIndex(-1);
       }
     });
-
-    combo2.setEditor(new BasicComboBoxEditor() {
-      private Component editorComponent;
-
-      @Override public Component getEditorComponent() {
-        editorComponent = Optional.ofNullable(editorComponent)
-            .orElseGet(() -> {
-              JTextComponent tc = (JTextComponent) super.getEditorComponent();
-              return new JLayer<>(tc, new PlaceholderLayerUI<>("- Select type -"));
-            });
-        return editorComponent;
-      }
-    });
-    combo2.setBorder(BorderFactory.createCompoundBorder(
-        combo2.getBorder(), BorderFactory.createEmptyBorder(0, 2, 0, 0)));
 
     JPanel p = new JPanel(new GridLayout(4, 1, 5, 5));
     setBorder(BorderFactory.createEmptyBorder(5, 20, 5, 20));
@@ -132,9 +133,11 @@ class PlaceholderLayerUI<E extends JTextComponent> extends LayerUI<E> {
         g2.setPaint(INACTIVE);
         // System.out.println("getInsets: " + tc.getInsets());
         // System.out.println("getMargin: " + tc.getMargin());
-        Insets i = tc.getMargin();
+        // Insets i = tc.getMargin();
+        Rectangle r = SwingUtilities.calculateInnerArea(tc, null);
         Dimension d = hint.getPreferredSize();
-        SwingUtilities.paintComponent(g2, hint, tc, i.left, i.top, d.width, d.height);
+        int yy = (int) (r.y + (r.height - d.height) / 2d);
+        SwingUtilities.paintComponent(g2, hint, tc, r.x, yy, d.width, d.height);
         g2.dispose();
       }
     }
