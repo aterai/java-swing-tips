c85e35a437b47c41312aed147420aa9fe5435ba4
aterai <aterai@outlook.com>
2023-03-24 00:44:18 +0900

    refactor: use 'SwingUtilities.layoutCompoundLabel(...)' to get width of the string instead of 'FontMetrics#stringWidth(...)'

--- ClippedCellTooltips/src/java/example/MainPanel.java
+++ ClippedCellTooltips/src/java/example/MainPanel.java
@@ -8,7 +8,9 @@ import java.awt.*;
 import java.util.Objects;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
 import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumnModel;
 import javax.swing.table.TableModel;
 
 public final class MainPanel extends JPanel {
@@ -37,7 +39,7 @@ public final class MainPanel extends JPanel {
           FontMetrics fm = l.getFontMetrics(l.getFont());
           String str = Objects.toString(getValueAt(row, column), "");
           int cellTextWidth = fm.stringWidth(str);
-          l.setToolTipText(cellTextWidth > rect.width ? str : null);
+          l.setToolTipText(cellTextWidth > rect.width ? str : getToolTipText());
         }
         return c;
       }
@@ -45,14 +47,14 @@ public final class MainPanel extends JPanel {
       @Override public void updateUI() {
         super.updateUI();
         TableCellRenderer r = new ToolTipHeaderRenderer();
-        for (int i = 0; i < getColumnModel().getColumnCount(); i++) {
-          getColumnModel().getColumn(i).setHeaderRenderer(r);
+        TableColumnModel columnModel = getColumnModel();
+        for (int i = 0; i < columnModel.getColumnCount(); i++) {
+          columnModel.getColumn(i).setHeaderRenderer(r);
         }
-        // JTableHeader h = getTableHeader();
-        // h.setDefaultRenderer(new ToolTipHeaderRenderer(h.getDefaultRenderer()));
       }
     };
     table.setAutoCreateRowSorter(true);
+
     add(new JScrollPane(table));
     setPreferredSize(new Dimension(320, 240));
   }
@@ -83,21 +85,37 @@ class ToolTipHeaderRenderer implements TableCellRenderer {
   // private final Icon icon = UIManager.getIcon("Table.ascendingSortIcon");
 
   @Override public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-    TableCellRenderer r = table.getTableHeader().getDefaultRenderer();
+    JTableHeader header = table.getTableHeader();
+    TableCellRenderer r = header.getDefaultRenderer();
     Component c = r.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
     if (c instanceof JLabel) {
       JLabel l = (JLabel) c;
       Insets i = l.getInsets();
-      int w = table.getCellRect(row, column, false).width - i.left + i.right;
-      Icon icon = l.getIcon();
-      if (icon != null) {
-        w -= icon.getIconWidth() + l.getIconTextGap();
-      }
-      FontMetrics fm = l.getFontMetrics(l.getFont());
-      String str = Objects.toString(value, "");
-      int cellTextWidth = fm.stringWidth(str);
-      l.setToolTipText(cellTextWidth > w ? str : null);
+      Rectangle rect = header.getHeaderRect(column); // = table.getCellRect(row, column, false);
+      rect.width -= i.left + i.right;
+      boolean isClipped = isClipped(l, rect);
+      // isClipped = l.getFontMetrics(l.getFont()).stringWidth(l.getText()) > rect.width;
+      l.setToolTipText(isClipped ? l.getText() : header.getToolTipText());
     }
     return c;
+  }
+
+  private static boolean isClipped(JLabel label, Rectangle viewR) {
+    Rectangle iconR = new Rectangle();
+    Rectangle textR = new Rectangle();
+    String str = SwingUtilities.layoutCompoundLabel(
+        label,
+        label.getFontMetrics(label.getFont()),
+        label.getText(),
+        label.getIcon(),
+        label.getVerticalAlignment(),
+        label.getHorizontalAlignment(),
+        label.getVerticalTextPosition(),
+        label.getHorizontalTextPosition(),
+        viewR,
+        iconR,
+        textR,
+        label.getIconTextGap());
+    return !Objects.equals(label.getText(), str);
   }
 }
