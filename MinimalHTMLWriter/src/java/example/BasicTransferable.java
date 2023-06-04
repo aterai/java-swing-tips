9e4dba3908c965cee8b71ea9a7141b1f627947ef
aterai <aterai@outlook.com>
2023-06-05 03:14:09 +0900

    add: MinimalHTMLWriter

---
+++ MinimalHTMLWriter/src/java/example/BasicTransferable.java
@@ -0,0 +1,302 @@
+// -*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+// @homepage@
+
+package example;
+
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.io.StringReader;
+import java.nio.charset.Charset;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Stream;
+import javax.swing.UIManager;
+
+// Copied from javax/swing/plaf/basic/BasicTransferable.java
+public class BasicTransferable implements Transferable {
+  private final String plainData;
+  private final String htmlData;
+  private static DataFlavor[] htmlFlavors;
+  private static DataFlavor[] stringFlavors;
+  private static DataFlavor[] plainFlavors;
+
+  static {
+    try {
+      htmlFlavors = new DataFlavor[3];
+      htmlFlavors[0] = new DataFlavor("text/html;class=java.lang.String");
+      htmlFlavors[1] = new DataFlavor("text/html;class=java.io.Reader");
+      htmlFlavors[2] = new DataFlavor("text/html;charset=unicode;class=java.io.InputStream");
+
+      plainFlavors = new DataFlavor[3];
+      plainFlavors[0] = new DataFlavor("text/plain;class=java.lang.String");
+      plainFlavors[1] = new DataFlavor("text/plain;class=java.io.Reader");
+      plainFlavors[2] = new DataFlavor("text/plain;charset=unicode;class=java.io.InputStream");
+
+      stringFlavors = new DataFlavor[2];
+      String mimeType = DataFlavor.javaJVMLocalObjectMimeType + ";class=java.lang.String";
+      stringFlavors[0] = new DataFlavor(mimeType);
+      stringFlavors[1] = DataFlavor.stringFlavor;
+    } catch (ClassNotFoundException ex) {
+      // System.err.println("error initializing javax.swing.plaf.basic.BasicTransferable");
+      UIManager.getLookAndFeel().provideErrorFeedback(null);
+      ex.printStackTrace();
+    }
+  }
+
+  public BasicTransferable(String plainData, String htmlData) {
+    this.plainData = plainData;
+    this.htmlData = htmlData;
+  }
+
+  /**
+   * Returns an array of DataFlavor objects indicating the flavors the data
+   * can be provided in.  The array should be ordered according to preference
+   * for providing the data (from most richly descriptive to least descriptive).
+   *
+   * @return an array of data flavors in which this data can be transferred
+   */
+  @Override public DataFlavor[] getTransferDataFlavors() {
+    DataFlavor[] richerFlavors = getRicherFlavors();
+    int numRicher = richerFlavors.length;
+    int numHtml = isHtmlSupported() ? htmlFlavors.length : 0;
+    int numPlain = isPlainSupported() ? plainFlavors.length : 0;
+    int numString = isPlainSupported() ? stringFlavors.length : 0;
+    int numFlavors = numRicher + numHtml + numPlain + numString;
+    DataFlavor[] flavors = new DataFlavor[numFlavors];
+
+    // fill in the array
+    int numDone = 0;
+    // if (numRicher > 0) {
+    System.arraycopy(richerFlavors, 0, flavors, numDone, numRicher);
+    numDone += numRicher;
+    // }
+    // if (numHtml > 0) {
+    System.arraycopy(htmlFlavors, 0, flavors, numDone, numHtml);
+    numDone += numHtml;
+    // }
+    // if (numPlain > 0) {
+    System.arraycopy(plainFlavors, 0, flavors, numDone, numPlain);
+    numDone += numPlain;
+    // }
+    // if (numString > 0) {
+    System.arraycopy(stringFlavors, 0, flavors, numDone, numString);
+    //   numDone += numString;
+    // }
+    return flavors;
+  }
+
+  /**
+   * Returns whether or not the specified data flavor is supported for
+   * this object.
+   *
+   * @param flavor  the requested flavor for the data
+   * @return boolean indicating whether or not the data flavor is supported
+   */
+  @Override public boolean isDataFlavorSupported(DataFlavor flavor) {
+    // DataFlavor[] flavors = getTransferDataFlavors();
+    // for (int i = 0; i < flavors.length; i++) {
+    //   if (flavors[i].equals(flavor)) {
+    //     return true;
+    //   }
+    // }
+    // return false;
+    return Stream.of(getTransferDataFlavors()).anyMatch(f -> f.equals(flavor));
+  }
+
+  /**
+   * Returns an object which represents the data to be transferred.  The class
+   * of the object returned is defined by the representation class of the flavor.
+   *
+   * @param flavor  the requested flavor for the data
+   * @exception IOException  if the data is no longer available in the requested flavor.
+   * @exception UnsupportedFlavorException  if the requested data flavor is not supported.
+   * @see DataFlavor#getRepresentationClass
+   */
+  @Override public Object getTransferData(DataFlavor flavor)
+      throws UnsupportedFlavorException, IOException {
+    // ???: DataFlavor[] richerFlavors = getRicherFlavors();
+    if (isRicherFlavor(flavor)) {
+      return null; // getRicherData(flavor);
+    } else if (isHtmlFlavor(flavor)) {
+      return getHtmlTransferData(flavor);
+    } else if (isPlainFlavor(flavor)) {
+      return getPlaneTransferData(flavor);
+    } else if (isStringFlavor(flavor)) {
+      return Objects.toString(getPlainData(), "");
+    }
+    throw new UnsupportedFlavorException(flavor);
+  }
+
+  // @see sun.datatransfer.DataFlavorUtil
+  public static String getTextCharset(DataFlavor flavor) {
+    // if (!isFlavorCharsetTextType(flavor)) {
+    //   return null;
+    // }
+    // String encoding = flavor.getParameter("charset");
+    // return Objects.nonNull(encoding) ? encoding : Charset.defaultCharset().name();
+    return Optional.ofNullable(flavor.getParameter("charset"))
+        .orElseGet(() -> Charset.defaultCharset().name());
+  }
+
+  private InputStream createInputStream(DataFlavor flavor, String data)
+      throws IOException, UnsupportedFlavorException {
+    String s = getTextCharset(flavor);
+    String cs = Optional.ofNullable(s).orElseThrow(() -> new UnsupportedFlavorException(flavor));
+    return new ByteArrayInputStream(data.getBytes(cs));
+  }
+
+  // --- richer subclass flavors ----------------------------------------------
+
+  private boolean isRicherFlavor(DataFlavor flavor) {
+    // DataFlavor[] richerFlavors = getRicherFlavors();
+    // int numFlavors = richerFlavors.length;
+    // for (int i = 0; i < numFlavors; i++) {
+    //   if (richerFlavors[i].equals(flavor)) {
+    //     return true;
+    //   }
+    // }
+    // return false;
+    return Stream.of(getRicherFlavors()).anyMatch(f -> f.equals(flavor));
+  }
+
+  /**
+   * Some subclasses will have flavors that are more descriptive than HTML
+   * or plain text.  If this method returns a non-null value, it will be
+   * placed at the start of the array of supported flavors.
+   */
+  private DataFlavor[] getRicherFlavors() {
+    return new DataFlavor[0]; // null;
+  }
+
+  // private Object getRicherData(DataFlavor flavor) throws UnsupportedFlavorException {
+  //   return null;
+  // }
+
+  // --- html flavors ----------------------------------------------------------
+
+  /**
+   * Returns whether or not the specified data flavor is an HTML flavor that
+   * is supported.
+   *
+   * @param flavor  the requested flavor for the data
+   * @return boolean indicating whether or not the data flavor is supported
+   */
+  private boolean isHtmlFlavor(DataFlavor flavor) {
+    // DataFlavor[] flavors = htmlFlavors;
+    // for (int i = 0; i < flavors.length; i++) {
+    //   if (flavors[i].equals(flavor)) {
+    //     return true;
+    //   }
+    // }
+    // return false;
+    return Stream.of(htmlFlavors).anyMatch(f -> f.equals(flavor));
+  }
+
+  /**
+   * Should the HTML flavors be offered?  If so, the method
+   * getHtmlData should be implemented to provide something reasonable.
+   */
+  private boolean isHtmlSupported() {
+    return Objects.nonNull(htmlData);
+  }
+
+  /**
+   * Fetch the data in a text/html format.
+   */
+  private String getHtmlData() {
+    return htmlData;
+  }
+
+  private Object getHtmlTransferData(DataFlavor flavor)
+      throws IOException, UnsupportedFlavorException {
+    // String data = getHtmlData();
+    // data = Objects.nonNull(data) ? data : "";
+    String data = Objects.toString(getHtmlData(), "");
+    if (String.class.equals(flavor.getRepresentationClass())) {
+      return data;
+    } else if (Reader.class.equals(flavor.getRepresentationClass())) {
+      return new StringReader(data);
+    } else if (InputStream.class.equals(flavor.getRepresentationClass())) {
+      // return new StringBufferInputStream(data);
+      return createInputStream(flavor, data);
+    }
+    throw new UnsupportedFlavorException(flavor);
+  }
+
+  // --- plain text flavors ----------------------------------------------------
+
+  /**
+   * Returns whether or not the specified data flavor is an plain flavor that
+   * is supported.
+   *
+   * @param flavor  the requested flavor for the data
+   * @return boolean indicating whether or not the data flavor is supported
+   */
+  private boolean isPlainFlavor(DataFlavor flavor) {
+    // DataFlavor[] flavors = plainFlavors;
+    // for (int i = 0; i < flavors.length; i++) {
+    //   if (flavors[i].equals(flavor)) {
+    //     return true;
+    //   }
+    // }
+    // return false;
+    return Stream.of(plainFlavors).anyMatch(f -> f.equals(flavor));
+  }
+
+  /**
+   * Should the plain text flavors be offered?  If so, the method
+   * getPlainData should be implemented to provide something reasonable.
+   */
+  private boolean isPlainSupported() {
+    return Objects.nonNull(plainData);
+  }
+
+  /**
+   * Fetch the data in a text/plain format.
+   */
+  private String getPlainData() {
+    return plainData;
+  }
+
+  private Object getPlaneTransferData(DataFlavor flavor)
+      throws IOException, UnsupportedFlavorException {
+    // String data = getPlainData();
+    // data = Objects.nonNull(data) ? data : "";
+    String data = Objects.toString(getPlainData(), "");
+    if (String.class.equals(flavor.getRepresentationClass())) {
+      return data;
+    } else if (Reader.class.equals(flavor.getRepresentationClass())) {
+      return new StringReader(data);
+    } else if (InputStream.class.equals(flavor.getRepresentationClass())) {
+      // return new StringBufferInputStream(data);
+      return createInputStream(flavor, data);
+    }
+    throw new UnsupportedFlavorException(flavor);
+  }
+
+  // --- string flavors --------------------------------------------------------
+
+  /**
+   * Returns whether the specified data flavor is a String flavor that
+   * is supported.
+   *
+   * @param flavor  the requested flavor for the data
+   * @return boolean indicating whether the data flavor is supported
+   */
+  private boolean isStringFlavor(DataFlavor flavor) {
+    // DataFlavor[] flavors = stringFlavors;
+    // for (int i = 0; i < flavors.length; i++) {
+    //   if (flavors[i].equals(flavor)) {
+    //     return true;
+    //   }
+    // }
+    // return false;
+    return Stream.of(stringFlavors).anyMatch(f -> f.equals(flavor));
+  }
+}
---
+++ MinimalHTMLWriter/src/java/example/MainPanel.java
@@ -0,0 +1,266 @@
+// -*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+// @homepage@
+
+package example;
+
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import javax.swing.*;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultEditorKit;
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.Element;
+import javax.swing.text.JTextComponent;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyleContext;
+import javax.swing.text.StyledDocument;
+import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.text.html.MinimalHTMLWriter;
+import javax.swing.text.html.parser.ParserDelegator;
+
+public final class MainPanel extends JPanel {
+  private MainPanel() {
+    super(new GridLayout(2, 1));
+    StyledDocument doc = new SimpleSyntaxDocument();
+    Style def = doc.getStyle(StyleContext.DEFAULT_STYLE);
+    StyleConstants.setForeground(doc.addStyle("red", def), Color.RED);
+    StyleConstants.setForeground(doc.addStyle("green", def), Color.GREEN);
+    StyleConstants.setForeground(doc.addStyle("blue", def), Color.BLUE);
+
+    JTextPane textPane = new JTextPane(doc);
+    textPane.setText("JTextPane(StyledDocument)\nred green, blue.");
+    textPane.setSelectedTextColor(null);
+    textPane.setSelectionColor(new Color(0x64_32_64_FF, true));
+    textPane.setComponentPopupMenu(new TextComponentPopupMenu());
+
+    add(new JScrollPane(textPane));
+    add(new JScrollPane(new JEditorPane("text/html", "JEditorPane")));
+    setPreferredSize(new Dimension(320, 240));
+  }
+
+  public static void main(String[] args) {
+    EventQueue.invokeLater(MainPanel::createAndShowGui);
+  }
+
+  private static void createAndShowGui() {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (UnsupportedLookAndFeelException ignored) {
+      Toolkit.getDefaultToolkit().beep();
+    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
+      ex.printStackTrace();
+      return;
+    }
+    JFrame frame = new JFrame("@title@");
+    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    frame.getContentPane().add(new MainPanel());
+    frame.pack();
+    frame.setLocationRelativeTo(null);
+    frame.setVisible(true);
+  }
+}
+
+class SimpleSyntaxDocument extends DefaultStyledDocument {
+  private static final String OPERANDS = ".,";
+
+  @Override public void insertString(int offset, String text, AttributeSet a) throws BadLocationException {
+    super.insertString(offset, text, a);
+    processChangedLines(offset, text.length());
+  }
+
+  @Override public void remove(int offset, int length) throws BadLocationException {
+    super.remove(offset, length);
+    processChangedLines(offset, 0);
+  }
+
+  private void processChangedLines(int offset, int length) throws BadLocationException {
+    Element root = getDefaultRootElement();
+    String content = getText(0, getLength());
+    int startLine = root.getElementIndex(offset);
+    int endLine = root.getElementIndex(offset + length);
+    for (int i = startLine; i <= endLine; i++) {
+      applyHighlighting(content, i);
+    }
+  }
+
+  private void applyHighlighting(String content, int line) {
+    Element root = getDefaultRootElement();
+    int startOffset = root.getElement(line).getStartOffset();
+    int endOffset = root.getElement(line).getEndOffset() - 1;
+    int lineLength = endOffset - startOffset;
+    int contentLength = content.length();
+    endOffset = endOffset >= contentLength ? contentLength - 1 : endOffset;
+    setCharacterAttributes(startOffset, lineLength, getStyle(StyleContext.DEFAULT_STYLE), true);
+    checkForTokens(content, startOffset, endOffset);
+  }
+
+  private void checkForTokens(String content, int startOffset, int endOffset) {
+    int index = startOffset;
+    while (index <= endOffset) {
+      while (isDelimiter(content.substring(index, index + 1))) {
+        if (index < endOffset) {
+          index++;
+        } else {
+          return;
+        }
+      }
+      index = getOtherToken(content, index, endOffset);
+    }
+  }
+
+  private int getOtherToken(String content, int startOffset, int endOffset) {
+    int endOfToken = startOffset + 1;
+    while (endOfToken <= endOffset) {
+      if (isDelimiter(content.substring(endOfToken, endOfToken + 1))) {
+        break;
+      }
+      endOfToken++;
+    }
+    String token = content.substring(startOffset, endOfToken);
+    Style s = getStyle(token);
+    // if (keywords.containsKey(token)) {
+    //  setCharacterAttributes(startOffset, endOfToken - startOffset, keywords.get(token), false);
+    if (Objects.nonNull(s)) {
+      setCharacterAttributes(startOffset, endOfToken - startOffset, s, false);
+    }
+    return endOfToken + 1;
+  }
+
+  protected boolean isDelimiter(String character) {
+    return Character.isWhitespace(character.charAt(0)) || OPERANDS.contains(character);
+  }
+}
+
+final class TextComponentPopupMenu extends JPopupMenu {
+  public TextComponentPopupMenu() {
+    super();
+    add(new DefaultEditorKit.CutAction());
+    add(new DefaultEditorKit.CopyAction());
+    add(new DefaultEditorKit.PasteAction());
+    add("delete").addActionListener(e -> {
+      Component c = getInvoker();
+      if (c instanceof JTextComponent) {
+        ((JTextComponent) c).replaceSelection(null);
+      }
+    });
+    addSeparator();
+    add("copy-html-and-text-to-clipboard").addActionListener(e -> {
+      Component c = getInvoker();
+      if (c instanceof JTextPane) {
+        copyHtmlTextToClipboard((JTextPane) c);
+      }
+    });
+    add("copy-html-to-clipboard").addActionListener(e -> {
+      Component c = getInvoker();
+      if (c instanceof JTextPane) {
+        copyHtmlToClipboard((JTextPane) c);
+      }
+    });
+  }
+
+  public void copyHtmlTextToClipboard(JTextPane textPane) {
+    Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+    int start = textPane.getSelectionStart();
+    int end = textPane.getSelectionEnd();
+    int length = end - start;
+    StyledDocument styledDocument = textPane.getStyledDocument();
+    try (OutputStream os = new ByteArrayOutputStream();
+         OutputStreamWriter writer = new OutputStreamWriter(os, StandardCharsets.UTF_8)) {
+      MinimalHTMLWriter w = new MinimalHTMLWriter(writer, styledDocument, start, length);
+      w.write();
+      writer.flush();
+      String contents = os.toString();
+      ParserDelegator delegator = new ParserDelegator();
+      StringBuilder plainBuf = new StringBuilder();
+      delegator.parse(new StringReader(contents), new HTMLEditorKit.ParserCallback() {
+        @Override public void handleText(char[] text, int pos) {
+          plainBuf.append(text);
+        }
+      }, Boolean.TRUE);
+
+      Transferable transferable = new BasicTransferable(plainBuf.toString(), contents);
+      clipboard.setContents(transferable, null);
+    } catch (IOException | BadLocationException e) {
+      UIManager.getLookAndFeel().provideErrorFeedback(textPane);
+      e.printStackTrace();
+    }
+  }
+
+  public void copyHtmlToClipboard(JTextPane textPane) {
+    Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+    int start = textPane.getSelectionStart();
+    int end = textPane.getSelectionEnd();
+    int length = end - start;
+    StyledDocument styledDocument = textPane.getStyledDocument();
+    try (OutputStream os = new ByteArrayOutputStream();
+         OutputStreamWriter writer = new OutputStreamWriter(os, StandardCharsets.UTF_8)) {
+      MinimalHTMLWriter w = new MinimalHTMLWriter(writer, styledDocument, start, length);
+      w.write();
+      writer.flush();
+      String contents = os.toString();
+      Transferable htmlTransferable = new HtmlTransferable(contents);
+      clipboard.setContents(htmlTransferable, null);
+    } catch (IOException | BadLocationException e) {
+      UIManager.getLookAndFeel().provideErrorFeedback(textPane);
+      e.printStackTrace();
+    }
+  }
+
+  @Override public void show(Component c, int x, int y) {
+    if (c instanceof JTextComponent) {
+      JTextComponent tc = (JTextComponent) c;
+      boolean hasSelectedText = Objects.nonNull(tc.getSelectedText());
+      for (MenuElement menuElement : getSubElements()) {
+        Component m = menuElement.getComponent();
+        Action a = m instanceof JMenuItem ? ((JMenuItem) m).getAction() : null;
+        if (a instanceof DefaultEditorKit.PasteAction) {
+          continue;
+        }
+        m.setEnabled(hasSelectedText);
+      }
+      super.show(c, x, y);
+    }
+  }
+}
+
+class HtmlTransferable implements Transferable {
+  private final String htmlFormattedText;
+
+  public HtmlTransferable(String htmlFormattedText) {
+    this.htmlFormattedText = htmlFormattedText;
+  }
+
+  @Override public DataFlavor[] getTransferDataFlavors() {
+    return new DataFlavor[] { DataFlavor.allHtmlFlavor };
+  }
+
+  @Override public boolean isDataFlavorSupported(DataFlavor flavor) {
+    for (DataFlavor supportedFlavor : getTransferDataFlavors()) {
+      if (supportedFlavor.equals(flavor)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  @Override public Object getTransferData(DataFlavor flavor)
+      throws UnsupportedFlavorException, IOException {
+    if (Objects.equals(flavor, DataFlavor.allHtmlFlavor)) {
+      return htmlFormattedText;
+    }
+    throw new UnsupportedFlavorException(flavor);
+  }
+}
