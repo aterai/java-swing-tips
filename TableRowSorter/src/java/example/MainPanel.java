b16e4744d75a7f48e342b470ca11fd8c202c4320
TERAI Atsuhiro <aterai@outlook.com>
2024-09-06 01:34:05 +0900

    refactor: extract method returning 'model' from a long surrounding method

--- TableRowSorter/src/java/example/MainPanel.java
+++ TableRowSorter/src/java/example/MainPanel.java
@@ -16,19 +16,11 @@ import javax.swing.table.TableRowSorter;
 public final class MainPanel extends JPanel {
   private MainPanel() {
     super(new BorderLayout());
-    String[] columnNames = {"String", "Integer", "Boolean"};
-    Object[][] data = {
-        {"aaa", 12, true}, {"bbb", 5, false}, {"CCC", 92, true}, {"DDD", 0, false}
-    };
-    TableModel model = new DefaultTableModel(data, columnNames) {
-      @Override public Class<?> getColumnClass(int column) {
-        return getValueAt(0, column).getClass();
-      }
-    };
-    JTable table = new JTable(model);
+    JTable table = new JTable(makeModel());
     // table.setAutoCreateRowSorter(true);
+    table.setRowSorter(new TableRowSorter<>(table.getModel()));
     // TableRowSorter<TableModel> sorter = new TableRowSorter<>(model);
-    table.setRowSorter(new TableRowSorter<>(model));
+    // table.setRowSorter(sorter);
     // sorter.setSortKeys(Arrays.asList(new RowSorter.SortKey(0, SortOrder.DESCENDING)));
 
     TableCellRenderer renderer = (tbl, value, isSelected, hasFocus, row, column) -> {
@@ -70,6 +62,18 @@ public final class MainPanel extends JPanel {
     add(button, BorderLayout.SOUTH);
     add(new JScrollPane(table));
     setPreferredSize(new Dimension(320, 240));
+  }
+
+  private static TableModel makeModel() {
+    String[] columnNames = {"String", "Integer", "Boolean"};
+    Object[][] data = {
+        {"aaa", 12, true}, {"bbb", 5, false}, {"CCC", 92, true}, {"DDD", 0, false}
+    };
+    return new DefaultTableModel(data, columnNames) {
+      @Override public Class<?> getColumnClass(int column) {
+        return getValueAt(0, column).getClass();
+      }
+    };
   }
 
   public static void main(String[] args) {
