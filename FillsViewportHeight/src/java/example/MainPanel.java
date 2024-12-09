bfa966e9c3f5ba10f29a0cb0f368e73410e6555f
TERAI Atsuhiro <aterai@outlook.com>
2024-12-09 09:21:43 +0900

    refactor: simplify the example

--- FillsViewportHeight/src/java/example/MainPanel.java
+++ FillsViewportHeight/src/java/example/MainPanel.java
@@ -9,38 +9,20 @@ import java.awt.event.ItemEvent;
 import java.util.stream.Stream;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableCellRenderer;
-import javax.swing.table.TableModel;
 
 public final class MainPanel extends JPanel {
   private MainPanel() {
     super(new BorderLayout());
-    JTable table = new JTable(makeModel()) {
-      private final Color evenColor = new Color(0xF5_F5_F5);
-
-      @Override public Component prepareRenderer(TableCellRenderer tcr, int row, int column) {
-        Component c = super.prepareRenderer(tcr, row, column);
-        if (isRowSelected(row)) {
-          c.setForeground(getSelectionForeground());
-          c.setBackground(getSelectionBackground());
-        } else {
-          c.setForeground(getForeground());
-          c.setBackground(row % 2 == 0 ? evenColor : getBackground());
-        }
-        return c;
-      }
-    };
-    // table.setRowSorter(new TableRowSorter<>(table.getModel()));
+    JTable table = new JTable(new DefaultTableModel(5, 3));
+    table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
     table.setAutoCreateRowSorter(true);
+    // table.setBackground(Color.BLUE);
+    // table.setOpaque(false);
+    // table.setBackground(scroll.getBackground());
 
     JScrollPane scroll = new JScrollPane(table);
     scroll.setBackground(Color.RED);
     scroll.getViewport().setBackground(Color.GREEN);
-    // table.setBackground(Color.BLUE);
-    // table.setOpaque(false);
-    // table.setBackground(scroll.getBackground());
-    table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-    table.setComponentPopupMenu(new TablePopupMenu());
     // scroll.getViewport().setComponentPopupMenu(makePop());
     // scroll.setComponentPopupMenu(makePop());
 
@@ -48,18 +30,6 @@ public final class MainPanel extends JPanel {
     add(makeColorBox(table), BorderLayout.SOUTH);
     add(scroll);
     setPreferredSize(new Dimension(320, 240));
-  }
-
-  private static TableModel makeModel() {
-    String[] columnNames = {"String", "Integer", "Boolean"};
-    Object[][] data = {
-        {"aaa", 12, true}, {"bbb", 5, false}, {"CCC", 92, true}, {"DDD", 0, false}
-    };
-    return new DefaultTableModel(data, columnNames) {
-      @Override public Class<?> getColumnClass(int column) {
-        return getValueAt(0, column).getClass();
-      }
-    };
   }
 
   private static Component makeToolBox(JTable table) {
@@ -120,37 +90,5 @@ public final class MainPanel extends JPanel {
     frame.pack();
     frame.setLocationRelativeTo(null);
     frame.setVisible(true);
-  }
-}
-
-final class TablePopupMenu extends JPopupMenu {
-  private final JMenuItem delete;
-
-  /* default */ TablePopupMenu() {
-    super();
-    add("add").addActionListener(e -> {
-      JTable table = (JTable) getInvoker();
-      DefaultTableModel model = (DefaultTableModel) table.getModel();
-      model.addRow(new Object[] {"example", model.getRowCount(), false});
-      Rectangle r = table.getCellRect(model.getRowCount() - 1, 0, true);
-      table.scrollRectToVisible(r);
-    });
-    addSeparator();
-    delete = add("delete");
-    delete.addActionListener(e -> {
-      JTable table = (JTable) getInvoker();
-      DefaultTableModel model = (DefaultTableModel) table.getModel();
-      int[] selection = table.getSelectedRows();
-      for (int i = selection.length - 1; i >= 0; i--) {
-        model.removeRow(table.convertRowIndexToModel(selection[i]));
-      }
-    });
-  }
-
-  @Override public void show(Component c, int x, int y) {
-    if (c instanceof JTable) {
-      delete.setEnabled(((JTable) c).getSelectedRowCount() > 0);
-      super.show(c, x, y);
-    }
   }
 }
