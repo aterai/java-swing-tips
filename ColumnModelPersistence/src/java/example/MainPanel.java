8cf00dd8e85a5a1660f6b783eac9279115afaeac
TERAI Atsuhiro <aterai@outlook.com>
2024-12-25 01:42:49 +0900

    Checkstyle: JavaNCSS

--- ColumnModelPersistence/src/java/example/MainPanel.java
+++ ColumnModelPersistence/src/java/example/MainPanel.java
@@ -21,6 +21,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import javax.swing.*;
 import javax.swing.event.AncestorEvent;
@@ -38,64 +39,17 @@ public final class MainPanel extends JPanel {
     JTable table = new JTable(new DefaultTableModel(data, columnNames));
     table.setAutoCreateRowSorter(true);
     table.getTableHeader().setComponentPopupMenu(new TableHeaderPopupMenu());
-
     JTextArea textArea = new JTextArea();
-
     JSplitPane sp = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
     sp.setResizeWeight(.5);
     sp.setTopComponent(new JScrollPane(table));
     sp.setBottomComponent(new JScrollPane(textArea));
 
     JButton encodeButton = new JButton("XMLEncoder");
-    encodeButton.addActionListener(e -> {
-      try {
-        Path path = File.createTempFile("output", ".xml").toPath();
-        // try (var xe = new XMLEncoder(new BufferedOutputStream(new FileOutputStream(file)))) {
-        try (XMLEncoder xe = new XMLEncoder(getOutputStream(path))) {
-          String[] constructors = {"column", "sortOrder"};
-          PersistenceDelegate d1 = new DefaultPersistenceDelegate(constructors);
-          xe.setPersistenceDelegate(RowSorter.SortKey.class, d1);
-          xe.writeObject(table.getRowSorter().getSortKeys());
-
-          PersistenceDelegate d2 = new DefaultTableModelPersistenceDelegate();
-          xe.setPersistenceDelegate(DefaultTableModel.class, d2);
-          xe.writeObject(table.getModel());
-
-          PersistenceDelegate d3 = new DefaultTableColumnModelPersistenceDelegate();
-          xe.setPersistenceDelegate(DefaultTableColumnModel.class, d3);
-          xe.writeObject(table.getColumnModel());
-        }
-        try (Reader r = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
-          textArea.read(r, "temp");
-        }
-      } catch (IOException ex) {
-        ex.printStackTrace();
-        textArea.setText(ex.getMessage());
-      }
-    });
+    encodeButton.addActionListener(e -> encode(table, textArea));
 
     JButton decodeButton = new JButton("XMLDecoder");
-    decodeButton.addActionListener(e -> {
-      String text = textArea.getText();
-      if (text.isEmpty()) {
-        return;
-      }
-      try (XMLDecoder xd = new XMLDecoder(getInputStream(text))) {
-        // @SuppressWarnings("unchecked")
-        // var keys = (List<? extends RowSorter.SortKey>) xd.readObject();
-        Class<RowSorter.SortKey> clz = RowSorter.SortKey.class;
-        List<? extends RowSorter.SortKey> keys = ((List<?>) xd.readObject()).stream()
-            .filter(clz::isInstance)
-            .map(clz::cast)
-            .collect(Collectors.toList());
-        DefaultTableModel model = (DefaultTableModel) xd.readObject();
-        table.setModel(model);
-        table.setAutoCreateRowSorter(true);
-        table.getRowSorter().setSortKeys(keys);
-        DefaultTableColumnModel cm = (DefaultTableColumnModel) xd.readObject();
-        table.setColumnModel(cm);
-      }
-    });
+    decodeButton.addActionListener(e -> decode(table, textArea));
 
     JButton clearButton = new JButton("clear");
     clearButton.addActionListener(e -> table.setModel(new DefaultTableModel()));
@@ -107,6 +61,55 @@ public final class MainPanel extends JPanel {
     add(sp);
     add(p, BorderLayout.SOUTH);
     setPreferredSize(new Dimension(320, 240));
+  }
+
+  private void encode(JTable table, JTextArea textArea) {
+    try {
+      Path path = File.createTempFile("output", ".xml").toPath();
+      // try (var xe = new XMLEncoder(new BufferedOutputStream(new FileOutputStream(file)))) {
+      try (XMLEncoder xe = new XMLEncoder(getOutputStream(path))) {
+        String[] constructors = {"column", "sortOrder"};
+        PersistenceDelegate d1 = new DefaultPersistenceDelegate(constructors);
+        xe.setPersistenceDelegate(RowSorter.SortKey.class, d1);
+        xe.writeObject(table.getRowSorter().getSortKeys());
+
+        PersistenceDelegate d2 = new DefaultTableModelPersistenceDelegate();
+        xe.setPersistenceDelegate(DefaultTableModel.class, d2);
+        xe.writeObject(table.getModel());
+
+        PersistenceDelegate d3 = new DefaultTableColumnModelPersistenceDelegate();
+        xe.setPersistenceDelegate(DefaultTableColumnModel.class, d3);
+        xe.writeObject(table.getColumnModel());
+      }
+      try (Reader r = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
+        textArea.read(r, "temp");
+      }
+    } catch (IOException ex) {
+      Logger.getGlobal().severe(ex::getMessage);
+      textArea.setText(ex.getMessage());
+    }
+  }
+
+  private void decode(JTable table, JTextArea textArea) {
+    String text = textArea.getText();
+    if (text.isEmpty()) {
+      return;
+    }
+    try (XMLDecoder xd = new XMLDecoder(getInputStream(text))) {
+      // @SuppressWarnings("unchecked")
+      // var keys = (List<? extends RowSorter.SortKey>) xd.readObject();
+      Class<RowSorter.SortKey> clz = RowSorter.SortKey.class;
+      List<? extends RowSorter.SortKey> keys = ((List<?>) xd.readObject()).stream()
+          .filter(clz::isInstance)
+          .map(clz::cast)
+          .collect(Collectors.toList());
+      DefaultTableModel model = (DefaultTableModel) xd.readObject();
+      table.setModel(model);
+      table.setAutoCreateRowSorter(true);
+      table.getRowSorter().setSortKeys(keys);
+      DefaultTableColumnModel cm = (DefaultTableColumnModel) xd.readObject();
+      table.setColumnModel(cm);
+    }
   }
 
   private BufferedOutputStream getOutputStream(Path path) throws IOException {
@@ -128,7 +131,7 @@ public final class MainPanel extends JPanel {
     } catch (UnsupportedLookAndFeelException ignored) {
       Toolkit.getDefaultToolkit().beep();
     } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
-      ex.printStackTrace();
+      Logger.getGlobal().severe(ex::getMessage);
       return;
     }
     JFrame frame = new JFrame("@title@");
