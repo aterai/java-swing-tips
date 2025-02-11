75323c68d264e749e6829b67427feffbfd65e07f
TERAI Atsuhiro <aterai@outlook.com>
2025-02-12 01:54:54 +0900

    Checkstyle: AnonInnerLength

--- examples/ComboBoxEditorVerifier/src/java/example/MainPanel.java
+++ examples/ComboBoxEditorVerifier/src/java/example/MainPanel.java
@@ -20,46 +20,25 @@ public final class MainPanel extends JPanel {
   private MainPanel() {
     super(new BorderLayout());
     String[] model = {"123456", "7890"};
-    JComboBox<String> combo = new JComboBox<>(model);
-    combo.setEditable(true);
+    JComboBox<String> combo = new EditableComboBox(model);
+    initComboBoxEditor(combo);
+    JPanel p = new JPanel(new GridLayout(5, 1));
+    p.add(new JLabel("Default:", SwingConstants.LEFT));
+    p.add(new EditableComboBox(model));
+    p.add(Box.createVerticalStrut(15));
+    p.add(new JLabel("6 >= str.length()", SwingConstants.LEFT));
+    p.add(combo);
+    add(p, BorderLayout.NORTH);
+    setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+    setPreferredSize(new Dimension(320, 240));
+  }
 
-    JComboBox<String> comboBox = new JComboBox<String>(model) {
-      private static final int MAX_HISTORY = 10;
-      private static final String ENTER_PRESSED = "enterPressed";
-      @Override public void updateUI() {
-        getActionMap().put(ENTER_PRESSED, null);
-        super.updateUI();
-        JComboBox<String> cb = this;
-        Action defaultAction = getActionMap().get(ENTER_PRESSED);
-        getActionMap().put(ENTER_PRESSED, new AbstractAction() {
-          @Override public void actionPerformed(ActionEvent e) {
-            boolean isPopupVisible = isPopupVisible();
-            setPopupVisible(false);
-            DefaultComboBoxModel<String> m = (DefaultComboBoxModel<String>) getModel();
-            String str = Objects.toString(getEditor().getItem(), "");
-            if (m.getIndexOf(str) < 0 && getInputVerifier().verify(cb)) {
-              m.removeElement(str);
-              m.insertElementAt(str, 0);
-              if (m.getSize() > MAX_HISTORY) {
-                m.removeElementAt(MAX_HISTORY);
-              }
-              setSelectedIndex(0);
-              setPopupVisible(isPopupVisible);
-            } else {
-              defaultAction.actionPerformed(e);
-            }
-          }
-        });
-      }
-    };
-    comboBox.setEditable(true);
-    comboBox.setInputVerifier(new LengthInputVerifier());
-    comboBox.setEditor(new BasicComboBoxEditor() {
+  private static void initComboBoxEditor(JComboBox<String> combo) {
+    combo.setEditor(new BasicComboBoxEditor() {
       private Component editorComponent;
       // // @see javax/swing/plaf/synth/SynthComboBoxUI.java
       // @Override public JTextField createEditorComponent() {
-      //   JTextField f = new JTextField("", 9);
-      //   f.setName("ComboBox.textField");
+      //   JTextField f = new JTextField("", 9); f.setName("ComboBox.textField");
       //   return f;
       // }
 
@@ -74,17 +53,8 @@ public final class MainPanel extends JPanel {
         return new JLayer<>(c, new ValidationLayerUI<>());
       }
     });
-    comboBox.addPopupMenuListener(new SelectItemMenuListener());
-
-    JPanel p = new JPanel(new GridLayout(5, 1));
-    p.add(new JLabel("Default:", SwingConstants.LEFT));
-    p.add(combo);
-    p.add(Box.createVerticalStrut(15));
-    p.add(new JLabel("6 >= str.length()", SwingConstants.LEFT));
-    p.add(comboBox);
-    add(p, BorderLayout.NORTH);
-    setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-    setPreferredSize(new Dimension(320, 240));
+    combo.setInputVerifier(new LengthInputVerifier());
+    combo.addPopupMenuListener(new SelectItemMenuListener());
   }
 
   public static void main(String[] args) {
@@ -124,6 +94,53 @@ class SelectItemMenuListener implements PopupMenuListener {
   }
 }
 
+class EditableComboBox extends JComboBox<String> {
+  private static final int MAX_HISTORY = 10;
+  private static final String ENTER_PRESSED = "enterPressed";
+  private Action defaultAction;
+
+  protected EditableComboBox(String... model) {
+    super(model);
+  }
+
+  @Override public void updateUI() {
+    getActionMap().put(ENTER_PRESSED, null);
+    super.updateUI();
+    defaultAction = getActionMap().get(ENTER_PRESSED);
+    getActionMap().put(ENTER_PRESSED, new EnterAction());
+    setEditable(true);
+  }
+
+  protected final class EnterAction extends AbstractAction {
+    @Override public void actionPerformed(ActionEvent e) {
+      boolean isPopupVisible = isPopupVisible();
+      DefaultComboBoxModel<String> m = (DefaultComboBoxModel<String>) getModel();
+      String s = Objects.toString(getEditor().getItem(), "");
+      InputVerifier v = getInputVerifier();
+      if (v != null && v.verify((JComboBox<?>) e.getSource()) && m.getIndexOf(s) < 0) {
+        setPopupVisible(false);
+        updateModel(m, s);
+        setPopupVisible(isPopupVisible);
+      } else if (v == null && m.getIndexOf(s) < 0) {
+        setPopupVisible(false);
+        updateModel(m, s);
+        setPopupVisible(isPopupVisible);
+      } else {
+        defaultAction.actionPerformed(e);
+      }
+    }
+
+    private void updateModel(DefaultComboBoxModel<String> m, String s) {
+      m.removeElement(s);
+      m.insertElementAt(s, 0);
+      if (m.getSize() > MAX_HISTORY) {
+        m.removeElementAt(MAX_HISTORY);
+      }
+      setSelectedIndex(0);
+    }
+  }
+}
+
 // @see https://docs.oracle.com/javase/tutorial/uiswing/examples/misc/FieldValidatorProject/src/FieldValidator.java
 class ValidationLayerUI<V extends JTextComponent> extends LayerUI<V> {
   @Override public void paint(Graphics g, JComponent c) {
@@ -131,29 +148,11 @@ class ValidationLayerUI<V extends JTextComponent> extends LayerUI<V> {
     // JTextComponent tc = ((JLayer<?>) c).getView();
     Container p = SwingUtilities.getAncestorOfClass(JComboBox.class, c);
     if (p instanceof JComboBox) {
-      JComboBox<?> cb = (JComboBox<?>) p;
-      getInputVerifier(cb).filter(iv -> !iv.verify(cb)).ifPresent(iv -> {
-        int w = c.getWidth();
-        int h = c.getHeight();
-        int s = 8;
-        int pad = 5;
-        int x = w - pad - s;
-        int y = (h - s) / 2;
-        Graphics2D g2 = (Graphics2D) g.create();
-        g2.translate(x, y);
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.setPaint(Color.RED);
-        g2.fillRect(0, 0, s + 1, s + 1);
-        g2.setPaint(Color.WHITE);
-        g2.drawLine(0, 0, s, s);
-        g2.drawLine(0, s, s, 0);
-        g2.dispose();
-      });
+      JComboBox<?> combo = (JComboBox<?>) p;
+      Optional.ofNullable(combo.getInputVerifier())
+          .filter(iv -> !iv.verify(combo))
+          .ifPresent(iv -> new ErrorIcon().paintIcon(c, g, 0, 0));
     }
-  }
-
-  private static Optional<? extends InputVerifier> getInputVerifier(JComponent c) {
-    return Optional.ofNullable(c.getInputVerifier());
   }
 }
 
@@ -167,5 +166,34 @@ class LengthInputVerifier extends InputVerifier {
   public static boolean maxLengthCheck(JComboBox<?> comboBox, int max) {
     String str = Objects.toString(comboBox.getEditor().getItem(), "");
     return max - str.length() >= 0;
+  }
+}
+
+class ErrorIcon implements Icon {
+  @Override public void paintIcon(Component c, Graphics g, int x, int y) {
+    int w = c.getWidth();
+    int h = c.getHeight();
+    int iw = getIconWidth();
+    int ih = getIconHeight();
+    int pad = 5;
+    int cx = w - pad - iw;
+    int cy = (h - ih) / 2;
+    Graphics2D g2 = (Graphics2D) g.create();
+    g2.translate(cx, cy);
+    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+    g2.setPaint(Color.RED);
+    g2.fillRect(0, 0, iw + 1, ih + 1);
+    g2.setPaint(Color.WHITE);
+    g2.drawLine(0, 0, iw, ih);
+    g2.drawLine(0, ih, iw, 0);
+    g2.dispose();
+  }
+
+  @Override public int getIconWidth() {
+    return 8;
+  }
+
+  @Override public int getIconHeight() {
+    return 8;
   }
 }
