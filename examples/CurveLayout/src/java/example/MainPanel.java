e9442114c0c9dbf8c97df248d008bb1e804eac33
TERAI Atsuhiro <aterai@outlook.com>
2025-02-25 03:04:22 +0900

    Checkstyle: AnonInnerLength

--- examples/CurveLayout/src/java/example/MainPanel.java
+++ examples/CurveLayout/src/java/example/MainPanel.java
@@ -5,65 +5,15 @@
 package example;
 
 import java.awt.*;
+import java.util.logging.Logger;
 import javax.swing.*;
 
 public final class MainPanel extends JPanel {
   private MainPanel() {
     super(new BorderLayout());
-    JPanel panel1 = new JPanel(new FlowLayout(FlowLayout.LEFT));
-    JPanel panel2 = new JPanel() {
-      public static final double A2 = 4d;
-      @Override protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D) g.create();
-        Rectangle r = SwingUtilities.calculateInnerArea(this, null);
-        g2.translate(r.x, r.y);
-        g2.setPaint(Color.RED);
-        int px = 0;
-        int py = 0;
-        for (int x = 0; x < r.width; x++) {
-          int y = (int) Math.pow(x / A2, 2d);
-          g2.drawLine(px, py, x, y);
-          px = x;
-          py = y;
-        }
-        g2.dispose();
-      }
-
-      @Override public void updateUI() {
-        super.updateUI();
-        setLayout(new FlowLayout() {
-          @SuppressWarnings("PMD.AvoidSynchronizedStatement")
-          @Override public void layoutContainer(Container target) {
-            synchronized (target.getTreeLock()) {
-              int nmembers = target.getComponentCount();
-              if (nmembers <= 0 || !(target instanceof JComponent)) {
-                return;
-              }
-              Rectangle r = SwingUtilities.calculateInnerArea((JComponent) target, null);
-              int vgap = getVgap();
-              int hgap = getHgap();
-              int rh = (r.height - vgap * 2) / nmembers;
-              int x = r.x + hgap;
-              int y = r.y + vgap;
-              for (Component m : target.getComponents()) {
-                if (m.isVisible()) {
-                  Dimension d = m.getPreferredSize();
-                  m.setSize(d.width, d.height);
-                  m.setLocation(x, y);
-                  y += vgap + Math.min(rh, d.height);
-                  x = (int) (A2 * Math.sqrt(y));
-                }
-              }
-            }
-          }
-        });
-      }
-    };
-
     JPanel p = new JPanel(new GridLayout(1, 2));
-    p.add(initPanel("FlowLayout(LEFT)", panel1));
-    p.add(initPanel("y=Math.pow(x/4.0,2.0)", panel2));
+    p.add(initPanel("FlowLayout(LEFT)", new JPanel(new FlowLayout(FlowLayout.LEFT))));
+    p.add(initPanel("y=Math.pow(x/4.0,2.0)", new CurvePanel()));
     add(p);
     setPreferredSize(new Dimension(320, 240));
   }
@@ -88,7 +38,7 @@ public final class MainPanel extends JPanel {
     } catch (UnsupportedLookAndFeelException ignored) {
       Toolkit.getDefaultToolkit().beep();
     } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
-      ex.printStackTrace();
+      Logger.getGlobal().severe(ex::getMessage);
       return;
     }
     JFrame frame = new JFrame("@title@");
@@ -97,5 +47,58 @@ public final class MainPanel extends JPanel {
     frame.pack();
     frame.setLocationRelativeTo(null);
     frame.setVisible(true);
+  }
+}
+
+class CurvePanel extends JPanel {
+  @Override protected void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    Graphics2D g2 = (Graphics2D) g.create();
+    Rectangle r = SwingUtilities.calculateInnerArea(this, null);
+    g2.translate(r.x, r.y);
+    g2.setPaint(Color.RED);
+    int px = 0;
+    int py = 0;
+    for (int x = 0; x < r.width; x++) {
+      int y = (int) Math.pow(x / CurveLayout.A2, 2d);
+      g2.drawLine(px, py, x, y);
+      px = x;
+      py = y;
+    }
+    g2.dispose();
+  }
+
+  @Override public void updateUI() {
+    super.updateUI();
+    setLayout(new CurveLayout());
+  }
+}
+
+class CurveLayout extends FlowLayout {
+  public static final double A2 = 4d;
+
+  @SuppressWarnings("PMD.AvoidSynchronizedStatement")
+  @Override public void layoutContainer(Container target) {
+    synchronized (target.getTreeLock()) {
+      int members = target.getComponentCount();
+      if (members <= 0 || !(target instanceof JComponent)) {
+        return;
+      }
+      Rectangle r = SwingUtilities.calculateInnerArea((JComponent) target, null);
+      int vg = getVgap();
+      int hg = getHgap();
+      int rh = (r.height - vg * 2) / members;
+      int x = r.x + hg;
+      int y = r.y + vg;
+      for (Component m : target.getComponents()) {
+        if (m.isVisible()) {
+          Dimension d = m.getPreferredSize();
+          m.setSize(d.width, d.height);
+          m.setLocation(x, y);
+          y += vg + Math.min(rh, d.height);
+          x = (int) (A2 * Math.sqrt(y));
+        }
+      }
+    }
   }
 }
