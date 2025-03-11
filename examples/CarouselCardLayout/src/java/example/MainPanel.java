565370ba1db2f92987fa49377a759bbedeac90db
TERAI Atsuhiro <aterai@outlook.com>
2025-03-12 02:27:27 +0900

    Checkstyle: AnonInnerLength

--- examples/CarouselCardLayout/src/java/example/MainPanel.java
+++ examples/CarouselCardLayout/src/java/example/MainPanel.java
@@ -5,46 +5,14 @@
 package example;
 
 import java.awt.*;
+import java.util.logging.Logger;
 import javax.swing.*;
 
 public final class MainPanel extends JPanel {
   private MainPanel() {
     super(new BorderLayout());
     CardLayout cardLayout = new CardLayout(50, 5);
-
-    JPanel cards = new JPanel(cardLayout) {
-      @Override protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        for (Component c : getComponents()) {
-          if (c.isVisible()) {
-            paintSideComponents(g, getComponentZOrder(c));
-            return;
-          }
-        }
-      }
-
-      private void paintSideComponents(Graphics g, int current) {
-        Graphics2D g2 = (Graphics2D) g.create();
-        g2.setComposite(AlphaComposite.SrcOver.derive(.5f));
-        Insets insets = getInsets();
-        int hgap = cardLayout.getHgap();
-        int vgap = cardLayout.getVgap();
-        int cw = getWidth() - (hgap * 2 + insets.left + insets.right);
-        // int ch = getHeight() - (vgap * 2 + insets.top + insets.bottom);
-        int gap = 10;
-        int nc = getComponentCount();
-
-        g2.translate(hgap + insets.left - cw - gap, vgap + insets.top);
-        Component prev = getComponent(current > 0 ? current - 1 : nc - 1);
-        prev.print(g2);
-
-        g2.translate((cw + gap) * 2, 0);
-        Component next = getComponent((current + 1) % nc);
-        next.print(g2);
-
-        g2.dispose();
-      }
-    };
+    JPanel cards = new CarouselCardPanel(cardLayout);
     cards.add(new JScrollPane(new JTree()), "JTree");
     cards.add(new JSplitPane(), "JSplitPane");
     cards.add(new JScrollPane(new JTable(9, 3)), "JTable");
@@ -83,7 +51,7 @@ public final class MainPanel extends JPanel {
     } catch (UnsupportedLookAndFeelException ignored) {
       Toolkit.getDefaultToolkit().beep();
     } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
-      ex.printStackTrace();
+      Logger.getGlobal().severe(ex::getMessage);
       return;
     }
     JFrame frame = new JFrame("@title@");
@@ -92,5 +60,47 @@ public final class MainPanel extends JPanel {
     frame.pack();
     frame.setLocationRelativeTo(null);
     frame.setVisible(true);
+  }
+}
+
+class CarouselCardPanel extends JPanel {
+  protected CarouselCardPanel(LayoutManager layout) {
+    super(layout);
+  }
+
+  @Override protected void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    for (Component c : getComponents()) {
+      if (c.isVisible()) {
+        paintSideComponents(g, getComponentZOrder(c));
+        return;
+      }
+    }
+  }
+
+  private void paintSideComponents(Graphics g, int current) {
+    LayoutManager layout = getLayout();
+    if (layout instanceof CardLayout) {
+      Graphics2D g2 = (Graphics2D) g.create();
+      g2.setComposite(AlphaComposite.SrcOver.derive(.5f));
+      Insets insets = getInsets();
+      CardLayout cardLayout = (CardLayout) layout;
+      int hg = cardLayout.getHgap();
+      int vg = cardLayout.getVgap();
+      int cw = getWidth() - (hg * 2 + insets.left + insets.right);
+      // int ch = getHeight() - (vg * 2 + insets.top + insets.bottom);
+      int gap = 10;
+      int nc = getComponentCount();
+
+      g2.translate(hg + insets.left - cw - gap, vg + insets.top);
+      Component prev = getComponent(current > 0 ? current - 1 : nc - 1);
+      prev.print(g2);
+
+      g2.translate((cw + gap) * 2, 0);
+      Component next = getComponent((current + 1) % nc);
+      next.print(g2);
+
+      g2.dispose();
+    }
   }
 }
