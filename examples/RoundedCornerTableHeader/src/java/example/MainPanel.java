44cc5a6cb1805487b7d9c84051940e3c8bf7e493
TERAI Atsuhiro <aterai@outlook.com>
2025-03-14 01:10:26 +0900

    Checkstyle: AnonInnerLength

--- examples/RoundedCornerTableHeader/src/java/example/MainPanel.java
+++ examples/RoundedCornerTableHeader/src/java/example/MainPanel.java
@@ -18,6 +18,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
+import java.util.logging.Logger;
 import javax.swing.*;
 import javax.swing.plaf.LayerUI;
 import javax.swing.table.DefaultTableCellRenderer;
@@ -28,37 +29,7 @@ public final class MainPanel extends JPanel {
   @SuppressWarnings("PMD.UseConcurrentHashMap")
   private final Map<DayOfWeek, Color> holidayColorMap = new EnumMap<>(DayOfWeek.class);
   private final JLabel monthLabel = new JLabel("", SwingConstants.CENTER);
-  private final JTable monthTable = new JTable() {
-    private void updateRowsHeight(JViewport vport) {
-      int height = vport.getExtentSize().height;
-      int rowCount = getModel().getRowCount();
-      int defaultRowHeight = height / rowCount;
-      int remainder = height % rowCount;
-      for (int i = 0; i < rowCount; i++) {
-        int a = Math.min(1, Math.max(0, remainder--));
-        setRowHeight(i, defaultRowHeight + a);
-      }
-    }
-
-    @Override public void updateUI() {
-      super.updateUI();
-      setFillsViewportHeight(true);
-      setBackground(Color.WHITE);
-      setShowVerticalLines(false);
-      setShowHorizontalLines(true);
-      setIntercellSpacing(new Dimension(0, 1));
-      setFont(getFont().deriveFont(Font.BOLD));
-      setDefaultRenderer(LocalDate.class, new CalendarTableRenderer());
-    }
-
-    @Override public void doLayout() {
-      super.doLayout();
-      Class<JViewport> clz = JViewport.class;
-      Optional.ofNullable(SwingUtilities.getAncestorOfClass(clz, this))
-          .filter(clz::isInstance).map(clz::cast)
-          .ifPresent(this::updateRowsHeight);
-    }
-  };
+  private final JTable monthTable = new MonthTable();
   private final List<Color> monthThemeColor = Arrays.asList(
       new Color(0xD5_0B_17), new Color(0x02_6C_B6), new Color(0xED_87_AD),
       new Color(0xCE_30_6A), new Color(0x48_B0_37), new Color(0xA4_62_A2),
@@ -129,6 +100,38 @@ public final class MainPanel extends JPanel {
     monthTable.getTableHeader().setBackground(color);
   }
 
+  private final class MonthTable extends JTable {
+    private void updateRowsHeight(JViewport viewport) {
+      int height = viewport.getExtentSize().height;
+      int rowCount = getModel().getRowCount();
+      int defaultRowHeight = height / rowCount;
+      int remainder = height % rowCount;
+      for (int i = 0; i < rowCount; i++) {
+        int a = Math.min(1, Math.max(0, remainder--));
+        setRowHeight(i, defaultRowHeight + a);
+      }
+    }
+
+    @Override public void updateUI() {
+      super.updateUI();
+      setFillsViewportHeight(true);
+      setBackground(Color.WHITE);
+      setShowVerticalLines(false);
+      setShowHorizontalLines(true);
+      setIntercellSpacing(new Dimension(0, 1));
+      setFont(getFont().deriveFont(Font.BOLD));
+      setDefaultRenderer(LocalDate.class, new CalendarTableRenderer());
+    }
+
+    @Override public void doLayout() {
+      super.doLayout();
+      Class<JViewport> clz = JViewport.class;
+      Optional.ofNullable(SwingUtilities.getAncestorOfClass(clz, this))
+          .filter(clz::isInstance).map(clz::cast)
+          .ifPresent(this::updateRowsHeight);
+    }
+  }
+
   private final class CalendarTableRenderer extends DefaultTableCellRenderer {
     private final JPanel panel = new JPanel(new BorderLayout());
 
@@ -195,7 +198,7 @@ public final class MainPanel extends JPanel {
     } catch (UnsupportedLookAndFeelException ignored) {
       Toolkit.getDefaultToolkit().beep();
     } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
-      ex.printStackTrace();
+      Logger.getGlobal().severe(ex::getMessage);
       return;
     }
     JFrame frame = new JFrame("@title@");
@@ -213,21 +216,7 @@ class RoundedHeaderRenderer extends DefaultTableCellRenderer {
       Graphics2D g2 = (Graphics2D) g.create();
       g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
       g2.setPaint(getBackground());
-      double r = 8d;
-      double rr = r * 4d * (Math.sqrt(2d) - 1d) / 3d; // = r * .5522;
-      double x = 0d;
-      double y = 0d;
-      double w = getWidth();
-      double h = getHeight();
-      Path2D p = new Path2D.Double();
-      p.moveTo(x, y + r);
-      p.curveTo(x, y + r - rr, x + r - rr, y, x + r, y);
-      p.lineTo(x + w, y);
-      p.lineTo(x + w, y + h);
-      p.lineTo(x + r, y + h);
-      p.curveTo(x + r - rr, y + h, x, y + h - r + rr, x, y + h - r);
-      p.closePath();
-      g2.fill(p);
+      g2.fill(makeFirstHeaderPath(getSize()));
       g2.dispose();
       super.paintComponent(g);
     }
@@ -237,21 +226,7 @@ class RoundedHeaderRenderer extends DefaultTableCellRenderer {
       Graphics2D g2 = (Graphics2D) g.create();
       g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
       g2.setPaint(getBackground());
-      double r = 8d;
-      double rr = r * 4d * (Math.sqrt(2d) - 1d) / 3d; // = r * .5522;
-      double x = 0d;
-      double y = 0d;
-      double w = getWidth();
-      double h = getHeight();
-      Path2D p = new Path2D.Double();
-      p.moveTo(x, y);
-      p.lineTo(x + w - r, y);
-      p.curveTo(x + w - r + rr, y, x + w, y + r - rr, x + w, y + r);
-      p.lineTo(x + w, y + h - r);
-      p.curveTo(x + w, y + h - r + rr, x + w - r + rr, y + h, x + w - r, y + h);
-      p.lineTo(x, y + h);
-      p.closePath();
-      g2.fill(p);
+      g2.fill(makeLastHeaderPath(getSize()));
       g2.dispose();
       super.paintComponent(g);
     }
@@ -282,6 +257,42 @@ class RoundedHeaderRenderer extends DefaultTableCellRenderer {
       l.setHorizontalAlignment(CENTER);
     }
     return c;
+  }
+
+  private static Path2D makeFirstHeaderPath(Dimension size) {
+    double r = 8d;
+    double rr = r * 4d * (Math.sqrt(2d) - 1d) / 3d; // = r * .5522;
+    double x = 0d;
+    double y = 0d;
+    double w = size.getWidth();
+    double h = size.getHeight();
+    Path2D p = new Path2D.Double();
+    p.moveTo(x, y + r);
+    p.curveTo(x, y + r - rr, x + r - rr, y, x + r, y);
+    p.lineTo(x + w, y);
+    p.lineTo(x + w, y + h);
+    p.lineTo(x + r, y + h);
+    p.curveTo(x + r - rr, y + h, x, y + h - r + rr, x, y + h - r);
+    p.closePath();
+    return p;
+  }
+
+  private static Path2D makeLastHeaderPath(Dimension size) {
+    double r = 8d;
+    double rr = r * 4d * (Math.sqrt(2d) - 1d) / 3d; // = r * .5522;
+    double x = 0d;
+    double y = 0d;
+    double w = size.getWidth();
+    double h = size.getHeight();
+    Path2D p = new Path2D.Double();
+    p.moveTo(x, y);
+    p.lineTo(x + w - r, y);
+    p.curveTo(x + w - r + rr, y, x + w, y + r - rr, x + w, y + r);
+    p.lineTo(x + w, y + h - r);
+    p.curveTo(x + w, y + h - r + rr, x + w - r + rr, y + h, x + w - r, y + h);
+    p.lineTo(x, y + h);
+    p.closePath();
+    return p;
   }
 }
 
