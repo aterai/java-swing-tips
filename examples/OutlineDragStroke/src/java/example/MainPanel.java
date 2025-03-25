cf562bab47c8e7eb774c3ebe186670f3ddf9ce97
TERAI Atsuhiro <aterai@outlook.com>
2025-03-25 10:07:14 +0900

    Checkstyle: AnonInnerLength

--- examples/OutlineDragStroke/src/java/example/MainPanel.java
+++ examples/OutlineDragStroke/src/java/example/MainPanel.java
@@ -5,6 +5,7 @@
 package example;
 
 import java.awt.*;
+import java.util.logging.Logger;
 import javax.swing.*;
 import javax.swing.plaf.LayerUI;
 
@@ -12,7 +13,8 @@ public final class MainPanel extends JPanel {
   private MainPanel() {
     super(new BorderLayout());
     Rectangle rubberBand = new Rectangle();
-    JDesktopPane desktop = makeDesktopPane(rubberBand);
+    JDesktopPane desktop = new JDesktopPane();
+    desktop.setDesktopManager(new OutlineDragDesktopManager(rubberBand));
     add(new JLayer<>(desktop, new LayerUI<JDesktopPane>() {
       @Override public void paint(Graphics g, JComponent c) {
         super.paint(g, c);
@@ -45,42 +47,6 @@ public final class MainPanel extends JPanel {
     setPreferredSize(new Dimension(320, 240));
   }
 
-  private static JDesktopPane makeDesktopPane(Rectangle rubberBand) {
-    JDesktopPane desktop = new JDesktopPane();
-    desktop.setDesktopManager(new DefaultDesktopManager() {
-      @Override public void beginResizingFrame(JComponent f, int direction) {
-        // System.out.println("beginResizingFrame");
-        // JDesktopPane dp = ((JInternalFrame) f).getDesktopPane();
-        desktop.setDragMode(JDesktopPane.OUTLINE_DRAG_MODE);
-        super.beginResizingFrame(f, direction);
-      }
-
-      @Override public void resizeFrame(JComponent f, int newX, int newY, int newWidth, int newHeight) {
-        // System.out.println("resizeFrame");
-        // JDesktopPane dp = ((JInternalFrame) f).getDesktopPane();
-        if (desktop.getDragMode() == JDesktopPane.OUTLINE_DRAG_MODE) {
-          super.resizeFrame(f, newX, newY, 0, 0);
-          rubberBand.setBounds(newX, newY, newWidth, newHeight);
-          desktop.repaint();
-        } else {
-          super.resizeFrame(f, newX, newY, newWidth, newHeight);
-        }
-      }
-
-      @Override public void endResizingFrame(JComponent f) {
-        // System.out.println("endResizingFrame");
-        // JDesktopPane dp = ((JInternalFrame) f).getDesktopPane();
-        desktop.setDragMode(JDesktopPane.LIVE_DRAG_MODE);
-        if (!rubberBand.isEmpty()) {
-          super.resizeFrame(f, rubberBand.x, rubberBand.y, rubberBand.width, rubberBand.height);
-          rubberBand.setBounds(0, 0, 0, 0);
-        }
-        super.endResizingFrame(f);
-      }
-    });
-    return desktop;
-  }
-
   public static Stroke makeDotStroke() {
     float[] dist = {1f, 1f};
     return new BasicStroke(1f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 1f, dist, 0f);
@@ -102,7 +68,7 @@ public final class MainPanel extends JPanel {
     } catch (UnsupportedLookAndFeelException ignored) {
       Toolkit.getDefaultToolkit().beep();
     } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
-      ex.printStackTrace();
+      Logger.getGlobal().severe(ex::getMessage);
       return;
     }
     JFrame frame = new JFrame("@title@");
@@ -111,5 +77,44 @@ public final class MainPanel extends JPanel {
     frame.pack();
     frame.setLocationRelativeTo(null);
     frame.setVisible(true);
+  }
+}
+
+class OutlineDragDesktopManager extends DefaultDesktopManager {
+  private final Rectangle rubberBand;
+
+  protected OutlineDragDesktopManager(Rectangle rubberBand) {
+    super();
+    this.rubberBand = rubberBand;
+  }
+
+  @Override public void beginResizingFrame(JComponent f, int direction) {
+    // System.out.println("beginResizingFrame");
+    JDesktopPane desktop = ((JInternalFrame) f).getDesktopPane();
+    desktop.setDragMode(JDesktopPane.OUTLINE_DRAG_MODE);
+    super.beginResizingFrame(f, direction);
+  }
+
+  @Override public void resizeFrame(JComponent f, int newX, int newY, int newWidth, int newHeight) {
+    // System.out.println("resizeFrame");
+    JDesktopPane desktop = ((JInternalFrame) f).getDesktopPane();
+    if (desktop.getDragMode() == JDesktopPane.OUTLINE_DRAG_MODE) {
+      super.resizeFrame(f, newX, newY, 0, 0);
+      rubberBand.setBounds(newX, newY, newWidth, newHeight);
+      desktop.repaint();
+    } else {
+      super.resizeFrame(f, newX, newY, newWidth, newHeight);
+    }
+  }
+
+  @Override public void endResizingFrame(JComponent f) {
+    // System.out.println("endResizingFrame");
+    JDesktopPane desktop = ((JInternalFrame) f).getDesktopPane();
+    desktop.setDragMode(JDesktopPane.LIVE_DRAG_MODE);
+    if (!rubberBand.isEmpty()) {
+      super.resizeFrame(f, rubberBand.x, rubberBand.y, rubberBand.width, rubberBand.height);
+      rubberBand.setBounds(0, 0, 0, 0);
+    }
+    super.endResizingFrame(f);
   }
 }
