56de83d81692f55b5525cddb38a2f37d7e83d539
TERAI Atsuhiro <aterai@outlook.com>
2025-03-26 04:16:26 +0900

    Checkstyle: AnonInnerLength

--- examples/MenuBarLayout/src/java/example/MainPanel.java
+++ examples/MenuBarLayout/src/java/example/MainPanel.java
@@ -5,27 +5,13 @@
 package example;
 
 import java.awt.*;
+import java.util.logging.Logger;
 import java.util.stream.Stream;
 import javax.swing.*;
 
 public final class MainPanel extends JPanel {
   private MainPanel() {
     super(new BorderLayout());
-    // addComponentListener(new ComponentAdapter() {
-    //   @Override public void componentResized(ComponentEvent e) {
-    //     System.out.println("componentResized");
-    //     ((JComponent) e.getSource()).revalidate();
-    //   }
-    // });
-    // frame.addWindowStateListener(new WindowStateListener() {
-    //   @Override public void windowStateChanged(WindowEvent e) {
-    //     EventQueue.invokeLater(() -> {
-    //       System.out.println("windowStateChanged");
-    //       JFrame f = (JFrame) e.getWindow();
-    //       f.getContentPane().revalidate();
-    //     });
-    //   }
-    // });
     EventQueue.invokeLater(() -> getRootPane().setJMenuBar(createMenuBar()));
     add(new JScrollPane(new JTextArea()));
     setPreferredSize(new Dimension(320, 240));
@@ -33,53 +19,7 @@ public final class MainPanel extends JPanel {
 
   private static JMenuBar createMenuBar() {
     JMenuBar menuBar = new JMenuBar();
-    menuBar.setLayout(new FlowLayout(FlowLayout.LEFT, 2, 2) {
-      @SuppressWarnings("PMD.AvoidSynchronizedStatement")
-      @Override public Dimension preferredLayoutSize(Container target) {
-        synchronized (target.getTreeLock()) {
-          int targetWidth = target.getSize().width;
-          targetWidth = targetWidth == 0 ? Integer.MAX_VALUE : targetWidth;
-          Insets insets = target.getInsets();
-          int hgap = getHgap();
-          int vgap = getVgap();
-          int maxWidth = targetWidth - insets.left - insets.right;
-          int height = vgap;
-          int rowWidth = hgap;
-          int rowHeight = 0;
-          for (Component m : target.getComponents()) {
-            if (m.isVisible()) {
-              Dimension d = m.getPreferredSize();
-              if (rowWidth + d.width > maxWidth) {
-                height += rowHeight;
-                rowWidth = hgap;
-                rowHeight = 0;
-              }
-              rowWidth += d.width + hgap;
-              rowHeight = Math.max(rowHeight, d.height + vgap);
-            }
-          }
-          height += rowHeight + insets.top + insets.bottom;
-          return new Dimension(targetWidth, height);
-        }
-      }
-      // // https://tips4java.wordpress.com/2008/11/06/wrap-layout/
-      // // WrapLayout.java
-      // // Rob Camick on November 6, 2008,
-      // private Dimension preferredLayoutSize;
-      // @Override public void layoutContainer(Container target) {
-      //   Dimension size = preferredLayoutSize(target);
-      //   if (size.equals(preferredLayoutSize)) {
-      //     super.layoutContainer(target);
-      //   } else {
-      //     preferredLayoutSize = size;
-      //     Container top = target;
-      //     while (!(top instanceof Window) && top.getParent() != null) {
-      //       top = top.getParent();
-      //     }
-      //     top.validate();
-      //   }
-      // }
-    });
+    menuBar.setLayout(new MenuBarLayout(2, 2));
     Stream.of(
         "File", "Edit", "View", "Navigate", "Code", "Analyze",
         "Refactor", "Build", "Run", "Help"
@@ -105,7 +45,7 @@ public final class MainPanel extends JPanel {
     } catch (UnsupportedLookAndFeelException ignored) {
       Toolkit.getDefaultToolkit().beep();
     } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
-      ex.printStackTrace();
+      Logger.getGlobal().severe(ex::getMessage);
       return;
     }
     JFrame frame = new JFrame("@title@");
@@ -115,4 +55,56 @@ public final class MainPanel extends JPanel {
     frame.setLocationRelativeTo(null);
     frame.setVisible(true);
   }
+}
+
+class MenuBarLayout extends FlowLayout {
+  protected MenuBarLayout(int hgp, int vgp) {
+    super(LEADING, hgp, vgp);
+  }
+
+  @SuppressWarnings("PMD.AvoidSynchronizedStatement")
+  @Override public Dimension preferredLayoutSize(Container target) {
+    synchronized (target.getTreeLock()) {
+      int targetWidth = target.getSize().width;
+      targetWidth = targetWidth == 0 ? Integer.MAX_VALUE : targetWidth;
+      Insets insets = target.getInsets();
+      int hgp = getHgap();
+      int vgp = getVgap();
+      int maxWidth = targetWidth - insets.left - insets.right;
+      int height = vgp;
+      int rowWidth = hgp;
+      int rowHeight = 0;
+      for (Component m : target.getComponents()) {
+        if (m.isVisible()) {
+          Dimension d = m.getPreferredSize();
+          if (rowWidth + d.width > maxWidth) {
+            height += rowHeight;
+            rowWidth = hgp;
+            rowHeight = 0;
+          }
+          rowWidth += d.width + hgp;
+          rowHeight = Math.max(rowHeight, d.height + vgp);
+        }
+      }
+      height += rowHeight + insets.top + insets.bottom;
+      return new Dimension(targetWidth, height);
+    }
+  }
+
+  // // https://tips4java.wordpress.com/2008/11/06/wrap-layout/
+  // // WrapLayout.java Rob Camick on November 6, 2008,
+  // private Dimension preferredLayoutSize;
+  // @Override public void layoutContainer(Container target) {
+  //   Dimension size = preferredLayoutSize(target);
+  //   if (size.equals(preferredLayoutSize)) {
+  //     super.layoutContainer(target);
+  //   } else {
+  //     preferredLayoutSize = size;
+  //     Container top = target;
+  //     while (!(top instanceof Window) && top.getParent() != null) {
+  //       top = top.getParent();
+  //     }
+  //     top.validate();
+  //   }
+  // }
 }
--- examples/NimbusFilledSlider/src/java/example/MainPanel.java
+++ examples/NimbusFilledSlider/src/java/example/MainPanel.java
@@ -5,55 +5,16 @@
 package example;
 
 import java.awt.*;
+import java.util.logging.Logger;
 import javax.swing.*;
 
 public final class MainPanel extends JPanel {
   private MainPanel() {
     super(new BorderLayout());
     UIDefaults d = new UIDefaults();
-    d.put("Slider:SliderTrack[Enabled].backgroundPainter", new Painter<JSlider>() {
-      @Override public void paint(Graphics2D g, JSlider c, int w, int h) {
-        // if (c.isInverted() || c.getOrientation() == SwingConstants.VERTICAL) {
-        //   super.paint(g, c, w, h);
-        //   return;
-        // }
-        int arc = 10;
-        int trackHeight = 8;
-        int trackWidth = w - 2;
-        int fillTop = 4;
-        int fillLeft = 1;
-
-        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g.setStroke(new BasicStroke(1.5f));
-        g.setColor(Color.GRAY);
-        g.fillRoundRect(fillLeft, fillTop, trackWidth, trackHeight, arc, arc);
-
-        int fillBottom = fillTop + trackHeight;
-        Rectangle r = new Rectangle(fillLeft, fillTop, trackWidth, fillBottom - fillTop);
-        int fillRight = getPositionForValue(c, r);
-        g.setColor(Color.ORANGE);
-        g.fillRect(fillLeft + 1, fillTop + 1, fillRight - fillLeft, fillBottom - fillTop);
-
-        g.setColor(Color.WHITE);
-        g.drawRoundRect(fillLeft, fillTop, trackWidth, trackHeight, arc, arc);
-      }
-
-      // @see javax/swing/plaf/basic/BasicSliderUI#xPositionForValue(int value)
-      private int getPositionForValue(JSlider slider, Rectangle trackRect) {
-        float value = slider.getValue();
-        float min = slider.getMinimum();
-        float max = slider.getMaximum();
-        float pixelsPerValue = trackRect.width / (max - min);
-        int trackLeft = trackRect.x;
-        int trackRight = trackRect.x + trackRect.width - 1;
-        int pos = trackLeft + Math.round(pixelsPerValue * (value - min));
-        return Math.max(trackLeft, Math.min(trackRight, pos));
-      }
-    });
-
+    d.put("Slider:SliderTrack[Enabled].backgroundPainter", new SliderTrackPainter());
     JSlider slider = new JSlider();
     slider.putClientProperty("Nimbus.Overrides", d);
-
     Box box = Box.createVerticalBox();
     box.add(Box.createVerticalStrut(5));
     box.add(makeTitledPanel("Default", new JSlider()));
@@ -83,7 +44,7 @@ public final class MainPanel extends JPanel {
     } catch (UnsupportedLookAndFeelException ignored) {
       Toolkit.getDefaultToolkit().beep();
     } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
-      ex.printStackTrace();
+      Logger.getGlobal().severe(ex::getMessage);
       return;
     }
     JFrame frame = new JFrame("@title@");
@@ -92,5 +53,45 @@ public final class MainPanel extends JPanel {
     frame.pack();
     frame.setLocationRelativeTo(null);
     frame.setVisible(true);
+  }
+}
+
+class SliderTrackPainter implements Painter<JSlider> {
+  @Override public void paint(Graphics2D g, JSlider c, int w, int h) {
+    // if (c.isInverted() || c.getOrientation() == SwingConstants.VERTICAL) {
+    //   super.paint(g, c, w, h);
+    //   return;
+    // }
+    int arc = 10;
+    int trackHeight = 8;
+    int trackWidth = w - 2;
+    int fillTop = 4;
+    int fillLeft = 1;
+
+    g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+    g.setStroke(new BasicStroke(1.5f));
+    g.setColor(Color.GRAY);
+    g.fillRoundRect(fillLeft, fillTop, trackWidth, trackHeight, arc, arc);
+
+    int fillBottom = fillTop + trackHeight;
+    Rectangle r = new Rectangle(fillLeft, fillTop, trackWidth, fillBottom - fillTop);
+    int fillRight = getPositionForValue(c, r);
+    g.setColor(Color.ORANGE);
+    g.fillRect(fillLeft + 1, fillTop + 1, fillRight - fillLeft, fillBottom - fillTop);
+
+    g.setColor(Color.WHITE);
+    g.drawRoundRect(fillLeft, fillTop, trackWidth, trackHeight, arc, arc);
+  }
+
+  // @see javax/swing/plaf/basic/BasicSliderUI#xPositionForValue(int value)
+  private static int getPositionForValue(JSlider slider, Rectangle trackRect) {
+    float value = slider.getValue();
+    float min = slider.getMinimum();
+    float max = slider.getMaximum();
+    float pixelsPerValue = trackRect.width / (max - min);
+    int trackLeft = trackRect.x;
+    int trackRight = trackRect.x + trackRect.width - 1;
+    int pos = trackLeft + Math.round(pixelsPerValue * (value - min));
+    return Math.max(trackLeft, Math.min(trackRight, pos));
   }
 }
