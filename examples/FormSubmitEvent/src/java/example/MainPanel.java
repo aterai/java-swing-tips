405c65e7a313a8b8eed251336143e598b733f1d5
TERAI Atsuhiro <aterai@outlook.com>
2025-04-19 13:49:16 +0900

    Checkstyle: LambdaBodyLength

--- examples/FormSubmitEvent/src/java/example/MainPanel.java
+++ examples/FormSubmitEvent/src/java/example/MainPanel.java
@@ -8,46 +8,49 @@ import java.awt.*;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.charset.Charset;
+import java.util.logging.Logger;
 import javax.swing.*;
 import javax.swing.text.html.FormSubmitEvent;
 import javax.swing.text.html.HTMLEditorKit;
 
 public final class MainPanel extends JPanel {
+  private final JTextArea logger = new JTextArea();
+
   private MainPanel() {
     super(new GridLayout(2, 1, 5, 5));
-    JTextArea logger = new JTextArea();
     logger.setEditable(false);
-
     JEditorPane editor = new JEditorPane();
     HTMLEditorKit kit = new HTMLEditorKit();
     kit.setAutoFormSubmission(false);
     editor.setEditorKit(kit);
     editor.setEditable(false);
-
     String form = "<form action='#'><input type='text' name='word' value='12345' /></form>";
     editor.setText("<html><h1>Form test</h1>" + form);
     editor.addHyperlinkListener(e -> {
       if (e instanceof FormSubmitEvent) {
-        String data = ((FormSubmitEvent) e).getData();
-        logger.append(data + "\n");
-
-        String charset = Charset.defaultCharset().toString();
-        logger.append("default charset: " + charset + "\n");
-
-        try {
-          String txt = URLDecoder.decode(data, charset);
-          logger.append(txt + "\n");
-        } catch (UnsupportedEncodingException ex) {
-          // ex.printStackTrace();
-          logger.append(ex.getMessage() + "\n");
-        }
+        decodeFormInfo((FormSubmitEvent) e);
       }
     });
-
     setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
     add(new JScrollPane(editor));
     add(new JScrollPane(logger));
     setPreferredSize(new Dimension(320, 240));
+  }
+
+  private void decodeFormInfo(FormSubmitEvent e) {
+    String data = e.getData();
+    append(data);
+    String charset = Charset.defaultCharset().toString();
+    append("default charset: " + charset);
+    try {
+      append(URLDecoder.decode(data, charset));
+    } catch (UnsupportedEncodingException ex) {
+      append(ex.getMessage());
+    }
+  }
+
+  private void append(String msg) {
+    logger.append(msg + "\n");
   }
 
   public static void main(String[] args) {
@@ -60,7 +63,7 @@ public final class MainPanel extends JPanel {
     } catch (UnsupportedLookAndFeelException ignored) {
       Toolkit.getDefaultToolkit().beep();
     } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
-      ex.printStackTrace();
+      Logger.getGlobal().severe(ex::getMessage);
       return;
     }
     JFrame frame = new JFrame("@title@");
