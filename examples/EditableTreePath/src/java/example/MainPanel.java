5321349c50ad7adb7589510a241d564f5c4e871a
TERAI Atsuhiro <aterai@outlook.com>
2025-04-30 04:21:50 +0900

    Checkstyle: AnonInnerLength

--- examples/EditableTreePath/src/java/example/MainPanel.java
+++ examples/EditableTreePath/src/java/example/MainPanel.java
@@ -9,6 +9,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.util.EventObject;
 import java.util.Optional;
+import java.util.logging.Logger;
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellEditor;
@@ -39,41 +40,9 @@ public final class MainPanel extends JPanel {
         appendLog(String.format("  getPathCount(): %d", path.getPathCount()));
         return Optional.ofNullable(path.getLastPathComponent())
             .filter(TreeNode.class::isInstance)
-            .map(node -> {
-              boolean isLeaf = ((TreeNode) node).isLeaf();
-              appendLog(String.format("  isLeaf: %s", isLeaf));
-              if (node instanceof DefaultMutableTreeNode) {
-                int lv = ((DefaultMutableTreeNode) node).getLevel();
-                appendLog(String.format("  getLevel: %d", lv));
-              }
-              return isLeaf;
-            })
+            .map(TreeNode.class::cast)
+            .map(MainPanel::isLeaf)
             .orElse(false);
-      }
-
-      private TreeCellEditor makeTreeCellEditor(JTree tree) {
-        return new DefaultTreeCellEditor(tree, (DefaultTreeCellRenderer) tree.getCellRenderer()) {
-          @Override public boolean isCellEditable(EventObject e) {
-            appendLog("TreeCellEditor#isCellEditable(EventObject)");
-            boolean ret;
-            if (e instanceof MouseEvent) {
-              MouseEvent me = (MouseEvent) e;
-              appendLog("  MouseEvent");
-              appendLog(String.format("  getPoint(): %s", me.getPoint()));
-              appendLog(String.format("  getClickCount: %d", me.getClickCount()));
-              appendLog(String.format("  isShiftDown: %s", me.isShiftDown()));
-              appendLog(String.format("  isControlDown: %s", me.isControlDown()));
-              ret = me.getClickCount() >= 2 || me.isShiftDown() || me.isControlDown();
-            } else if (e instanceof KeyEvent) {
-              appendLog("  KeyEvent");
-              ret = super.isCellEditable(e);
-            } else { // e == null
-              appendLog("  startEditing Action(F2)");
-              ret = super.isCellEditable(e);
-            }
-            return ret;
-          }
-        };
       }
     };
 
@@ -83,6 +52,45 @@ public final class MainPanel extends JPanel {
     sp.setBottomComponent(new JScrollPane(LOG));
     add(sp);
     setPreferredSize(new Dimension(320, 240));
+  }
+
+  private static boolean isLeaf(TreeNode node) {
+    boolean isLeaf = node.isLeaf();
+    appendLog(String.format("  isLeaf: %s", isLeaf));
+    if (node instanceof DefaultMutableTreeNode) {
+      int lv = ((DefaultMutableTreeNode) node).getLevel();
+      appendLog(String.format("  getLevel: %d", lv));
+    }
+    return isLeaf;
+  }
+
+  private static TreeCellEditor makeTreeCellEditor(JTree tree) {
+    return new DefaultTreeCellEditor(tree, (DefaultTreeCellRenderer) tree.getCellRenderer()) {
+      @Override public boolean isCellEditable(EventObject e) {
+        appendLog("TreeCellEditor#isCellEditable(EventObject)");
+        boolean ret;
+        if (e instanceof MouseEvent) {
+          MouseEvent me = (MouseEvent) e;
+          info(me);
+          ret = me.getClickCount() >= 2 || me.isShiftDown() || me.isControlDown();
+        } else if (e instanceof KeyEvent) {
+          appendLog("  KeyEvent");
+          ret = super.isCellEditable(e);
+        } else { // e == null
+          appendLog("  startEditing Action(F2)");
+          ret = super.isCellEditable(e);
+        }
+        return ret;
+      }
+    };
+  }
+
+  private static void info(MouseEvent e) {
+    appendLog("  MouseEvent");
+    appendLog(String.format("  getPoint(): %s", e.getPoint()));
+    appendLog(String.format("  getClickCount: %d", e.getClickCount()));
+    appendLog(String.format("  isShiftDown: %s", e.isShiftDown()));
+    appendLog(String.format("  isControlDown: %s", e.isControlDown()));
   }
 
   private static void appendLog(String str) {
@@ -99,7 +107,7 @@ public final class MainPanel extends JPanel {
     } catch (UnsupportedLookAndFeelException ignored) {
       Toolkit.getDefaultToolkit().beep();
     } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
-      ex.printStackTrace();
+      Logger.getGlobal().severe(ex::getMessage);
       return;
     }
     JFrame frame = new JFrame("@title@");
