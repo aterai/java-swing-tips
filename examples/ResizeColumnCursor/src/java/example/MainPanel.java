f6b9365fd455349850a7441a15b1bd1a9d60cebc
TERAI Atsuhiro <aterai@outlook.com>
2025-05-18 07:18:48 +0900

    Checkstyle: LambdaBodyLength

--- examples/ResizeColumnCursor/src/java/example/MainPanel.java
+++ examples/ResizeColumnCursor/src/java/example/MainPanel.java
@@ -12,6 +12,7 @@ import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.util.Optional;
+import java.util.logging.Logger;
 import javax.swing.*;
 import javax.swing.event.MouseInputListener;
 import javax.swing.plaf.TableHeaderUI;
@@ -87,7 +88,7 @@ public final class MainPanel extends JPanel {
     } catch (UnsupportedLookAndFeelException ignored) {
       Toolkit.getDefaultToolkit().beep();
     } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
-      ex.printStackTrace();
+      Logger.getGlobal().severe(ex::getMessage);
       return;
     }
     JFrame frame = new JFrame("@title@");
@@ -129,12 +130,11 @@ final class ResizeUtils {
         })
         .map(r -> {
           int column = header.columnAtPoint(p);
-          int midPoint = r.x + r.width / 2;
           int colIdx;
           if (header.getComponentOrientation().isLeftToRight()) {
-            colIdx = p.x < midPoint ? column - 1 : column;
+            colIdx = p.x < r.getCenterX() ? column - 1 : column;
           } else {
-            colIdx = p.x < midPoint ? column : column - 1;
+            colIdx = p.x < r.getCenterX() ? column : column - 1;
           }
           return colIdx == -1 ? null : header.getColumnModel().getColumn(colIdx);
         })
