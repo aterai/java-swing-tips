8a9e2f41bb8af1acc925f7afed93da2301602978
TERAI Atsuhiro <aterai@outlook.com>
2025-06-29 01:17:10 +0900

    Checkstyle: ExecutableStatementCount

--- examples/ChangeSelection/src/java/example/MainPanel.java
+++ examples/ChangeSelection/src/java/example/MainPanel.java
@@ -7,33 +7,35 @@ package example;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.util.logging.Logger;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 
 public final class MainPanel extends JPanel {
+  private final JTable table = makeTable();
+  private final SpinnerNumberModel rowField;
+  private final SpinnerNumberModel colField;
+  private final JCheckBox toggle = new JCheckBox("toggle", false);
+  private final JCheckBox extend = new JCheckBox("extend", false);
+
   private MainPanel() {
     super(new BorderLayout());
-    JTable table = new JTable(makeModel());
-    table.setCellSelectionEnabled(true);
+    rowField = new SpinnerNumberModel(1, 0, table.getRowCount() - 1, 1);
+    colField = new SpinnerNumberModel(2, 0, table.getColumnCount() - 1, 1);
 
-    String actionMapKey = "clear-selection";
-    table.getActionMap().put(actionMapKey, new AbstractAction(actionMapKey) {
-      @Override public void actionPerformed(ActionEvent e) {
-        table.clearSelection();
-        requestFocusInWindow();
-      }
-    });
-    InputMap im = table.getInputMap(WHEN_FOCUSED);
-    im.put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), actionMapKey);
+    String title = "JTable#changeSelection(int, int, boolean, boolean)";
+    JPanel panel = new JPanel(new BorderLayout());
+    panel.setBorder(BorderFactory.createTitledBorder(title));
+    panel.add(makeToolBox(), BorderLayout.NORTH);
+    panel.add(makeButtonPanel(), BorderLayout.SOUTH);
 
-    int rowCount = table.getRowCount() - 1;
-    int colCount = table.getColumnCount() - 1;
-    SpinnerNumberModel rowField = new SpinnerNumberModel(1, 0, rowCount, 1);
-    SpinnerNumberModel colField = new SpinnerNumberModel(2, 0, colCount, 1);
-    JCheckBox toggle = new JCheckBox("toggle", false);
-    JCheckBox extend = new JCheckBox("extend", false);
+    add(panel, BorderLayout.NORTH);
+    add(new JScrollPane(table));
+    setPreferredSize(new Dimension(320, 240));
+  }
 
+  private Box makeToolBox() {
     Box box = Box.createHorizontalBox();
     box.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
     box.add(new JLabel("row:"));
@@ -42,7 +44,10 @@ public final class MainPanel extends JPanel {
     box.add(new JSpinner(colField));
     box.add(toggle);
     box.add(extend);
+    return box;
+  }
 
+  private JPanel makeButtonPanel() {
     JButton changeSelection = new JButton("changeSelection");
     changeSelection.addActionListener(e -> {
       int row = rowField.getNumber().intValue();
@@ -62,16 +67,22 @@ public final class MainPanel extends JPanel {
     JPanel p = new JPanel(new FlowLayout(FlowLayout.RIGHT, 5, 5));
     p.add(changeSelection);
     p.add(clear);
+    return p;
+  }
 
-    String title = "JTable#changeSelection(int, int, boolean, boolean)";
-    JPanel panel = new JPanel(new BorderLayout());
-    panel.setBorder(BorderFactory.createTitledBorder(title));
-    panel.add(box, BorderLayout.NORTH);
-    panel.add(p, BorderLayout.SOUTH);
-
-    add(panel, BorderLayout.NORTH);
-    add(new JScrollPane(table));
-    setPreferredSize(new Dimension(320, 240));
+  private static JTable makeTable() {
+    JTable table = new JTable(makeModel());
+    table.setCellSelectionEnabled(true);
+    String actionMapKey = "clear-selection";
+    table.getActionMap().put(actionMapKey, new AbstractAction(actionMapKey) {
+      @Override public void actionPerformed(ActionEvent e) {
+        table.clearSelection();
+        table.requestFocusInWindow();
+      }
+    });
+    InputMap im = table.getInputMap(WHEN_FOCUSED);
+    im.put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), actionMapKey);
+    return table;
   }
 
   private static TableModel makeModel() {
@@ -105,7 +116,7 @@ public final class MainPanel extends JPanel {
     } catch (UnsupportedLookAndFeelException ignored) {
       Toolkit.getDefaultToolkit().beep();
     } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
-      ex.printStackTrace();
+      Logger.getGlobal().severe(ex::getMessage);
       return;
     }
     JFrame frame = new JFrame("@title@");
