e7b14be378fe4edfca38fc56c46156018e4d844b
TERAI Atsuhiro <aterai@outlook.com>
2025-07-26 11:29:40 +0900

    refactor: if the image does not exist, MissingIcon is used instead

--- examples/RotateAnimatedGifImageIcon/src/java/example/MainPanel.java
+++ examples/RotateAnimatedGifImageIcon/src/java/example/MainPanel.java
@@ -6,7 +6,9 @@ package example;
 
 import java.awt.*;
 import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
 import java.net.URL;
+import java.util.Optional;
 import java.util.logging.Logger;
 import javax.swing.*;
 
@@ -15,57 +17,39 @@ public final class MainPanel extends JPanel {
     super(new GridLayout(2, 2));
     ClassLoader cl = Thread.currentThread().getContextClassLoader();
     URL url = cl.getResource("example/duke.running.gif");
-    assert url != null;
-    ImageIcon imageIcon = new ImageIcon(url);
+    ImageIcon imageIcon = Optional.ofNullable(url)
+        .map(ImageIcon::new)
+        .orElseGet(() -> new ImageIcon(makeMissingImage()));
+
     JLabel label0 = new JLabel(imageIcon);
     label0.setBorder(BorderFactory.createTitledBorder("Default ImageIcon"));
+    add(label0);
 
     JLabel label1 = new JLabel(new ClockwiseRotateIcon(imageIcon));
     label1.setBorder(BorderFactory.createTitledBorder("Wrapping with another Icon"));
-
-    Image img = imageIcon.getImage();
-    JPanel label2 = new JPanel() {
-      @Override protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D) g.create();
-        int x = getWidth() / 2;
-        int y = getHeight() / 2;
-        g2.setTransform(AffineTransform.getQuadrantRotateInstance(1, x, y));
-        int x2 = x - img.getWidth(this) / 2;
-        int y2 = y - img.getHeight(this) / 2;
-        // imageIcon.paintIcon(this, g2, x2, y2);
-        g2.drawImage(img, x2, y2, this);
-        g2.dispose();
-      }
-    };
-    label2.setBorder(BorderFactory.createTitledBorder("Override JPanel#paintComponent(...)"));
-
-    Icon icon3 = new ImageIcon(url) {
-      @SuppressWarnings("PMD.AvoidSynchronizedAtMethodLevel")
-      @Override public synchronized void paintIcon(Component c, Graphics g, int x, int y) {
-        Graphics2D g2 = (Graphics2D) g.create();
-        g2.translate(x + getIconHeight(), y);
-        g2.transform(AffineTransform.getQuadrantRotateInstance(1));
-        super.paintIcon(c, g2, 0, 0);
-        g2.dispose();
-      }
-
-      @Override public int getIconWidth() {
-        return super.getIconHeight();
-      }
-
-      @Override public int getIconHeight() {
-        return super.getIconWidth();
-      }
-    };
-    JLabel label3 = new JLabel(icon3);
-    label3.setBorder(BorderFactory.createTitledBorder("Override ImageIcon#paintIcon(...)"));
-
-    add(label0);
     add(label1);
+
+    JPanel label2 = new ImagePanel(imageIcon.getImage());
+    label2.setBorder(BorderFactory.createTitledBorder("Override JPanel#paintComponent(...)"));
     add(label2);
-    add(label3);
+
+    if (url != null) {
+      JLabel label3 = new JLabel(new RotateImageIcon(url));
+      label3.setBorder(BorderFactory.createTitledBorder("Override ImageIcon#paintIcon(...)"));
+      add(label3);
+    }
     setPreferredSize(new Dimension(320, 240));
+  }
+
+  private static Image makeMissingImage() {
+    Icon missingIcon = UIManager.getIcon("html.missingImage");
+    int iw = missingIcon.getIconWidth();
+    int ih = missingIcon.getIconHeight();
+    BufferedImage bi = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+    Graphics2D g2 = bi.createGraphics();
+    missingIcon.paintIcon(null, g2, (16 - iw) / 2, (16 - ih) / 2);
+    g2.dispose();
+    return bi;
   }
 
   public static void main(String[] args) {
@@ -111,5 +95,50 @@ class ClockwiseRotateIcon implements Icon {
 
   @Override public int getIconHeight() {
     return icon.getIconWidth();
+  }
+}
+
+class ImagePanel extends JPanel {
+  private final Image image;
+
+  protected ImagePanel(Image image) {
+    super();
+    this.image = image;
+  }
+
+  @Override protected void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    Graphics2D g2 = (Graphics2D) g.create();
+    int x = getWidth() / 2;
+    int y = getHeight() / 2;
+    g2.setTransform(AffineTransform.getQuadrantRotateInstance(1, x, y));
+    int x2 = x - image.getWidth(this) / 2;
+    int y2 = y - image.getHeight(this) / 2;
+    // imageIcon.paintIcon(this, g2, x2, y2);
+    g2.drawImage(image, x2, y2, this);
+    g2.dispose();
+  }
+}
+
+class RotateImageIcon extends ImageIcon {
+  protected RotateImageIcon(URL url) {
+    super(url);
+  }
+
+  @SuppressWarnings("PMD.AvoidSynchronizedAtMethodLevel")
+  @Override public synchronized void paintIcon(Component c, Graphics g, int x, int y) {
+    Graphics2D g2 = (Graphics2D) g.create();
+    g2.translate(x + getIconHeight(), y);
+    g2.transform(AffineTransform.getQuadrantRotateInstance(1));
+    super.paintIcon(c, g2, 0, 0);
+    g2.dispose();
+  }
+
+  @Override public int getIconWidth() {
+    return super.getIconHeight();
+  }
+
+  @Override public int getIconHeight() {
+    return super.getIconWidth();
   }
 }
