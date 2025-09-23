b8e38a3b759d5effff0957d6d901da011b4dfc14
TERAI Atsuhiro <aterai@outlook.com>
2025-09-23 09:06:55 +0900

    Checkstyle: ReturnCount

--- examples/ArrowButtonlessScrollBar/src/java/example/MainPanel.java
+++ examples/ArrowButtonlessScrollBar/src/java/example/MainPanel.java
@@ -120,25 +120,24 @@ class ArrowButtonlessScrollBarUI extends BasicScrollBarUI {
   }
 
   @Override protected void paintThumb(Graphics g, JComponent c, Rectangle r) {
-    JScrollBar sb = (JScrollBar) c;
-    if (!sb.isEnabled()) {
-      return;
-    }
-    BoundedRangeModel m = sb.getModel();
-    if (m.getMaximum() - m.getMinimum() - m.getExtent() > 0) {
-      Graphics2D g2 = (Graphics2D) g.create();
-      g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-      Color color;
-      if (isDragging) {
-        color = thumbDarkShadowColor;
-      } else if (isThumbRollover()) {
-        color = thumbLightShadowColor;
-      } else {
-        color = thumbColor;
+    if (c instanceof JScrollBar && c.isEnabled()) {
+      BoundedRangeModel m = ((JScrollBar) c).getModel();
+      if (m.getMaximum() - m.getMinimum() - m.getExtent() > 0) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(
+            RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        Color color;
+        if (isDragging) {
+          color = thumbDarkShadowColor;
+        } else if (isThumbRollover()) {
+          color = thumbLightShadowColor;
+        } else {
+          color = thumbColor;
+        }
+        g2.setPaint(color);
+        g2.fillRect(r.x + 1, r.y + 1, r.width - 2, r.height - 2);
+        g2.dispose();
       }
-      g2.setPaint(color);
-      g2.fillRect(r.x + 1, r.y + 1, r.width - 2, r.height - 2);
-      g2.dispose();
     }
   }
 }
