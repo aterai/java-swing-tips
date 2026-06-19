1e7544592c9b0fb73dbfcfc88f62afe71e7af79e
TERAI Atsuhiro <aterai@outlook.com>
2026-06-20 01:16:49 +0900

    ErrorProne: ImmutableEnumChecker

--- examples/TableSortIcon/src/java/example/MainPanel.java
+++ examples/TableSortIcon/src/java/example/MainPanel.java
@@ -19,19 +19,19 @@ import javax.swing.table.TableModel;
 public final class MainPanel extends JPanel {
   private MainPanel() {
     super(new BorderLayout());
-    JTable table = new JTable(makeModel());
+    JTable table = new JTable(createModel());
     table.setAutoCreateRowSorter(true);
 
     JButton clearButton = new JButton("clear SortKeys");
     clearButton.addActionListener(e -> table.getRowSorter().setSortKeys(null));
 
-    add(makeRadioPane(table), BorderLayout.NORTH);
+    add(createRadioPane(table), BorderLayout.NORTH);
     add(clearButton, BorderLayout.SOUTH);
     add(new JScrollPane(table));
     setPreferredSize(new Dimension(320, 240));
   }
 
-  private static TableModel makeModel() {
+  private static TableModel createModel() {
     String[] columnNames = {"String", "Integer", "Boolean"};
     Object[][] data = {
         {"aaa", 12, true}, {"bbb", 5, false}, {"CCC", 92, true}, {"DDD", 0, false},
@@ -43,7 +43,7 @@ public final class MainPanel extends JPanel {
     };
   }
 
-  private Box makeRadioPane(JTable table) {
+  private Box createRadioPane(JTable table) {
     ButtonGroup bg = new ButtonGroup();
     ItemListener handler = e -> {
       if (e.getStateChange() == ItemEvent.SELECTED) {
@@ -106,45 +106,68 @@ class EmptyIcon implements Icon {
   }
 }
 
-@SuppressWarnings("ImmutableEnumChecker")
 enum SortIconType {
-  DEFAULT(
-      UIManager.getLookAndFeelDefaults().getIcon("Table.ascendingSortIcon"),
-      UIManager.getLookAndFeelDefaults().getIcon("Table.descendingSortIcon")
-  ),
-  EMPTY(
-      new EmptyIcon(),
-      new EmptyIcon()
-  ),
-  CUSTOM(
-      makeIcon("example/ascending.png"),
-      makeIcon("example/descending.png")
-  );
-  private final Icon ascending;
-  private final Icon descending;
+  DEFAULT, EMPTY, CUSTOM;
 
-  SortIconType(Icon ascending, Icon descending) {
-    this.ascending = ascending;
-    this.descending = descending;
-  }
-
+  @SuppressWarnings("PMD.ExhaustiveSwitchHasDefault")
   public Icon getAscendingIcon() {
-    return ascending;
+    Icon icon;
+    switch (this) {
+      case EMPTY:
+        icon = new EmptyIcon();
+        break;
+      case CUSTOM:
+        icon = createIcon("example/ascending.png");
+        break;
+      case DEFAULT:
+      default:
+        UIDefaults uiDefaults = UIManager.getLookAndFeelDefaults();
+        icon = uiDefaults.getIcon("Table.ascendingSortIcon");
+        break;
+    }
+    return icon;
+    // // Java 12:
+    // return switch (this) {
+    //   case DEFAULT -> uiDefaults.getIcon("Table.ascendingSortIcon");
+    //   case EMPTY -> new EmptyIcon();
+    //   case CUSTOM -> createIcon("example/ascending.png");
+    // };
   }
 
+  @SuppressWarnings("PMD.ExhaustiveSwitchHasDefault")
   public Icon getDescendingIcon() {
-    return descending;
+    Icon icon;
+    switch (this) {
+      case EMPTY:
+        icon = new EmptyIcon();
+        break;
+      case CUSTOM:
+        icon = createIcon("example/descending.png");
+        break;
+      case DEFAULT:
+      default:
+        UIDefaults uiDefaults = UIManager.getLookAndFeelDefaults();
+        icon = uiDefaults.getIcon("Table.descendingSortIcon");
+        break;
+    }
+    return icon;
+    // // Java 12:
+    // return switch (this) {
+    //   case DEFAULT -> uiDefaults.getIcon("Table.descendingSortIcon");
+    //   case EMPTY -> new EmptyIcon();
+    //   case CUSTOM -> createIcon("example/descending.png");
+    // };
   }
 
-  private static Icon makeIcon(String path) {
+  private static Icon createIcon(String path) {
     ClassLoader cl = Thread.currentThread().getContextClassLoader();
     ImageIcon icon = Optional.ofNullable(cl.getResource(path))
         .map(ImageIcon::new)
-        .orElseGet(() -> new ImageIcon(makeMissingImage()));
+        .orElseGet(() -> new ImageIcon(createMissingImage()));
     return new IconUIResource(icon);
   }
 
-  private static Image makeMissingImage() {
+  private static Image createMissingImage() {
     Icon missingIcon = UIManager.getIcon("html.missingImage");
     int iw = missingIcon.getIconWidth();
     int ih = missingIcon.getIconHeight();
